b9dd478b16f39c8847ed67c767e2b40b682928b0
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-05-20 19:21:35 +0200

    add new premake actions to copy DLLs required on windows
    Former-commit-id: de7be0cc51713e7cae198c54ac1dadb5fead24fe
    Former-commit-id: 8072ae01fcadb975c970c7ab023b3c660997c510
    Former-commit-id: 2805feaac51898f527cd61b98ffccca66aabf149

--- premake4.lua
+++ premake4.lua
@@ -3,3 +3,33 @@ solution "minko"
 
 include 'examples'
 include 'framework'
+
+newaction {
+	trigger = "copyDLLs",
+	description = "Copy the required DLLs into the app. output folder",
+	execute = function()
+	  os.mkdir(project(1).basedir .. "/bin")
+	  os.copyfile(
+		project(2).basedir .. "/lib/win32/bin/freeglut.dll",
+		project(1).basedir .. "/bin/freeglut.dll"
+	  )
+	end
+}
+
+newaction {
+	trigger = "removeDLLs",
+	description = "Remove the DLLs copied by copyDLLs",
+	execute = function()
+	  os.rmdir(project(1).basedir .. "/bin")
+	end
+}
+
+newaction {
+	trigger = "cleanVS",
+	description = "Remove additional files added by VS",
+	execute = function()
+		os.rmdir("minko.sdf")
+		os.rmdir("minko.v11.suo")
+		os.rmdir("minko.suo")
+	end
+}
--- scripts/premake_vs2010.bat
+++ scripts/premake_vs2010.bat
@@ -1,3 +1,5 @@
 cd ..
 call premake_clean.bat
 scripts\premake4.exe --os=windows --platform=x32 vs2010
+scripts\premake4.exe --os=windows --platform=x32 copyDLLs
+ 
