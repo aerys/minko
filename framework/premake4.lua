9d3e597b6cd3f16db7a67bc68a5af9e1574afd18
Warren Seine <warren.seine@aerys.in>
2013-06-14 15:50:01 +0200

    Fix compilation.
    Former-commit-id: 1056a34591e193f3e6e13f0b168e5ae2b997f91f
    Former-commit-id: ede83659740ecef8da00120564dd81985329f5a1
    Former-commit-id: 39d37efe11b62abf52c1d90db2bb076198eaa8a2

--- framework/premake4.lua
+++ framework/premake4.lua
@@ -1,43 +1,45 @@
 -- A project defines one build target
 project "minko-framework"
-   kind "StaticLib"
-   language "C++"
-   files { "**.hpp", "**.cpp" }
-   includedirs { "src" }
-   targetdir "bin"
+  kind "StaticLib"
+  language "C++"
+  files { "**.hpp", "**.cpp" }
+  includedirs { "src" }
+  targetdir "bin"
 
-   configuration { "debug", "linux" }
-      defines { "DEBUG" }
-      flags { "Symbols" }
-      links { "GL", "GLU", "glut" }
-      buildoptions "-std=c++0x"
-      linkoptions "-std=c++0x"
+  configuration { "debug", "linux" }
+    defines { "DEBUG" }
+    flags { "Symbols" }
+    links { "GL", "GLU", "glut" }
+    buildoptions "-std=c++0x"
+    linkoptions "-std=c++0x"
 
-   configuration { "release", "linux" }
-      defines { "NDEBUG" }
-      flags { "Optimize" }
-      links { "GL", "GLU", "glut" }
-      buildoptions "-std=c++0x"
-      linkoptions "-std=c++0x"
+  configuration { "release", "linux" }
+    defines { "NDEBUG" }
+    flags { "Optimize" }
+    links { "GL", "GLU", "glut" }
+    buildoptions "-std=c++0x"
+    linkoptions "-std=c++0x"
 
-   configuration { "Debug", "x32", "windows" }
-	  defines { "DEBUG" }
-	  flags { "Symbols" }
-	  includedirs { "lib/win32/includes" }
-	  
-   configuration { "Release", "x32", "windows" }
-	  defines { "NDEBUG" }
-	  flags { "Optimize" }
-	  includedirs { "lib/win32/includes" }
-	  
-   configuration { "debug", "macosx" }
-	  defines { "DEBUG" }
-      buildoptions { "-std=c++11", "-stdlib=libc++" }
-      linkoptions { "-std=c++11", "-stdlib=libc++" }
-      libdirs { "/opt/local/lib/" }
+  configuration { "Debug", "x32", "windows" }
+    defines { "DEBUG" }
+    flags { "Symbols" }
+    includedirs { "lib/win32/includes" }
 
-   configuration { "release", "macosx" }
-	  defines { "NDEBUG" }
-	  buildoptions { "-std=c++11", "-stdlib=libc++" }
-      linkoptions { "-std=c++11", "-stdlib=libc++" }
-      libdirs { "/opt/local/lib/" }
+  configuration { "Release", "x32", "windows" }
+    defines { "NDEBUG" }
+    flags { "Optimize" }
+    includedirs { "lib/win32/includes" }
+
+  configuration { "debug", "macosx" }
+    defines { "DEBUG" }
+    buildoptions { "-std=c++11", "-stdlib=libc++" }
+    linkoptions { "-std=c++11", "-stdlib=libc++" }
+    libdirs { "/opt/local/lib/" }
+    includedirs { "/opt/local/include/" }
+
+  configuration { "release", "macosx" }
+    defines { "NDEBUG" }
+    buildoptions { "-std=c++11", "-stdlib=libc++" }
+    linkoptions { "-std=c++11", "-stdlib=libc++" }
+    libdirs { "/opt/local/lib/" }
+    includedirs { "/opt/local/include/" }
