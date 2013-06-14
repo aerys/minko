39d37efe11b62abf52c1d90db2bb076198eaa8a2
Warren Seine <warren.seine@aerys.in>
2013-06-14 15:50:01 +0200

    Fix compilation.
    Former-commit-id: 1056a34591e193f3e6e13f0b168e5ae2b997f91f
    Former-commit-id: ede83659740ecef8da00120564dd81985329f5a1

--- examples/premake4.lua
+++ examples/premake4.lua
@@ -1,49 +1,52 @@
 -- A project defines one build target
 project "minko-examples"
-   kind "ConsoleApp"
-   language "C++"
-   files { "**.hpp", "**.cpp" }
-   includedirs { "src", "../framework/src" }
-   targetdir "bin"
+  kind "ConsoleApp"
+  language "C++"
+  files { "**.hpp", "**.cpp" }
+  includedirs { "src", "../framework/src" }
+  targetdir "bin"
 
-   configuration { "debug", "linux" }
-      defines { "DEBUG" }
-      flags { "Symbols" }
-      links { "minko-framework", "GL", "GLU", "glut", "m" }
-      buildoptions "-std=c++0x"
-      linkoptions "-std=c++0x"
-      
-   configuration { "release", "linux" }
-      defines { "NDEBUG" }
-      flags { "OptimizeSpeed" }
-      links { "minko-framework", "GL", "GLU", "glut", "m" }
-      buildoptions "-std=c++0x"
-      linkoptions "-std=c++0x"
-	  
-   configuration { "debug", "x32", "windows" }
-	  defines { "DEBUG" }
-	  flags { "Symbols" }
-	  links { "minko-framework", "freeglut", "glew32" }
-	  libdirs { "../framework/lib/win32/bin" }
-	  includedirs { "../framework/lib/win32/includes" }
+  configuration { "debug", "linux" }
+    defines { "DEBUG" }
+    flags { "Symbols" }
+    links { "minko-framework", "GL", "GLU", "glut", "m" }
+    buildoptions "-std=c++0x"
+    linkoptions "-std=c++0x"
 
-	  
-   configuration { "release", "x32", "windows" }
-	  defines { "NDEBUG" }
-	  flags { "OptimizeSpeed" }
-	  links { "minko-framework", "freeglut", "glew32" }
-	  libdirs { "../framework/lib/win32/bin" }
-	  includedirs { "../framework/lib/win32/includes" }
+  configuration { "release", "linux" }
+    defines { "NDEBUG" }
+    flags { "OptimizeSpeed" }
+    links { "minko-framework", "GL", "GLU", "glut", "m" }
+    buildoptions "-std=c++0x"
+    linkoptions "-std=c++0x"
 
-   configuration { "debug", "macosx" }
-	  defines { "DEBUG" }
-     buildoptions { "-std=c++11", "-stdlib=libc++" }
-     linkoptions { "-std=c++11", "-stdlib=libc++" }
-     libdirs { "/opt/local/lib/" }
+  configuration { "debug", "x32", "windows" }
+    defines { "DEBUG" }
+    flags { "Symbols" }
+    links { "minko-framework", "freeglut", "glew32" }
+    libdirs { "../framework/lib/win32/bin" }
+    includedirs { "../framework/lib/win32/includes" }
 
-   configuration { "release", "macosx" }
-	  defines { "NDEBUG" }
-     flags { "OptimizeSpeed" }
-	  buildoptions { "-std=c++11", "-stdlib=libc++" }
-     linkoptions { "-std=c++11", "-stdlib=libc++" }
-     libdirs { "/opt/local/lib/" }
+
+  configuration { "release", "x32", "windows" }
+    defines { "NDEBUG" }
+    flags { "OptimizeSpeed" }
+    links { "minko-framework", "freeglut", "glew32" }
+    libdirs { "../framework/lib/win32/bin" }
+    includedirs { "../framework/lib/win32/includes" }
+
+  configuration { "debug", "macosx" }
+    defines { "DEBUG" }
+    buildoptions { "-std=c++11", "-stdlib=libc++" }
+    linkoptions { "-std=c++11", "-stdlib=libc++" }
+    libdirs { "/opt/local/lib/" }
+    links { "minko-framework", "GL", "GLU", "glut", "m" }
+    includedirs { "/opt/local/include/" }
+
+  configuration { "release", "macosx" }
+    defines { "NDEBUG" }
+    flags { "OptimizeSpeed" }
+    buildoptions { "-std=c++11", "-stdlib=libc++" }
+    linkoptions { "-std=c++11", "-stdlib=libc++" }
+    links { "minko-framework", "GL", "GLU", "glut", "m" }
+    includedirs { "/opt/local/include/" }
