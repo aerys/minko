14f0dba397555aee30f3697aa1d888367d92d475
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-06-17 23:03:28 +0200

    working EffectParser
    Former-commit-id: d1c8aeb976693ad7373319e02fd7f3a80932e605
    Former-commit-id: b06ad02dd5edad570056dccf06c901c232e1ea0a

--- examples/premake4.lua
+++ examples/premake4.lua
@@ -2,56 +2,38 @@
 project "minko-examples"
   kind "ConsoleApp"
   language "C++"
-  files { "**.hpp", "**.cpp" }
-  includedirs { "src", "../framework/src" }
+  files { "**.hpp", "**.cpp", "**.h" }
+  includedirs {
+    "src",
+    "../framework/src"
+  }
 
   configuration { "debug"}
+    defines { "DEBUG" }
+    flags { "Symbols" }
     targetdir "bin/debug"
 
   configuration { "release" }
+    defines { "NDEBUG" }
+    flags { "OptimizeSpeed" }
     targetdir "bin/release"
 
-  configuration { "debug", "linux" }
-    defines { "DEBUG" }
-    flags { "Symbols" }
+  -- linux
+  configuration { "linux" }
     links { "minko-framework", "GL", "GLU", "glut", "m" }
     buildoptions "-std=c++0x"
     linkoptions "-std=c++0x"
 
-  configuration { "release", "linux" }
-    defines { "NDEBUG" }
-    flags { "OptimizeSpeed" }
-    links { "minko-framework", "GL", "GLU", "glut", "m" }
-    buildoptions "-std=c++0x"
-    linkoptions "-std=c++0x"
-
-  configuration { "debug", "x32", "windows" }
-    defines { "DEBUG" }
-    flags { "Symbols" }
+  -- windows
+  configuration { "windows", "x32" }
     links { "minko-framework", "freeglut", "glew32" }
-    libdirs { "../framework/lib/win32/bin" }
-    includedirs { "../framework/lib/win32/includes" }
+    libdirs { "../framework/lib/gl/win32/bin" }
+    includedirs { "../framework/lib/gl/win32/includes" }
 
-
-  configuration { "release", "x32", "windows" }
-    defines { "NDEBUG" }
-    flags { "OptimizeSpeed" }
-    links { "minko-framework", "freeglut", "glew32" }
-    libdirs { "../framework/lib/win32/bin" }
-    includedirs { "../framework/lib/win32/includes" }
-
+  -- macos
   configuration { "debug", "macosx" }
-    defines { "DEBUG" }
     buildoptions { "-std=c++11", "-stdlib=libc++" }
     linkoptions { "-std=c++11", "-stdlib=libc++" }
     libdirs { "/opt/local/lib/" }
-    links { "minko-framework", "GL", "GLU", "glut", "m" }
-    includedirs { "/opt/local/include/" }
-
-  configuration { "release", "macosx" }
-    defines { "NDEBUG" }
-    flags { "OptimizeSpeed" }
-    buildoptions { "-std=c++11", "-stdlib=libc++" }
-    linkoptions { "-std=c++11", "-stdlib=libc++" }
     links { "minko-framework", "GL", "GLU", "glut", "m" }
     includedirs { "/opt/local/include/" }
--- framework/premake4.lua
+++ framework/premake4.lua
@@ -2,48 +2,37 @@
 project "minko-framework"
   kind "StaticLib"
   language "C++"
-  files { "**.hpp", "**.cpp" }
-  includedirs { "src" }
+  files { "**.hpp", "**.cpp", "**.h" }
+  includedirs {
+    "src",
+    "lib/jsoncpp/src"
+  }
+  defines {
+    "JSON_IS_AMALGAMATION"
+  }
   
   configuration { "debug"}
+    defines { "DEBUG" }
+    flags { "Symbols" }
     targetdir "bin/debug"
 
   configuration { "release" }
+    defines { "NDEBUG" }
+    flags { "OptimizeSpeed" }
     targetdir "bin/release"
 
-  configuration { "debug", "linux" }
-    defines { "DEBUG" }
-    flags { "Symbols" }
+  -- linux
+  configuration { "linux" }
     links { "GL", "GLU", "glut" }
     buildoptions "-std=c++0x"
     linkoptions "-std=c++0x"
 
-  configuration { "release", "linux" }
-    defines { "NDEBUG" }
-    flags { "Optimize" }
-    links { "GL", "GLU", "glut" }
-    buildoptions "-std=c++0x"
-    linkoptions "-std=c++0x"
+  -- windows
+  configuration { "windows", "x32" }
+    includedirs { "lib/gl/win32/includes" }
 
-  configuration { "Debug", "x32", "windows" }
-    defines { "DEBUG" }
-    flags { "Symbols" }
-    includedirs { "lib/win32/includes" }
-
-  configuration { "Release", "x32", "windows" }
-    defines { "NDEBUG" }
-    flags { "Optimize" }
-    includedirs { "lib/win32/includes" }
-
-  configuration { "debug", "macosx" }
-    defines { "DEBUG" }
-    buildoptions { "-std=c++11", "-stdlib=libc++" }
-    linkoptions { "-std=c++11", "-stdlib=libc++" }
-    libdirs { "/opt/local/lib/" }
-    includedirs { "/opt/local/include/" }
-
-  configuration { "release", "macosx" }
-    defines { "NDEBUG" }
+  -- macos
+  configuration { "macosx" }
     buildoptions { "-std=c++11", "-stdlib=libc++" }
     linkoptions { "-std=c++11", "-stdlib=libc++" }
     libdirs { "/opt/local/lib/" }
