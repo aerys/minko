8571050045de3635eedc722d2f6e9d0fb57ee620
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-06-30 00:14:03 +0200

    added "includes" directive for effect files
    Former-commit-id: 391c0a32f4eb556b78666dd371b0173578cf1226

--- framework/src/minko/file/EffectParser.hpp
+++ framework/src/minko/file/EffectParser.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "minko/Common.hpp"
+#include "minko/Signal.hpp"
 
 namespace
 {
@@ -12,12 +13,39 @@ namespace minko
 {
 	namespace file
 	{
-		class EffectParser
+		class EffectParser :
+			public std::enable_shared_from_this<EffectParser>
 		{
+		public:
+			typedef std::shared_ptr<EffectParser>	ptr;
+
 		private:
-			std::shared_ptr<Effect>	_effect;
+			typedef std::shared_ptr<Loader> LoaderPtr;
+
+		private:
+			std::shared_ptr<Effect>									_effect;
+			
+			unsigned int											_numDependencies;
+			unsigned int											_numLoadedDependencies;
+
+			std::shared_ptr<AbstractContext>						_context;
+			std::vector<std::pair<std::string, std::string>>		_programs;
+			std::unordered_map<std::string, std::string>			_bindings;
+			std::string												_dependenciesCode;
+
+			std::unordered_map<LoaderPtr, Signal<LoaderPtr>::cd>	_loaderCompleteCds;
+			std::unordered_map<LoaderPtr, Signal<LoaderPtr>::cd>	_loaderErrorCds;
+
+			std::shared_ptr<Signal<ptr>>							_complete;
 
 		public:
+			inline static
+			ptr
+			create()
+			{
+				return std::shared_ptr<EffectParser>(new EffectParser());
+			}
+
 			inline
 			std::shared_ptr<Effect>
 			effect()
@@ -25,8 +53,28 @@ namespace minko
 				return _effect;
 			}
 
+			inline
+			std::shared_ptr<Signal<ptr>>
+			complete()
+			{
+				return _complete;
+			}
+
 			void
-			parse(std::shared_ptr<AbstractContext> context, const std::string& data);
+			parse(std::shared_ptr<Options>	options,
+				  const std::vector<char>&	data);
+
+			void
+			dependencyCompleteHandler(std::shared_ptr<Loader> loader);
+
+			void
+			dependencyErrorHandler(std::shared_ptr<Loader> loader);
+
+			void
+			finalize();
+
+		private:
+			EffectParser();
 		};
 	}
 }
