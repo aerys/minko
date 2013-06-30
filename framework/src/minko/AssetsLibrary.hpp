66e95114b62a8b1a41d99776b2f15405456dd8ea
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-06-30 13:12:59 +0200

    implemented AssetsLibrary working for effect files
    Former-commit-id: ec7f56f6b004c2d310912349df90966dcb576ca7 [formerly 623fec85cac51ff79769bd2e2104e5159d2706d9] [formerly 68044731887bbfc3027295a5d682c7970a26e5a6]
    Former-commit-id: 7e7783e2918998e02bb00005895022d89e57a3bd [formerly a5e5110bd9b2abff4dd5acb379cad0fba3682542] [formerly 44fde61ac4ee0c08b22776e6ed4fa081e178b7ae]
    Former-commit-id: a536b48113f61fcc48a220e58239ac56c1cc710d
    Former-commit-id: 702d662d1e11f1564d337cf9e7030ad217ad9347

--- framework/src/minko/AssetsLibrary.hpp
+++ framework/src/minko/AssetsLibrary.hpp
@@ -20,6 +20,15 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 #pragma once
 
 #include "minko/Common.hpp"
+#include "minko/Signal.hpp"
+#include "minko/file/AbstractParser.hpp"
+
+namespace
+{
+	using namespace minko::scene::data::geometry;
+	using namespace minko::render;
+	using namespace minko::render::context;
+}
 
 namespace minko
 {
@@ -27,14 +36,30 @@ namespace minko
 		public std::enable_shared_from_this<AssetsLibrary>
 	{
 	public:
-		typedef std::shared_ptr<AssetsLibrary>						Ptr;
+		typedef std::shared_ptr<AssetsLibrary>			Ptr;
 
 	private:
-		typedef std::shared_ptr<render::geometry::Geometry>			GeometryPtr;
-		typedef std::shared_ptr<render::context::AbstractContext>	AbsContextPtr;
+		typedef std::shared_ptr<AbstractContext>		AbsContextPtr;
+		typedef std::shared_ptr<Effect>					EffectPtr;
+		typedef std::shared_ptr<Geometry>				GeometryPtr;
+		typedef std::shared_ptr<file::AbstractParser>	AbsParserPtr;
+		typedef std::function<AbsParserPtr(void)>		Handler;
 
 	private:
-		std::map<std::string, GeometryPtr>	_geometries;
+		AbsContextPtr													_context;
+		std::shared_ptr<file::Options>									_defaultOptions;
+		std::unordered_map<std::string, Handler>						_parsers;
+
+		std::unordered_map<std::string, GeometryPtr>					_geometries;
+		std::unordered_map<std::string, EffectPtr>						_effects;
+		std::unordered_map<std::string, std::vector<char>>				_blobs;
+
+		std::list<std::string>											_filesQueue;
+		std::unordered_map<std::string, std::shared_ptr<file::Loader>>	_filenameToLoader;
+
+		std::vector<Signal<std::shared_ptr<file::Loader>>::Slot>		_loaderCompleteSlots;
+
+		std::shared_ptr<Signal<Ptr>>									_complete;
 
 	public:
 		inline static
@@ -44,7 +69,67 @@ namespace minko
 			return std::shared_ptr<AssetsLibrary>(new AssetsLibrary(context));
 		}
 
+		inline
+		AbsContextPtr
+		context()
+		{
+			return _context;
+		}
+
+		inline
+		std::shared_ptr<file::Options>
+		defaultOptions()
+		{
+			return _defaultOptions;
+		}
+
+		inline
+		std::shared_ptr<Signal<Ptr>>
+		complete()
+		{
+			return _complete;
+		}
+
+		GeometryPtr
+		geometry(const std::string& name);
+
+		Ptr
+		geometry(const std::string& name, std::shared_ptr<Geometry> geometry);
+
+		EffectPtr
+		effect(const std::string& name);
+
+		Ptr
+		effect(const std::string& name, std::shared_ptr<Effect> effect);
+
+		const std::vector<char>&
+		blob(const std::string& name);
+
+		Ptr
+		blob(const std::string& name, const std::vector<char>& blob);
+
+		template <typename T>
+		typename std::enable_if<std::is_base_of<file::AbstractParser, T>::value, Ptr>::type
+		registerParser(const std::string& extension)
+		{
+			_parsers[extension] = T::create;
+
+			return shared_from_this();
+		}
+
+		Ptr
+		queue(const std::string& filename);
+
+		Ptr
+		load(const std::string& filename);
+
+		Ptr
+		load();
+
 	private:
 		AssetsLibrary(AbsContextPtr context);
+
+		void
+		loaderCompleteHandler(std::shared_ptr<file::Loader> loader);
 	};
 }
