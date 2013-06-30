977ac8293db1b4dafc917cbbd851feceabe8e9d2
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-06-30 15:17:46 +0200

    flatten src tree (yes... AGAIN!)
    Former-commit-id: 327314f2a63eaf4113bcc5b523c053b197ab42e7
    Former-commit-id: 89fe1718cc8d7608af9c1aa03de16ed3a851a830
    Former-commit-id: 56d316e0f4c2ac27b91601b323e55c3e8b5b3bae

--- framework/src/minko/resource/IndexStream.cpp
+++ framework/src/minko/resource/IndexStream.cpp
@@ -17,63 +17,14 @@ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-#pragma once
+#include "IndexStream.hpp"
+#include "minko/render/context/AbstractContext.hpp"
 
-#include "minko/Common.hpp"
+using namespace minko::resource;
 
-namespace minko
+void
+IndexStream::upload()
 {
-	namespace render
-	{
-		namespace stream
-		{
-			class VertexAttribute
-			{
-			public:
-				typedef std::shared_ptr<VertexAttribute> Ptr;
-
-			private:
-				const std::string	_name;
-				const unsigned int	_size;
-				const unsigned int	_offset;
-
-			public:
-				inline static
-				Ptr
-				create(const std::string& name, unsigned int size, unsigned int offset)
-				{
-					return std::shared_ptr<VertexAttribute>(new VertexAttribute(name, size, offset));
-				}
-
-				inline
-				const std::string&
-				name()
-				{
-					return _name;
-				}
-
-				inline
-				const unsigned int
-				size()
-				{
-					return _size;
-				}
-
-				inline
-				const unsigned int
-				offset()
-				{
-					return _offset;
-				}
-
-			private:
-				VertexAttribute(const std::string& name, const unsigned int size, const unsigned int offset) :
-					_name(name),
-					_size(size),
-					_offset(offset)
-				{
-				}
-			};
-		}
-	}
+	_buffer = _context->createIndexBuffer(_data.size());
+	_context->uploaderIndexBufferData(_buffer, 0, _data.size(), &_data[0]);
 }
