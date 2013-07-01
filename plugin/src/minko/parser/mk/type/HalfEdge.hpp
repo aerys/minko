1180e53c25a607453b3a7e1a8dbe15f38eecd1bb
Alexandre Cyprien <alexandre.cyprien@aerys.in>
2013-07-01 17:15:43 +0200

    Add HalfEdge data structure for mk
    Former-commit-id: ddd8c958ddb51820e8db0e76d399d72cd9b45b41 [formerly 1022339a150a82234673d9f45b90982f12d8f735] [formerly 283683e2afd251ff5785ea155dceb6c06b16f315]
    Former-commit-id: 67eeb4564590dc233ff8bc03b7c252debd17f4d9 [formerly b69b70d819a056cbb9b79acee73fe85e7e44fa04] [formerly c7dfd12f5e3222bb1b8f9ee138ce413f041fa7db]
    Former-commit-id: a1da6447f01524a8f8dad1f512d86222f4722d16
    Former-commit-id: 6a46db3a1d8b7f96911b0413d6acb0269cb42578

---
+++ plugin/src/minko/parser/mk/type/HalfEdge.hpp
@@ -0,0 +1,143 @@
+#pragma once
+
+#include "minko/MkCommon.hpp"
+
+namespace minko
+{
+	namespace parser
+	{
+		namespace mk
+		{
+
+			class HalfEdge
+			{
+			public :
+				typedef std::shared_ptr<HalfEdge>	HalfEdgePtr;
+
+			public:
+				inline static
+				HalfEdgePtr	create(unsigned int startNodeId,
+								   unsigned int endNodeId,
+								   unsigned int edgeId)
+				{
+					auto he = std::shared_ptr<HalfEdge>(new HalfEdge(startNodeId, endNodeId, edgeId));
+
+					return he;
+				}
+
+			private:
+				unsigned int				_startNodeId;
+				unsigned int				_endNodeId;
+				unsigned int				_edgeId;
+				HalfEdgePtr					_next;
+				HalfEdgePtr					_prec;
+				HalfEdgePtr					_adjacent;
+				std::vector<HalfEdgePtr>	_face;
+				std::vector<HalfEdgePtr>	_firstReverseFace;
+				std::vector<HalfEdgePtr>	_secondReverseFace;
+
+			private :
+				HalfEdge(unsigned int startNodeId,
+						 unsigned int endNodeId,
+						 unsigned int edgeId);
+
+			public:
+				bool			indiceInEdge(unsigned int indice);
+				bool			indiceInFace(unsigned int indice);
+				unsigned int	getThirdVertex();
+
+				inline unsigned int startNodeId() const
+				{
+					return _startNodeId;
+				}
+
+				inline unsigned int endNodeId() const
+				{
+					return _endNodeId;
+				}
+
+				inline unsigned int edgeId() const
+				{
+					return _edgeId;
+				}
+
+				inline HalfEdgePtr next() const
+				{
+					return _next;
+				}
+
+				inline void next(HalfEdgePtr value)
+				{
+					_next = value;
+				}
+
+				inline HalfEdgePtr prec() const
+				{
+					return _prec;
+				}
+
+				inline void prec(HalfEdgePtr value)
+				{
+					_prec = value;
+				}
+
+				inline HalfEdgePtr adjacent() const
+				{
+					return _adjacent;
+				}
+
+				inline void adjacent(HalfEdgePtr value)
+				{
+					_adjacent = value;
+				}
+
+				inline const std::vector<HalfEdgePtr>& face() const
+				{
+					return _face;
+				}
+
+				inline std::vector<HalfEdgePtr>& face() 
+				{
+					return _face;
+				}
+
+				void setFace(HalfEdgePtr he1,
+							 HalfEdgePtr he2,
+				 			 HalfEdgePtr he3);
+
+				inline std::vector<HalfEdgePtr>& secondReverseFace() 
+				{
+					if (_secondReverseFace.size() == 0)
+					{
+						_secondReverseFace.push_back(_face[2]);
+						_secondReverseFace.push_back(_face[0]);
+						_secondReverseFace.push_back(_face[1]);
+					}
+
+					return _secondReverseFace;
+				}
+
+				inline std::vector<HalfEdgePtr>& firstReverseFace() 
+				{
+					if (_firstReverseFace.size() == 0)
+					{
+						_firstReverseFace.push_back(_face[1]);
+						_firstReverseFace.push_back(_face[2]);
+						_firstReverseFace.push_back(_face[0]);
+					}
+
+					return _firstReverseFace;
+				}
+			};
+			
+			
+			// debug
+			
+			std::ostream &operator<<(std::ostream &flux, const HalfEdge& halfEdge);
+
+			std::ostream &operator<<(std::ostream &flux, std::shared_ptr<HalfEdge> halfEdge);
+
+		}
+	}
+}
+
