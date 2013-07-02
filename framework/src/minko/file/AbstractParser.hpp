65d77959ca6fc2a1bb486c51df76a3af17dd39a9
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-07-02 16:52:43 +0200

    remove useless anonymous namespace
    Former-commit-id: efe4e2d08bbb118b7dc0753929218e093c7f50bf
    Former-commit-id: 2817dabfe14b10869d1589b23cc2672e1d4b4282

--- framework/src/minko/file/AbstractParser.hpp
+++ framework/src/minko/file/AbstractParser.hpp
@@ -22,10 +22,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 #include "minko/Common.hpp"
 #include "minko/Signal.hpp"
 
-namespace
-{
-}
-
 namespace minko
 {
 	namespace file
