e58fe2027324e822d34c39d02964eaa48d998344
Alexandre Cyprien <alexandre.cyprien@aerys.in>
2013-07-02 14:17:23 +0200

    Fix submeshes detection
    Former-commit-id: d9a1dd5005c5fbf1583dbb77e9da0ba30706969c
    Former-commit-id: 6b4a91f2126f5a0680a7d6f6bedc0f6e98e4e3c2

---
+++ plugin/src/minko/MkCommon.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "minko/Minko.hpp"
+#include <queue>
+
+namespace minko
+{
+	namespace parser
+	{
+		namespace mk
+		{
+			class HalfEdge;
+			class HalfEdgeCollection;
+		}
+	}
+}
