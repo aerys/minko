06373d5ca318c13c996406f9d3edd736d3556e61
Alexandre Cyprien <alexandre.cyprien@aerys.in>
2013-07-02 14:17:23 +0200

    Fix submeshes detection
    Former-commit-id: 0dfa07fb6b5e77d02c4d6801dd279cbd9868cea2
    Former-commit-id: 74d78b1eecfc9fe126d0c0b02476925ace756535

--- examples/src/Main.cpp
+++ examples/src/Main.cpp
@@ -1,19 +1,19 @@
 #include <time.h>
 
 #include "minko/Minko.hpp"
-#include "minko/MinkoJPEG.hpp"
-#include "minko/MinkoPNG.hpp"
+#include "minko/MinkoMk.hpp"
 
-#include "GLFW/glfw3.h"
 
 #define FRAMERATE 60
 
+
+using namespace minko::parser::mk;
 using namespace minko::controller;
 using namespace minko::math;
 
 RenderingController::Ptr renderingController;
-auto mesh = scene::Node::create("mesh");
-auto group = scene::Node::create("group");
+auto mesh	= scene::Node::create("mesh");
+auto group	= scene::Node::create("group");
 
 void
 printFramerate(const unsigned int delay = 1)
@@ -33,7 +33,25 @@ printFramerate(const unsigned int delay = 1)
 	}
 }
 
-/*void screenshotFunc(int)
+void
+renderScene()
+{
+	mesh->controller<TransformController>()->transform()->prependRotationY(.01f);
+
+	renderingController->render();
+
+	printFramerate();
+
+	glutSwapBuffers();
+}
+
+void timerFunc(int)
+{
+	glutTimerFunc(1000 / FRAMERATE, timerFunc, 0);
+	glutPostRedisplay();
+}
+
+void screenshotFunc(int)
 {
 	const int width = 800, height = 600;
 
@@ -42,7 +60,7 @@ printFramerate(const unsigned int delay = 1)
 	glReadPixels(0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE, pixels);
 
 	int i, j;
-	FILE *fp = fopen("screenshot.ppm", "wb");
+	FILE *fp = fopen("screenshot.ppm", "wb"); /* b - binary mode */
 	fprintf(fp, "P6\n%d %d\n255\n", width, height);
 
 	for (j = 0; j < height; ++j)
@@ -50,9 +68,9 @@ printFramerate(const unsigned int delay = 1)
 		for (i = 0; i < width; ++i)
 		{
 			static unsigned char color[3];
-			color[0] = pixels[(width * j + i) * 3 + 0];
-			color[1] = pixels[(width * j + i) * 3 + 1];
-			color[2] = pixels[(width * j + i) * 3 + 2];
+			color[0] = pixels[(width * j + i) * 3 + 0];  /* red */
+			color[1] = pixels[(width * j + i) * 3 + 1];  /* green */
+			color[2] = pixels[(width * j + i) * 3 + 2];  /* blue */
 			(void) fwrite(color, 1, 3, fp);
 		}
 	}
@@ -60,31 +78,31 @@ printFramerate(const unsigned int delay = 1)
 	fclose(fp);
 
 	delete[] pixels;
-}*/
+}
 
 int main(int argc, char** argv)
 {
-    glfwInit();
-    auto window = glfwCreateWindow(800, 600, "Minko Examples", NULL, NULL);
-    glfwMakeContextCurrent(window);
+	// glut/glew init
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+	glutInitWindowSize(800, 600);
+	glutCreateWindow("Minko Examples");
+
+#ifdef _WIN32
+	glewInit();
+#endif
+	// !glut/glew init
 
 	auto context = render::OpenGLES2Context::create();
-
-    context->setBlendMode(render::Blending::Mode::DEFAULT);
-
 	auto assets	= AssetsLibrary::create(context)
 		->registerParser<file::EffectParser>("effect")
-		->registerParser<file::JPEGParser>("jpg")
-		->registerParser<file::PNGParser>("png")
 		->geometry("cube", geometry::CubeGeometry::create(context))
-		//->geometry("sphere", geometry::SphereGeometry::create(context, 40))
-		->queue("textures/collage.jpg")
-        ->queue("textures/box3.png")
-		->queue("effects/Texture.effect")
-		//->queue("effects/Red.effect")
-		->queue("effects/Basic.effect");
+		->geometry("sphere", geometry::SphereGeometry::create(context))
+		->queue("DirectionalLight.effect")
+		->queue("Red.effect")
+		->queue("Basic.effect");
 
-	/*assets->defaultOptions()->includePath("effects");*/
+	assets->defaultOptions()->includePath("effects");
 
 	auto _ = assets->complete()->connect([](AssetsLibrary::Ptr assets)
 	{
@@ -93,12 +111,10 @@ int main(int argc, char** argv)
 
 		root->addChild(group)->addChild(camera);
 
-        renderingController = RenderingController::create(assets->context());
-        renderingController->backgroundColor(0x7F7F7FFF);
-		camera->addController(renderingController);
+		camera->addController(renderingController = RenderingController::create(assets->context()));
 
 		auto view = Matrix4x4::create()->perspective(.785f, 800.f / 600.f, .1f, 1000.f);
-		auto color = Vector4::create(0.f, 0.f, 1.f, .1f);
+		auto color = Vector4::create(0.f, 0.f, 1.f, 1.f);
 		auto lightDirection = Vector3::create(0.f, -1.f, -1.f);
 
 		mesh->addController(TransformController::create());
@@ -108,16 +124,29 @@ int main(int argc, char** argv)
 			data::Provider::create()
 				->set("material/diffuse/rgba",			color)
 				->set("transform/worldToScreenMatrix",	view)
-				->set("light/direction",				lightDirection)
-				->set("material/diffuse/map",			assets->texture("textures/box3.png")),
-			assets->effect("texture")
+				->set("light/direction",				lightDirection),
+			assets->effect("basic")
 		));
 
 		group->addChild(mesh);
 
+		/*
+		for (auto i = 0; i < 50000; ++i)
+		{
+			group->addChild(Node::create()->addController(SurfaceController::create(
+				assets->geometry("cube"),
+				data::Provider::create()
+					->set("material/diffuse/rgba",			color)
+					->set("transform/worldToScreenMatrix",	view)
+					->set("light/direction",				lightDirection),
+				assets->effect("directional light")
+			)));
+		}
+		*/
 	});
 
 	assets->load();
+	HalfEdgeCollection* halfEdgeCollection = new HalfEdgeCollection(assets->geometry("sphere")->indices());
 
 	// auto oglContext = context;
 	// auto fx = assets->effect("directional light");
@@ -129,24 +158,11 @@ int main(int argc, char** argv)
 	// std::cout << "== program info logs ==" << std::endl;
 	// std::cout << oglContext->getProgramInfoLogs(fx->shaders()[0]->program()) << std::endl;
 
-	//glutTimerFunc(1000 / FRAMERATE, timerFunc, 0);
-	//glutTimerFunc(1000, screenshotFunc, 0);
+	glutTimerFunc(1000 / FRAMERATE, timerFunc, 0);
+	glutTimerFunc(1000, screenshotFunc, 0);
 
-	while(!glfwWindowShouldClose(window))
-    {
-        mesh->controller<TransformController>()->transform()->prependRotationY(.01f);
+	glutDisplayFunc(renderScene);
+	glutMainLoop();
 
-	    renderingController->render();
-
-	    printFramerate();
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    glfwDestroyWindow(window);
- 
-    glfwTerminate();
-
-    exit(EXIT_SUCCESS);
+	return 0;
 }
