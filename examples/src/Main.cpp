2ecf03c699eac13e8c2510fc33336910ee6dbb46
Alexandre Cyprien <alexandre.cyprien@aerys.in>
2013-07-02 14:17:23 +0200

    Fix submeshes detection
    Former-commit-id: 6b4a91f2126f5a0680a7d6f6bedc0f6e98e4e3c2 [formerly d9a1dd5005c5fbf1583dbb77e9da0ba30706969c]
    Former-commit-id: 0f91c1868aa34a873828a5b4fbef0068f6d40a58 [formerly 72f8fef3aff4bc1115bd3ad791dd642ea6b20528]
    Former-commit-id: 990198a5979410376222e03cd0e1793e8f3c1b09

--- examples/src/Main.cpp
+++ examples/src/Main.cpp
@@ -1,19 +1,19 @@
 #include <time.h>
 
 #include "minko/Minko.hpp"
-#include "minko/MinkoJPEG.hpp"
-#include "minko/MinkoPNG.hpp"
+#include "minko/MinkoMk.hpp"
 
-#include "GLFW/glfw3.h"
 
 #define FRAMERATE 60
 
+
+using namespace minko::parser::mk;
 using namespace minko::controller;
 using namespace minko::math;
 
 RenderingController::Ptr renderingController;
-auto mesh = scene::Node::create("mesh");
-auto group = scene::Node::create("group");
+auto mesh	= scene::Node::create("mesh");
+auto group	= scene::Node::create("group");
 
 void
 printFramerate(const unsigned int delay = 1)
@@ -33,7 +33,25 @@ printFramerate(const unsigned int delay = 1)
 	}
 }
 
-/*void screenshotFunc(int)
+void
+renderScene()
+{
+	mesh->controller<TransformController>()->transform()->prependRotationY(.01f);
+
+	renderingController->render();
+
+	printFramerate();
+
+	glutSwapBuffers();
+}
+
+void timerFunc(int)
+{
+	glutTimerFunc(1000 / FRAMERATE, timerFunc, 0);
+	glutPostRedisplay();
+}
+
+void screenshotFunc(int)
 {
 	const int width = 800, height = 600;
 
@@ -42,7 +60,7 @@ printFramerate(const unsigned int delay = 1)
 	glReadPixels(0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE, pixels);
 
 	int i, j;
-	FILE *fp = fopen("screenshot.ppm", "wb");
+	FILE *fp = fopen("screenshot.ppm", "wb"); /* b - binary mode */
 	fprintf(fp, "P6\n%d %d\n255\n", width, height);
 
 	for (j = 0; j < height; ++j)
@@ -50,9 +68,9 @@ printFramerate(const unsigned int delay = 1)
 		for (i = 0; i < width; ++i)
 		{
 			static unsigned char color[3];
-			color[0] = pixels[(width * j + i) * 3 + 0];
-			color[1] = pixels[(width * j + i) * 3 + 1];
-			color[2] = pixels[(width * j + i) * 3 + 2];
+			color[0] = pixels[(width * j + i) * 3 + 0];  /* red */
+			color[1] = pixels[(width * j + i) * 3 + 1];  /* green */
+			color[2] = pixels[(width * j + i) * 3 + 2];  /* blue */
 			(void) fwrite(color, 1, 3, fp);
 		}
 	}
@@ -60,30 +78,31 @@ printFramerate(const unsigned int delay = 1)
 	fclose(fp);
 
 	delete[] pixels;
-}*/
+}
 
 int main(int argc, char** argv)
 {
-    glfwInit();
-    auto window = glfwCreateWindow(800, 600, "Minko Examples", NULL, NULL);
-    glfwMakeContextCurrent(window);
+	// glut/glew init
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+	glutInitWindowSize(800, 600);
+	glutCreateWindow("Minko Examples");
+
+#ifdef _WIN32
+	glewInit();
+#endif
+	// !glut/glew init
 
 	auto context = render::OpenGLES2Context::create();
 	auto assets	= AssetsLibrary::create(context)
-		->registerParser<file::JPEGParser>("jpg")
-		->registerParser<file::PNGParser>("png")
+		->registerParser<file::EffectParser>("effect")
 		->geometry("cube", geometry::CubeGeometry::create(context))
-		->geometry("sphere", geometry::SphereGeometry::create(context, 40))
-		->queue("collage.jpg")
-        ->queue("box3.png")
-		//->queue("DirectionalLight.effect")
-		//->queue("VertexNormal.effect")
-		//->queue("Texture.effect")
-		//->queue("Red.effect")
+		->geometry("sphere", geometry::SphereGeometry::create(context))
+		->queue("DirectionalLight.effect")
+		->queue("Red.effect")
 		->queue("Basic.effect");
 
-	assets->defaultOptions()->includePaths().push_back("effects");
-	assets->defaultOptions()->includePaths().push_back("textures");
+	assets->defaultOptions()->includePath("effects");
 
 	auto _ = assets->complete()->connect([](AssetsLibrary::Ptr assets)
 	{
@@ -92,63 +111,58 @@ int main(int argc, char** argv)
 
 		root->addChild(group)->addChild(camera);
 
-        renderingController = RenderingController::create(assets->context());
-        renderingController->backgroundColor(0x7F7F7FFF);
-		camera->addController(renderingController);
+		camera->addController(renderingController = RenderingController::create(assets->context()));
 
 		auto view = Matrix4x4::create()->perspective(.785f, 800.f / 600.f, .1f, 1000.f);
 		auto color = Vector4::create(0.f, 0.f, 1.f, 1.f);
 		auto lightDirection = Vector3::create(0.f, -1.f, -1.f);
 
-		mesh->addController(Transform::create());
-		mesh->controller<Transform>()->transform()->appendTranslation(0.f, 0.f, -3.f);
-		mesh->addController(Surface::create(
-			assets->geometry("sphere"),
+		mesh->addController(TransformController::create());
+		mesh->controller<TransformController>()->transform()->appendTranslation(0.f, 0.f, -3.f);
+		mesh->addController(SurfaceController::create(
+			assets->geometry("cube"),
 			data::Provider::create()
-				->set("material/blending",				render::Blending::Mode::ALPHA)
-				->set("material/depthFunc",				render::CompareMode::LESS)
-				->set("material/depthMask",				true)
 				->set("material/diffuse/rgba",			color)
 				->set("transform/worldToScreenMatrix",	view)
-				->set("light/direction",				lightDirection)
-				->set("material/diffuse/map",			assets->texture("box3.png")),
+				->set("light/direction",				lightDirection),
 			assets->effect("basic")
 		));
-		
+
 		group->addChild(mesh);
+
+		/*
+		for (auto i = 0; i < 50000; ++i)
+		{
+			group->addChild(Node::create()->addController(SurfaceController::create(
+				assets->geometry("cube"),
+				data::Provider::create()
+					->set("material/diffuse/rgba",			color)
+					->set("transform/worldToScreenMatrix",	view)
+					->set("light/direction",				lightDirection),
+				assets->effect("directional light")
+			)));
+		}
+		*/
 	});
 
 	assets->load();
+	HalfEdgeCollection* halfEdgeCollection = new HalfEdgeCollection(assets->geometry("sphere")->indices());
 
-	/*
-	auto fx = assets->effect("directional light");
+	// auto oglContext = context;
+	// auto fx = assets->effect("directional light");
 
-	std::cout << "== vertex shader compilation logs ==" << std::endl;
-	std::cout << context->getShaderCompilationLogs(fx->shaders()[0]->vertexShader()) << std::endl;
-	std::cout << "== fragment shader compilation logs ==" << std::endl;
-	std::cout << context->getShaderCompilationLogs(fx->shaders()[0]->fragmentShader()) << std::endl;
-	std::cout << "== program info logs ==" << std::endl;
-	std::cout << context->getProgramInfoLogs(fx->shaders()[0]->id()) << std::endl;
-	*/
+	// std::cout << "== vertex shader compilation logs ==" << std::endl;
+	// std::cout << oglContext->getShaderCompilationLogs(fx->shaders()[0]->vertexShader()) << std::endl;
+	// std::cout << "== fragment shader compilation logs ==" << std::endl;
+	// std::cout << oglContext->getShaderCompilationLogs(fx->shaders()[0]->fragmentShader()) << std::endl;
+	// std::cout << "== program info logs ==" << std::endl;
+	// std::cout << oglContext->getProgramInfoLogs(fx->shaders()[0]->program()) << std::endl;
 
-	//glutTimerFunc(1000 / FRAMERATE, timerFunc, 0);
-	//glutTimerFunc(1000, screenshotFunc, 0);
+	glutTimerFunc(1000 / FRAMERATE, timerFunc, 0);
+	glutTimerFunc(1000, screenshotFunc, 0);
 
-	while(!glfwWindowShouldClose(window))
-    {
-        mesh->controller<Transform>()->transform()->prependRotationY(.01f);
+	glutDisplayFunc(renderScene);
+	glutMainLoop();
 
-	    renderingController->render();
-
-	    printFramerate();
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    glfwDestroyWindow(window);
- 
-    glfwTerminate();
-
-    exit(EXIT_SUCCESS);
+	return 0;
 }
