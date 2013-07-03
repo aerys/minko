cf3f1281135d4ea297222eb61e93a04e69af5633
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-07-03 10:30:51 +0200

    clean linking and use glfw instead of (deprecated) glut
    Former-commit-id: fdb6327a9106014fd7ef27f445bd356af944be32 [formerly 37ae7e87e9855d5aceb55b2f7c71cc80802ddff4] [formerly 1a666718e4fd8453b8490a94922dafbdbef3de28]
    Former-commit-id: 076baddfd887244071e7b0033e39825e923578b1 [formerly 36572180071cfbc9aabd33d4855672fc8add566d] [formerly 46caddd85ff9124d830e328c90a75d1de63fa313]
    Former-commit-id: 64ecaa9ab52dead59c71ea003660b25d589d1d4f
    Former-commit-id: ce17f6d0b6ce4bcddf302191c5c4e9b89072acf5

--- scripts/premake_vs2010.bat
+++ scripts/premake_vs2010.bat
@@ -1,6 +1,6 @@
-cd ..
 call premake_clean.bat
 call premake_templates.bat
+cd ..
 scripts\premake4.exe --os=windows --platform=x32 vs2010
 scripts\premake4.exe --os=windows --platform=x32 copyDLLs
 cd scripts
