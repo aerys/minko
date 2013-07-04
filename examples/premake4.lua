2eee29222c9661d445c03bd8ca570b6fc8d2f428
Warren Seine <warren.seine@aerys.in>
2013-07-04 22:24:03 +0200

    Add glfw3 dependency on Linux and Mac OS X.
    Former-commit-id: 14d5e3816939de8c79fb6208b623bb87237402c7
    Former-commit-id: cbed67a3b29e0319fe05f6e670773035fe5a3e12

--- examples/premake4.lua
+++ examples/premake4.lua
@@ -3,15 +3,15 @@ project "minko-examples"
   kind "ConsoleApp"
   language "C++"
   links {
-  "minko-jpeg",
-  "minko-png",
-  "minko-mk",
-  "minko-framework"
+    "minko-jpeg",
+    "minko-png",
+    "minko-mk",
+    "minko-framework"
   }
   files { "**.hpp", "**.h", "**.cpp" }
   includedirs {
     "src",
-  "lib/glfw/include",
+    "lib/glfw/include",
     "../framework/src",
     "../plugins/jpeg/src",
     "../plugins/png/src",
@@ -30,22 +30,22 @@ project "minko-examples"
 
   -- linux
   configuration { "linux" }
-    links { "GL", "GLU", "GLEW", "m" }
+    links { "GL", "glfw3", "m" }
     buildoptions "-std=c++0x"
     linkoptions "-std=c++0x"
 
   -- windows
   configuration { "windows", "x32" }
     links { "OpenGL32", "glfw3dll", "glew32" }
-  libdirs {
-    "lib/glfw/bin/win32",
-    "../framework/lib/glew/bin/win32"
-  }
+    libdirs {
+      "lib/glfw/bin/win32",
+      "../framework/lib/glew/bin/win32"
+    }
 
   -- macos
   configuration { "debug", "macosx" }
     buildoptions { "-std=c++11", "-stdlib=libc++" }
     linkoptions { "-std=c++11", "-stdlib=libc++" }
+    links { "GL", "glfw3", "m" }
     libdirs { "/opt/local/lib/" }
-    links { "GL", "GLU", "GLEW", "m" }
     includedirs { "/opt/local/include/" }
