180439d88f68e3afbe9352b01ea54cffba61f33c
Alexandre Cyprien <alexandre.cyprien@aerys.in>
2013-07-04 19:06:40 +0200

    Add mk examples : qark read/write and HalfEdgeCollection construction from indexStream
    Former-commit-id: fac55959b9f8222160b40ca4ebd9d2b4a4e6405f
    Former-commit-id: aa323a4929650e83d73022d17cacf46d6fcc0df3
    Former-commit-id: d65de8ece0182ae1f34cfe01ec76e4a69d291cc7

--- examples/src/Main.cpp
+++ examples/src/Main.cpp
@@ -1,19 +1,20 @@
 #include <time.h>
 
 #include "minko/Minko.hpp"
+#include "minko/MinkoJPEG.hpp"
+#include "minko/MinkoPNG.hpp"
 #include "minko/MinkoMk.hpp"
 
+#include "GLFW/glfw3.h"
 
 #define FRAMERATE 60
 
-
-using namespace minko::parser::mk;
 using namespace minko::controller;
 using namespace minko::math;
 
 RenderingController::Ptr renderingController;
-auto mesh	= scene::Node::create("mesh");
-auto group	= scene::Node::create("group");
+auto mesh = scene::Node::create("mesh");
+auto group = scene::Node::create("group");
 
 void
 printFramerate(const unsigned int delay = 1)
@@ -33,25 +34,7 @@ printFramerate(const unsigned int delay = 1)
 	}
 }
 
-void
-renderScene()
-{
-	mesh->controller<TransformController>()->transform()->prependRotationY(.01f);
-
-	renderingController->render();
-
-	printFramerate();
-
-	glutSwapBuffers();
-}
-
-void timerFunc(int)
-{
-	glutTimerFunc(1000 / FRAMERATE, timerFunc, 0);
-	glutPostRedisplay();
-}
-
-void screenshotFunc(int)
+/*void screenshotFunc(int)
 {
 	const int width = 800, height = 600;
 
@@ -60,7 +43,7 @@ void screenshotFunc(int)
 	glReadPixels(0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE, pixels);
 
 	int i, j;
-	FILE *fp = fopen("screenshot.ppm", "wb"); /* b - binary mode */
+	FILE *fp = fopen("screenshot.ppm", "wb");
 	fprintf(fp, "P6\n%d %d\n255\n", width, height);
 
 	for (j = 0; j < height; ++j)
@@ -68,9 +51,9 @@ void screenshotFunc(int)
 		for (i = 0; i < width; ++i)
 		{
 			static unsigned char color[3];
-			color[0] = pixels[(width * j + i) * 3 + 0];  /* red */
-			color[1] = pixels[(width * j + i) * 3 + 1];  /* green */
-			color[2] = pixels[(width * j + i) * 3 + 2];  /* blue */
+			color[0] = pixels[(width * j + i) * 3 + 0];
+			color[1] = pixels[(width * j + i) * 3 + 1];
+			color[2] = pixels[(width * j + i) * 3 + 2];
 			(void) fwrite(color, 1, 3, fp);
 		}
 	}
@@ -78,31 +61,64 @@ void screenshotFunc(int)
 	fclose(fp);
 
 	delete[] pixels;
+}*/
+
+void
+testMk(AssetsLibrary::Ptr assets)
+{
+	std::shared_ptr<data::HalfEdgeCollection> halfEdgeCollection = data::HalfEdgeCollection::create(assets->geometry("cube")->indices());
+
+	std::map<std::string, Any> qarkData;
+
+	int myints[] = {3, 4, 5, 6};
+	std::vector<Any> toto(myints, myints + sizeof(myints) / sizeof(int));
+
+	std::string myStrings[] = {"a", "b", "c", "d"};
+	std::vector<Any> strings(myStrings, myStrings + sizeof(myStrings) / sizeof(std::string));
+
+	qarkData["plop"] = 42;
+	qarkData["plip"] = std::string("sali");
+	qarkData["plup"] = toto;
+
+	std::map<std::string, Any> plap;
+
+	plap["youhou"] = 4;
+	plap["yahaa"]  = strings;
+	
+
+	qarkData["plap"] = plap;
+
+
+	minko::Qark::ByteArray compressedData = minko::Qark::encode(qarkData);
+	std::cout << std::endl << std::endl << "-----------------" << std::endl << std::endl <<std::flush;
+	minko::Qark::Object    obj 			= minko::Qark::decode(compressedData);
+
+	std::map<std::string, Any> decodedMap = minko::Any::cast<std::map<std::string, Any>>(obj);
 }
 
 int main(int argc, char** argv)
 {
-	// glut/glew init
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-	glutInitWindowSize(800, 600);
-	glutCreateWindow("Minko Examples");
-
-#ifdef _WIN32
-	glewInit();
-#endif
-	// !glut/glew init
+    glfwInit();
+    auto window = glfwCreateWindow(800, 600, "Minko Examples", NULL, NULL);
+    glfwMakeContextCurrent(window);
 
 	auto context = render::OpenGLES2Context::create();
+
+    context->setBlendMode(render::Blending::Mode::DEFAULT);
+
 	auto assets	= AssetsLibrary::create(context)
 		->registerParser<file::EffectParser>("effect")
+		->registerParser<file::JPEGParser>("jpg")
+		->registerParser<file::PNGParser>("png")
 		->geometry("cube", geometry::CubeGeometry::create(context))
-		->geometry("sphere", geometry::SphereGeometry::create(context))
-		->queue("DirectionalLight.effect")
-		->queue("Red.effect")
-		->queue("Basic.effect");
+		//->geometry("sphere", geometry::SphereGeometry::create(context, 40))
+		->queue("textures/collage.jpg")
+        ->queue("textures/box3.png")
+		->queue("effects/Texture.effect")
+		//->queue("effects/Red.effect")
+		->queue("effects/Basic.effect");
 
-	assets->defaultOptions()->includePath("effects");
+	/*assets->defaultOptions()->includePath("effects");*/
 
 	auto _ = assets->complete()->connect([](AssetsLibrary::Ptr assets)
 	{
@@ -111,10 +127,12 @@ int main(int argc, char** argv)
 
 		root->addChild(group)->addChild(camera);
 
-		camera->addController(renderingController = RenderingController::create(assets->context()));
+        renderingController = RenderingController::create(assets->context());
+        renderingController->backgroundColor(0x7F7F7FFF);
+		camera->addController(renderingController);
 
 		auto view = Matrix4x4::create()->perspective(.785f, 800.f / 600.f, .1f, 1000.f);
-		auto color = Vector4::create(0.f, 0.f, 1.f, 1.f);
+		auto color = Vector4::create(0.f, 0.f, 1.f, .1f);
 		auto lightDirection = Vector3::create(0.f, -1.f, -1.f);
 
 		mesh->addController(TransformController::create());
@@ -124,29 +142,16 @@ int main(int argc, char** argv)
 			data::Provider::create()
 				->set("material/diffuse/rgba",			color)
 				->set("transform/worldToScreenMatrix",	view)
-				->set("light/direction",				lightDirection),
-			assets->effect("basic")
+				->set("light/direction",				lightDirection)
+				->set("material/diffuse/map",			assets->texture("textures/box3.png")),
+			assets->effect("texture")
 		));
 
 		group->addChild(mesh);
-
-		/*
-		for (auto i = 0; i < 50000; ++i)
-		{
-			group->addChild(Node::create()->addController(SurfaceController::create(
-				assets->geometry("cube"),
-				data::Provider::create()
-					->set("material/diffuse/rgba",			color)
-					->set("transform/worldToScreenMatrix",	view)
-					->set("light/direction",				lightDirection),
-				assets->effect("directional light")
-			)));
-		}
-		*/
+		//testMk(assets);
 	});
 
 	assets->load();
-	HalfEdgeCollection* halfEdgeCollection = new HalfEdgeCollection(assets->geometry("sphere")->indices());
 
 	// auto oglContext = context;
 	// auto fx = assets->effect("directional light");
@@ -158,11 +163,25 @@ int main(int argc, char** argv)
 	// std::cout << "== program info logs ==" << std::endl;
 	// std::cout << oglContext->getProgramInfoLogs(fx->shaders()[0]->program()) << std::endl;
 
-	glutTimerFunc(1000 / FRAMERATE, timerFunc, 0);
-	glutTimerFunc(1000, screenshotFunc, 0);
+	//glutTimerFunc(1000 / FRAMERATE, timerFunc, 0);
+	//glutTimerFunc(1000, screenshotFunc, 0);
 
-	glutDisplayFunc(renderScene);
-	glutMainLoop();
 
-	return 0;
+	while(!glfwWindowShouldClose(window))
+    {
+        mesh->controller<TransformController>()->transform()->prependRotationY(.01f);
+
+	    renderingController->render();
+
+	    printFramerate();
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+ 
+    glfwTerminate();
+
+    exit(EXIT_SUCCESS);
 }
