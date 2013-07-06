41d2a61ddf7e6807ea0c5a95fdb27d696613b215
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-07-06 18:19:35 +0200

    include GL/gl.h instead of GL/glut.h
    Former-commit-id: cac3e8cdac91101fbacfde68eca373a7a5ccf6de [formerly f966cdea8df5aa541a57360515a0141af5378223] [formerly 0c72d9ec8ecd7ad00184ef5194a7ddf86c3353e0]
    Former-commit-id: dcf77af885018cc7a121aa3ccc7d1e6c9afff15f [formerly 10d1dfea5eca373c77aed8b47534e5fba991c5b6] [formerly e54c7f826793a4cab104cdd413d8b1247cc8e166]
    Former-commit-id: 5287fff935af4a5508957d6250f14c3b355992d8
    Former-commit-id: e34734d2008f90d7a39bde1d4ba4ccf2f93b21fc

--- framework/src/minko/render/OpenGLES2Context.cpp
+++ framework/src/minko/render/OpenGLES2Context.cpp
@@ -23,11 +23,11 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 
 #define GL_GLEXT_PROTOTYPES
 #ifdef __APPLE__
-# include <GL/glut.h>
+# include <GL/gl.h>
 #elif _WIN32
 # include "GL/glew.h"
 #else
-# include <GL/glut.h>
+# include <GL/gl.h>
 #endif
 
 using namespace minko::render;
