4a0d93402811ab899cd14791f43a7b87f8f5f498
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-07-07 19:26:40 +0200

    fix coding style
    Former-commit-id: 282006c6f3b54e3081c3a6ecf00e01f71be3b46c
    Former-commit-id: 5db7a4d0626b105542e0ace0ed256c18c1a8ae74
    Former-commit-id: b970d9facd406bde045b646b75876d8b44fe27b8

--- framework/src/minko/controller/PerspectiveCamera.hpp
+++ framework/src/minko/controller/PerspectiveCamera.hpp
@@ -20,6 +20,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 #pragma once
 
 #include "minko/Common.hpp"
+
 #include "minko/controller/AbstractController.hpp"
 #include "minko/data/Provider.hpp"
 #include "minko/math/Matrix4x4.hpp"
