c05d2be6609c4c71560a1e828421c1c56f28264f
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-07-07 14:36:42 +0200

    move resource classes into minko/render
    Former-commit-id: 15936121bd99ef6573f1859b3a2daeb05fb2c2a0
    Former-commit-id: d3cf91d97b9a77f519f27a35f31629ad060f554e
    Former-commit-id: 30cdd24ec9ec934743a5f699a697b3a16fca246d

--- framework/src/minko/geometry/CubeGeometry.hpp
+++ framework/src/minko/geometry/CubeGeometry.hpp
@@ -21,8 +21,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 
 #include "minko/Common.hpp"
 #include "Geometry.hpp"
-#include "minko/resource/VertexStream.hpp"
-#include "minko/resource/IndexStream.hpp"
+#include "minko/render/VertexStream.hpp"
+#include "minko/render/IndexStream.hpp"
 
 namespace minko
 {
