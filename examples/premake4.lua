b3218bfce601ed8118f07b3a5922a4695246a505
Alexandre Cyprien <alexandre.cyprien@aerys.in>
2013-07-07 11:42:03 +0200

    Add deserialization of the scene hierarchie
    Former-commit-id: b6b0ec4d131ee41f559932cc8ee687cbecd9ff2e
    Former-commit-id: d9a2a7e6665ee6ebd035d0f0e541039393d5c9ac
    Former-commit-id: 1bb140ab0ff58afb3edc0afeb552ae42157fa30e

--- examples/premake4.lua
+++ examples/premake4.lua
@@ -5,6 +5,7 @@ project "minko-examples"
   links {
     "minko-jpeg",
     "minko-png",
+    "minko-mk",
     "minko-framework"
   }
   files { "**.hpp", "**.h", "**.cpp" }
@@ -13,7 +14,8 @@ project "minko-examples"
     "lib/glfw/include",
     "../framework/src",
     "../plugins/jpeg/src",
-    "../plugins/png/src"
+    "../plugins/png/src",
+    "../plugins/mk/src"
   }
 
   configuration { "debug"}
---
+++ plugins/mk/src/minko/deserialize/SceneDeserializer.hpp
@@ -0,0 +1,117 @@
+/*
+Copyright (c) 2013 Aerys
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#pragma once
+
+#include "minko/Common.hpp"
+#include "minko/Any.hpp"
+#include "minko/scene/Node.hpp"
+#include "minko/Qark.hpp"
+#include "minko/AssetsLibrary.hpp"
+#include "minko/render/AbstractContext.hpp"
+#include "minko/controller/AbstractController.hpp"
+#include "minko/file/Options.hpp"
+#include "minko/deserialize/MkTypes.hpp"
+#include "minko/controller/SurfaceController.hpp"
+#include "minko/controller/TransformController.hpp"
+#include "minko/geometry/CubeGeometry.hpp"
+#include "minko/math/Vector4.hpp"
+#include "minko/math/Matrix4x4.hpp"
+
+namespace minko
+{
+	namespace deserialize
+	{
+		class SceneDeserializer
+		{
+			// typedef
+		public:
+			typedef std::shared_ptr<SceneDeserializer>									Ptr;
+			typedef std::shared_ptr<render::AbstractContext>							ContextPtr;
+			typedef std::map<scene::Node, std::vector<controller::AbstractController>>	ControllerMap;
+			typedef std::map<scene::Node, uint>											NodeMap;
+			typedef std::shared_ptr<file::Options>										OptionsPtr;
+		
+		private:
+			typedef	 std::map<std::string, Any> NodeInfo;
+			typedef  std::shared_ptr<scene::Node> (SceneDeserializer::*NodeDeserializer)(NodeInfo		nodeInfo,
+																						 OptionsPtr		options,
+																						 ControllerMap	controllerMap,
+																						 NodeMap		nodeMap);
+
+			// attributes
+		private:
+			std::shared_ptr<AssetsLibrary>		_deserializedAssets;
+			ContextPtr							_context; 
+			std::map<int, NodeDeserializer>		_nodeDeserializer;
+
+			// method
+		public:
+			inline static
+			Ptr
+			create(ContextPtr context)
+			{
+				return std::shared_ptr<SceneDeserializer>(new SceneDeserializer(context));
+			}
+
+			inline
+			std::shared_ptr<AssetsLibrary>
+			deserializedAssets()
+			{
+				return _deserializedAssets;
+			}
+
+			std::shared_ptr<scene::Node>
+			deserializeScene(Qark::Object	sceneObject,
+							 Qark::Object	assetsObject,
+							 OptionsPtr		options,
+							 ControllerMap	controllerMap,
+							 NodeMap		nodeMap);
+
+		private:
+			std::shared_ptr<scene::Node>
+			deserializeNode(Qark::Object	nodeObject,
+							OptionsPtr		options,
+							ControllerMap	controllerMap,
+							NodeMap			nodeMap);
+
+
+			std::shared_ptr<scene::Node>
+			deserializeGroup(NodeInfo		nodeInfo,
+							OptionsPtr		options,
+							ControllerMap	controllerMap,
+							NodeMap			nodeMap);
+
+			std::shared_ptr<scene::Node>
+			deserializeMesh(NodeInfo		nodeInfo,
+							OptionsPtr		options,
+							ControllerMap	controllerMap,
+							NodeMap			nodeMap);
+
+			void
+			initializeNodeDeserializer();
+
+			SceneDeserializer(ContextPtr context) : 
+				_context(context),
+				_deserializedAssets(AssetsLibrary::create(context))
+			{
+			}
+		};
+	}
+}
