f63984a0e66ad2c62a13ea99e9c1339443c718e5
Alexandre Cyprien <alexandre.cyprien@aerys.in>
2013-07-12 11:49:36 +0200

    Clean main
    Former-commit-id: c4adbd8f0e7e64f6c2aec1ae9b62508131f2708e [formerly fa694f229c99555beddf4f57a448db20d341084e]
    Former-commit-id: 04343495628e12358332de0a52f0445b42481bbc [formerly 9022bfea44510646d10316fadcc5fd7eed239c24]
    Former-commit-id: 588c5c484db7465406814b91e6e1ff072d1be13a

--- examples/src/Main.cpp
+++ examples/src/Main.cpp
@@ -34,66 +34,10 @@ printFramerate(const unsigned int delay = 1)
 	}
 }
 
-/*void screenshotFunc(int)
-{
-	const int width = 800, height = 600;
-
-	char* pixels = new char[3 * width * height];
-
-	glReadPixels(0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE, pixels);
-
-	int i, j;
-	FILE *fp = fopen("screenshot.ppm", "wb");
-	fprintf(fp, "P6\n%d %d\n255\n", width, height);
-
-	for (j = 0; j < height; ++j)
-	{
-		for (i = 0; i < width; ++i)
-		{
-			static unsigned char color[3];
-			color[0] = pixels[(width * j + i) * 3 + 0];
-			color[1] = pixels[(width * j + i) * 3 + 1];
-			color[2] = pixels[(width * j + i) * 3 + 2];
-			(void) fwrite(color, 1, 3, fp);
-		}
-	}
-
-	fclose(fp);
-
-	delete[] pixels;
-}*/
-
 void
 testMk(AssetsLibrary::Ptr assets)
 {
 	std::shared_ptr<data::HalfEdgeCollection> halfEdgeCollection = data::HalfEdgeCollection::create(assets->geometry("cube")->indices());
-
-	std::map<std::string, Any> qarkData;
-
-	int myints[] = {3, 4, 5, 6};
-	std::vector<Any> toto(myints, myints + sizeof(myints) / sizeof(int));
-
-	std::string myStrings[] = {"a", "b", "c", "d"};
-	std::vector<Any> strings(myStrings, myStrings + sizeof(myStrings) / sizeof(std::string));
-
-	qarkData["plop"] = 42;
-	qarkData["plip"] = std::string("sali");
-	qarkData["plup"] = toto;
-
-	std::map<std::string, Any> plap;
-
-	plap["youhou"] = 4;
-	plap["yahaa"]  = strings;
-	
-
-	qarkData["plap"] = plap;
-
-
-	minko::Qark::ByteArray compressedData = minko::Qark::encode(0);
-	std::cout << std::endl << std::endl << "-----------------" << std::endl << std::endl <<std::flush;
-	minko::Qark::Object    obj 			= minko::Qark::decode(compressedData);
-
-	//std::map<std::string, Any> decodedMap = minko::Any::cast<std::map<std::string, Any>>(obj);
 }
 
 int main(int argc, char** argv)
@@ -137,53 +81,18 @@ int main(int argc, char** argv)
 		camera->addController(renderingController);
 
 		group->addController(TransformController::create());
-		group->controller<TransformController>()->transform()->appendTranslation(0.f, -4.f, -5.f);
+		group->controller<TransformController>()->transform()->appendTranslation(0.f, -4.f, -0.f);
 		group->controller<TransformController>()->transform()->prependRotationY(3.14/4);
-
-		/*auto view = Matrix4x4::create()->perspective(.785f, 800.f / 600.f, .1f, 1000.f);
-		auto color = Vector4::create(0.f, 0.f, 1.f, 1.f);
-		auto lightDirection = Vector3::create(0.f, -1.f, -1.f);
-
-		mesh->addController(TransformController::create());
-		mesh->controller<TransformController>()->transform()->appendTranslation(0.f, 0.f, -10.f);
-		mesh->addController(SurfaceController::create(
-			assets->geometry("sphere"),
-			data::Provider::create()
-				->set("material/blending",				render::Blending::Mode::ALPHA)
-				->set("material/depthFunc",				render::CompareMode::LESS)
-				->set("material/depthMask",				true)
-				->set("material/diffuse/rgba",			color)
-				->set("transform/worldToScreenMatrix",	view)
-				->set("light/direction",				lightDirection)
-				->set("material/diffuse/map",			assets->texture("box3.png")),
-			assets->effect("vertex normal")
-		));*/
 
 		group->addChild(assets->node("models/sponza.mk"));
 
-		//group->addChild(mesh);
 		//testMk(assets);
 	});
 
 	assets->load();
 
-	/*
-	auto fx = assets->effect("directional light");
-
-	std::cout << "== vertex shader compilation logs ==" << std::endl;
-	std::cout << context->getShaderCompilationLogs(fx->shaders()[0]->vertexShader()) << std::endl;
-	std::cout << "== fragment shader compilation logs ==" << std::endl;
-	std::cout << context->getShaderCompilationLogs(fx->shaders()[0]->fragmentShader()) << std::endl;
-	std::cout << "== program info logs ==" << std::endl;
-	std::cout << context->getProgramInfoLogs(fx->shaders()[0]->id()) << std::endl;
-	*/
-
-	//glutTimerFunc(1000 / FRAMERATE, timerFunc, 0);
-	//glutTimerFunc(1000, screenshotFunc, 0);
-
 	while(!glfwWindowShouldClose(window))
     {
-        //mesh->controller<TransformController>()->transform()->prependRotationY(.01f);
 		group->controller<TransformController>()->transform()->prependRotationY(.01f);
 	    renderingController->render();
 
