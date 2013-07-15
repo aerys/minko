9b0073b6d2b0a185489313973aad33952ecf181a
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-07-16 00:53:25 +0200

    working PerspectiveCamera component
    Former-commit-id: 3b6f4fc3e4dae1ca841b844f92b49b1ac448470f
    Former-commit-id: c180834412e4742457d2429b131ca461b2c2e75c

--- framework/src/minko/component/PerspectiveCamera.hpp
+++ framework/src/minko/component/PerspectiveCamera.hpp
@@ -21,9 +21,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 
 #include "minko/Common.hpp"
 
-#include "minko/component/AbstractComponent.hpp"
-#include "minko/data/Provider.hpp"
-#include "minko/math/Matrix4x4.hpp"
+#include "minko/component/AbstractRootDataComponent.hpp"
 #include "minko/data/Container.hpp"
 
 namespace minko
@@ -31,8 +29,7 @@ namespace minko
 	namespace component
 	{
 		class PerspectiveCamera :
-			public AbstractComponent,
-			public std::enable_shared_from_this<PerspectiveCamera>
+            public AbstractRootDataComponent
 		{
 		public:
 			typedef std::shared_ptr<PerspectiveCamera> Ptr;
@@ -48,11 +45,8 @@ namespace minko
 			std::shared_ptr<math::Matrix4x4>				_view;
 			std::shared_ptr<math::Matrix4x4>				_projection;
 			std::shared_ptr<math::Matrix4x4>				_viewProjection;
+            std::shared_ptr<math::Vector3>                  _position;
 
-			std::shared_ptr<data::Provider>					_data;
-
-			Signal<AbsCtrlPtr, NodePtr>::Slot				_targetAddedSlot;
-			Signal<AbsCtrlPtr, NodePtr>::Slot				_targetRemovedSlot;
 			data::Container::PropertyChangedSignal::Slot	_modelToWorldChangedSlot;
 
 		public:
@@ -60,30 +54,26 @@ namespace minko
 			Ptr
 			create(float fov, float aspectRatio, float zNear, float zFar)
 			{
-				auto ctrl  = std::shared_ptr<PerspectiveCamera>(
-                    new PerspectiveCamera(fov, aspectRatio, zNear, zFar)
-                );
+				auto ctrl  = std::shared_ptr<PerspectiveCamera>(new PerspectiveCamera(fov, aspectRatio, zNear, zFar));
 
 				ctrl->initialize();
 
 				return ctrl;
 			}
 
+        protected:
+            void
+            targetAddedHandler(AbstractComponent::Ptr ctrl, NodePtr target);
+
 		private:
 			PerspectiveCamera(float fov, float aspectRatio, float zNear, float zFar);
 
 			void
-			initialize();
-
-			void
-			targetAddedHandler(AbsCtrlPtr ctrl, NodePtr node);
-
-			void
-			targetRemovedHandler(AbsCtrlPtr ctrl, NodePtr node);
-
-			void
 			localToWorldChangedHandler(std::shared_ptr<data::Container> data,
 									   const std::string&				propertyName);
+
+            void
+            updateMatrices(std::shared_ptr<math::Matrix4x4> modelToWorldMatrix);
 		};
 	}
 }
--- framework/src/minko/math/Matrix4x4.hpp
+++ framework/src/minko/math/Matrix4x4.hpp
@@ -141,6 +141,9 @@ namespace minko
 			Ptr
 			transpose();
 
+            std::shared_ptr<Vector3>
+            transform(std::shared_ptr<Vector3> v, std::shared_ptr<Vector3> output = nullptr);
+
 			inline
 			Ptr
 			operator*(Ptr value)
@@ -175,10 +178,7 @@ namespace minko
 			}
 
 			Ptr
-			perspective(float fov,
-		                float ratio,
-		                float zNear,
-		                float zFar);
+			perspective(float fov, float ratio, float zNear, float zFar);
 
 			/**
 			 * Builds a (left-handed) view transform.
