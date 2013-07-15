6b1d1e0259a7587aa120f7b49c8ae1c256b6e799
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-07-16 00:53:25 +0200

    working PerspectiveCamera component
    Former-commit-id: 1f3401090fc410b4a3da69dbd93cb92769c75c4f
    Former-commit-id: dc4b0bd3063c1ff4266c4b3a6ae664c7f39bae3a

--- framework/src/minko/math/Matrix4x4.hpp
+++ framework/src/minko/math/Matrix4x4.hpp
@@ -154,6 +154,9 @@ namespace minko
 			Ptr
 			transpose();
 
+            std::shared_ptr<Vector3>
+            transform(std::shared_ptr<Vector3> v, std::shared_ptr<Vector3> output = nullptr);
+
 			inline
 			Ptr
 			operator*(Ptr value)
@@ -191,10 +194,7 @@ namespace minko
 			}
 
 			Ptr
-			perspective(float fov,
-		                float ratio,
-		                float zNear,
-		                float zFar);
+			perspective(float fov, float ratio, float zNear, float zFar);
 
 			/**
 			 * Builds a (left-handed) view transform.
