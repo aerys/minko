b803ca5bc21c7a57bacccf5d5878f27d2527d8c8
Warren Seine <warren.seine@aerys.in>
2013-07-16 09:47:12 +0200

    Fix compilation on Mac OS X.
    Former-commit-id: 34ab85a6431acd19aaf6147e832f16d7ed7b80f8 [formerly 11d26fff0c44bdcf2415c76f011f8a1afaffb833]
    Former-commit-id: ff5e211756d185a22572d5b6345d35ba0e9ad45d [formerly 8489802d3f67a8c508325ae359ee37748544fcce]
    Former-commit-id: ffeb1d9d8c51dbe0d7330ad56eaa45820659a7c2

--- plugins/png/premake4.lua
+++ plugins/png/premake4.lua
@@ -1,40 +1,38 @@
 project "minko-png"
-  kind "StaticLib"
-  language "C++"
-  files { "**.hpp", "**.h", "**.cpp", "**.c" }
-  includedirs {
-	"src",
-	"lib/lodepng/src",
-	"../../framework/src"
-  }
-  links { "minko-framework" }
-  defines
-  {
-	"LODEPNG_NO_COMPILE_ENCODER",
-	"LODEPNG_NO_COMPILE_DISK"
-  }
+	kind "StaticLib"
+	language "C++"
+	files { "**.hpp", "**.h", "**.cpp", "**.c" }
+	includedirs {
+		"src",
+		"lib/lodepng/src",
+		"../../framework/src"
+	}
+	links { "minko-framework" }
+	defines
+	{
+		"LODEPNG_NO_COMPILE_ENCODER",
+		"LODEPNG_NO_COMPILE_DISK"
+	}
 
-  configuration { "debug"}
-    defines { "DEBUG" }
-    flags { "Symbols" }
-    targetdir "bin/debug"
+	configuration { "debug"}
+		defines { "DEBUG" }
+		flags { "Symbols" }
+		targetdir "bin/debug"
 
-  configuration { "release" }
-    defines { "NDEBUG" }
-    flags { "OptimizeSpeed" }
-    targetdir "bin/release"
+	configuration { "release" }
+		defines { "NDEBUG" }
+		flags { "OptimizeSpeed" }
+		targetdir "bin/release"
 
-  -- linux
-  configuration { "linux" }
-    buildoptions "-std=c++0x"
-    linkoptions "-std=c++0x"
+	-- linux
+	configuration { "linux" }
+		buildoptions "-std=c++11"
 
-  -- windows
-  configuration { "windows", "x32" }
+	-- windows
+	configuration { "windows", "x32" }
 
-  -- macos
-  configuration { "debug", "macosx" }
-    buildoptions { "-std=c++11", "-stdlib=libc++" }
-    linkoptions { "-std=c++11", "-stdlib=libc++" }
-    libdirs { "/opt/local/lib/" }
-    includedirs { "/opt/local/include/" }
+	-- macos
+	configuration { "debug", "macosx" }
+		buildoptions { "-std=c++11", "-stdlib=libc++" }
+		libdirs { "/opt/local/lib/" }
+		includedirs { "/opt/local/include/" }
