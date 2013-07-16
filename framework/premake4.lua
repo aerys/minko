aff2a90e25196dc83545ce450e373e123c2da9a3
Warren Seine <warren.seine@aerys.in>
2013-07-16 09:47:12 +0200

    Fix compilation on Mac OS X.
    Former-commit-id: b3c6f35baff05731fa0ba5ed77770e550e4f4032 [formerly 34ab85a6431acd19aaf6147e832f16d7ed7b80f8] [formerly 11d26fff0c44bdcf2415c76f011f8a1afaffb833]
    Former-commit-id: 9b54d95631db1a2446179c6e7d88786bff074dcb [formerly ff5e211756d185a22572d5b6345d35ba0e9ad45d] [formerly 8489802d3f67a8c508325ae359ee37748544fcce]
    Former-commit-id: ffeb1d9d8c51dbe0d7330ad56eaa45820659a7c2
    Former-commit-id: b803ca5bc21c7a57bacccf5d5878f27d2527d8c8

--- framework/premake4.lua
+++ framework/premake4.lua
@@ -1,41 +1,41 @@
 -- A project defines one build target
 project "minko-framework"
-  kind "StaticLib"
-  language "C++"
-  files { "**.hpp", "**.h", "**.cpp", "**.c" }
-  includedirs {
-    "src",
-    "lib/jsoncpp/src"
-  }
-  defines {
-    "JSON_IS_AMALGAMATION"
-  }
-  
-  configuration { "debug"}
-    defines { "DEBUG" }
-    flags { "Symbols" }
-    targetdir "bin/debug"
+	kind "StaticLib"
+	language "C++"
+	files { "**.hpp", "**.h", "**.cpp", "**.c" }
+	includedirs {
+		"src",
+		"lib/jsoncpp/src"
+	}
+	defines {
+		"JSON_IS_AMALGAMATION"
+	}
+	
+	configuration { "debug"}
+		defines { "DEBUG" }
+		flags { "Symbols" }
+		targetdir "bin/debug"
 
-  configuration { "release" }
-    defines { "NDEBUG" }
-    flags { "OptimizeSpeed" }
-    targetdir "bin/release"
+	configuration { "release" }
+		defines { "NDEBUG" }
+		flags { "OptimizeSpeed" }
+		targetdir "bin/release"
 
-  -- linux
-  configuration { "linux" }
-    links { "GL", "GLU" }
-    buildoptions "-std=c++0x"
-    linkoptions "-std=c++0x"
+	-- linux
+	configuration { "linux" }
+		links { "GL", "GLU" }
+		buildoptions "-std=c++11"
+		linkoptions "-std=c++11"
 
-  -- windows
-  configuration { "windows", "x32" }
+	-- windows
+	configuration { "windows", "x32" }
 	links { "glew32" }
-    includedirs { "lib/glew/include" }
+		includedirs { "lib/glew/include" }
 	libdirs { "lib/glew/bin/win32" }
 
-  -- macos
-  configuration { "macosx" }
-    buildoptions { "-std=c++11", "-stdlib=libc++" }
-    linkoptions { "-std=c++11", "-stdlib=libc++" }
-    libdirs { "/opt/local/lib/" }
-    includedirs { "/opt/local/include/" }
+	-- macos
+	configuration { "macosx" }
+		buildoptions { "-std=c++11", "-stdlib=libc++" }
+		linkoptions { }
+		libdirs { "/opt/local/lib/" }
+		includedirs { "/opt/local/include/" }
