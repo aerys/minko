ff5e211756d185a22572d5b6345d35ba0e9ad45d
Warren Seine <warren.seine@aerys.in>
2013-07-16 09:47:12 +0200

    Fix compilation on Mac OS X.
    Former-commit-id: 11d26fff0c44bdcf2415c76f011f8a1afaffb833
    Former-commit-id: 8489802d3f67a8c508325ae359ee37748544fcce

--- examples/premake4.lua
+++ examples/premake4.lua
@@ -1,49 +1,49 @@
 -- A project defines one build target
 project "minko-examples"
-  kind "ConsoleApp"
-  language "C++"
-  links {
-    "minko-jpeg",
-    "minko-png",
-    "minko-framework"
-  }
-  files { "**.hpp", "**.h", "**.cpp" }
-  includedirs {
-    "src",
-    "lib/glfw/include",
-    "../framework/src",
-    "../plugins/jpeg/src",
-    "../plugins/png/src"
-  }
+	kind "ConsoleApp"
+	language "C++"
+	links {
+		"minko-jpeg",
+		"minko-png",
+		"minko-framework"
+	}
+	files { "**.hpp", "**.h", "**.cpp" }
+	includedirs {
+		"src",
+		"lib/glfw/include",
+		"../framework/src",
+		"../plugins/jpeg/src",
+		"../plugins/png/src"
+	}
 
-  configuration { "debug"}
-    defines { "DEBUG" }
-    flags { "Symbols" }
-    targetdir "bin/debug"
+	configuration { "debug"}
+		defines { "DEBUG" }
+		flags { "Symbols" }
+		targetdir "bin/debug"
 
-  configuration { "release" }
-    defines { "NDEBUG" }
-    flags { "OptimizeSpeed" }
-    targetdir "bin/release"
+	configuration { "release" }
+		defines { "NDEBUG" }
+		flags { "OptimizeSpeed" }
+		targetdir "bin/release"
 
-  -- linux
-  configuration { "linux" }
-    links { "GL", "glfw3", "m", "Xrandr", "Xxf86vm", "Xi", "rt" }
-    buildoptions "-std=c++0x"
-    linkoptions "-std=c++0x"
+	-- linux
+	configuration { "linux" }
+		links { "GL", "glfw3", "m", "Xrandr", "Xxf86vm", "Xi", "rt" }
+		buildoptions "-std=c++11"
+		linkoptions "-std=c++11"
 
-  -- windows
-  configuration { "windows", "x32" }
-    links { "OpenGL32", "glfw3dll", "glew32" }
-    libdirs {
-      "lib/glfw/bin/win32",
-      "../framework/lib/glew/bin/win32"
-    }
+	-- windows
+	configuration { "windows", "x32" }
+		links { "OpenGL32", "glfw3dll", "glew32" }
+		libdirs {
+			"lib/glfw/bin/win32",
+			"../framework/lib/glew/bin/win32"
+		}
 
-  -- macos
-  configuration { "debug", "macosx" }
-    buildoptions { "-std=c++11", "-stdlib=libc++" }
-    linkoptions { "-std=c++11", "-stdlib=libc++" }
-    links { "glfw3", "m", "Cocoa.framework", "OpenGL.framework", "IOKit.framework" }
-    libdirs { "/opt/local/lib/" }
-    includedirs { "/opt/local/include/" }
+	-- macos
+	configuration { "debug", "macosx" }
+		buildoptions { "-std=c++11", "-stdlib=libc++" }
+		linkoptions { "-std=c++11", "-stdlib=libc++" }
+		links { "glfw3", "m", "Cocoa.framework", "OpenGL.framework", "IOKit.framework" }
+		libdirs { "/opt/local/lib/" }
+		includedirs { "/opt/local/include/" }
