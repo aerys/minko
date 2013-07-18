1304febc69c292a1af013ec335cc9e9d9877d3ce
Pierre-Edouard Landes <pierreedouard.landes@aerys.in>
2013-07-18 10:38:08 +0200

    Finished prettyfying the code.
    Former-commit-id: 572793ee0fac8e83a1c7c851e5f1b6487fc4e629 [formerly cdc1a1ec707638eaa3bda987f9e264af1cf6d1fc]
    Former-commit-id: 568a5ab7b285b7a51612fc4889b0f0e0900d764b [formerly a872e5d0c6271fbce7ae3369e8e47289c3f0ec00]
    Former-commit-id: 639a6312e674762acf560a7d01ac0332044988eb

--- plugins/bullet/src/minko/component/bullet/BoxShape.cpp
+++ plugins/bullet/src/minko/component/bullet/BoxShape.cpp
@@ -18,7 +18,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 */
 
 #include "BoxShape.hpp"
-#include "minko/math/Matrix4x4.hpp"
+
+#include <minko/math/Matrix4x4.hpp>
 
 using namespace minko;
 using namespace minko::math;
