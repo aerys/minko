52e17b08da5e8974998a4c88d3e417cc160f3d57
Warren Seine <warren.seine@aerys.in>
2013-07-18 23:13:08 +0200

    Fix compilation under Xcode.
    Former-commit-id: 81e2d1cf272e8814ea8fe8505a9ba0519055af44 [formerly c5a0dc60594dd74956d9ea5bb513bc11429ac902]
    Former-commit-id: 950788f401a2eb1bf03c92af9fa07cc745c771c0 [formerly 1d53cb391e528f0f95a926c165e0eeca5e9856d4]
    Former-commit-id: c3780695f1058a6aae77a406af43879989e1f0b5

--- plugins/mk/premake4.lua
+++ plugins/mk/premake4.lua
@@ -1,34 +1,34 @@
 project "minko-mk"
-  kind "StaticLib"
-  language "C++"
-  files { "**.hpp", "**.h", "**.cpp", "**.c" }
-  includedirs {
-	"src",
-	"../../framework/src"
-  }
-  links { "minko-framework" }
-  
-  configuration { "debug"}
-    defines { "DEBUG" }
-    flags { "Symbols" }
-    targetdir "bin/debug"
+	kind "StaticLib"
+	language "C++"
+	files { "**.hpp", "**.h", "**.cpp", "**.c" }
+	includedirs {
+		"src",
+		"../../framework/src"
+	}
+	links { "minko-framework" }
 
-  configuration { "release" }
-    defines { "NDEBUG" }
-    flags { "OptimizeSpeed" }
-    targetdir "bin/release"
+	configuration { "debug"}
+		defines { "DEBUG" }
+		flags { "Symbols" }
+		targetdir "bin/debug"
 
-  -- linux
-  configuration { "linux" }
-    buildoptions "-std=c++0x"
-    linkoptions "-std=c++0x"
+	configuration { "release" }
+		defines { "NDEBUG" }
+		flags { "OptimizeSpeed" }
+		targetdir "bin/release"
 
-  -- windows
-  configuration { "windows", "x32" }
+	-- linux
+	configuration { "linux" }
+		buildoptions "-std=c++0x"
+		-- linkoptions "-std=c++0x"
 
-  -- macos
-  configuration { "debug", "macosx" }
-    buildoptions { "-std=c++11", "-stdlib=libc++" }
-    linkoptions { "-std=c++11", "-stdlib=libc++" }
-    libdirs { "/opt/local/lib/" }
-    includedirs { "/opt/local/include/" }
+	-- windows
+	configuration { "windows", "x32" }
+
+	-- macos
+	configuration { "debug", "macosx" }
+		buildoptions { "-std=c++11", "-stdlib=libc++" }
+		-- linkoptions { "-std=c++11", "-stdlib=libc++" }
+		libdirs { "/opt/local/lib/" }
+		includedirs { "/opt/local/include/" }
