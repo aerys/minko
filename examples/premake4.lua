7d7093794b69ce84858f32142e54df5dd98eed69
Warren Seine <warren.seine@aerys.in>
2013-07-18 21:14:30 +0200

    Fix Vagrant deployment.
    Former-commit-id: 14c4ede75c9236d16261ba67879a4e056b68b34b

--- examples/premake4.lua
+++ examples/premake4.lua
@@ -25,14 +25,13 @@ project "minko-examples"
 
 	configuration { "release" }
 		defines { "NDEBUG" }
-		-- flags { "OptimizeSpeed" }
+		flags { "OptimizeSpeed" }
 		targetdir "bin/release"
 
 	-- linux
 	configuration { "linux" }
 		links { "GL", "glfw3", "m", "Xrandr", "Xxf86vm", "Xi", "rt" }
 		buildoptions "-std=c++11"
-		linkoptions "-std=c++11"
 
 	-- windows
 	configuration { "windows", "x32" }
@@ -45,12 +44,14 @@ project "minko-examples"
 	-- macos
 	configuration { "macosx" }
 		buildoptions { "-std=c++11", "-stdlib=libc++" }
-		linkoptions { "-std=c++11", "-stdlib=libc++" }
 		links { "glfw3", "m", "Cocoa.framework", "OpenGL.framework", "IOKit.framework" }
 		libdirs { "/opt/local/lib/" }
 		includedirs { "/opt/local/include/" }
 
 	configuration { "emscripten", "release" }
 		buildoptions { "-std=c++11" }
-		-- prebuildcommands { "grep '\\.bc' Makefile || sed -i Makefile -r -e 's/^\s*TARGET\s*=.*$/&.bc/'" }
-		postbuildcommands { 'BIN=bin/release/minko-examples; cp ${BIN} ${BIN}.bc; emcc ${BIN} -o ${BIN}.html -O1 -s ASM_JS=1 --preload-dir ../framework/bin/release/' }
+		local bin = "bin/release/minko-examples"
+		postbuildcommands {
+			'cp ' .. bin .. ' ' .. bin .. '.bc',
+			'emcc ' .. bin .. '.bc -o ' .. bin .. '.html -O1 -s ASM_JS=1 -s TOTAL_MEMORY=1073741824 --preload-dir effect --preload-dir texture'
+		}
