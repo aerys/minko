1683c9bde4153da6f8036bafd0f387f5e5d9d2f1
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-07-18 14:05:34 +0200

    add light example
    Former-commit-id: 1ef22eba7e7b4cb0b7d472ed801245f62d8d0a0d

---
+++ examples/light/premake4.lua
@@ -0,0 +1,52 @@
+-- A project defines one build target
+project "minko-example-light"
+	kind "ConsoleApp"
+	language "C++"
+	links {
+		"minko-jpeg",
+		"minko-png",
+		"minko-framework"
+	}
+	files { "**.hpp", "**.h", "**.cpp" }
+	includedirs {
+		"src",
+		"../../lib/glfw/include",
+		"../../framework/src",
+		"../../plugins/jpeg/src",
+		"../../plugins/png/src"
+	}
+	defines {
+		"MINKO_FRAMEWORK_EFFECTS_PATH=\"" .. path.getabsolute("../../framework/effect" .. "\"")
+	}
+
+	configuration { "debug"}
+		defines { "DEBUG" }
+		flags { "Symbols" }
+		targetdir "bin/debug"
+
+	configuration { "release" }
+		defines { "NDEBUG" }
+		flags { "OptimizeSpeed" }
+		targetdir "bin/release"
+
+	-- linux
+	configuration { "linux" }
+		links { "GL", "glfw3", "m", "Xrandr", "Xxf86vm", "Xi", "rt" }
+		buildoptions "-std=c++11"
+		linkoptions "-std=c++11"
+
+	-- windows
+	configuration { "windows", "x32" }
+		links { "OpenGL32", "glfw3dll", "glew32" }
+		libdirs {
+			"../../lib/glfw/bin/win32",
+			"../../framework/lib/glew/bin/win32"
+		}
+
+	-- macos
+	configuration { "debug", "macosx" }
+		buildoptions { "-std=c++11", "-stdlib=libc++" }
+		linkoptions { "-std=c++11", "-stdlib=libc++" }
+		links { "glfw3", "m", "Cocoa.framework", "OpenGL.framework", "IOKit.framework" }
+		libdirs { "/opt/local/lib/" }
+		includedirs { "/opt/local/include/" }
