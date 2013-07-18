bacc0e295d8169029ec88fd5b7f7f4da05b44afb
Warren Seine <warren.seine@aerys.in>
2013-07-18 12:02:03 +0200

    Refactor the folders / build system. Add README.md.
    Former-commit-id: f85c6dfb2a7ce351df5dd975195ee727550c3b70 [formerly 9a62a4a46e897ae834db151f7f780daab1077cc1] [formerly de3bb68f7680648e372eeb25d52ff3eff7aa8bba]
    Former-commit-id: c4a3090418c59294a9996c4c6140dfc01aca5b7b [formerly e0745b3e163729cb59b1d75a0484ea1201e147f2] [formerly 1639b99921d5853e28bbc5033e3ebb382f9ae576]
    Former-commit-id: 220a28590a1974b63dfa0763ff7de428b8e7501c
    Former-commit-id: 644174552411b87700d874dabed91275a6881710

--- scripts/premake_vs2010.bat
+++
@@ -1,7 +0,0 @@
-call premake_clean.bat
-call premake_templates.bat
-cd ..
-scripts\premake4.exe --os=windows --platform=x32 vs2010
-scripts\premake4.exe --os=windows --platform=x32 copyDLLs
-cd scripts
- 
