46bb2c49f5438717d660af380103bebb25dccf89
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-07-21 12:48:09 +0200

    add QuadGeometry
    Former-commit-id: 0384c1f69b0530799a19fa05bc044c02c1bbf3de
    Former-commit-id: e055d0efbabe5da0ac8f9fa117928f8465c41021

---
+++ framework/src/minko/geometry/QuadGeometry.cpp
@@ -0,0 +1,38 @@
+/*
+Copyright (c) 2013 Aerys
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include "QuadGeometry.hpp"
+
+#include "minko/render/VertexBuffer.hpp"
+#include "minko/render/IndexBuffer.hpp"
+
+using namespace minko::geometry;
+
+QuadGeometry::QuadGeometry(std::shared_ptr<render::AbstractContext> context)
+{
+    float xyzData[]         = {-.5f, .5f, 0.f, -.5f, -.5f, 0.f, .5f, .5f, 0.f, .5f, -.5f, 0.f};
+    unsigned short ind[]    = {0, 1, 2, 2, 1, 3};
+    auto vertexBuffer       = render::VertexBuffer::create(context, std::begin(xyzData), std::end(xyzData));
+    auto indexBuffer        = render::IndexBuffer::create(context, std::begin(ind), std::end(ind));
+
+    vertexBuffer->addAttribute("position", 3, 0);
+    addVertexBuffer(vertexBuffer);
+
+    indices(indexBuffer);
+}
