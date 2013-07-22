dbbb7a8083424143f3ac2204d691d5d9cb654c96
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-07-23 01:47:10 +0200

    use GL_CLAMP_TO_EDGE instead of GL_CLAMP for WebGL compatibility
    Former-commit-id: 4ebbddb349fd53b8120b06fc31fcefdca2d819e3
    Former-commit-id: 2eb3981ada763ddcaf37a2a8ce83d404e2eeea8a
    Former-commit-id: 7fa858322f57f58b215bd1c751b8d403c8eb7ebf

--- framework/src/minko/render/OpenGLES2Context.cpp
+++ framework/src/minko/render/OpenGLES2Context.cpp
@@ -449,8 +449,8 @@ OpenGLES2Context::createTexture(unsigned int 	width,
 	glBindTexture(GL_TEXTURE_2D, texture);
 
     // default sampler states
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
@@ -571,8 +571,8 @@ OpenGLES2Context::setSamplerStateAt(const unsigned int position, WrapMode wrappi
         switch (wrapping)
         {
         case WrapMode::CLAMP :
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
             break;
         case WrapMode::REPEAT :
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
