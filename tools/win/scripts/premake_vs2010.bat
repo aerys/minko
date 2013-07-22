1e862dd0174672700d808b3d46e9e1963e4aa215
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-07-22 08:35:22 +0200

    set assets in an "asset" directory that gets copied using a post-build action, same for DLLs
    Former-commit-id: 5330f3608fe57328c8ce33cdd365b1c8a6513926

--- tools/win/scripts/premake_vs2010.bat
+++ tools/win/scripts/premake_vs2010.bat
@@ -2,5 +2,4 @@ call premake_clean.bat
 call premake_templates.bat
 chdir ..\..\..
 tools\win\bin\premake4.exe --os=windows --platform=x32 vs2010
-tools\win\bin\premake4.exe --os=windows --platform=x32 copyDLLs
- cd tools\win\scripts
+cd tools\win\scripts
