0b36395c5daf074d1f0cad096d9a3d48aeb27614
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-07-22 08:36:36 +0200

    properly remove copied assets/DLLs by extending the "clean" premake action and remove deprecated cleanVS action
    Former-commit-id: 935d4b726aec749377d4de61f63bf1d3427758b3

--- tools/all/lib/vs.lua
+++ tools/all/lib/vs.lua
@@ -1,40 +1,8 @@
-newaction {
-	trigger = "copyDLLs",
-	description = "Copy the required DLLs into the app. output folder",
-	execute = function()
-		os.mkdir(project(1).basedir .. "/bin/release")
-		os.mkdir(project(1).basedir .. "/bin/debug")
-
-		for _, file in ipairs(os.matchfiles("**.dll")) do
-			os.copyfile(
-				file,
-				project(1).basedir .. "/bin/release/" .. path.getname(file)
-			)
-			os.copyfile(
-				file,
-				project(1).basedir .. "/bin/debug/" .. path.getname(file)
-			)
-		end
-	end
-}
-
-newaction {
-	trigger = "removeDLLs",
-	description = "Remove the DLLs copied by copyDLLs",
-	execute = function()
-	  os.rmdir(project(1).basedir .. "/bin")
-	end
-}
-
-newaction {
-	trigger = "cleanVS",
-	description = "Remove additional files added by VS",
-	execute = function()
-		os.rmdir("minko.sdf")
-		os.rmdir("minko.v11.suo")
-		os.rmdir("minko.suo")
-	end
-}
+if _ACTION == 'clean' then
+	os.remove(solution().basedir .. "/minko.sdf")
+	os.remove(solution().basedir .. "/minko.v11.suo")
+	os.remove(solution().basedir .. "/minko.suo")
+end
 
 newaction {
 	trigger = "installTemplates",
