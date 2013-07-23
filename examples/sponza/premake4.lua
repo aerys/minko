6a78846c28d386b22d38b56e5aa2a928d16fd861
Loic Bethmont <loic.bethmont@aerys.in>
2013-07-23 13:27:47 +0200

    Switched back to -O2 for emscripten final linking.
    Former-commit-id: deb68b3a52cae5ab6a7780410d4526e57bbcb1fd
    Former-commit-id: feeb16325df7fadcac98b4268c508b27002773fb

--- examples/sponza/premake4.lua
+++ examples/sponza/premake4.lua
@@ -124,5 +124,5 @@ project "minko-example-sponza"
 		local bin = "bin/release/" .. project().name
 		postbuildcommands {
 			'cp ' .. bin .. ' ' .. bin .. '.bc',
-			'emcc ' .. bin .. '.bc -o ' .. bin .. '.html -O1 -s ASM_JS=1 -s TOTAL_MEMORY=1073741824 --preload-file effect --preload-file texture --preload-file model'
+			'emcc ' .. bin .. '.bc -o ' .. bin .. '.html -O2 -s ASM_JS=1 -s TOTAL_MEMORY=1073741824 --preload-file effect --preload-file texture --preload-file model'
 		}
