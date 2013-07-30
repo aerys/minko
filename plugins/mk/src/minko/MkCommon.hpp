53027b7706def937182cb929b7667bd0871193df
Alexandre Cyprien <alexandre.cyprien@aerys.in>
2013-07-30 17:19:28 +0200

    Clean type deserializer Optimize matrix deserialization
    Former-commit-id: 3d188ce79008a8353100d29b8b24f634cfb83d2a

--- plugins/mk/src/minko/MkCommon.hpp
+++ plugins/mk/src/minko/MkCommon.hpp
@@ -2,3 +2,31 @@
 
 #include "minko/Common.hpp"
 #include <queue>
+
+namespace minko
+{
+	class Qark;
+
+	namespace data
+	{
+		class HalfEdge;
+		class HalfEdgeCollection;
+	}
+
+	namespace deserialize
+	{
+		class AssetsDeserializer;
+		class GeometryDeserializer;
+		class MkTypes;
+		class NameConverter;
+		class NodeDeserializer;
+		class SceneDeserializer;
+		class TypeDeserializer;
+	}
+
+	namespace file
+	{
+		class MkOptions;
+		class MkParser;
+	}
+}
