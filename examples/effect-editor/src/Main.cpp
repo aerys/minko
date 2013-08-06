285edddf920d6b4800c742470aeb30e46426ebc6
Pierre-Edouard Landes <pierreedouard.landes@aerys.in>
2013-08-06 18:01:10 +0200

    Attempt to open minko window inside qt.
    Former-commit-id: df0315de911e6229fcdc1ac17e549895ad57cb12

--- examples/effect-editor/src/Main.cpp
+++ examples/effect-editor/src/Main.cpp
@@ -1,12 +1,154 @@
-#include <minko/Minko.hpp>
+
+#include "QOpenGLWindow.hpp"
+
+#include "openglwindow.h"
+
+#include <QtGui/QGuiApplication>
+#include <QtGui/QMatrix4x4>
+#include <QtGui/QOpenGLShaderProgram>
+#include <QtGui/QScreen>
+
+#include <QtCore/qmath.h>
 
 using namespace minko;
 
-int
-main(int argc, char** argv)
-{
-	std::cout << "Hello World !" << std::endl;
 
-	return 0;
+//! [1]
+class TriangleWindow : public OpenGLWindow
+{
+public:
+    TriangleWindow();
+
+    void initialize();
+    void render();
+
+private:
+    GLuint loadShader(GLenum type, const char *source);
+
+    GLuint m_posAttr;
+    GLuint m_colAttr;
+    GLuint m_matrixUniform;
+
+    QOpenGLShaderProgram *m_program;
+    int m_frame;
+};
+
+TriangleWindow::TriangleWindow()
+    : m_program(0)
+    , m_frame(0)
+{
+}
+//! [1]
+
+//! [2]
+int 
+main(int argc, char **argv)
+{
+    QGuiApplication app(argc, argv);
+
+    QSurfaceFormat format;
+    format.setSamples(16);
+
+	/*
+    TriangleWindow window;
+    window.setFormat(format);
+    window.resize(640, 480);
+    window.show();
+
+    window.setAnimating(true);
+	*/
+
+	QOpenGLWindow window;
+	window.setFormat(format);
+	window.resize(640, 480);
+	window.show();
+
+    return app.exec();
+}
+//! [2]
+
+
+//! [3]
+static const char *vertexShaderSource =
+    "attribute highp vec4 posAttr;\n"
+    "attribute lowp vec4 colAttr;\n"
+    "varying lowp vec4 col;\n"
+    "uniform highp mat4 matrix;\n"
+    "void main() {\n"
+    "   col = colAttr;\n"
+    "   gl_Position = matrix * posAttr;\n"
+    "}\n";
+
+static const char *fragmentShaderSource =
+    "varying lowp vec4 col;\n"
+    "void main() {\n"
+    "   gl_FragColor = col;\n"
+    "}\n";
+//! [3]
+
+//! [4]
+GLuint TriangleWindow::loadShader(GLenum type, const char *source)
+{
+    GLuint shader = glCreateShader(type);
+    glShaderSource(shader, 1, &source, 0);
+    glCompileShader(shader);
+    return shader;
 }
 
+void TriangleWindow::initialize()
+{
+    m_program = new QOpenGLShaderProgram(this);
+    m_program->addShaderFromSourceCode(QOpenGLShader::Vertex, vertexShaderSource);
+    m_program->addShaderFromSourceCode(QOpenGLShader::Fragment, fragmentShaderSource);
+    m_program->link();
+    m_posAttr = m_program->attributeLocation("posAttr");
+    m_colAttr = m_program->attributeLocation("colAttr");
+    m_matrixUniform = m_program->uniformLocation("matrix");
+}
+//! [4]
+
+//! [5]
+void TriangleWindow::render()
+{
+    const qreal retinaScale = devicePixelRatio();
+    glViewport(0, 0, width() * retinaScale, height() * retinaScale);
+
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    m_program->bind();
+
+    QMatrix4x4 matrix;
+    matrix.perspective(60, 4.0/3.0, 0.1, 100.0);
+    matrix.translate(0, 0, -2);
+    matrix.rotate(100.0f * m_frame / screen()->refreshRate(), 0, 1, 0);
+
+    m_program->setUniformValue(m_matrixUniform, matrix);
+
+    GLfloat vertices[] = {
+        0.0f, 0.707f,
+        -0.5f, -0.5f,
+        0.5f, -0.5f
+    };
+
+    GLfloat colors[] = {
+        1.0f, 0.0f, 0.0f,
+        0.0f, 1.0f, 0.0f,
+        0.0f, 0.0f, 1.0f
+    };
+
+    glVertexAttribPointer(m_posAttr, 2, GL_FLOAT, GL_FALSE, 0, vertices);
+    glVertexAttribPointer(m_colAttr, 3, GL_FLOAT, GL_FALSE, 0, colors);
+
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glDisableVertexAttribArray(1);
+    glDisableVertexAttribArray(0);
+
+    m_program->release();
+
+    ++m_frame;
+}
+//! [5]
