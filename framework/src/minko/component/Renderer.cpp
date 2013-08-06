271e57b72cb92c8973b4961fc09e39596ca9ad07
Pierre-Edouard Landes <pierreedouard.landes@aerys.in>
2013-08-06 17:57:51 +0200

    Removed the using namespace minko from the Common.hpp file (ambiguity with generated Qt files notably).
    Former-commit-id: fba38e3206295646d5a43316e8063cebf4672265
    Former-commit-id: 45dd4867d452e32f7ead423db227a91d7bf6b346

--- framework/src/minko/component/Renderer.cpp
+++ framework/src/minko/component/Renderer.cpp
@@ -30,6 +30,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 #include "minko/component/SceneManager.hpp"
 #include "minko/file/AssetLibrary.hpp"
 
+using namespace minko;
 using namespace minko::component;
 using namespace minko::scene;
 
