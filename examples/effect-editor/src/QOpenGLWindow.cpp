e1f8a42b87c4273ae4c3bf8174e25049f4e8a9e7
Pierre-Edouard Landes <pierreedouard.landes@aerys.in>
2013-08-07 14:58:56 +0200

    Display a minko scene using a minko OpenGL ES 2 context.
    Former-commit-id: d15851676c64e1f2fa20d0f3ffd8425b33e658ca [formerly dddb57c7010c4f02f95dee6039e8fe6bc9546d2f] [formerly 504333db1207e863a5239aac50d6eeb65dd8399c]
    Former-commit-id: e3da089caa3d0104ae8608c56a9223a76554233c [formerly c72de0a7b36e60ce67c9a1021462b60f869a000b] [formerly 4acce27aa27efcf9cac76cab831d61c29d279cbd]
    Former-commit-id: 816f5c40a1dfe3198e12bee0d4b73d347dcf5ab2
    Former-commit-id: b6642539f3e50e52b7869e75a3db5301776d5fcc

--- examples/effect-editor/src/QOpenGLWindow.cpp
+++ examples/effect-editor/src/QOpenGLWindow.cpp
@@ -1,12 +1,16 @@
 #include "QOpenGLWindow.hpp"
 
+#include <QtCore/QCoreApplication>
 #include <QtGui/QPainter>
+#include <QtGui/QResizeEvent>
+#include <QtGui/QPaintEvent>
 
+#include <minko/MinkoPNG.hpp>
+#include <minko/math/Vector4.hpp>
 #include <minko/render/OpenGLES2Context.hpp>
 
+
 using namespace minko;
-using namespace minko::render;
-using namespace minko::component;
 
 /*explicit*/
 QOpenGLWindow::QOpenGLWindow(QWindow* parent):
@@ -15,7 +19,11 @@ QOpenGLWindow::QOpenGLWindow(QWindow* parent):
 	_qcontext(nullptr),
 	_context(nullptr),
 	_renderer(nullptr),
-	_sceneManager(nullptr)
+	_sceneManager(nullptr),
+	_initialized(false),
+	_animating(false),
+	_updatePending(false),
+	_box(nullptr)
 {
 	setSurfaceType(QWindow::OpenGLSurface);
 }
@@ -26,11 +34,26 @@ QOpenGLWindow::~QOpenGLWindow()
 		delete _qdevice;
 }
 
+/*virtual*/
+void
+QOpenGLWindow::initialize()
+{
+#ifdef DEBUG_QGLWINDOW
+	std::cout << "QOpenGLWindow initialize" << std::endl;
+#endif // DEBUG_QGLWINDOW
+
+	initializeWindow();
+	initializeScene();
+}
+
 void
 QOpenGLWindow::initializeWindow()
 {
 	if (_qdevice == nullptr)
+	{
 		_qdevice	= new QOpenGLPaintDevice;
+	}
+	_qdevice->setSize(size());
 
 	if (_qcontext == nullptr)
 	{
@@ -42,13 +65,11 @@ QOpenGLWindow::initializeWindow()
 
 	if (_context == nullptr)
 	{
-		_context	= OpenGLES2Context::create(); 
-
-#ifdef DEBUG
+		_context	= render::OpenGLES2Context::create(); 
+#ifdef DEBUG_QGLWINDOW
 	std::cout << "OpenGL context created\n\t" << _context->driverInfo() << std::endl;
-#endif // DEBUG
+#endif // DEBUG_QGLWINDOW
 	}
-	
 }
 
 void
@@ -57,16 +78,64 @@ QOpenGLWindow::initializeScene()
 	if (_context == nullptr)
 		initializeWindow();
 
-	_sceneManager	= SceneManager::create(_context);
-	_renderer		= Renderer::create();
+	_sceneManager = component::SceneManager::create(_context);
+	_sceneManager->assets()->defaultOptions()->includePaths().insert("assets");
+	_sceneManager->assets()
+		->registerParser<file::PNGParser>("png")
+		->geometry("cube", geometry::CubeGeometry::create(_context))
+		->queue("texture/box.png")
+		->queue("effect/Basic.effect");
 
+	_renderer	= component::Renderer::create();
+	_renderer->backgroundColor(0x7F7F7FFF);
 
+	_box		= scene::Node::create("box");
+
+	auto _ = _sceneManager->assets()->complete()->connect( [=](file::AssetLibrary::Ptr assets)
+	{
+		auto root	= scene::Node::create("root");
+		auto camera	= scene::Node::create("camera");
+
+		root->addComponent(_sceneManager);
+
+		camera->addComponent(_renderer);
+		camera->addComponent(component::Transform::create());
+		camera->component<component::Transform>()->transform()
+			->lookAt(math::Vector3::zero(), math::Vector3::create(0.0f, 0.0f, 3.0f), math::Vector3::yAxis());
+
+		camera->addComponent(component::PerspectiveCamera::create(
+			PI * 0.25f, 
+			width() / (float)height(), 
+			0.1f, 
+			1000.0f
+		));
+		root->addChild(camera);
+
+		_box->addComponent(component::Transform::create());
+		_box->addComponent(component::Surface::create(
+			assets->geometry("cube"),
+			data::Provider::create()
+				->set("material.diffuseColor", math::Vector4::create(1.0f, 1.0f, 1.0f, 1.0f))
+				->set("material.diffuseMap", assets->texture("texture/box.png")),
+			assets->effect("effect/Basic.effect")
+		));
+		root->addChild(_box);
+
+		_initialized = true;
+	});
+
+	_sceneManager->assets()->load();
 }
 
 /*virtual*/
+/*
 void
 QOpenGLWindow::render()
 {
+#ifdef DEBUG_QGLWINDOW
+	std::cout << "QOpenGLWindow::render" << std::endl;
+#endif // DEBUG_QGLWINDOW
+
 	if (_qdevice == nullptr)
 		initializeWindow();
 
@@ -75,26 +144,44 @@ QOpenGLWindow::render()
 	QPainter painter(_qdevice);
 	render(&painter);
 }
+*/
 
 /*virtual*/
+/*
 void 
 QOpenGLWindow::render(QPainter*)
 {
-
+#ifdef DEBUG_QGLWINDOW
+	std::cout << "QOpenGLWindow::render(QPainter*)" << std::endl;
+#endif // DEBUG_QGLWINDOW
 }
-
-/*virtual*/
-void
-QOpenGLWindow::initialize()
-{
-}
+*/
 
 void 
 QOpenGLWindow::exposeEvent(QExposeEvent*)
 {
+	/*
+#ifdef DEBUG_QGLWINDOW
+	std::cout << "QOpenGLWindow::exposeEvent\texposed ? " << isExposed() << std::endl;
+#endif // DEBUG_QGLWINDOW
+*/
     if (isExposed())
+		//update();
         renderNow();
 }
+
+void
+QOpenGLWindow::resizeEvent(QResizeEvent* e)
+{
+	std::cout << "resize event" << e->size().width() << " x " << e->size().height() << std::endl;
+
+	if (_context == nullptr)
+		return;
+
+	const float retinaScale = devicePixelRatio();
+	_context->configureViewport(0, 0, width()*retinaScale, height()*retinaScale);
+}
+
 
 bool 
 QOpenGLWindow::event(QEvent* event)
@@ -102,7 +189,13 @@ QOpenGLWindow::event(QEvent* event)
     switch (event->type()) 
 	{
     case QEvent::UpdateRequest:
-        //m_update_pending = false;
+
+		/*
+#ifdef DEBUG_QGLWINDOW
+	std::cout << "QOpenGLWindow::event\tupdate request" << std::endl;
+#endif // DEBUG_QGLWINDOW
+	*/
+		_updatePending = false;
         renderNow();
 		return true;
 
@@ -114,10 +207,47 @@ QOpenGLWindow::event(QEvent* event)
 void
 QOpenGLWindow::renderNow()
 {
-	if (_renderer == nullptr)
-		initializeScene();
+	/*
+#ifdef DEBUG_QGLWINDOW
+	std::cout << "QOpenGLWindow::renderNow" << std::endl;
+#endif // DEBUG_QGLWINDOW
+	*/
+	if (!isExposed())
+		return;
+
+	if (!_initialized)
+		initialize();
+
+	//render();
 
 	_renderer->render();
+	_sceneManager->nextFrame();
 
-	//_sceneManager->nextFrame();
+	_qcontext->swapBuffers(this);
+
+	if (_animating)
+		renderLater();
+}
+
+void
+QOpenGLWindow::renderLater()
+{
+	if (!_updatePending) 
+	{
+        _updatePending = true;
+
+		if (_box)
+		_box->component<component::Transform>()->transform()
+			->appendRotationY(0.01f);
+
+        QCoreApplication::postEvent(this, new QEvent(QEvent::UpdateRequest));
+    }
+}
+
+void
+QOpenGLWindow::setAnimating(bool value)
+{
+	_animating = value;
+	if (_animating)
+		renderLater();
 }
