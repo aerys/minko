bb8861450649740fe474d7fbe13231c0cbd0cb6f
Pierre-Edouard Landes <pierreedouard.landes@aerys.in>
2013-08-07 18:51:16 +0200

    Replaced QTimer by QAbstractEventDispatcher, animation and resize works now. :D
    Former-commit-id: 675f4f74f250346b21b57719a4a0d7f3493af00d
    Former-commit-id: f9fb864caedd1de7438dbdfbc51f2f668e71ac62

--- examples/effect-editor/src/QMinkoGLWidget.hpp
+++ examples/effect-editor/src/QMinkoGLWidget.hpp
@@ -23,27 +23,19 @@ class QMinkoGLWidget :
 		NodePtr				_camera;
 		NodePtr				_model;
 		bool				_initialized;
-		bool				_animating;
-		bool				_updatePending;
-
-		QTimer				*_qtimer;
 
 	public: 
-		QMinkoGLWidget(QWidget * parent = nullptr, 
-					  const QGLWidget * shareWidget = nullptr, 
-					  Qt::WindowFlags f = 0) :
+		QMinkoGLWidget(QWidget*			parent		= nullptr, 
+					  const QGLWidget*	shareWidget	= nullptr, 
+					  Qt::WindowFlags	f			= 0) :
 			QGLWidget(parent, shareWidget, f),
 			_context(nullptr),
 			_renderer(nullptr),
 			_sceneManager(nullptr),
 			_camera(nullptr),
 			_initialized(false),
-			_animating(false),
-			_updatePending(false),
-			_model(nullptr),
-			_qtimer(new QTimer(this))
+			_model(nullptr)
 		{
-			QObject::connect(_qtimer, SIGNAL(timeout()), this, SLOT(updateGL()));
 		}
 		
 		~QMinkoGLWidget()
