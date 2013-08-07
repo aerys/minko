ffccc011cacf116e113908781b5345b985e41dfe
Pierre-Edouard Landes <pierreedouard.landes@aerys.in>
2013-08-07 18:51:16 +0200

    Replaced QTimer by QAbstractEventDispatcher, animation and resize works now. :D
    Former-commit-id: 675f4f74f250346b21b57719a4a0d7f3493af00d
    Former-commit-id: 6651d325c3e2f14e131aa1706d7e92036ace3263

--- examples/effect-editor/src/Main.cpp
+++ examples/effect-editor/src/Main.cpp
@@ -1,5 +1,6 @@
 
 #include "QMinkoGLWidget.hpp"
+#include "QMinkoEffectEditor.hpp"
 //#include "QOpenGLWindow.hpp"
 
 #include "openglwindow.h"
@@ -15,38 +16,18 @@
 
 using namespace minko;
 
-
-//! [1]
-class TriangleWindow : public OpenGLWindow
-{
-public:
-    TriangleWindow();
-
-    void initialize();
-    void render();
-
-private:
-    GLuint loadShader(GLenum type, const char *source);
-
-    GLuint m_posAttr;
-    GLuint m_colAttr;
-    GLuint m_matrixUniform;
-
-    QOpenGLShaderProgram *m_program;
-    int m_frame;
-};
-
-TriangleWindow::TriangleWindow()
-    : m_program(0)
-    , m_frame(0)
-{
-}
-//! [1]
-
-//! [2]
 int 
 main(int argc, char **argv)
 {
+	/*
+	QApplication a(argc, argv);
+	QMinkoEffectEditor w;
+	w.show();
+	
+	return a.exec();
+	*/
+	
+	
 	QApplication app(argc, argv);
     app.setApplicationName("cube");
     app.setApplicationVersion("0.1");
@@ -57,115 +38,6 @@ main(int argc, char **argv)
 #else
 	std::cerr << "OpenGL support required." << std::endl;
 #endif // QT_NO_OPENGL
-
-	/*
-    QGuiApplication app(argc, argv);
-
-    QSurfaceFormat format;
-    format.setSamples(16);
+	return app.exec();
 	
-    //TriangleWindow window;
-    //window.setFormat(format);
-    //window.resize(640, 480);
-    //window.show();
-
-    //window.setAnimating(true);
-
-	
-	
-	QOpenGLWindow window;
-	window.setFormat(format);
-	window.resize(640, 480);
-	window.show();
-	//window.setAnimating(true);
-	*/
-
-    return app.exec();
 }
-//! [2]
-
-
-//! [3]
-static const char *vertexShaderSource =
-    "attribute highp vec4 posAttr;\n"
-    "attribute lowp vec4 colAttr;\n"
-    "varying lowp vec4 col;\n"
-    "uniform highp mat4 matrix;\n"
-    "void main() {\n"
-    "   col = colAttr;\n"
-    "   gl_Position = matrix * posAttr;\n"
-    "}\n";
-
-static const char *fragmentShaderSource =
-    "varying lowp vec4 col;\n"
-    "void main() {\n"
-    "   gl_FragColor = col;\n"
-    "}\n";
-//! [3]
-
-//! [4]
-GLuint TriangleWindow::loadShader(GLenum type, const char *source)
-{
-    GLuint shader = glCreateShader(type);
-    glShaderSource(shader, 1, &source, 0);
-    glCompileShader(shader);
-    return shader;
-}
-
-void TriangleWindow::initialize()
-{
-    m_program = new QOpenGLShaderProgram(this);
-    m_program->addShaderFromSourceCode(QOpenGLShader::Vertex, vertexShaderSource);
-    m_program->addShaderFromSourceCode(QOpenGLShader::Fragment, fragmentShaderSource);
-    m_program->link();
-    m_posAttr = m_program->attributeLocation("posAttr");
-    m_colAttr = m_program->attributeLocation("colAttr");
-    m_matrixUniform = m_program->uniformLocation("matrix");
-}
-//! [4]
-
-//! [5]
-void TriangleWindow::render()
-{
-    const qreal retinaScale = devicePixelRatio();
-    glViewport(0, 0, width() * retinaScale, height() * retinaScale);
-
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    m_program->bind();
-
-    QMatrix4x4 matrix;
-    matrix.perspective(60, 4.0/3.0, 0.1, 100.0);
-    matrix.translate(0, 0, -2);
-    matrix.rotate(100.0f * m_frame / screen()->refreshRate(), 0, 1, 0);
-
-    m_program->setUniformValue(m_matrixUniform, matrix);
-
-    GLfloat vertices[] = {
-        0.0f, 0.707f,
-        -0.5f, -0.5f,
-        0.5f, -0.5f
-    };
-
-    GLfloat colors[] = {
-        1.0f, 0.0f, 0.0f,
-        0.0f, 1.0f, 0.0f,
-        0.0f, 0.0f, 1.0f
-    };
-
-    glVertexAttribPointer(m_posAttr, 2, GL_FLOAT, GL_FALSE, 0, vertices);
-    glVertexAttribPointer(m_colAttr, 3, GL_FLOAT, GL_FALSE, 0, colors);
-
-    glEnableVertexAttribArray(0);
-    glEnableVertexAttribArray(1);
-
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-
-    glDisableVertexAttribArray(1);
-    glDisableVertexAttribArray(0);
-
-    m_program->release();
-
-    ++m_frame;
-}
-//! [5]
