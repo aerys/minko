20a47ea2dcd6862d11c00c305e82ecc54eb252fb
Pierre-Edouard Landes <pierreedouard.landes@aerys.in>
2013-08-09 18:13:46 +0200

    Added the simple button icons, revamped global layout, and started i/o callbacks.
    Former-commit-id: 7350bc11b5f68273480c7326bb9b5f3a37a80eea
    Former-commit-id: c1b487dd484ae75c685fd38633fdc7fde314d5a9
    Former-commit-id: 562ab33dcbe1a0d21c38964baff5c980cec0e0cf

--- examples/effect-editor/src/QMinkoEffectEditor.cpp
+++ examples/effect-editor/src/QMinkoEffectEditor.cpp
@@ -1,20 +1,22 @@
 #include "QMinkoEffectEditor.hpp"
 
 #include "ui/ui_QMinkoEffectEditor.h"
+#include <QtWidgets/QToolButton>
+#include <QtWidgets/QFileDialog>
 #include <QtWebKitWidgets/QWebFrame>
 
 QMinkoEffectEditor::QMinkoEffectEditor(QWidget *parent) :
     QMainWindow(parent),
     _ui(new Ui::QMinkoEffectEditor),
-	_qSignalMapper(new QSignalMapper(this))
+	_qAddEditorSignalMapper(new QSignalMapper(this))
 {
     _ui->setupUi(this);
+
+	setupToolButtons();
 
 	_qTabFrames[TAB_VERTEX_SOURCE]		= _ui->vertexWebView->page()->mainFrame();
 	_qTabFrames[TAB_FRAGMENT_SOURCE]	= _ui->fragmentWebView->page()->mainFrame();
 	_qTabFrames[TAB_BINDINGS]			= _ui->bindingsWebView->page()->mainFrame();
-
-
 
 	setupSourceTabs();
 }
@@ -22,6 +24,18 @@ QMinkoEffectEditor::QMinkoEffectEditor(QWidget *parent) :
 QMinkoEffectEditor::~QMinkoEffectEditor()
 {
     delete _ui;
+}
+
+void
+QMinkoEffectEditor::setupToolButtons()
+{
+	_ui->loadMkToolButton->setIcon(QIcon(":/resources/icon-load-mk.png"));
+	_ui->loadEffectToolButton->setIcon(QIcon(":/resources/icon-load-effect.png"));
+	_ui->saveEffectToolButton->setIcon(QIcon(":/resources/icon-save-effect.png"));
+
+	QObject::connect(_ui->loadMkToolButton,		SIGNAL(clicked()),	this,	SLOT(loadMk()));
+	QObject::connect(_ui->loadEffectToolButton,	SIGNAL(clicked()),	this,	SLOT(loadEffect()));
+	QObject::connect(_ui->saveEffectToolButton,	SIGNAL(clicked()),	this,	SLOT(saveEffect()));
 }
 
 void
@@ -34,17 +48,14 @@ QMinkoEffectEditor::setupSourceTabs()
 		QObject::connect(
 			_qTabFrames[tabIndex], 
 			SIGNAL(javaScriptWindowObjectCleared()),
-			_qSignalMapper,
+			_qAddEditorSignalMapper,
 			SLOT(map())
 		);
 
-		_qSignalMapper->setMapping(
-			_qTabFrames[tabIndex], 
-			(int)tabIndex
-		);
+		_qAddEditorSignalMapper->setMapping(_qTabFrames[tabIndex], (int)tabIndex);
 
 		QObject::connect(
-			_qSignalMapper, 
+			_qAddEditorSignalMapper, 
 			SIGNAL(mapped(int)),
 			this,
 			SLOT(addEditorToJavaScript(int))
@@ -52,7 +63,24 @@ QMinkoEffectEditor::setupSourceTabs()
 	}
 }
 
-/*slot*/
+void
+QMinkoEffectEditor::loadMk(const QString& filename)
+{
+	std::cout << "load mk file: " << qPrintable(filename) << std::endl;
+}
+
+void
+QMinkoEffectEditor::loadEffect(const QString& filename)
+{
+	std::cout << "load effect file: " << qPrintable(filename) << std::endl;
+}
+
+void
+QMinkoEffectEditor::saveEffect(const QString& filename)
+{
+	std::cout << "save effect file: " << qPrintable(filename) << std::endl;
+}
+
 void
 QMinkoEffectEditor::addEditorToJavaScript(int tabIndex)
 {
@@ -64,7 +92,57 @@ QMinkoEffectEditor::addEditorToJavaScript(int tabIndex)
 
 /*slot*/
 void
-QMinkoEffectEditor::sourcesSavedSlot()
+QMinkoEffectEditor::sourcesChangedSlot()
 {
+
 	std::cout << "sources changed" << std::endl;
+	QVariant qVariant = _qTabFrames[TAB_VERTEX_SOURCE]->evaluateJavaScript("codeMirror.getValue()");
+}
+
+/*slot*/
+void
+QMinkoEffectEditor::loadMk()
+{
+	const QString& filename = QFileDialog::getOpenFileName(
+		this,
+		"Load *.mk file",
+		QString(),
+		"MK files (*.mk)"
+	);
+	if (filename.isEmpty())
+		return;
+
+	loadMk(filename);
+}
+
+/*slot*/
+void
+QMinkoEffectEditor::loadEffect()
+{
+	const QString& filename = QFileDialog::getOpenFileName(
+		this,
+		"Load *.effect file",
+		QString(),
+		"Effect files (*.effect)"
+	);
+	if (filename.isEmpty())
+		return;
+
+	loadEffect(filename);
+}
+
+/*slot*/
+void
+QMinkoEffectEditor::saveEffect()
+{
+	const QString& filename = QFileDialog::getSaveFileName(
+		this,
+		"Save *.effect file",
+		QString(),
+		"Effect files (*.effect)"
+	);
+	if (filename.isEmpty())
+		return;
+
+	saveEffect(filename);
 }
