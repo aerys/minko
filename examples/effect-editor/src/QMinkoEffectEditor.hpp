da98b205c75b8ba386305d01271c5ef3a9a0525d
Pierre-Edouard Landes <pierreedouard.landes@aerys.in>
2013-08-09 16:45:57 +0200

    Fixed the premake file, and now catches signals fired from the JS code back to Qt.
    Former-commit-id: 712e8cdfa05cd71f537aa4280c888fa9f239b8db [formerly a895c3e567a252d9fdbc83033cf98e48bfd6eb29] [formerly ca7e29487f484b296b265fca7341b6c29a44e716]
    Former-commit-id: b87b39c54e5ba567a5f4192f5415da4f4bf2e178 [formerly a9a66c8b2c8f6e2581ae084fac0b4e3f40689caa] [formerly a28a797675bb742257adada8eb9b335d36bb9530]
    Former-commit-id: bf6e322d0cffd4ce10fd6ad846673aa59d7a9cbf
    Former-commit-id: ac1d0614e121a2b69fa94f1d147f6268ea0ba828

--- examples/effect-editor/src/QMinkoEffectEditor.hpp
+++ examples/effect-editor/src/QMinkoEffectEditor.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <QtCore/QSignalMapper>
 #include <QtWidgets/QMainWindow>
+#include <QtWebKitWidgets/QWebFrame>
 
 #include "QMinkoGLWidget.hpp"
 
@@ -14,8 +16,20 @@ class QMinkoEffectEditor :
 {
     Q_OBJECT
     
+	public:
+		typedef enum
+		{
+			TAB_VERTEX_SOURCE = 0,
+			TAB_FRAGMENT_SOURCE,
+			TAB_BINDINGS,
+			NUM_TABS
+		}
+		TabType;
+
 	private:
-		Ui::QMinkoEffectEditor	*ui;
+		Ui::QMinkoEffectEditor	*_ui;
+		QWebFrame				*_qTabFrames[NUM_TABS];
+		QSignalMapper			*_qSignalMapper;
 
 	public:
 	    explicit 
@@ -25,4 +39,24 @@ class QMinkoEffectEditor :
 
 		void
 		setupSourceTabs();
+
+	public slots:
+		void
+		sourcesSavedSlot();
+
+	private slots:
+		void
+		addEditorToJavaScript(int tabIndex);
+
+		void
+		addEditorToVertexSourceJavaScript();
+
+		void
+		addEditorToFragmentSourceJavaScript();
+
+		void
+		addEditorToBindingsSourceJavaScript();
+
+	private:
+		DISALLOW_COPY_AND_ASSIGN(QMinkoEffectEditor);
 };
