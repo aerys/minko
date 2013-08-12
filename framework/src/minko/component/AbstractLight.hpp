f8fd77acbec1a0c6bfd068563ec8e5e7e2fc1ac5
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-08-12 20:37:11 +0200

    add missing AbstractLight class
    Former-commit-id: bb2d656ad67eed8f88c541e025477d120fb8d671
    Former-commit-id: eb47ed49b47b0cd9331aa35e13d4a40a3d4cad09
    Former-commit-id: ee2b30483f0923cd549ccb9f09f94e1cbd32824d

---
+++ framework/src/minko/component/AbstractLight.hpp
@@ -0,0 +1,90 @@
+/*
+Copyright (c) 2013 Aerys
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#pragma once
+
+#include "minko/Common.hpp"
+
+#include "minko/component/AbstractRootDataComponent.hpp"
+
+namespace minko
+{
+	namespace component
+	{
+		class AbstractLight :
+			public AbstractRootDataComponent
+		{
+		public:
+			typedef std::shared_ptr<AbstractLight> Ptr;
+
+		private:
+			bool							_enabled;
+			float							_priority;
+			std::shared_ptr<math::Vector3>	_color;
+
+		public:
+			inline static
+			Ptr
+			create()
+			{
+				return std::shared_ptr<AbstractLight>(new AbstractLight());
+			}
+
+			inline
+			const bool
+			enabled()
+			{
+				return _enabled;
+			}
+
+			inline
+			void
+			enabled(bool enabled)
+			{
+				_enabled = enabled;
+			}
+
+			inline
+			const float
+			priority()
+			{
+				return _priority;
+			}
+
+			inline
+			void
+			priority(float priority)
+			{
+				_priority = priority;
+			}
+
+			inline
+			std::shared_ptr<math::Vector3>
+			color()
+			{
+				return _color;
+			}
+
+		private:
+			AbstractLight()
+			{
+			}
+		};
+	}
+}
