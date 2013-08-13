9ba3bc9ac6708e1b12ece1e378d2f4f928fe8db2
Pierre-Edouard Landes <pierreedouard.landes@aerys.in>
2013-08-13 16:04:15 +0200

    Cna now add/remove bindings using the QMinkoBindingsWidget.
    Former-commit-id: cd91e3467ef22d8bf6096ce52ef6a6aa7b87282a
    Former-commit-id: 42e804bf1707d14a4dc180c09ac9ef6fa8517aa1

--- examples/effect-editor/src/QMinkoBindingsWidget.hpp
+++ examples/effect-editor/src/QMinkoBindingsWidget.hpp
@@ -1,22 +1,64 @@
-#ifndef QMINKOBINDINGSWIDGET_H
-#define QMINKOBINDINGSWIDGET_H
+#pragma once
 
-#include <QWidget>
+#include <iostream>
 
-namespace Ui {
-class QMinkoBindingsWidget;
+#include <QtCore/QSignalMapper>
+#include <QtWidgets/QWidget>
+#include <QtWidgets/QToolButton>
+#include <QtWidgets/QTableWidget>
+
+namespace Ui 
+{
+	class QMinkoBindingsWidget;
 }
 
-class QMinkoBindingsWidget : public QWidget
+class QMinkoBindingsWidget : 
+	public QWidget
 {
     Q_OBJECT
-    
+
 public:
-    explicit QMinkoBindingsWidget(QWidget *parent = 0);
-    ~QMinkoBindingsWidget();
+	typedef enum
+	{
+		BIND_ATTRIBUTE = 0,
+		BIND_UNIFORM,
+		BIND_STATE,
+		NUM_BIND_TYPES
+	} BindingType;
+
+private:
+	Ui::QMinkoBindingsWidget			*_ui;
+	QToolButton							*_qAddButtons		[NUM_BIND_TYPES];
+	QToolButton							*_qRemoveButtons	[NUM_BIND_TYPES];
+	QTableWidget						*_qBindingsTables	[NUM_BIND_TYPES];
+	QSignalMapper						*_qAddSignalMapper;	
+	QSignalMapper						*_qRemoveSignalMapper;
+
+	std::map<std::string, std::string>	_bindings			[NUM_BIND_TYPES];
+
+public:
+    
+	explicit 
+	QMinkoBindingsWidget(QWidget *parent = 0);
+
+	~QMinkoBindingsWidget();
     
 private:
-    Ui::QMinkoBindingsWidget *ui;
-};
 
-#endif // QMINKOBINDINGSWIDGET_H
+	void
+	setupBindingsButtons();
+
+	void
+	setupBindingsTables();
+
+private slots:
+	void
+	addBinding(int bindType);
+
+	void
+	removeBinding(int bindType);
+
+private:
+	bool
+	bindingExists(int bindType, const std::string&) const;
+};
