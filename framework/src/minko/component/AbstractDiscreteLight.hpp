fa3c4eb4f6c53b24a97dabe052fdeb714d185403
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-08-15 23:35:43 +0200

    add an AbstractDiscreteLight class to handle "discrete" light, meaning mostly any light but the ambient this new class will provide diffuse and specular float properties and an updateModelToWorld() method to override in order to update model space values such as the world space direction of the DirectionalLight
    Former-commit-id: 05ae3cf2750516fcfc3b645b13f4104d9acddf7c [formerly e80c333f86d77e10cf6728414aea2a0b755aac78] [formerly eb80a47758ca03b8acc836315c6fe39eb4bb3020]
    Former-commit-id: 85c4a331629d3e9128c040e605c81dbb6188d8fb [formerly 5ca6a1d46e9dd61aabf017bcefed537261846937] [formerly a00fe279c41971cd63465b09cca4a9de38d384cc]
    Former-commit-id: 3b91e4deb65d1a06450520733fe7695312937259
    Former-commit-id: 6dfdfebd81027f8fe893055c2a9e495f5095b023

---
+++ framework/src/minko/component/AbstractDiscreteLight.hpp
@@ -0,0 +1,90 @@
+/*
+Copyright (c) 2013 Aerys
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#pragma once
+
+#include "minko/Common.hpp"
+
+#include "minko/component/AbstractLight.hpp"
+#include "minko/Signal.hpp"
+
+#include "minko/math/Matrix4x4.hpp"
+#include "minko/scene/Node.hpp"
+#include "minko/data/Container.hpp"
+
+namespace minko
+{
+	namespace component
+	{
+		class AbstractDiscreteLight :
+			public AbstractLight
+		{
+		private:
+			Signal<std::shared_ptr<data::Container>, const std::string&>::Slot	_modelToWorldChangedSlot;
+
+		public:
+			inline
+		    float
+		    diffuse()
+		    {
+		    	return data()->get<float>("diffuse");
+		    }
+
+		    inline
+		    void
+		    diffuse(float diffuse)
+		    {
+		    	data()->set<float>("diffuse", diffuse);
+		    }
+
+			inline
+			float
+			specular()
+			{
+				return data()->get<float>("specular");
+			}
+
+			inline
+			void
+			specular(float specular)
+			{
+				data()->set<float>("specular", specular);
+			}
+
+		protected:
+			AbstractDiscreteLight(const std::string& arrayName);
+
+			virtual
+            void
+            targetAddedHandler(AbstractComponent::Ptr cmp, std::shared_ptr<scene::Node> target);
+
+            virtual
+            void
+            targetRemovedHandler(AbstractComponent::Ptr cmp, std::shared_ptr<scene::Node> target);
+
+            void
+            modelToWorldMatrixChangedHandler(std::shared_ptr<data::Container> 	container,
+            								 const std::string& 				propertyName);
+
+            virtual
+            void
+            updateModelToWorldMatrix(std::shared_ptr<math::Matrix4x4> modelToWorld) = 0;
+		};
+	}
+}
