aca2b44f99c4bccd425cf25d010e9bd5cff21831
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-08-15 23:50:52 +0200

    fixed Phong implementation
    Former-commit-id: 5b7fb0d897ad1753ad873c384d3a21be42af4acc [formerly e5fd27d46c7e82f77ee4d5d1094f258245d73364]
    Former-commit-id: ba80349833c37cfb6bf4e22af4f6e4e676ed1734 [formerly ffade9ea75037b463a308c02ea5d793f8366e1b1]
    Former-commit-id: 812f09262546a42837e181d1568cb1daece35d25

--- framework/effect/Phong.glsl
+++ framework/effect/Phong.glsl
@@ -1,33 +1,28 @@
 struct DirectionalLight
 {
 	vec3 color;
-	vec3 direction;
+	float diffuse;
+	float specular;
+	vec3 direction;	
 };
 
-float phong_lambert(vec3 normal, vec3 lightDirection)
+float phong_diffuseReflection(vec3 normal, vec3 lightDirection)
 {
 	return max(dot(normal, lightDirection), 0.);
 }
 
-vec3 phong_diffuseReflection(vec3 normal, vec3 lightDirection, vec3 lightColor)
-{
-	return lightColor * phong_lambert(normal, lightDirection);
-}
-
-vec3 phong_specularReflection(vec3 normal, vec3 lightDirection, vec3 viewVector, vec3 specularColor, float shininess)
+float phong_specularReflection(vec3 normal, vec3 lightDirection, vec3 viewVector, float shininess)
 {
 	float NdotL = dot(normal, lightDirection);
 	
 	if (NdotL < 0.0)
 	{
-		return vec3(0.0);
+		return 0.0;
 	}
 	else
 	{
 		vec3 reflected = 2.0 * NdotL * normal - lightDirection;
 
-		float specular = pow(max(dot(reflected, viewVector), 0.0), shininess);
-
-		return specular * specularColor;
+		return pow(max(dot(reflected, viewVector), 0.0), shininess);
 	}
 }
