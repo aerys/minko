5ca6a1d46e9dd61aabf017bcefed537261846937
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-08-15 23:35:43 +0200

    add an AbstractDiscreteLight class to handle "discrete" light, meaning mostly any light but the ambient this new class will provide diffuse and specular float properties and an updateModelToWorld() method to override in order to update model space values such as the world space direction of the DirectionalLight
    Former-commit-id: eb80a47758ca03b8acc836315c6fe39eb4bb3020
    Former-commit-id: a00fe279c41971cd63465b09cca4a9de38d384cc

---
+++ framework/src/minko/component/AbstractDiscreteLight.cpp
@@ -0,0 +1,56 @@
+/*
+Copyright (c) 2013 Aerys
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include "AbstractDiscreteLight.hpp"
+
+using namespace minko::component;
+
+AbstractDiscreteLight::AbstractDiscreteLight(const std::string& arrayName) :
+	AbstractLight(arrayName)
+{
+}
+
+void
+AbstractDiscreteLight::targetAddedHandler(AbstractComponent::Ptr cmp, std::shared_ptr<scene::Node> target)
+{
+	AbstractRootDataComponent::targetAddedHandler(cmp, target);
+
+	_modelToWorldChangedSlot = target->data()->propertyChanged("transform.modelToWorldMatrix")
+		->connect(std::bind(
+			&AbstractDiscreteLight::modelToWorldMatrixChangedHandler,
+			std::dynamic_pointer_cast<AbstractDiscreteLight>(shared_from_this()),
+			std::placeholders::_1,
+			std::placeholders::_2
+		));
+}
+
+void
+AbstractDiscreteLight::targetRemovedHandler(AbstractComponent::Ptr cmp, std::shared_ptr<scene::Node> target)
+{
+	AbstractRootDataComponent::targetRemovedHandler(cmp, target);
+
+	_modelToWorldChangedSlot = nullptr;
+}
+
+void
+AbstractDiscreteLight::modelToWorldMatrixChangedHandler(std::shared_ptr<data::Container> 	container,
+								 						const std::string& 					propertyName)
+{
+	updateModelToWorldMatrix(container->get<math::Matrix4x4::Ptr>(propertyName));
+}
