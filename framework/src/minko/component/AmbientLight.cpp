e36386132c8e2f797387f47b270c5a729538367d
Pierre-Edouard Landes <pierreedouard.landes@aerys.in>
2013-08-19 15:13:32 +0200

    Added the DrawCall::reset() function.
    Former-commit-id: e68d645f10e62e4c2518f9939adb7d3ffaca78a2

--- framework/src/minko/component/AmbientLight.cpp
+++ framework/src/minko/component/AmbientLight.cpp
@@ -19,6 +19,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 
 #include "AmbientLight.hpp"
 
+using namespace minko;
 using namespace minko::component;
 
 uint AmbientLight::_counter = 0;
