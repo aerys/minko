e63e00b293d4fd09bf65ac690a270624aa06feaf
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-08-19 11:14:38 +0200

    add missing CPP files
    Former-commit-id: 4f8832c4150ae2c78d7d46bb1d4177bcac6a5f8d

---
+++ framework/src/minko/component/LightManager.cpp
@@ -0,0 +1,198 @@
+/*
+Copyright (c) 2013 Aerys
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include "minko/component/LightManager.hpp"
+
+#include "minko/data/Provider.hpp"
+#include "minko/data/Container.hpp"
+#include "minko/scene/Node.hpp"
+#include "minko/scene/NodeSet.hpp"
+#include "minko/component/AmbientLight.hpp"
+#include "minko/component/DirectionalLight.hpp"
+
+using namespace minko::component;
+
+LightManager::LightManager() :
+	AbstractComponent(),
+	std::enable_shared_from_this<LightManager>(),
+	_data(data::Provider::create()),
+	_targetAddedSlot(nullptr),
+	_targetRemovedSlot(nullptr),
+	_addedSlot(nullptr),
+	_removedSlot(nullptr),
+	_componentAddedSlot(nullptr),
+	_componentRemovedSlot(nullptr)
+{
+}
+
+void
+LightManager::initialize()
+{
+	_targetAddedSlot = targetAdded()->connect(std::bind(
+		&LightManager::targetAddedHandler,
+		shared_from_this(),
+		std::placeholders::_1,
+		std::placeholders::_2
+	));
+
+	_targetRemovedSlot = targetRemoved()->connect(std::bind(
+		&LightManager::targetRemovedHandler,
+		shared_from_this(),
+		std::placeholders::_1,
+		std::placeholders::_2
+	));
+}
+
+void
+LightManager::targetAddedHandler(AbsCmpPtr cmp, NodePtr target)
+{
+	if (target->components<LightManager>().size() > 1)
+		throw std::logic_error("There cannot be more than one LightManager.");
+	if (target->root() != target)
+		throw std::invalid_argument("target");
+
+	_addedSlot = target->added()->connect(std::bind(
+		&LightManager::addedHandler,
+		std::dynamic_pointer_cast<LightManager>(shared_from_this()),
+		std::placeholders::_1,
+		std::placeholders::_2,
+		std::placeholders::_3
+	));
+
+	_removedSlot = target->removed()->connect(std::bind(
+		&LightManager::removedHandler,
+		std::dynamic_pointer_cast<LightManager>(shared_from_this()),
+		std::placeholders::_1,
+		std::placeholders::_2,
+		std::placeholders::_3
+	));
+
+	_componentAddedSlot = target->componentAdded()->connect(std::bind(
+		&LightManager::componentAddedHandler,
+		std::dynamic_pointer_cast<LightManager>(shared_from_this()),
+		std::placeholders::_1,
+		std::placeholders::_2,
+		std::placeholders::_3
+	));
+
+	_componentRemovedSlot = target->componentRemoved()->connect(std::bind(
+		&LightManager::componentRemovedHandler,
+		std::dynamic_pointer_cast<LightManager>(shared_from_this()),
+		std::placeholders::_1,
+		std::placeholders::_2,
+		std::placeholders::_3
+	));
+
+	target->data()->addProvider(_data);
+
+	addedHandler(nullptr, target, nullptr);
+}
+
+void
+LightManager::targetRemovedHandler(AbsCmpPtr cmp, NodePtr target)
+{
+	_ambientLights.clear();
+	_directionalLights.clear();
+	_addedSlot = nullptr;
+	_removedSlot = nullptr;
+	_componentAddedSlot = nullptr;
+	_componentRemovedSlot = nullptr;
+
+	target->data()->removeProvider(_data);
+}
+
+void
+LightManager::addedHandler(NodePtr node, NodePtr target, NodePtr ancestor)
+{
+	auto numAmbientLights = _ambientLights.size();
+	auto numDirectionalLights = _directionalLights.size();
+
+	auto descendants = scene::NodeSet::create(target)->descendants(true);
+	for (auto& descendant : descendants->nodes())
+	{
+		for (auto& ambientLight : descendant->components<AmbientLight>())
+			_ambientLights.insert(ambientLight);
+
+		for (auto& directionalLight : descendant->components<DirectionalLight>())
+			_directionalLights.insert(directionalLight);
+	}
+
+	if (numAmbientLights != _ambientLights.size())
+		setLightArrayLength("ambientLights.length", _ambientLights.size());
+	if (numDirectionalLights != _directionalLights.size())
+		setLightArrayLength("directionalLights.length", _directionalLights.size());
+}
+
+void
+LightManager::removedHandler(NodePtr node, NodePtr target, NodePtr ancestor)
+{
+	auto numAmbientLights = _ambientLights.size();
+	auto numDirectionalLights = _directionalLights.size();
+
+	auto descendants = scene::NodeSet::create(target)->descendants(true);
+	for (auto& descendant : descendants->nodes())
+	{
+		for (auto& ambientLight : descendant->components<AmbientLight>())
+			_ambientLights.erase(ambientLight);
+
+		for (auto& directionalLight : descendant->components<DirectionalLight>())
+			_directionalLights.erase(directionalLight);
+	}
+
+	if (numAmbientLights != _ambientLights.size())
+		setLightArrayLength("ambientLights.length", _ambientLights.size());
+	if (numDirectionalLights != _directionalLights.size())
+		setLightArrayLength("directionalLights.length", _directionalLights.size());
+}
+
+void
+LightManager::componentAddedHandler(NodePtr node, NodePtr target, AbsCmpPtr cmp)
+{
+	auto ambientLight = std::dynamic_pointer_cast<AmbientLight>(cmp);
+	auto directionalLight = std::dynamic_pointer_cast<DirectionalLight>(cmp);
+
+	if (ambientLight && _ambientLights.insert(ambientLight).second)
+		setLightArrayLength("ambientLights.length", _ambientLights.size());
+	else if (directionalLight && _directionalLights.insert(directionalLight).second)
+		setLightArrayLength("directionalLights.length", _directionalLights.size());
+}
+
+void
+LightManager::componentRemovedHandler(NodePtr node, NodePtr target, AbsCmpPtr cmp)
+{
+	auto ambientLight = std::dynamic_pointer_cast<AmbientLight>(cmp);
+	auto directionalLight = std::dynamic_pointer_cast<DirectionalLight>(cmp);
+
+	if (ambientLight && _ambientLights.erase(ambientLight))
+		setLightArrayLength("ambientLights.length", _ambientLights.size());
+	else if (directionalLight && _directionalLights.erase(directionalLight))
+		setLightArrayLength("directionalLights.length", _directionalLights.size());
+}
+
+void
+LightManager::setLightArrayLength(const std::string& arrayName, int length)
+{
+	if (length == 0)
+	{
+		if (_data->hasProperty(arrayName))
+			_data->unset(arrayName);
+	}
+	else
+		_data->set<int>(arrayName, length);
+}
