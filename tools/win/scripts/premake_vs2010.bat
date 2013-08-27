2a15d06421fadc175b449642bd90261c489218e4
Pierre-Edouard Landes <pierreedouard.landes@aerys.in>
2013-08-27 15:47:38 +0200

    Put back glsl-optimizer.
    Former-commit-id: e85ecbd893977b9af59c4cc0b637de3a56b2cba9
    Former-commit-id: 0610ad9a1af3686a053e5fe2d66fbea9c74bd348

--- tools/win/scripts/premake_vs2010.bat
+++ tools/win/scripts/premake_vs2010.bat
@@ -1,5 +1,5 @@
 call premake_clean.bat
 call premake_templates.bat
 chdir ..\..\..
-tools\win\bin\premake4.exe --os=windows --no-glsl-optimizer=true --platform=x32 vs2010
+tools\win\bin\premake4.exe --os=windows --platform=x32 vs2010
 cd tools\win\scripts
