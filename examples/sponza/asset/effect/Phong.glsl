d553440b6c531bf46aaec83bf337b192cad8621c
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-08-28 15:50:17 +0200

    restore (deprecated but) compatible Phong.glsl
    Former-commit-id: f795b98e22864f58dbb47f7900ec5d9a4348eec7 [formerly 3a5a7c1d0f211fbc11151a875da24478c5b7fe50]
    Former-commit-id: 4d2f84929d6197c2e6f26de0e8bd66605dcf4038 [formerly 59b1cd3d3d03e53e66008c332b27eb41b59c1249]
    Former-commit-id: 49ace22d32efdcced18bfc21571f7b3c2bd976f2

---
+++ examples/sponza/asset/effect/Phong.glsl
@@ -0,0 +1,27 @@
+float phong_lambert(vec3 normal,  vec3 lightDirection)
+{
+	return max(dot(normal, lightDirection), 0.);
+}
+
+vec3 phong_diffuseReflection(vec3 normal,  vec3 lightDirection,  vec3 lightColor)
+{
+	return lightColor * phong_lambert(normal, lightDirection);
+}
+
+vec3 phong_specularReflection(vec3 normal,  vec3 lightDirection,  vec3 viewVector,  vec3 specularColor,  float shininess)
+{
+	float NdotL = dot(normal, lightDirection);
+	
+	if (NdotL < 0.0)
+	{
+		return vec3(0.0);
+	}
+	else
+	{
+		vec3 reflected = 2.0 * NdotL * normal - lightDirection;
+
+		float specular = pow(max(dot(reflected, viewVector), 0.0), shininess);
+
+		return specular * specularColor;
+	}
+}
