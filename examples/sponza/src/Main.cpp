c60d7859928ea34c61e25f3c639f1bf813bd7680
Julien Garagnon <julien.garagnon@aerys.in>
2013-09-20 16:48:32 +0200

    Add EGL init to sponza
    Former-commit-id: adfd97e1d24791f833e3fccf3db6e50a89e6b842
    Former-commit-id: 587c876ecca671a06b043faff2ee72eb8d98f475

--- examples/sponza/src/Main.cpp
+++ examples/sponza/src/Main.cpp
@@ -18,6 +18,13 @@
 # include "SDL2/SDL.h"
 #endif
 
+#ifdef _WIN32
+#include "SDL2/SDL_syswm.h"
+#include <EGL/egl.h>
+#include <GLES2/gl2.h>
+#include <GLES2/gl2ext.h>
+#endif
+
 #include "minko/component/SponzaLighting.hpp"
 #include "minko/component/Fire.hpp"
 
@@ -95,50 +102,50 @@ SDLMouseMoveHandler()
 void
 SDL_KeyboardHandler(bool collider, std::shared_ptr<Matrix4x4> cameraTransform)
 {
-    if (!collider)
-    {
+	if (!collider)
+	{
 	const Uint8* keyboardState = SDL_GetKeyboardState(NULL);
 	if (keyboardState[SDL_SCANCODE_UP] ||
 		keyboardState[SDL_SCANCODE_W] ||
 		keyboardState[SDL_SCANCODE_Z])
-	    cameraTransform->prependTranslation(0.f, 0.f, -CAMERA_LIN_SPEED);
+		cameraTransform->prependTranslation(0.f, 0.f, -CAMERA_LIN_SPEED);
 	else if (keyboardState[SDL_SCANCODE_DOWN] ||
 		keyboardState[SDL_SCANCODE_S])
-	    cameraTransform->prependTranslation(0.f, 0.f, CAMERA_LIN_SPEED);
+		cameraTransform->prependTranslation(0.f, 0.f, CAMERA_LIN_SPEED);
 	if (keyboardState[SDL_SCANCODE_LEFT] ||
 		keyboardState[SDL_SCANCODE_A] ||
 		keyboardState[SDL_SCANCODE_Q])
-	    cameraTransform->prependRotation(-CAMERA_ANG_SPEED, Vector3::yAxis());
+		cameraTransform->prependRotation(-CAMERA_ANG_SPEED, Vector3::yAxis());
 	else if (keyboardState[SDL_SCANCODE_RIGHT] ||
 		keyboardState[SDL_SCANCODE_D])
-	    cameraTransform->prependRotation(CAMERA_ANG_SPEED, Vector3::yAxis());
-    }
-    else
-    {
+		cameraTransform->prependRotation(CAMERA_ANG_SPEED, Vector3::yAxis());
+	}
+	else
+	{
 
 	const Uint8* keyboardState = SDL_GetKeyboardState(NULL);
 	if (keyboardState[SDL_SCANCODE_UP] ||
 		keyboardState[SDL_SCANCODE_W] ||
 		keyboardState[SDL_SCANCODE_Z])
-	    // go forward
-	    cameraTransform->prependTranslation(Vector3::create(0.0f, 0.0f, -CAMERA_LIN_SPEED));
+		// go forward
+		cameraTransform->prependTranslation(Vector3::create(0.0f, 0.0f, -CAMERA_LIN_SPEED));
 	else if (keyboardState[SDL_SCANCODE_DOWN] ||
 		keyboardState[SDL_SCANCODE_S])
-	    // go backward
-	    cameraTransform->prependTranslation(Vector3::create(0.0f, 0.0f, CAMERA_LIN_SPEED));
+		// go backward
+		cameraTransform->prependTranslation(Vector3::create(0.0f, 0.0f, CAMERA_LIN_SPEED));
 	if (keyboardState[SDL_SCANCODE_LEFT] ||
 		keyboardState[SDL_SCANCODE_A] ||
 		keyboardState[SDL_SCANCODE_Q])
-	    cameraTransform->prependTranslation(-CAMERA_LIN_SPEED, 0.0f, 0.0f);
+		cameraTransform->prependTranslation(-CAMERA_LIN_SPEED, 0.0f, 0.0f);
 	else if (keyboardState[SDL_SCANCODE_RIGHT] ||
 		keyboardState[SDL_SCANCODE_D])
-	    cameraTransform->prependTranslation(CAMERA_LIN_SPEED, 0.0f, 0.0f);
+		cameraTransform->prependTranslation(CAMERA_LIN_SPEED, 0.0f, 0.0f);
 
 	eye = cameraTransform->translation();
 
 	if (keyboardState[SDL_SCANCODE_SPACE] && eye->y() <= 0.5f)
-	    cameraTransform->prependTranslation(0.0f, 4 * CAMERA_LIN_SPEED, 0.0f);
-    }
+		cameraTransform->prependTranslation(0.0f, 4 * CAMERA_LIN_SPEED, 0.0f);
+	}
 }
 
 template <typename T>
@@ -423,6 +430,113 @@ printFramerate(const unsigned int delay = 1)
 	}
 }
 
+
+#ifdef _WIN32
+typedef struct
+{
+   /// Window width
+   GLint       width;
+   /// Window height
+   GLint       height;
+   /// Window handle
+   EGLNativeWindowType  hWnd;
+   /// EGL display
+   EGLDisplay  eglDisplay;
+   /// EGL context
+   EGLContext  eglContext;
+   /// EGL surface
+   EGLSurface  eglSurface;
+} ESContext; 
+
+ESContext* initContext(SDL_Window* window)
+{
+	EGLint configAttribList[] =
+	{
+		EGL_RED_SIZE,       8,
+		EGL_GREEN_SIZE,     8,
+		EGL_BLUE_SIZE,      8,
+		EGL_ALPHA_SIZE,     8,
+		EGL_DEPTH_SIZE,     16,
+		EGL_STENCIL_SIZE,   8,
+		EGL_SAMPLE_BUFFERS, 0,
+		EGL_NONE
+	};
+	EGLint surfaceAttribList[] =
+	{
+		EGL_RENDER_BUFFER, EGL_BACK_BUFFER,
+		EGL_NONE, EGL_NONE
+	};
+
+	SDL_SysWMinfo info;
+	SDL_VERSION(&info.version);
+	if (!SDL_GetWindowWMInfo(window, &info))
+		return GL_FALSE;
+	EGLNativeWindowType hWnd = info.info.win.window;
+
+	ESContext* es_context = new ESContext();
+	es_context->width = 800;
+	es_context->height = 600;
+	es_context->hWnd = hWnd;
+
+	EGLDisplay display;
+	EGLint numConfigs;
+	EGLint majorVersion;
+	EGLint minorVersion;
+	EGLContext context;
+	EGLSurface surface;
+	EGLConfig config;
+	EGLint contextAttribs[] = { EGL_CONTEXT_CLIENT_VERSION, 2, EGL_NONE, EGL_NONE }; 
+
+	display = eglGetDisplay(GetDC(hWnd)); // EGL_DEFAULT_DISPLAY
+	if ( display == EGL_NO_DISPLAY )
+	{
+		return EGL_FALSE;
+	}
+
+	// Initialize EGL
+	if ( !eglInitialize(display, &majorVersion, &minorVersion) )
+	{
+		return EGL_FALSE;
+	}
+
+	// Get configs
+	if ( !eglGetConfigs(display, NULL, 0, &numConfigs) )
+	{
+		return EGL_FALSE;
+	}
+
+	// Choose config
+	if ( !eglChooseConfig(display, configAttribList, &config, 1, &numConfigs) )
+	{
+		return EGL_FALSE;
+	}
+
+	// Create a surface
+	surface = eglCreateWindowSurface(display, config, (EGLNativeWindowType)hWnd, surfaceAttribList);
+	if ( surface == EGL_NO_SURFACE )
+	{
+		return EGL_FALSE;
+	}
+
+	// Create a GL context
+	context = eglCreateContext(display, config, EGL_NO_CONTEXT, contextAttribs );
+	if ( context == EGL_NO_CONTEXT )
+	{
+		return EGL_FALSE;
+	}   
+
+	// Make the context current
+	if ( !eglMakeCurrent(display, surface, surface, context) )
+	{
+		return EGL_FALSE;
+	}
+	es_context->eglDisplay = display;
+	es_context->eglSurface = surface;
+	es_context->eglContext = context;
+	return es_context;
+}
+#endif
+
 int
 main(int argc, char** argv)
 {
@@ -438,6 +552,20 @@ main(int argc, char** argv)
 	);
 
 	SDL_Init(SDL_INIT_VIDEO);
+	auto window = SDL_CreateWindow("Sponza Example",
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_OPENGL);
+
+#ifdef _WIN32
+	ESContext* escontext;
+	if (!(escontext = initContext(window)))
+		throw std::runtime_error("Could not create eglContext");
+
+	std::cout << "EGLContext Initialized" << std::endl;
+#else
+	SDL_GLContext glcontext = SDL_GL_CreateContext(window);
+#endif
 
 #ifdef EMSCRIPTEN
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
@@ -552,41 +680,41 @@ main(int argc, char** argv)
 
 		if (cameraCollider == nullptr)
 		{
-		    SDL_Event event;
-		    while (SDL_PollEvent(&event))
-		    {
+			SDL_Event event;
+			while (SDL_PollEvent(&event))
+			{
 			switch (event.type)
 			{
-			    case SDL_MOUSEMOTION:
-				SDLMouseMoveHandler();
+				case SDL_MOUSEMOTION:
+				SDLMouseMoveHandler(window);
 				break;
-			    case SDL_QUIT:
+				case SDL_QUIT:
 				done = true;
 				break;
-			    default:
+				default:
 				break;
 			}
-		    }
-		    SDL_KeyboardHandler(false, cameraTransform);
+			}
+			SDL_KeyboardHandler(false, cameraTransform);
 		}
 		else
 		{
 			SDL_Event event;
 			while (SDL_PollEvent(&event))
 			{
-			    switch (event.type)
-			    {
+				switch (event.type)
+				{
 				case SDL_MOUSEMOTION:
-				    SDLMouseMoveHandler();
-				    break;
+					SDLMouseMoveHandler(window);
+					break;
 				case SDL_KEYDOWN:
-				    break;
+					break;
 				case SDL_QUIT:
-				    done = true;
-				    break;
+					done = true;
+					break;
 				default:
-				    break;
-			    }
+					break;
+				}
 			}
 			SDL_KeyboardHandler(true, cameraTransform);
 
@@ -609,10 +737,10 @@ main(int argc, char** argv)
 		sponzaLighting->step();
 		renderer->render();
 
-#ifdef EMSCRIPTEN
-		SDL_GL_SwapBuffers();
+#ifdef _WIN32
+		eglSwapBuffers(escontext->eglDisplay, escontext->eglSurface); 
 #else
-                SDL_GL_SwapWindow(window);
+		SDL_GL_SwapWindow(window);
 #endif
 		SDL_PumpEvents();
 	}
