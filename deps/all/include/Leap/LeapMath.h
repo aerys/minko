2ef5b40a8da38c974187f616109e8e8416132287
Julien Garagnon <julien.garagnon@aerys.in>
2013-09-30 19:50:48 +0200

    First working sample with leap
    Former-commit-id: 496872734887960b4dcd1b65bedadc7d5695e42a

--- deps/all/include/Leap/LeapMath.h
+++ deps/all/include/Leap/LeapMath.h
@@ -20,7 +20,7 @@ namespace Leap {
  * The constant pi as a single precision floating point number.
  * @since 1.0
  */
-static const float PI          = 3.1415926536f;
+//static const float PI          = 3.1415926536f;
 /**
  * The constant ratio to convert an angle measure from degrees to radians.
  * Multiply a value in degrees by this constant to convert to radians.
