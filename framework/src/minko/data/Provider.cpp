f6c147a7af739e359d2a1aa1369bbbae828ecb46
Pierre-Edouard Landes <pierreedouard.landes@aerys.in>
2013-10-14 17:36:55 +0200

    Fixed dynamic shader fork when removing properties (rendering now changes as soon as a program signature-defining property is removed from a provider).
    Former-commit-id: 86954ee362985bf204631e3f905d538f1834818e [formerly f92a0c6667eb727cd554cf4ee4d500285eb14724]
    Former-commit-id: 106f7b1f927da563d1c105c5554b1aab725ad7c4 [formerly 7147a81dce4753e71ffe7c9eefa4bbc23d34d725]
    Former-commit-id: 40f9768aa75390ad2636e32fefa31d7baccbcc78

--- framework/src/minko/data/Provider.cpp
+++ framework/src/minko/data/Provider.cpp
@@ -38,18 +38,16 @@ Provider::Provider() :
 void
 Provider::unset(const std::string& propertyName)
 {
-	auto formattedPropertyName = formatPropertyName(propertyName);
-
-	_values.erase(formattedPropertyName);
-	_valueChangedSlots.erase(formattedPropertyName);
-	_referenceChangedSlots.erase(formattedPropertyName);
-
-	if (_values.count(formattedPropertyName) == 0)
+	const auto& formattedPropertyName = formatPropertyName(propertyName);
+	
+	if (_values.count(formattedPropertyName) != 0)
 	{
 		_names.erase(std::find(_names.begin(), _names.end(), formattedPropertyName));
+		_values.erase(formattedPropertyName);
+		_valueChangedSlots.erase(formattedPropertyName);
+		_referenceChangedSlots.erase(formattedPropertyName);
 
 		_propertyRemoved->execute(shared_from_this(), formattedPropertyName);
-		_propReferenceChanged->execute(shared_from_this(), formattedPropertyName);
 	}
 }
 
