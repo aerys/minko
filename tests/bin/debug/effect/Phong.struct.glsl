6aced954bb4aa34e164e20e16ff487ef137b1ca8
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-10-16 12:59:23 +0200

    bootstraped tests suite using googletest
    Former-commit-id: e46c9c2663cc0c4cf1c127c42599c5fe6ac4874e
    Former-commit-id: cb6e67ce13f1195bec8116ab37069ff7e2814ba4
    Former-commit-id: 655231eeac6c2068c8cb8b26afe0eb1a614c244c

---
+++ tests/bin/debug/effect/Phong.struct.glsl
@@ -0,0 +1,34 @@
+struct AmbientLight
+{
+	vec3 color;
+	float ambient;	
+};
+
+struct DirectionalLight
+{
+	vec3 color;
+	float diffuse;
+	float specular;
+	vec3 direction;	
+};
+
+struct PointLight
+{
+	vec3 color;
+	float diffuse;
+	float specular;
+	vec2 attenuationDistance;
+	vec3 position;
+};
+
+struct SpotLight
+{
+	vec3 color;
+	float diffuse;
+	float specular;
+	float attenuationDistance;
+	vec3 position;
+	vec3 direction;
+	float cosInnerConeAngle;
+	float cosOuterConeAngle;
+};
