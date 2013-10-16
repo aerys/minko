3e67cfebfe9d9ec009b4b787a0b240c811a5c67c
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-10-16 12:59:23 +0200

    bootstraped tests suite using googletest
    Former-commit-id: 5131a3bffa572cb75603231f9519c5bdfdbdd4f7 [formerly 35a1e3ff3036b7ce27791e482c588cc0c09b2120] [formerly e46c9c2663cc0c4cf1c127c42599c5fe6ac4874e]
    Former-commit-id: 6aced954bb4aa34e164e20e16ff487ef137b1ca8 [formerly 655231eeac6c2068c8cb8b26afe0eb1a614c244c] [formerly cb6e67ce13f1195bec8116ab37069ff7e2814ba4]
    Former-commit-id: 264cf1dc736f1115b7f37e39e862759cea0f3123
    Former-commit-id: faec77222991c6d30210a91e7df9a4e765a7fae8

---
+++ tests/lib/googletest/premake4.lua
@@ -0,0 +1,34 @@
+project "googletest"
+	kind "StaticLib"
+	language "C++"
+	files {
+		"src/**.h",
+		"src/**.cc"
+	}
+	includedirs {
+		".",
+		"src",
+		"include"
+	}
+	
+	configuration { "debug"}
+		defines { "DEBUG", "_DEBUG" }
+		flags { "Symbols" }
+		targetdir "bin/debug"
+
+	configuration { "release" }
+		defines { "NDEBUG" }
+		flags { "OptimizeSpeed" }
+		targetdir "bin/release"
+
+	-- linux
+	configuration { "linux" }
+
+	-- windows
+	configuration { "windows", "x32" }
+
+	-- macos
+	configuration { "macosx" }
+
+	-- emscripten
+	configuration { "emscripten" }
