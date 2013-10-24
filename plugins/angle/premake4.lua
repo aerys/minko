81031a94228c05299a0d0aed9c74dcfb253cf235
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-10-24 21:27:57 +0200

    major refactor of the build system with a proper API for plugins ANGLE is now support through the "minko-angle" plugin a cross-platform SDL init. plugin has been added
    Former-commit-id: a4d3063b413856fe502d3cf2e58d657381cb3ced
    Former-commit-id: ef81209837a4a9dceaa3eb6cf4245797b6d761b3

---
+++ plugins/angle/premake4.lua
@@ -0,0 +1,4 @@
+newoption {
+	trigger			= "with-angle",
+	description		= "Enable the ANGLE plugin to use the DirectX backend provided by the Google ANGLE project."
+}
---
+++ plugins/sdl/plugin.lua
@@ -0,0 +1,35 @@
+-- sdl plugin
+if minko.plugin.enabled("sdl") then
+	configuration { "windows" }
+		links { "minko-sdl", "SDL2", "SDL2main" }
+		libdirs { minko.sdk.path("plugins/sdl/lib/win/SDL/lib") }
+		includedirs {
+			minko.sdk.path("plugins/sdl/lib/SDL2/include"),
+			minko.sdk.path("plugins/sdl/src")
+		}
+		postbuildcommands {
+			minko.vs.getdllscopycommand(minko.sdk.path("plugins/sdl/lib/win/SDL/lib"))
+		}
+		
+	configuration { "linux" }
+		links { "minko-sdl", "SDL2" }
+		includedirs {
+			minko.sdk.path("plugins/sdl/lib/SDL2/include"),
+			minko.sdk.path("plugins/sdl/src")
+		}
+
+	configuration { "macosx" }
+		links { "minko-sdl", "SDL2.framework" }
+		includedirs {
+			minko.sdk.path("plugins/sdl/lib/SDL2/include"),
+			minko.sdk.path("plugins/sdl/src")
+		}
+
+	configuration { "emscripten" }
+		links { "minko-sdl" }
+		defines { "HAVE_M_PI" }
+		includedirs {
+			minko.sdk.path("plugins/sdl/lib/emscripten/SDL/include"),
+			minko.sdk.path("plugins/sdl/src")
+		}
+end
