1c2a97d7b952b4e5d88998e6aca5951bf9c62fca
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-10-29 14:07:06 +0100

    bootstrap oculus plugin
    Former-commit-id: 50508a24e25523fc03d1679487b6ad1d09919dc5
    Former-commit-id: c7616e9dd7009dcd55bb1502fb3f7ac92820dca9
    Former-commit-id: 36bdfbfcde6d197c5c5f03b896cbb8fd24d17fa8

---
+++ plugins/oculus/src/minko/component/OculusVRCamera.hpp
@@ -0,0 +1,72 @@
+/*
+Copyright (c) 2013 Aerys
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#pragma once
+
+#include "minko/Common.hpp"
+
+#include "minko/Signal.hpp"
+#include "minko/component/AbstractComponent.hpp"
+
+namespace minko
+{
+	namespace component
+	{
+		class OculusVRCamera :
+			public AbstractComponent,
+			public std::enable_shared_from_this<OculusVRCamera>
+		{
+		public:
+			typedef std::shared_ptr<OculusVRCamera>	Ptr;
+
+		private:
+			typedef std::shared_ptr<scene::Node>		NodePtr;
+			typedef std::shared_ptr<AbstractComponent>	AbsCmpPtr;
+
+		private:
+			float								_aspectRatio;
+
+			Signal<NodePtr, AbsCmpPtr>::Slot	_targetAddedSlot;
+			Signal<NodePtr, AbsCmpPtr>::Slot	_targetRemovedSlot;
+
+		public:
+			inline static
+			Ptr
+			create(float aspectRatio)
+			{
+				auto oc = std::shared_ptr<OculusVRCamera>(new OculusVRCamera);
+
+				oc->initialize();
+
+				return oc;
+			}
+
+		private:
+			void
+			initialize();
+
+			void
+			targetAddedHandler(AbsCmpPtr component, NodePtr node);
+
+			void
+			targetRemovedHandler(AbsCmpPtr component, NodePtr node);
+		};
+	}
+}
+
