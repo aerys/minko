d1aa7e6ab44ace65a404667d88074e368fd3aa34
Warren Seine <warren.seine@aerys.in>
2013-10-29 13:54:34 +0100

    Fix compilation with Clang.
    Former-commit-id: 96d7e90a760bb4835960908181259fe7988aa33a

--- sdk.lua
+++ sdk.lua
@@ -10,3 +10,31 @@ dofile(MINKO_HOME .. '/tools/all/lib/minko.path.lua')
 dofile(MINKO_HOME .. '/tools/all/lib/minko.plugin.lua')
 dofile(MINKO_HOME .. '/tools/all/lib/minko.vs.lua')
 dofile(MINKO_HOME .. '/tools/all/lib/minko.project.lua')
+
+minko.sdk.newplatform {
+	name = "emscripten",
+	description = "Emscripten C++ to JS toolchain",
+	gcc = {
+		cc = path.getabsolute("../../lin/bin/emcc.sh"),
+		cxx = path.getabsolute("../../lin/bin/em++.sh"),
+		ar = path.getabsolute("../../lin/bin/emar.sh"),
+		cppflags = "-MMD -DEMSCRIPTEN"
+	}
+}
+
+minko.sdk.newplatform {
+	name = "clang",
+	description = "Clang",
+	gcc = {
+		cc = "clang",
+		cxx = "clang++",
+		ar = "ar",
+		cppflags = "-MMD "
+	}
+}
+
+if _OPTIONS.platform then
+	print("Selected target platform: " .. _OPTIONS["platform"])
+    -- overwrite the native platform with the options::platform
+    premake.gcc.platforms['Native'] = premake.gcc.platforms[_OPTIONS.platform]
+end
