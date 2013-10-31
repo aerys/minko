3e797f765c0a87fc3482603107fb953bf771d9a1
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-10-31 10:07:31 +0100

    moved the Particles.effect to the "particles" plugin and make sure it is copied in the relevant target directories using post-build commands in the particles/plugin.lua build conf. file
    Former-commit-id: 208bead328b5ae32067b26c2f821f75e52e5e430 [formerly 05960fdc95414ed4acc28291dc5e0908fd763b36]
    Former-commit-id: cae280647d3ca40f07f954f791c17fa66e957218 [formerly 0aa9deb0065b76655f81e46ab6379f7edfcfe4b7]
    Former-commit-id: 59d18955b8c4e433eb66053b7248bd4147c5c431

--- plugins/particles/plugin.lua
+++ plugins/particles/plugin.lua
@@ -2,4 +2,20 @@
 if minko.plugin.enabled("particles") then
 	links { "minko-particles" }
 	includedirs { minko.sdk.path("plugins/particles/src") }
+	
+	configuration { "windows" }
+		postbuildcommands {
+			'xcopy /y /i "' .. minko.sdk.path('/plugins/particles/asset/effect') .. '" "$(TargetDir)\\effect"',
+		}
+		
+	configuration { "linux" }
+		postbuildcommands {
+			'cp -r ' .. minko.sdk.path('/plugins/particles/asset/effect') .. ' ${TARGETDIR} || :',
+		}
+	
+	configuration { "macosx" }
+		postbuildcommands {
+			'cp -r ' .. minko.sdk.path('/plugins/particles/asset/effect') .. ' . || :',
+		}		
+	
 end
