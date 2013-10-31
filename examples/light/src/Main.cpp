40531a4cc0bb00149a9c72e545957d137be6de42
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-10-31 10:32:51 +0100

    implemented a Color static class with a Color::hslToRgb() method and started using it wherever relevant
    Former-commit-id: 58c6e03cc1a7bdf225a8e91ae869e1cf186b046d
    Former-commit-id: 73ce8241a29c73d28a59985c86622719ee7d954a

--- examples/light/src/Main.cpp
+++ examples/light/src/Main.cpp
@@ -45,43 +45,6 @@ createPointLight(Vector3::Ptr color, Vector3::Ptr position, file::AssetLibrary::
 	return pointLight;
 }
 
-float
-hue2rgb(float p, float q, float t)
-{
-	if (t < 0.f)
-		t += 1.f;
-	if (t > 1.f)
-		t -= 1.f;
-	if (t < 1.f/6.f)
-		return p + (q - p) * 6.f * t;
-	if (t < 1.f/2.f)
-		return q;
-	if (t < 2.f/3.f)
-		return p + (q - p) * (2.f/3.f - t) * 6.f;
-
-	return p;
-}
-
-Vector3::Ptr
-hslToRgb(float h, float s, float l)
-{
-    float r, g, b;
-
-    if (s == 0)
-        r = g = b = l; // achromatic
-    else
-	{
-        float q = l < 0.5f ? l * (1.f + s) : l + s - l * s;
-        float p = 2.f * l - q;
-
-        r = hue2rgb(p, q, h + 1.f/3.f);
-        g = hue2rgb(p, q, h);
-        b = hue2rgb(p, q, h - 1.f/3.f);
-    }
-
-	return Vector3::create(r, g, b);
-}
-
 int main(int argc, char** argv)
 {
 	MinkoSDL::initialize("Minko Examples - Light", 800, 600);
@@ -214,7 +177,7 @@ int main(int argc, char** argv)
 
 				auto r = rand() / (float)RAND_MAX;
 				auto theta = 2.0f * PI *  r;
-				auto color = hslToRgb(r, 1.f, .5f);
+				auto color = Color::hslToRgb(r, 1.f, .5f);
 				auto pos = Vector3::create(
 					cosf(theta) * 5.f + rand() / ((float)RAND_MAX * 3.f),
 					2.5f + rand() / (float)RAND_MAX,
