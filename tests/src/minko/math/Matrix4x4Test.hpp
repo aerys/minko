a29cc740815f4f2c669a024f3ddb975b7f43f102
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-11-01 11:48:34 +0100

    added more tests for the Matrix4x4 class, the Matrix4x4Test.TransformRotationXRandom test is failing
    Former-commit-id: f74c5f5fef1eac842dd71ac656c365ab1314649c
    Former-commit-id: 7e264d874ffceb2da2bddc3ad7cfbbc6e120c666

--- tests/src/minko/math/Matrix4x4Test.hpp
+++ tests/src/minko/math/Matrix4x4Test.hpp
@@ -23,6 +23,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 
 #include "gtest/gtest.h"
 
+#include "minko/math/Matrix4x4.hpp"
+
 namespace minko
 {
 	namespace math
@@ -30,6 +32,32 @@ namespace minko
 		class Matrix4x4Test :
 			public ::testing::Test
 		{
+		public:
+			static inline
+			float
+			random(float max = 1000.f)
+			{
+				return rand() / (float)RAND_MAX * 1000.f;
+			}
+
+			static inline
+			std::shared_ptr<Matrix4x4>
+			randomMatrix(float max = 1000.f)
+			{
+				return Matrix4x4::create()->initialize(
+					random(max), random(max), random(max), random(max),
+					random(max), random(max), random(max), random(max),
+					random(max), random(max), random(max), random(max),
+					random(max), random(max), random(max), random(max)
+				);
+			}
+
+			static inline
+			bool
+			nearEqual(float x, float y, float epsilon = 1e-10)
+			{
+				return abs(x - y) < epsilon;
+			}
 		};
 	}
 }
