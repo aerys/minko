bb64e0eb1fb4b6ffac6326117545f8f9e6b0f804
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-11-07 13:36:47 +0100

    added math::clp2 and math::flp2 methods to get upper/lower power of two values
    Former-commit-id: 367f2812d8af694c324801c6bdd768a310f6ed7d
    Former-commit-id: 6be061b1a767d6498cde06690230cda67323822d

--- framework/src/minko/Common.hpp
+++ framework/src/minko/Common.hpp
@@ -153,6 +153,33 @@ namespace minko
 		class Vector4;
 		class Matrix4x4;
 		class Quaternion;
+
+		inline
+		unsigned int
+		flp2(unsigned int x)
+		{
+			x = x | (x >> 1);
+			x = x | (x >> 2);
+			x = x | (x >> 4);
+			x = x | (x >> 8);
+			x = x | (x >> 16);
+
+			return x - (x >> 1);
+		}
+
+		inline
+		unsigned int
+		clp2(unsigned int x)
+		{
+			x = x - 1;
+			x = x | (x >> 1);
+			x = x | (x >> 2);
+			x = x | (x >> 4);
+			x = x | (x >> 8);
+			x = x | (x >> 16);
+
+			return x + 1;
+		}
 	}
 
 	namespace file
