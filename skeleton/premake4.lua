f342717a14ed2a236cd98991e0d3eb23e4d82e56
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-11-09 10:26:41 +0100

    update build system to ease up the creation of external apps linked with the SDK and provide a ready to build app skeleton
    Former-commit-id: e15d639e70b649c24a420066238e4b4958a0df55

--- skeleton/premake4.lua
+++ skeleton/premake4.lua
@@ -1,103 +1,23 @@
-project "minko-example-cube"
-	kind "ConsoleApp"
-	language "C++"
-	files {
-		"src/**.hpp",
-		"src/**.cpp"
-	}
-	includedirs {
-		"src",
-		"../../deps/all/include"
-	}
-		
-	-- minko-webgl
-	-- ugly, but couldn't find a better solution to maintain linking order.
-	if _OPTIONS["platform"] == "emscripten" then
-		links { "minko-webgl" }
-	end
+dofile(os.getenv("MINKO_HOME") .. "/sdk.lua")
 
-	-- minko-png
-	links { "minko-png" }
-	includedirs { "../../plugins/png/src" }
-	-- minko-framework
-	links { "minko-framework" }
-	includedirs { "../../framework/src" }
-	if not _OPTIONS["no-glsl-optimizer"] then
-		links { "glsl-optimizer" }
-	end
+PROJECT_NAME = path.getname(os.getcwd())
 
-	configuration { "debug"}
-		defines { "DEBUG" }
-		flags { "Symbols" }
-		targetdir "bin/debug"
+minko.project.solution(PROJECT_NAME)
 
-	configuration { "release" }
-		defines { "NDEBUG" }
-		flags { "OptimizeSpeed" }
-		targetdir "bin/release"
+	minko.project.application(PROJECT_NAME)
 
-	-- linux
-	configuration { "linux" }
-		links { "GL", "GLU", "SDL2", "m", "Xrandr", "Xxf86vm", "Xi", "rt", "X11", "pthread" }
-		libdirs { "../../deps/lin/lib" }
-		includedirs { "../../deps/lin/include" }
-		buildoptions { "-std=c++11" }
-		postbuildcommands {
-			'cp -r ../../framework/effect ${TARGETDIR} || :',
-			'cp -r asset/* ${TARGETDIR} || :'
-		}
+		language "c++"
+		kind "ConsoleApp"
 
-	-- windows
-	configuration { "windows", "x32" }
-		links { "SDL2", "SDL2main" }
-		libdirs { "../../deps/win/lib" }
-		includedirs { "../../deps/win/include" }
-		if _OPTIONS[ "directX" ] then
-			defines { "MINKO_ANGLE" }
-			links { "libGLESv2", "libEGL" }
-		else
-			links { "OpenGL32", "glew32" }
-		end
-		postbuildcommands {
-			-- copy framework effects
-			'xcopy /y /e /i ..\\..\\framework\\effect\\* "$(TargetDir)effect"',
-			-- copy assets
-			'xcopy /y /e /i asset\\* "$(TargetDir)"',
-			-- copy dlls
-			'for /r %%x in (..\\..\\deps\\win\\lib\\*.dll) do xcopy /y /e /i "%%x" "$(TargetDir)"'
-		}
+		files { "src/**.cpp", "src/**.hpp" }
+		includedirs { "src" }
 
-	-- macos
-	configuration { "macosx" }
-		buildoptions { "-std=c++11", "-stdlib=libc++" }
-		linkoptions { "-std=c++11", "-stdlib=libc++" }
-		links {
-			"m",
-			"SDL2.framework",
-			"Cocoa.framework",
-			"OpenGL.framework",
-			"IOKit.framework"
-		}
-		libdirs {
-			"../../deps/mac/lib"
-		}
-		includedirs {
-			"../../deps/mac/include"
-		}
-		postbuildcommands {
-			'cp -r ../../framework/effect . || :',
-			'cp -r asset/* . || :'
-		}
+		-- plugins
+		minko.plugin.enable("sdl")
+		--minko.plugin.enable("bullet")
+		--minko.plugin.enable("jpeg")
+		--minko.plugin.enable("mk")
+		--minko.plugin.enable("particles")
+		--minko.plugin.enable("png")
+		--minko.plugin.enable("sdl")
 
-	-- emscripten
-	configuration { "emscripten" }
-		flags { "Optimize" }
-		links { "minko-webgl" }
-		includedirs { "../../plugins/webgl/src" }
-		buildoptions { "-std=c++11" }
-		local bin = "bin/release/" .. project().name
-		postbuildcommands {
-			'cp ' .. bin .. ' ' .. bin .. '.bc',
-			'emcc ' .. bin .. '.bc -o ' .. bin .. '.js -O1 -s ASM_JS=1 -s TOTAL_MEMORY=1073741824 --preload-dir effect --preload-dir texture',
-			'emcc ' .. bin .. '.bc -o ' .. bin .. '.html -O1 -s ASM_JS=1 -s TOTAL_MEMORY=1073741824 --preload-dir effect --preload-dir texture'
-		}
