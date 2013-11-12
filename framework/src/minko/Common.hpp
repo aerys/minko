aaed3630dbbf14d628d5fd299e13829ef182abad
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-11-12 11:50:15 +0100

    remove useless #include <array>
    Former-commit-id: 97f9fd3d9228c2ba4f466ac3ac75865ff5f99b2e [formerly e55292ae0994c668ceaaeeb5fd4dec8db0101e77] [formerly 2876298eea7c51abd161ec597b9ccce7e9ca6d00]
    Former-commit-id: e360651f11fddfa8311a0b9d12ad3fc7f174ab1b [formerly 4589c4d2dd66b1e4f95aae44deaf0f07e86220b0] [formerly 35084f2da2e56189b6d2ff43d4fe6190e5c8b731]
    Former-commit-id: e0470d34c0b5fd990a50c1291b6d9157b2bb26dd
    Former-commit-id: be4bdcdb42adb55a7317679d7587e8a2347392c2

--- framework/src/minko/Common.hpp
+++ framework/src/minko/Common.hpp
@@ -20,7 +20,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 #pragma once
 
 #include <algorithm>
-#include <array>
 #include <cmath>
 #include <cstring>
 #include <exception>
