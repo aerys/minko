1df8004480b34c71a13d72cbbce81383c343722a
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-11-13 16:00:40 +0100

    now properly updaing the BoundingBox based on the target node geometry and model to world transform
    Former-commit-id: 99a2596c475d5faae7bd30561da90ccb1d186c6e
    Former-commit-id: 8e1c61d03a5b60e530404f3eb5a82f7daae8a319
    Former-commit-id: 8bc3baf9b2d45c7f280fa0392ad02c4139823bb1

--- framework/src/minko/component/BoundingBox.cpp
+++ framework/src/minko/component/BoundingBox.cpp
@@ -20,13 +20,18 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 #include "BoundingBox.hpp"
 
 #include "minko/math/Box.hpp"
+#include "minko/math/Vector3.hpp"
 #include "minko/scene/Node.hpp"
 #include "minko/component/Transform.hpp"
+#include "minko/component/Surface.hpp"
+#include "minko/geometry/Geometry.hpp"
+#include "minko/data/Container.hpp"
 
 using namespace minko;
+using namespace math;
 using namespace minko::component;
 
-BoundingBox::BoundingBox(std::shared_ptr<math::Vector3> topRight, std::shared_ptr<math::Vector3> bottomLeft) :
+BoundingBox::BoundingBox(Vector3::Ptr topRight, Vector3::Ptr bottomLeft) :
 	_fixed(true),
 	_box(math::Box::create(topRight, bottomLeft)),
 	_worldSpaceBox(math::Box::create(topRight, bottomLeft))
@@ -50,12 +55,29 @@ BoundingBox::initialize()
 		if (targets().size() > 1)
 			throw std::logic_error("The same BoundingBox cannot have 2 different targets");
 
+		_modelToWorldChangedSlot = target->data()->propertyValueChanged("transform.modelToWorldMatrix")->connect(
+			[&](data::Container::Ptr data, const std::string& propertyName)
+			{
+				updateWorldSpaceBox();
+			}
+		);
+
+		auto componentAddedOrRemovedCallback = [&](scene::Node::Ptr node, scene::Node::Ptr target, AbstractComponent::Ptr cmp)
+		{
+			if (std::dynamic_pointer_cast<Surface>(cmp))
+				update();
+		};
+
+		_componentAddedSlot = target->componentAdded()->connect(componentAddedOrRemovedCallback);
+		_componentRemovedSlot = target->componentAdded()->connect(componentAddedOrRemovedCallback);
+
 		update();
 	});
 
 	_targetRemovedSlot = targetRemoved()->connect([&](AbstractComponent::Ptr cmp, scene::Node::Ptr target)
 	{
-
+		_componentAddedSlot = nullptr;
+		_componentRemovedSlot = nullptr;
 	});
 }
 
@@ -63,26 +85,109 @@ void
 BoundingBox::update()
 {
 	auto target = targets()[0];
+	auto surfaces = target->components<Surface>();
 
 	if (!_fixed)
 	{
-		//for (auto& geometry : target->components<geometry::Geometry>())
+		if (!surfaces.empty())
 		{
-			// get geometry bounds and update _box
+			auto min = Vector3::create(
+				std::numeric_limits<float>::max(),
+				std::numeric_limits<float>::max(),
+				std::numeric_limits<float>::max()
+			);
+			auto max = Vector3::create(
+				std::numeric_limits<float>::lowest(),
+				std::numeric_limits<float>::lowest(),
+				std::numeric_limits<float>::lowest()
+			);
+
+			for (auto& surface : surfaces)
+			{
+				auto geom = surface->geometry();
+				auto xyzBuffer = geom->vertexBuffer("position");
+				auto attr = xyzBuffer->attribute("position");
+				auto offset = std::get<2>(*attr);
+
+				for (uint i = 0; i < xyzBuffer->numVertices(); ++i)
+				{
+					auto x = xyzBuffer->data()[i * xyzBuffer->vertexSize() + offset];
+					auto y = xyzBuffer->data()[i * xyzBuffer->vertexSize() + offset + 1];
+					auto z = xyzBuffer->data()[i * xyzBuffer->vertexSize() + offset + 2];
+
+					if (x < min->x())
+						min->x(x);
+					else if (x > max->x())
+						max->x(x);
+
+					if (y < min->y())
+						min->y(y);
+					else if (y > max->y())
+						max->y(y);
+
+					if (z < min->z())
+						min->z(z);
+					else if (z > max->z())
+						max->z(z);
+				}
+			}
+
+			_box->bottomLeft()->copyFrom(min);
+			_box->topRight()->copyFrom(max);
+		}
+		else
+		{
+			_box->bottomLeft()->copyFrom(Vector3::zero());
+			_box->topRight()->copyFrom(Vector3::zero());
 		}
 	}
 
-	// update _worldSpaceBox
-	auto t = target->component<Transform>();
+	updateWorldSpaceBox();
+}
 
-	if (t)
-	{
-		t->transform()->transform(_box->topRight(), _worldSpaceBox->topRight());
-		t->transform()->transform(_box->bottomLeft(), _worldSpaceBox->bottomLeft());
-	}
-	else
+void
+BoundingBox::updateWorldSpaceBox()
+{
+	if (!targets()[0]->data()->hasProperty("transform.modelToWorldMatrix"))
 	{
 		_worldSpaceBox->topRight()->copyFrom(_box->topRight());
 		_worldSpaceBox->bottomLeft()->copyFrom(_box->bottomLeft());
+	}
+	else
+	{
+		auto t = targets()[0]->data()->get<Matrix4x4::Ptr>("transform.modelToWorldMatrix");
+		auto vertices = _box->getVertices();
+
+		for (auto& vertex : vertices)
+			t->transform(vertex, vertex);
+
+		auto min = _worldSpaceBox->bottomLeft()->setTo(
+			std::numeric_limits<float>::max(),
+			std::numeric_limits<float>::max(),
+			std::numeric_limits<float>::max()
+		);
+		auto max = _worldSpaceBox->topRight()->setTo(
+			std::numeric_limits<float>::lowest(),
+			std::numeric_limits<float>::lowest(),
+			std::numeric_limits<float>::lowest()
+		);
+
+		for (auto& vertex : vertices)
+		{
+			if (vertex->x() > max->x())
+				max->x(vertex->x());
+			else if (vertex->x() < min->x())
+				min->x(vertex->x());
+
+			if (vertex->y() > max->y())
+				max->y(vertex->y());
+			else if (vertex->y() < min->y())
+				min->y(vertex->y());
+
+			if (vertex->z() > max->z())
+				max->z(vertex->z());
+			else if (vertex->z() < min->z())
+				min->z(vertex->z());
+		}
 	}
 }
