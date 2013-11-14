2b96ec2ba38978a222632005932465b5df1c5a74
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-11-14 01:10:58 +0100

    add working MousePicking component based on BoundingBox and ray/box intersection
    Former-commit-id: 5130569388bdef4f068dd0dbbf26fc4514200c28 [formerly d27fd494820b7de2e1e754726d08dc8c25d5706a] [formerly ec16d8f5fff0197163800f618651e25ff72622af]
    Former-commit-id: 63e599184f78cb74cd74bfeeefb2b29024c454d6 [formerly 5aef24b9ce558783088ac4daad957abc304e8300] [formerly 83d35252ccdaeea65a43723cc556dc3dcb1f8efd]
    Former-commit-id: 851ac67e127993c5330c0e8e49cee14526c2d87f
    Former-commit-id: dce487fb5a87182fdae84ec5b835366483fe0e55

---
+++ framework/src/minko/component/MousePicking.cpp
@@ -0,0 +1,92 @@
+/*
+Copyright (c) 2013 Aerys
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include "MousePicking.hpp"
+
+#include "minko/scene/Node.hpp"
+#include "minko/scene/NodeSet.hpp"
+#include "minko/component/BoundingBox.hpp"
+#include "minko/math/AbstractShape.hpp"
+#include "minko/math/Ray.hpp"
+
+using namespace minko;
+using namespace minko::component;
+
+MousePicking::MousePicking() :
+	_move(MouseSignal::create()),
+	_over(MouseSignal::create()),
+	_out(MouseSignal::create()),
+	_rollOver(MouseSignal::create()),
+	_rollOut(MouseSignal::create()),
+	_leftButtonUp(MouseSignal::create()),
+	_leftButtonDown(MouseSignal::create()),
+	_previousRayOrigin(math::Vector3::create())
+{
+
+}
+
+void
+MousePicking::initialize()
+{
+	/*
+	_targetAddedSlot = targetAdded()->connect([&](AbstractComponent::Ptr cmp, scene::Node::Ptr target)
+	{
+
+	});
+
+	_targetRemovedSlot = targetRemoved()->connect([&](AbstractComponent::Ptr cmp, scene::Node::Ptr target)
+	{
+
+	});
+	*/
+}
+
+void
+MousePicking::pick(std::shared_ptr<math::Ray> ray)
+{
+	MousePicking::HitList hits;
+
+	auto descendants = scene::NodeSet::create(targets())
+		->descendants(true)
+		->where([&](scene::Node::Ptr node) { return node->hasComponent<BoundingBox>(); });
+	
+	std::unordered_map<scene::Node::Ptr, float> distance;
+
+	for (auto& descendant : descendants->nodes())
+		for (auto& box : descendant->components<BoundingBox>())
+		{
+			auto distance = 0.f;
+
+			if (box->shape()->cast(ray, distance))
+				hits.push_back(Hit(descendant, distance));
+		}
+
+	hits.sort([&](Hit& a, Hit& b) { return a.second < b.second; });
+
+	if (!hits.empty())
+	{
+		if (!_previousRayOrigin->equals(ray->origin()))
+		{
+			_move->execute(shared_from_this(), hits, ray);
+			_previousRayOrigin->copyFrom(ray->origin());
+		}
+
+		_over->execute(shared_from_this(), hits, ray);
+	}
+}
