38dc5b9ea297ffd706965354ed9863cbab59bb1e
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-11-19 13:49:28 +0100

    feature #2394: add the concept of binding "source", which can be "target" (default), "renderer" or "root"
    Former-commit-id: daa507ade719e60340a24b6a15cdfef0fbe52648
    Former-commit-id: 90d39fa436fca8c923bfe31814ccac089865f398
    Former-commit-id: fa8a73d34be99d26b0db41dc9a0f7fd428366cd2

--- framework/src/minko/Common.hpp
+++ framework/src/minko/Common.hpp
@@ -121,7 +121,16 @@ namespace minko
 		class ValueBase;
 		class Value;
 		class Container;
-        typedef std::unordered_map<std::string, std::string> BindingMap;
+        
+		enum class BindingSource
+		{
+			TARGET,
+			RENDERER,
+			ROOT
+		};
+
+		typedef std::pair<std::string, BindingSource> Binding;
+		typedef std::unordered_map<std::string, Binding> BindingMap;
 
 		enum class MacroBindingDefaultValueSemantic
 		{
@@ -142,7 +151,7 @@ namespace minko
 			MacroBindingDefaultValue value;
 		};
 
-		typedef std::tuple<std::string, MacroBindingDefault, int, int>	MacroBinding;
+		typedef std::tuple<std::string, BindingSource, MacroBindingDefault, int, int>	MacroBinding;
 
 		typedef std::unordered_map<std::string, MacroBinding> MacroBindingMap;
 	}
