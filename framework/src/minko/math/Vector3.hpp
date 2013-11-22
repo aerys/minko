6027b1555128cc23cffc07682e35d17db8b3cb4e
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-11-22 12:03:53 +0100

    fix typo: Vector3::substract is now known as Vector3::subtract added Vector3::scaleBy()
    Former-commit-id: ec912b166ab04da18058e2510506e6313cd772b1
    Former-commit-id: 9627dad4db89a4ff27ed6ff7e5150a68ff541529

--- framework/src/minko/math/Vector3.hpp
+++ framework/src/minko/math/Vector3.hpp
@@ -169,7 +169,7 @@ namespace minko
 
 			inline
 			Ptr
-			substract(Ptr v)
+			subtract(Ptr v)
 			{
 				return setTo(_x - v->_x, _y - v->_y, _z - v->_z);
 			}
@@ -305,7 +305,7 @@ namespace minko
 			float
 			lengthSquared() const
 			{
-				return _x*_x + _y*_y + _z*_z;
+				return _x * _x + _y * _y + _z * _z;
 			}
 
 			inline
@@ -313,6 +313,17 @@ namespace minko
 			length() const
 			{
 				return sqrtf(lengthSquared());
+			}
+
+			inline
+			Ptr
+			scaleBy(float scale)
+			{
+				_x *= scale;
+				_y *= scale;
+				_z *= scale;
+
+				return std::static_pointer_cast<Vector3>(shared_from_this());
 			}
 
 		protected:
