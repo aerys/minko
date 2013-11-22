b506f0675eedfe00e26f5c55bc38042f6656e39b
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-11-22 15:40:15 +0100

    use file::separator instead of "/" in Loader::load()
    Former-commit-id: 7e1831d873a58fe1b0f6c4ee40ba2ab830ecd8a8
    Former-commit-id: 9ae2d1a19a7ae7bcb4c9614d84a1e4f170337b5a

--- framework/src/minko/file/Loader.cpp
+++ framework/src/minko/file/Loader.cpp
@@ -45,7 +45,7 @@ Loader::load(const std::string& filename, std::shared_ptr<Options> options)
 	if (!file.is_open())
 		for (auto path : _options->includePaths())
 		{
-			auto testFilename = options->uriFunction()(path + "/" + filename);
+			auto testFilename = options->uriFunction()(path + file::separator + filename);
 
 			file.open(testFilename, flags);
 			if (file.is_open())
