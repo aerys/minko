0c630fd060849844171951ac73ef4f2d522f1ed3
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-11-24 19:11:42 +0100

    updated test.lua to make use of a (phony) script instance variable
    Former-commit-id: 15d340a878ee9c567dd509d2b18e1c08d08154cb
    Former-commit-id: dc7445b9bfaddc33ff41cc72e2cd2504501c70e9

--- examples/lua-scripts/asset/script/test.lua
+++ examples/lua-scripts/asset/script/test.lua
@@ -1,5 +1,19 @@
-test = {}
+test = {
+	counter = 0
+}
 
-function test.update()
-	print("update")
+function test:start(nodeName)
+	print("test.start")
+end
+
+function test:update(nodeName)
+	self.counter = self.counter + 1
+	if (self.counter >= 100) then
+		print("ok: " .. self.counter)
+		self.counter = 0
+	end
+end
+
+function test:stop(nodeName)
+	print("test.stop")
 end
