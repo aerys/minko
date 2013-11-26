fef5b52943531a10307c6a466fbade223b9e36cb
Pierre-Edouard Landes <pierreedouard.landes@aerys.in>
2013-11-26 17:59:10 +0100

    Reincorporated changes made to deleted ASSSIMPParser and started skinning effect all over again.
    Former-commit-id: bc0accf04814aa18f6d13f861b79c75b8479b7b5
    Former-commit-id: 58e0866b80c275dc441c6b0ba200f7eaca22593f

--- framework/effect/Skinning.vertex.glsl
+++ framework/effect/Skinning.vertex.glsl
@@ -4,25 +4,23 @@
 	precision mediump float;
 #endif
 
-attribute vec3 position;
-attribute vec2 uv;
+attribute	vec3	position;
+attribute	vec2 	uv;
 
-attribute vec4 boneIdsA;
-attribute vec4 boneIdsB;
-attribute vec4 boneWeightsA;
-attribute vec4 boneWeightsB;
+attribute	vec4	boneIdsA;
+attribute	vec4	boneIdsB;
+attribute	vec4	boneWeightsA;
+attribute	vec4	boneWeightsB;
 
-uniform mat4 modelToWorldMatrix;
-uniform mat4 worldToScreenMatrix;
+uniform		mat4	modelToWorldMatrix;
+uniform		mat4	worldToScreenMatrix;
 
-varying vec2 vertexUV;
+varying 	vec2 	vertexUV;
 
-#ifdef IS_MODEL_SKINNED
-varying vec4 interpBoneIdsA;
-varying vec4 interpBoneIdsB;
-varying vec4 interpBoneWeightsA;
-varying vec4 interpBoneWeightsB;
-#endif // IS_MODEL_SKINNED
+varying 	vec4 	interpBoneIdsA;
+//varying vec4 interpBoneIdsB;
+//varying vec4 interpBoneWeightsA;
+//varying vec4 interpBoneWeightsB;
 
 void main(void)
 {
@@ -38,13 +36,13 @@ void main(void)
 
 	gl_Position =  worldToScreenMatrix * pos;
 	
-	#ifdef IS_MODEL_SKINNED
-		// for bebugging purposes only
+	#ifdef IS_SKINNED
+		// for debugging purposes only
 		interpBoneIdsA = boneIdsA;
-		interpBoneIdsB = boneIdsB;
-		interpBoneWeightsA = boneWeightsA;
-		interpBoneWeightsB = boneWeightsB;
-	#endif // IS_MODEL_SKINNED
+		//interpBoneIdsB = boneIdsB;
+		//interpBoneWeightsA = boneWeightsA;
+		//interpBoneWeightsB = boneWeightsB;
+	#endif // IS_SKINNED
 }
 
 #endif // VERTEX_SHADER
