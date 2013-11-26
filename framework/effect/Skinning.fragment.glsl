c7bfaf25eb485312cee50e9207a56675574bcf99
Pierre-Edouard Landes <pierreedouard.landes@aerys.in>
2013-11-26 17:59:10 +0100

    Reincorporated changes made to deleted ASSSIMPParser and started skinning effect all over again.
    Former-commit-id: bc0accf04814aa18f6d13f861b79c75b8479b7b5
    Former-commit-id: c909a3791bc04bd51881d7076bcbc24840b7b3a0

--- framework/effect/Skinning.fragment.glsl
+++ framework/effect/Skinning.fragment.glsl
@@ -7,22 +7,22 @@
 uniform vec4        diffuseColor;
 uniform sampler2D   diffuseMap;
 
-varying vec2 vertexUV;
+varying vec2 		vertexUV;
 
-#ifdef IS_MODEL_SKINNED
-varying vec4 interpBoneIdsA;
-varying vec4 interpBoneIdsB;
-varying vec4 interpBoneWeightsA;
-varying vec4 interpBoneWeightsB;
-#endif // IS_MODEL_SKINNED
+varying vec4 		interpBoneIdsA;
+varying vec4 		interpBoneIdsB;
+varying vec4 		interpBoneWeightsA;
+varying vec4		interpBoneWeightsB;
 
 void main(void)
 {
-#ifdef IS_MODEL_SKINNED
-	gl_FragColor = vec4(interpBoneWeightsA.x * interpBoneIdsA.x / 8.0, interpBoneWeightsA.y * interpBoneIdsA.y / 8.0, interpBoneWeightsA.z * interpBoneIdsA.z / 8.0, 1.0);
+#ifdef IS_SKINNED
+	//gl_FragColor = vec4(0.2, 0.8, 0.6, 1.0);
+	gl_FragColor = vec4(interpBoneIdsA.x / 8.0, interpBoneIdsA.y / 8.0, interpBoneIdsA.z / 8.0, 1.0);
+//	gl_FragColor = vec4(interpBoneWeightsA.x * interpBoneIdsA.x / 8.0, interpBoneWeightsA.y * interpBoneIdsA.y / 8.0, interpBoneWeightsA.z * interpBoneIdsA.z / 8.0, 1.0);
 #else
 	gl_FragColor = vec4(0.8, 0.2, 0.2, 1.0);
-#endif // IS_MODEL_SKINNED
+#endif // IS_SKINNED
 	return;
 
 	#ifdef DIFFUSE_MAP
