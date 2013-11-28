d84f498451b43fbffdad2c3b44554292445290ad
Pierre-Edouard Landes <pierreedouard.landes@aerys.in>
2013-11-28 14:36:38 +0100

    Added missing mouse-related classes.
    Former-commit-id: f912f7928469ab700ae038e000c78f5b811f41d2 [formerly 1a2c116afcc957b7e6285f63e73c4e6513fc9da0]
    Former-commit-id: a81e3ea578f339e6a744ebb15113ae923488aaa3 [formerly 0b759c3255ba8272a0debd80b14fc616470dae9d]
    Former-commit-id: 891c0fbafd237c9cb3f82e988a8c9e99f795499b

---
+++ framework/src/minko/component/MouseManager.cpp
@@ -0,0 +1,134 @@
+/*
+Copyright (c) 2013 Aerys
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include "MouseManager.hpp"
+
+#include "minko/scene/Node.hpp"
+#include "minko/scene/NodeSet.hpp"
+#include "minko/component/BoundingBox.hpp"
+#include "minko/math/AbstractShape.hpp"
+#include "minko/math/Box.hpp"
+#include "minko/math/Ray.hpp"
+#include "minko/component/MousePicking.hpp"
+#include "minko/component/Surface.hpp"
+#include "minko/geometry/Geometry.hpp"
+#include "minko/component/Transform.hpp"
+
+using namespace minko;
+using namespace minko::component;
+
+MouseManager::MouseManager() :
+	_ray(math::Ray::create()),
+	_previousRayOrigin(math::Vector3::create()),
+	_lastItemUnderCursor(nullptr)
+{
+
+}
+
+void
+MouseManager::initialize()
+{
+	if (_mouse)
+	{
+		_targetAddedSlot = targetAdded()->connect([&](AbstractComponent::Ptr cmp, scene::Node::Ptr target)
+		{
+			_mouseMoveSlot = _mouse->move()->connect([&](MousePtr m, int dx, int dy)
+			{
+				// FIXME: should unproject from properties stored in data()
+				auto cam = targets()[0]->component<PerspectiveCamera>();
+
+				if (cam)
+					pick(cam->unproject(m->normalizedX(), m->normalizedY(), _ray));
+			});
+			_mouseLeftButtonDownSlot = _mouse->leftButtonDown()->connect([&](MousePtr m)
+			{
+				// FIXME
+			});
+
+		});
+
+		_targetRemovedSlot = targetRemoved()->connect([&](AbstractComponent::Ptr cmp, scene::Node::Ptr node)
+		{
+			_mouseMoveSlot = nullptr;
+			_mouseLeftButtonDownSlot = nullptr;
+		});
+	}
+}
+
+void
+MouseManager::pick(std::shared_ptr<math::Ray> ray)
+{
+	MouseManager::HitList hits;
+
+	auto target = targets()[0];
+	auto descendants = scene::NodeSet::create(target->root())
+		->descendants(true)
+		->where([&](scene::Node::Ptr node) { return node->hasComponent<BoundingBox>(); });
+
+	std::unordered_map<scene::Node::Ptr, float> distance;
+	math::Ray::Ptr localRay = math::Ray::create();
+
+	for (auto& descendant : descendants->nodes())
+	{
+		auto bboxHit = false;
+		auto distance = 0.f;
+
+		if (descendant->component<BoundingBox>()->box()->cast(ray, distance))
+		{
+			hits.push_back(Hit(descendant, distance));
+
+			/*
+			auto transform = descendant->component<Transform>();
+			uint triangleId = 0;
+
+			if (transform)
+			{
+				transform->worldToModel(ray->origin(), localRay->origin());
+				transform->deltaWorldToModel(ray->direction(), localRay->direction());
+			}
+
+			for (auto& surface : descendant->components<Surface>())
+			{
+				if (surface->geometry()->cast(transform ? localRay : ray, distance, triangleId))
+				{
+					hits.push_back(Hit(descendant, distance));
+				}
+			}
+			*/
+		}
+	}
+
+	hits.sort([&](Hit& a, Hit& b) { return a.second < b.second; });
+
+	if (!hits.empty())
+	{
+		auto mp = hits.front().first->component<MousePicking>();
+
+		if (!_previousRayOrigin->equals(ray->origin()))
+		{
+			//_move->execute(shared_from_this(), hits, ray);
+			if (mp)
+				mp->move()->execute(mp, hits, ray);
+
+			_previousRayOrigin->copyFrom(ray->origin());
+		}
+
+		//_over->execute(shared_from_this(), hits, ray);
+	}
+}
