87553a81749503e6de5617fc38827a81b2bbe10a
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-12-03 21:02:14 +0100

    moved LuaGlue init. into LuaScript::initialize()
    Former-commit-id: a084b205cfd606eb06e110e9e26d496e71939326
    Former-commit-id: 304fc604db92a91c808e7038bd3a075a806e3167

--- plugins/lua/src/minko/component/LuaScript.cpp
+++ plugins/lua/src/minko/component/LuaScript.cpp
@@ -19,16 +19,70 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 
 #include "LuaScript.hpp"
 
-#include "minko/scene/Node.hpp"
-
 #include "LuaGlue/LuaGlue.h"
+
+#include "minko/scene/Node.hpp"
+#include "minko/math/Vector2.hpp"
+#include "minko/math/Vector3.hpp"
+#include "minko/math/Vector4.hpp"
+#include "minko/math/Matrix4x4.hpp"
+#include "minko/data/Container.hpp"
 
 using namespace minko;
 using namespace minko::component;
 
+LuaGlue LuaScript::_state;
+bool LuaScript::_initialized = false;
+
+LuaScript::LuaScript(const std::string& name) :
+    _scriptName(name)
+{
+
+}
+
+void
+LuaScript::initialize()
+{
+    AbstractScript::initialize();
+
+    if (!_initialized)
+        initializeLuaBindings();
+
+    auto name = _scriptName.c_str();
+
+    _state.Class<LuaStub>(name);
+    _state.open().glue();
+
+    _class = _state.lookupClass(name);
+}
+
+void
+LuaScript::loadScript(const std::string& script)
+{
+    if (luaL_loadstring(_state.state(), script.c_str()))
+    {
+        auto error = lua_tostring(_state.state(), -1);
+        
+        std::cerr << error << std::endl;
+        throw std::runtime_error(error);
+    }
+    if (lua_pcall(_state.state(), 0, 0, 0))
+    {
+        auto error = lua_tostring(_state.state(), -1);
+
+        std::cerr << error << std::endl;
+        throw std::runtime_error(error);
+    }
+}
+
 void
 LuaScript::runScriptMethod(const std::string& methodName, scene::Node::Ptr target)
 {
+    //std::cout << "lua: " << methodName << ", " << target.get() << std::endl;
+    dynamic_cast<LuaGlueClass<LuaScript::LuaStub>*>(_class)->invokeVoidMethod(methodName.c_str(), &_stub, target);
+    //class->invokeVoidMethod(methodName.c_str(), &_stub, target);
+
+    /*
     lua_getglobal(_state.state(), _scriptName.c_str());
     auto index = lua_gettop(_state.state());
     lua_getfield(_state.state(), index, methodName.c_str());
@@ -39,6 +93,7 @@ LuaScript::runScriptMethod(const std::string& methodName, scene::Node::Ptr targe
         if (lua_pcall(_state.state(), 2, 0, 0) != 0)
             std::cerr << "error running function '" << methodName << "': " << lua_tostring(_state.state(), -1) << std::endl;
     }
+    */
 }
 
 void
@@ -57,4 +112,74 @@ void
 LuaScript::stop(scene::Node::Ptr node)
 {
     runScriptMethod("stop", node);
+}
+
+
+void
+LuaScript::initializeLuaBindings()
+{
+    _initialized = true;
+
+    _state
+        .Class<math::Vector2>("Vector2")
+            .method("getX", static_cast<float (math::Vector2::*)(void)>(&math::Vector2::x))
+            .method("setX", static_cast<void (math::Vector2::*)(float)>(&math::Vector2::x))
+            .method("getY", static_cast<float (math::Vector2::*)(void)>(&math::Vector2::y))
+            .method("setY", static_cast<void (math::Vector2::*)(float)>(&math::Vector2::y))
+            .method("toString", &math::Vector2::toString)
+        .end()
+        .Class<math::Vector3>("Vector3")
+            .method("getX", static_cast<float (math::Vector3::*)(void)>(&math::Vector3::x))
+            .method("setX", static_cast<void (math::Vector3::*)(float)>(&math::Vector3::x))
+            .method("getY", static_cast<float (math::Vector3::*)(void)>(&math::Vector3::y))
+            .method("setY", static_cast<void (math::Vector3::*)(float)>(&math::Vector3::y))
+            .method("getZ", static_cast<float (math::Vector3::*)(void)>(&math::Vector3::z))
+            .method("setZ", static_cast<void (math::Vector3::*)(float)>(&math::Vector3::z))
+            .method("toString", &math::Vector3::toString)
+        .end()
+        .Class<math::Vector4>("Vector4")
+            .method("getX", static_cast<float (math::Vector4::*)(void)>(&math::Vector4::x))
+            .method("setX", static_cast<void (math::Vector4::*)(float)>(&math::Vector4::x))
+            .method("getY", static_cast<float (math::Vector4::*)(void)>(&math::Vector4::y))
+            .method("setY", static_cast<void (math::Vector4::*)(float)>(&math::Vector4::y))
+            .method("getZ", static_cast<float (math::Vector4::*)(void)>(&math::Vector4::z))
+            .method("setZ", static_cast<void (math::Vector4::*)(float)>(&math::Vector4::z))
+            .method("getW", static_cast<float (math::Vector4::*)(void)>(&math::Vector4::w))
+            .method("setW", static_cast<void (math::Vector4::*)(float)>(&math::Vector4::w))
+            .method("toString", &math::Vector4::toString)
+        .end()
+        .Class<math::Matrix4x4>("Matrix4x4")
+            .method("appendRotationX",      &math::Matrix4x4::appendRotationX)
+            .method("appendRotationY",      &math::Matrix4x4::appendRotationY)
+            .method("appendRotationZ",      &math::Matrix4x4::appendRotationZ)
+            .method("appendRotation",       &math::Matrix4x4::appendRotation)
+            .method("appendTranslation",    static_cast<math::Matrix4x4::Ptr (math::Matrix4x4::*)(float, float, float)>(&math::Matrix4x4::appendTranslation))
+            .method("appendTranslation",    static_cast<math::Matrix4x4::Ptr(math::Matrix4x4::*)(math::Vector3::Ptr)>(&math::Matrix4x4::appendTranslation))
+            .method("prependRotationX",     &math::Matrix4x4::prependRotationX)
+            .method("prependRotationY",     &math::Matrix4x4::prependRotationY)
+            .method("prependRotationZ",     &math::Matrix4x4::prependRotationZ)
+            .method("prependRotation",      &math::Matrix4x4::prependRotation)
+            .method("prependTranslation",   static_cast<math::Matrix4x4::Ptr(math::Matrix4x4::*)(float, float, float)>(&math::Matrix4x4::prependTranslation))
+            .method("prependTranslation",   static_cast<math::Matrix4x4::Ptr(math::Matrix4x4::*)(math::Vector3::Ptr)>(&math::Matrix4x4::prependTranslation))
+        .end()
+        .Class<data::Container>("Container")
+            //.method("create",       &data::Container::create)
+            .method("hasProperty",  &data::Container::hasProperty)
+            .method("getFloat",     &data::Container::get<float>)
+            .method("getInt",       &data::Container::get<int>)
+            .method("getUint",      &data::Container::get<unsigned int>)
+            .method("getMatrix4x4", &data::Container::get<math::Matrix4x4::Ptr>)
+        .end()
+        .Class<scene::Node>("Node")
+            .method("getName",          static_cast<const std::string& (scene::Node::*)(void)>(&scene::Node::name))
+            .method("setName",          static_cast<void (scene::Node::*)(const std::string&)>(&scene::Node::name))
+            .method("getData",          &scene::Node::data)
+            .method("addChild",         &scene::Node::addChild)
+            .method("removeChild",      &scene::Node::removeChild)
+            .method("contains",         &scene::Node::contains)
+            .method("addComponent",     &scene::Node::addComponent)
+            .method("removeComponent",  &scene::Node::removeComponent)
+        .end()
+        .open()
+        .glue();
 }
