99849959d22a8d661ea3faa2dc9d9e58458e539f
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-12-03 20:56:50 +0100

    move application related build options into the minko.project.application function
    Former-commit-id: e1d000a4283b82fd2aa618f7fff7dd0d35f25928
    Former-commit-id: f9636c58c1624278b43029985af6998c8eed479c
    Former-commit-id: 844face94e01371c383fa06c3bcddd12fb34189d

--- tools/all/lib/minko.project.lua
+++ tools/all/lib/minko.project.lua
@@ -6,25 +6,13 @@ minko.project.library = function(name)
 	includedirs { minko.sdk.path("/framework/src") }
 	
 	configuration { "windows" }
-		links { "OpenGL32", "glew32" }
-		libdirs { minko.sdk.path("/deps/win/lib") }
 		includedirs { minko.sdk.path("/deps/win/include") }
 		
 	configuration { "macosx" }
-		libdirs { "/deps/mac/lib" }
 		includedirs { minko.sdk.path("/deps/mac/include") }
 		buildoptions { "-std=c++11 -stdlib=libc++" }
-		links {
-			"m",
-			"SDL2.framework",
-			"Cocoa.framework",
-			"OpenGL.framework",
-			"IOKit.framework"
-		}
 	
 	configuration { "linux" }
-		links { "GL", "SDL2", "m", "Xrandr", "Xxf86vm", "Xi", "rt", "X11", "pthread" }
-		libdirs { minko.sdk.path("/deps/lin/lib") }
 		includedirs { minko.sdk.path("/deps/lin/include") }
 		buildoptions { "-std=c++11" }		
 		
@@ -55,6 +43,11 @@ minko.project.application = function(name)
 		targetdir "bin/release"
 	
 	configuration { "windows" }
+		libdirs { minko.sdk.path("/deps/win/lib") }
+		links {
+			"OpenGL32",
+			"glew32"
+		}
 		postbuildcommands {
 			'xcopy /y /i "' .. minko.sdk.path('/framework/effect') .. '" "$(TargetDir)\\effect"',
 			'xcopy /y /s asset\\* "$(TargetDir)"',
@@ -62,13 +55,32 @@ minko.project.application = function(name)
 		}
 		
 	configuration { "linux" }
+		libdirs { minko.sdk.path("/deps/lin/lib") }
+		links {
+			"GL",
+			"m",
+			"Xrandr",
+			"Xxf86vm",
+			"Xi",
+			"rt",
+			"X11",
+			"pthread"
+		}
 		postbuildcommands {
 			'cp -r ' .. minko.sdk.path('/framework/effect') .. ' ${TARGETDIR} || :',
 			'cp -r asset/* ${TARGETDIR} || :'
 		}
 	
 	configuration { "macosx" }
+		libdirs { "/deps/mac/lib" }
 		linkoptions { "-stdlib=libc++" }
+		links {
+			"m",
+			"SDL2.framework",
+			"Cocoa.framework",
+			"OpenGL.framework",
+			"IOKit.framework"
+		}
 		postbuildcommands {
 			'cp -r ' .. minko.sdk.path('/framework/effect') .. ' . || :',
 			'cp -r asset/* . || :'
