c2b2845fb5842a6f0fd658b2a5d5bb9a5cbcf492
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-12-03 21:12:59 +0100

    create a separate solution for examples
    Former-commit-id: 1a97b29d5a87f752fab6c1205282864d269c957b
    Former-commit-id: 57b5abe0d371d8699e3df577ffd15aa3ff44f8d6

--- premake4.lua
+++ premake4.lua
@@ -8,27 +8,17 @@ solution "minko"
 	-- plugins
 	include 'plugins/jpeg'
 	include 'plugins/png'
-	-- include 'plugins/mk'
-	-- include 'plugins/bullet'
-	-- include 'plugins/particles'
+	include 'plugins/mk'
+	include 'plugins/bullet'
+	include 'plugins/particles'
 	include 'plugins/sdl'
 	include 'plugins/angle'
-	-- include 'plugins/fx'
+	include 'plugins/fx'
 	include 'plugins/assimp'
-	--include 'plugins/offscreen'
+	include 'plugins/offscreen'
 	if _OPTIONS["platform"] == "emscripten" then
 		include 'plugins/webgl'
 	end
-
-	-- examples
-	include 'examples/sponza'
-	--include 'examples/cube'
-	include 'examples/light'
-	include 'examples/stencil'
-	include 'examples/effect-config'
-	--include 'examples/raycasting'
-	include 'examples/assimp'
-	--include 'examples/cube-offscreen'
 
 	-- tests
 	if _ACTION ~= "vs2010" and _OPTIONS["platform"] ~= "emscripten" then
