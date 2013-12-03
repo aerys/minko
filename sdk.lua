bc0d1cd6d5e277d0966af45f3c4a6efa86a64c86
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-12-03 20:53:41 +0100

    add the "dist" action to generate a platform-specific redistributable SDK
    Former-commit-id: ee49febfd8d674d59fd72b32a58de3fbf3608314
    Former-commit-id: ab246fc50e11eeca998533cb3a632a2578a5cdee

--- sdk.lua
+++ sdk.lua
@@ -71,3 +71,64 @@ if _OPTIONS["no-stencil"] then
 	defines { "MINKO_NO_STENCIL" }
 end
 
+-- actions
+newaction {
+	trigger		= "dist",
+	description	= "Generate the distributable version of the Minko SDK.",
+	execute		= function()
+	
+		os.rmdir('dist')
+		
+		os.mkdir('dist/framework')
+		os.mkdir('dist/framework/bin/release')
+		os.mkdir('dist/framework/bin/debug')
+		os.mkdir('dist/framework/include')
+		os.mkdir('dist/plugin')
+		
+		-- skeleton
+		os.mkdir('dist/skeleton')
+		minko.os.copyfiles('skeleton', 'dist/skeleton')
+		
+		-- docs
+		os.mkdir('dist/doc')
+		minko.os.copyfiles('doc/html', 'dist/doc')
+		
+		-- tools
+		os.mkdir('dist/tool/' .. os.get())
+		
+		-- deps
+		os.mkdir('dist/deps')
+		minko.os.copyfiles('deps', 'dist/deps')
+		
+		for i, v in ipairs(solution()['projects']) do
+			print(v['basedir'])
+
+			-- plugins
+			if string.startswith(v['name'], "plugin-") then
+				local dir = 'dist/plugin/' .. path.getbasename(v['basedir'])
+				local binDir = dir .. '/bin'
+				
+				os.mkdir(dir)
+				os.copyfile(v['basedir'] .. '/plugin.lua', dir .. '/plugin.lua')
+				
+				-- bin
+				os.mkdir(binDir .. '/debug/' .. os.get())
+				os.mkdir(binDir .. '/release/' .. os.get())
+				minko.os.copyfiles(v['basedir'] .. '/bin/debug', binDir .. '/debug/' .. os.get())
+				minko.os.copyfiles(v['basedir'] .. '/bin/release', binDir .. '/release/' .. os.get())
+				
+				-- includes
+				if os.isdir(v['basedir'] .. '/include') then
+					os.mkdir(dir .. '/include')
+					minko.os.copyfiles(v['basedir'] .. '/include', dir .. '/include')
+				end
+				
+				-- assets
+				if os.isdir(v['basedir'] .. '/asset') then
+					os.mkdir(dir .. '/asset')
+					minko.os.copyfiles(v['basedir'] .. '/asset', dir .. '/asset')
+				end
+			end
+		end
+	end
+}
