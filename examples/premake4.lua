a607dd0924cfb79f6fee67656148b30177992bdc
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-12-03 21:12:59 +0100

    create a separate solution for examples
    Former-commit-id: 1a97b29d5a87f752fab6c1205282864d269c957b
    Former-commit-id: 38524bb0691ee3ffafcd2f17166c8c05edee2c0b

---
+++ examples/premake4.lua
@@ -0,0 +1,11 @@
+dofile("../sdk.lua")
+
+minko.project.solution("minko-examples")
+	include('assimp')
+	include('cube')
+	include('effect-config')
+	include('light')
+	include('raycasting')
+	include('sponza')
+	include('stencil')
+	
