ee011050a1a5ab0f3a29c7284c285fcf01293c2f
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-12-04 01:14:08 +0100

    move core framework header public interface files into /framework/include
    Former-commit-id: 979854c0cd7a3178b8686f9ae935fc1940a5925c

--- framework/src/minko/file/Options.cpp
+++ framework/src/minko/file/Options.cpp
@@ -17,7 +17,7 @@ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-#include "Options.hpp"
+#include "minko/file/Options.hpp"
 
 #include "minko/material/Material.hpp"
 #include "minko/file/Loader.hpp"
