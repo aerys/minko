e1763baf8bfcc94709314b578f2c6117a2209698
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-12-05 16:01:02 +0100

    fix windows-only premake batch scripts to generate the examples solution too
    Former-commit-id: 3696363c6907a1309ec853f72b42328bc49aff10

--- tools/win/scripts/premake_vs2010_ANGLE.bat
+++ tools/win/scripts/premake_vs2010_ANGLE.bat
@@ -1,6 +1,12 @@
 call premake_clean.bat
 call premake_templates.bat
+
 chdir ..\..\..
 tools\win\bin\premake4.exe --os=windows --platform=x32 --no-tests --with-angle vs2010
 cd tools\win\scripts
+
+chdir ..\..\..\examples
+tools\win\bin\premake4.exe --os=windows --platform=x32 --with-angle vs2010
+cd tools\win\scripts
+
 timeout /T 30
