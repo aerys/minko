5e97e84330150a82cf92808d6c27b4a63610e174
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-12-06 19:25:58 +0100

    add the minko.plugin.links() function to link a plugin sources or its static library equivalent depending on whether we're working with a distributable SDK or not
    Former-commit-id: 9002dae1204e0c82741854dba93aeec331a0bed4

--- tools/all/lib/minko.plugin.lua
+++ tools/all/lib/minko.plugin.lua
@@ -1,19 +1,23 @@
-minko.plugin = {}
+minko.plugin = {
+	_enabled = {}
+}
 
 minko.plugin.import = function(name)
-	include(minko.sdk.path("/plugins/" .. name))
+	if not MINKO_SDK_DIST then
+		include(minko.sdk.path("/plugins/" .. name))
+	end
 end
 
 minko.plugin.enable = function(name)
 	local projectName = project()["name"]
 	local terms = configuration()["terms"]
 
+	minko.plugin._enabled[name] = true
+
 	minko.plugin.import(name)
 	
 	project(projectName)
 	configuration { unpack(terms) }
-	
-	_OPTIONS["with-" .. name] = "true"
 	
 	dofile(minko.sdk.path("/plugins/" .. name .."/plugin.lua"))
 
@@ -21,5 +25,30 @@ minko.plugin.enable = function(name)
 end
 
 minko.plugin.enabled = function(name)
-	return _OPTIONS["with-" .. name] ~= nil
+	return minko.plugin._enabled[name] or _OPTIONS["with-" .. name] ~= nil
+end
+
+minko.plugin.links = function(names)
+
+	local terms = configuration()["terms"]
+
+	configuration {}
+
+	for _, name in ipairs(names) do
+
+		local projectName = "plugin-" .. name
+
+		if MINKO_SDK_DIST then
+			configuration { 'debug' }
+				links { minko.sdk.path('plugins/' .. name .. '/bin/debug/' .. minko.sdk.gettargetplatform() .. '/' .. projectName) }
+			configuration { 'release' }
+				links { minko.sdk.path('plugins/' .. name .. '/bin/release/' .. minko.sdk.gettargetplatform() .. '/' .. projectName) }
+		else
+			links { "plugin-" .. name }
+		end
+
+	end
+
+	configuration { unpack(terms) }
+
 end
