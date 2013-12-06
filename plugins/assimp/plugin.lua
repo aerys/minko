2f4b463ff0a9fccd6d5211e0bb39ca75e3b2f347
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-12-06 19:33:59 +0100

    update plugin.lua files to use minko.plugin.links() instead of premake's links()
    Former-commit-id: 4a22953a72be22a32b1e65616aa3f8c94172be3a
    Former-commit-id: b7621584b67ba1418655b6c061bcc88e6087542a

--- plugins/assimp/plugin.lua
+++ plugins/assimp/plugin.lua
@@ -1,6 +1,6 @@
 -- assimp plugin
 if minko.plugin.enabled("assimp") then
-	links { "plugin-assimp" }
+	minko.plugin.links { "assimp" }
 	includedirs { minko.sdk.path("plugins/assimp/include") }
 	defines { "MINKO_PLUGIN_ASSIMP" }
 end
--- plugins/webgl/plugin.lua
+++ plugins/webgl/plugin.lua
@@ -2,6 +2,6 @@
 if minko.plugin.enabled("webgl") then
 	defines { "MINKO_PLUGIN_WEBGL" }
 	configuration { "emscripten" }
-		links { "plugin-webgl" }
+		minko.plugin.links { "webgl" }
 		includedirs { minko.sdk.path("plugins/webgl/include") }
 end
