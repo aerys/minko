8a873f7e87f855edeff68fded2cfcdd028719e15
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-12-07 18:47:47 +0100

    update premake_vs2010.bat script to use --no-tests because they won't work in VS
    Former-commit-id: d166b26654c8afc2edbcf572d5f9cb8c4b929a75
    Former-commit-id: ee3b928d2f471e48b3a4ad93cb11cde752d438d3

--- tools/win/scripts/premake_vs2010.bat
+++ tools/win/scripts/premake_vs2010.bat
@@ -1,6 +1,6 @@
 call premake_clean.bat
 call premake_templates.bat
 chdir ..\..\..
-tools\win\bin\premake4.exe --os=windows --platform=x32 vs2010
+tools\win\bin\premake4.exe --os=windows --platform=x32 --no-tests vs2010
 cd tools\win\scripts
 timeout /T 30
