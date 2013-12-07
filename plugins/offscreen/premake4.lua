90bdc32afbed01ddfdaa2a65646ce740adfbc3ab
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-12-07 19:43:31 +0100

    add "include/**.hpp" to project files wherever its relevant
    Former-commit-id: ceda55c9e44ab37d342df351cfb878b0ddee0ab7 [formerly 3493f1b914b8b6665049860c5e0234393d634ae1]
    Former-commit-id: df75ab95fd09c2a69631da76ad01488308b4773c [formerly 85782072675047180867633a0f9960572e2d093f]
    Former-commit-id: b40324acee1cfdce71ac42d170f4fac9002d63df

--- plugins/offscreen/premake4.lua
+++ plugins/offscreen/premake4.lua
@@ -8,7 +8,7 @@ PROJECT_NAME = path.getname(os.getcwd())
 minko.project.library("minko-plugin-" .. PROJECT_NAME)
 	kind "StaticLib"
 	language "C++"
-	files { "**.hpp", "**.h", "**.cpp", "**.c" }
+	files { "**.hpp", "**.h", "**.cpp", "**.c", "include/**.hpp" }
 	includedirs { "include", "src", "lib/osmesa/include" }
 	
 	configuration { "debug"}
