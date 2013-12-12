c9f5855e7ef4f4a1760ba95fb524dcda56642caa
Pierre-Edouard Landes <pierreedouard.landes@aerys.in>
2013-12-12 10:48:10 +0100

    Made small changes to compile under vs 2012.
    Former-commit-id: 46adefd75dda1247b90b2ba7405741bb9b8e9e48
    Former-commit-id: e221cbb6bbd99795c607a2ebfd2709831188ad07

--- plugins/oculus/asset/effect/OculusVR/OculusVR.fragment.glsl
+++ plugins/oculus/asset/effect/OculusVR/OculusVR.fragment.glsl
@@ -8,17 +8,28 @@ uniform vec2 uScreenCenter;
 uniform float uOffset;
 uniform vec2 uScaleIn;
 uniform vec2 uScale;
-uniform vec4 uHmdWarpParam;
+//uniform vec4 uHmdWarpParam;
+uniform vec2 uDistortionK01;
+uniform vec2 uDistortionK23;
 
 varying vec2 vUv;
 
 vec2 HmdWarp(vec2 in01, vec2 LensCenter)
 {
-   vec2 theta = (in01 - LensCenter) * uScaleIn; // Scales to [-1, 1]
-   float rSq = theta.x * theta.x + theta.y * theta.y;
-   vec2 rvector = theta * (uHmdWarpParam.x + uHmdWarpParam.y * rSq +
-	  uHmdWarpParam.z * rSq * rSq +
-	  uHmdWarpParam.w * rSq * rSq * rSq);
+   vec2		theta			= (in01 - LensCenter) * uScaleIn; // Scales to [-1, 1]
+   
+   float 	rSq				= theta.x * theta.x + theta.y * theta.y;
+   float	distortionK0	= uDistortionK01.x; // uHmdWarpParam.x
+   float	distortionK1	= uDistortionK01.y; // uHmdWarpParam.y
+   float	distortionK2	= uDistortionK23.x; // uHmdWarpParam.z
+   float	distortionK3	= uDistortionK23.y; // uHmdWarpParam.w
+   
+   vec2 rvector = theta * (
+		distortionK0 + 
+		distortionK1 * rSq +
+		distortionK2 * rSq * rSq +
+		distortionK3 * rSq * rSq * rSq
+	);
 	  
    return LensCenter + uScale * rvector;
 }
