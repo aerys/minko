da68eaf4b2b9c5b384ebf8e98b68ea35f1b78798
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-12-15 19:15:52 +0100

    moved interface hpp files into /include
    Former-commit-id: f3b914a57d1d2585c6781f5eef3d181e3e172edb
    Former-commit-id: 7af88b979ac558ab957f8e4ef2f7af67db8e1c29
    Former-commit-id: 5f14513d815892b9ef478ff5fdbf36908205ee9f

--- plugins/lua/src/minko/file/LuaScriptParser.cpp
+++ plugins/lua/src/minko/file/LuaScriptParser.cpp
@@ -17,7 +17,7 @@ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-#include "LuaScriptParser.hpp"
+#include "minko/file/LuaScriptParser.hpp"
 
 #include "minko/component/LuaScript.hpp"
 #include "minko/file/AssetLibrary.hpp"
