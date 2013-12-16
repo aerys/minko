08177541ecee1cb4d7f32061fce9ecc575a398b0
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-12-17 00:20:33 +0100

    fix lua's plugin config incluedirs directory
    Former-commit-id: 4a472863a024d5ff61298d7ce3a6110662eadf27
    Former-commit-id: e3aea5be7e7230cf7632ef0f65267395f0c140c3

--- plugins/lua/plugin.lua
+++ plugins/lua/plugin.lua
@@ -1,6 +1,6 @@
 -- lua plugin
 if minko.plugin.enabled("lua") then
 	links { "plugin-lua" }
-	includedirs { minko.sdk.path("plugins/lua/src") }
+	includedirs { minko.sdk.path("plugins/lua/include") }
 	defines { "MINKO_PLUGIN_LUA" }
 end
