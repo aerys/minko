8d5eede7b75a23b1b205fde2c3ec6c5f9ecea826
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-12-17 00:21:16 +0100

    the 'lua-scripts' example is now 100% Lua example
    Former-commit-id: b80257ce261e3df94c41cfb3ff2cb6dd631089c7
    Former-commit-id: e74b2eb4c441315328d6bce6f011d842fb7fd52b
    Former-commit-id: fe4e0d896db3afc8457d9d3086d5d82107d8d94d

--- examples/lua-scripts/asset/script/rotate.lua
+++ examples/lua-scripts/asset/script/rotate.lua
@@ -17,6 +17,44 @@ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ]]--
 
+function rotate:start(node)
+	self.oldX = self.mouse.x
+	self.oldY = self.mouse.y
+	self.rotationSpeedX = 0.
+	self.rotationSpeedY = 0.
+	self.distance = 3.
+	self.lookAt = Vector3.create(0., 0., 0.)
+	self.up = Vector3.create(0., 1., 0.)
+	self.yaw = 0.
+	self.pitch = math.pi * .5
+	self.minPitch = 0.00001
+	self.maxPitch = math.pi - self.minPitch
+end
+
 function rotate:update(node)
-	node:getData():getMatrix4x4("transform.modelToWorldMatrix"):appendRotationY(0.2)
+	local x = self.mouse.x
+	local y = self.mouse.y
+	if self.mouse.leftButtonIsDown then
+		self.rotationSpeedX = self.rotationSpeedX + (x - self.oldX) / 1000
+		self.rotationSpeedY = self.rotationSpeedY + (y - self.oldY) / 1000
+	end
+	self.oldX = x
+	self.oldY = y
+
+	self.yaw = self.yaw + self.rotationSpeedX
+	self.rotationSpeedX = self.rotationSpeedX * .9
+
+	self.pitch = self.pitch - self.rotationSpeedY
+	self.pitch = math.min(math.max(self.pitch, self.minPitch), self.maxPitch)
+	self.rotationSpeedY = self.rotationSpeedY * .9
+
+	self:getModelToWorldMatrix(node):lookAt(
+		self.lookAt,
+		Vector3.create(
+			self.lookAt.x + self.distance * math.cos(self.yaw) * math.sin(self.pitch),
+			self.lookAt.y + self.distance * math.cos(self.pitch),
+			self.lookAt.z + self.distance * math.sin(self.yaw) * math.sin(self.pitch)
+		),
+		self.up
+	)
 end
