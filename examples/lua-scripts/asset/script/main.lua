b32fc292ee9acdf51a12bc8ec1f9a83c800a3f6e
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-12-17 00:21:16 +0100

    the 'lua-scripts' example is now 100% Lua example
    Former-commit-id: b80257ce261e3df94c41cfb3ff2cb6dd631089c7
    Former-commit-id: dd68c0da7b6b462698ad0f2790a1704204b623ae

---
+++ examples/lua-scripts/asset/script/main.lua
@@ -0,0 +1,53 @@
+--[[
+Copyright (c) 2013 Aerys
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+]]--
+
+function main:start(node)
+	self.assets
+		:queue('effect/Basic.effect')
+		:queue('effect/Phong.effect')
+		:queue('texture/box.png')
+		:queue('script/rotate.lua')
+		:load()
+	
+	local cube = Node.create()
+		:addComponent(Transform.create())
+		:addComponent(Surface.create(
+			CubeGeometry.create(self.assets.context),
+			Material.create():setTexture('diffuseMap', self.assets:texture('texture/box.png')),
+			self.assets:effect('effect/Phong.effect')
+		))
+	node:addChild(cube)
+
+	self.camera = Node.create()
+		:addComponent(Renderer.create())
+		:addComponent(Transform.createFromMatrix(Matrix4x4.create():lookAt(
+			Vector3.zero(),
+			Vector3.create(0., 0., 3.),
+			Vector3.up()
+		)))
+		:addComponent(PerspectiveCamera.create(800. / 600., math.pi * .25, .1, 1000.))
+		:addComponent(self.assets:script('script/rotate.lua'))
+		:addComponent(DirectionalLight.create(.4))
+	node:addChild(self.camera)
+
+	local lights = Node.create()
+		:addComponent(AmbientLight.create(.2))
+		:addComponent(DirectionalLight.create(.4))
+	node:addChild(lights)
+end
