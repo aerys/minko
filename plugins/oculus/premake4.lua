38861065fbb3a90edebf7fb5663b848fc9c84e3d
Pierre-Edouard Landes <pierreedouard.landes@aerys.in>
2013-12-17 11:59:17 +0100

    Added LibOVR to the Oculus plugin.
    Former-commit-id: 4b19dff605c87667f967cf822ba7c9881da5fd3a
    Former-commit-id: 794eeb1ccdb8769efa184d24b1a5ddfd18e85cf4

--- plugins/oculus/premake4.lua
+++ plugins/oculus/premake4.lua
@@ -8,8 +8,43 @@ PROJECT_NAME = path.getname(os.getcwd())
 minko.project.library("minko-plugin-" .. PROJECT_NAME)
 	kind "StaticLib"
 	language "C++"
-	files { "**.hpp", "**.h", "**.cpp", "**.c", "include/**.hpp" }
+	files { 
+		"**.hpp", 
+		"**.h", 
+		"**.cpp", 
+		"**.c", 
+		"include/**.hpp",
+		"lib/LibOVR/Src/**"
+	}
 	includedirs {
 		"include",
-		"src"
+		"src",
+		"lib/LibOVR/Include",
+		"lib/LibOVR/Src"
 	}
+	
+	excludes { "lib/LibOVR/Include/OVRVersion.h" }
+	
+	configuration { "windows" }
+		excludes {
+			"lib/LibOVR/Src/OVR_Linux_*",
+			"lib/LibOVR/Src/OVR_OSX_*",
+			"lib/LibOVR/Src/Kernel/OVR_ThreadsPthread.cpp"
+		}
+		defines { "UNICODE", "_UNICODE" } -- should also undefine _MCBS
+		
+	configuration { "linux" }
+		excludes {
+			"lib/LibOVR/Src/OVR_Win32_*",
+			"lib/LibOVR/Src/OVR_OSX_*",
+			"lib/LibOVR/Src/Kernel/OVR_ThreadsWinAPI.cpp"
+		}
+	
+	configuration { "macosx" }
+		excludes {
+			"lib/LibOVR/Src/OVR_Win32_*",
+			"lib/LibOVR/Src/OVR_Linux_*",
+			"lib/LibOVR/Src/Kernel/OVR_ThreadsWinAPI.cpp"
+		}
+		
+	configuration { }
