27f825fa50ca16cdf649381c6cb2cab005925b8e
Warren Seine <warren.seine@aerys.in>
2013-12-19 12:47:52 +0100

    Initial support for Premake5.
    Former-commit-id: 48f8ae5bb27c05f7641398e6691088b46a788acf
    Former-commit-id: 0f40fe4b4edc906f0bebca0ec2cab46f0a337b13
    Former-commit-id: c00c4a9e1d768d1856ecb517ad649b86bf67f4c0

--- plugins/webgl/premake4.lua
+++ plugins/webgl/premake4.lua
@@ -10,13 +10,3 @@ minko.project.library("minko-plugin-" .. PROJECT_NAME)
 	language "C++"
 	files { "**.hpp", "**.h", "**.cpp", "**.c", "include/**.hpp" }
 	includedirs { "include", "src" }
-	
-	configuration { "debug"}
-		defines { "DEBUG" }
-		flags { "Symbols" }
-		targetdir "bin/debug"
-
-	configuration { "release" }
-		defines { "NDEBUG" }
-		flags { "OptimizeSpeed" }
-		targetdir "bin/release"
