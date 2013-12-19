44fc797bb9d4d3de22f55cbac8bbaea807afd448
Pierre-Edouard Landes <pierreedouard.landes@aerys.in>
2013-12-19 16:02:12 +0100

    Finished line-geometry example.
    Former-commit-id: 577720fc36abea9c71323f09d095891a7e351234

--- examples/line-geometry/src/Main.cpp
+++ examples/line-geometry/src/Main.cpp
@@ -18,36 +18,43 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 */
 
 #include "minko/Minko.hpp"
-#include "minko/MinkoPNG.hpp"
 #include "minko/MinkoSDL.hpp"
 
 using namespace minko;
+using namespace minko::scene;
+using namespace minko::geometry;
 using namespace minko::component;
+using namespace minko::data;
 using namespace minko::math;
+using namespace minko::render;
 
-const std::string TEXTURE_FILENAME = "texture/box.png";
+struct Star
+{
+	Node::Ptr	node;
+	float		angRate;
+};
+
+void
+addStar(Node::Ptr, file::AssetLibrary::Ptr, std::vector<Star>&);
+
+LineGeometry::Ptr
+createStarLineGeometry(unsigned int,
+						float inRadius,
+						float outRadius,
+						AbstractContext::Ptr);
 
 int main(int argc, char** argv)
 {
-	auto canvas = Canvas::create("Minko Example - Cube", 800, 600);
+	std::cout << "Press [A] to add stars." << std::endl;
+
+	auto canvas = Canvas::create("Minko Example - Line Geometry", 800, 600);
 
 	auto sceneManager = SceneManager::create(canvas->context());
-	
-	// setup assets
-	sceneManager->assets()->defaultOptions()->resizeSmoothly(true);
-	sceneManager->assets()->defaultOptions()->generateMipmaps(true);
-	sceneManager->assets()
-		->registerParser<file::PNGParser>("png")
-		->queue(TEXTURE_FILENAME)
-		->queue("effect/Basic.effect");
+	sceneManager->assets()->queue("effect/Line.effect");
 
-	sceneManager->assets()->geometry("cube", geometry::CubeGeometry::create(sceneManager->assets()->context()));
-	
-
-	auto _ = sceneManager->assets()->complete()->connect([=](file::AssetLibrary::Ptr assets)
+	std::vector<Star> stars;
+	auto _ = sceneManager->assets()->complete()->connect([&](file::AssetLibrary::Ptr assets)
 	{
-		auto cubeMaterial = material::BasicMaterial::create()->diffuseMap(assets->texture(TEXTURE_FILENAME));
-
 		auto root = scene::Node::create("root")
 			->addComponent(sceneManager);
 
@@ -57,16 +64,18 @@ int main(int argc, char** argv)
 				Matrix4x4::create()->lookAt(Vector3::zero(), Vector3::create(0.f, 0.f, 3.f))
 			))
 			->addComponent(PerspectiveCamera::create(800.f / 600.f, (float)PI * 0.25f, .1f, 1000.f));
+
 		root->addChild(camera);
-		
-		auto mesh = scene::Node::create("mesh")
-			->addComponent(Transform::create())
-			->addComponent(Surface::create(
-				geometry::CubeGeometry::create(sceneManager->assets()->context()),
-				material::BasicMaterial::create()->diffuseMap(assets->texture(TEXTURE_FILENAME)),
-				assets->effect("effect/Basic.effect")
-			));
-		root->addChild(mesh);
+
+		root->data()->addProvider(canvas->data()); // FIXME
+
+		addStar(root, assets, stars);
+
+		auto keyDown = canvas->keyboard()->keyDown()->connect([&](input::Keyboard::Ptr k, input::Keyboard::State s)
+		{
+			if (s[input::Keyboard::ScanCode::A])
+				addStar(root, assets, stars);
+		});
 
 		auto resized = canvas->resized()->connect([&](Canvas::Ptr canvas, uint w, uint h)
 		{
@@ -75,7 +84,8 @@ int main(int argc, char** argv)
 
 		auto enterFrame = canvas->enterFrame()->connect([&](Canvas::Ptr canvas, uint time, uint deltaTime)
 		{
-			mesh->component<Transform>()->matrix()->appendRotationY(.01f);
+			for (auto& star : stars)
+				star.node->component<Transform>()->matrix()->appendRotationY(star.angRate);
 
 			sceneManager->nextFrame();
 		});
@@ -88,4 +98,83 @@ int main(int argc, char** argv)
 	return 0;
 }
 
+void
+addStar(Node::Ptr root, file::AssetLibrary::Ptr assets, std::vector<Star>& stars)
+{
+	const unsigned int MAX_NUM_STARS = 20;
+	if (stars.size() == MAX_NUM_STARS)
+	{
+		std::cerr << "cannot add more stars." << std::endl;
+		return;
+	}
+	
+	unsigned int	numBranches = 4 + rand() % 4;
+	float			outRadius   = 0.9f + 0.2f * (rand() / (float)RAND_MAX - 0.5f);
+	float			inRadius	= outRadius * (0.5f + 0.1f * (rand() / (float)RAND_MAX - 0.5f));
+	
+	auto starNode = Node::create("star_" + std::to_string(stars.size()))
+		->addComponent(Surface::create(
+			createStarLineGeometry(numBranches, inRadius, outRadius, assets->context()),
+			Provider::create()
+				->set("material.diffuseColor",	Color::hslaToRgba(rand() / (float)RAND_MAX, 0.75f, 0.6f, 1.0f))
+				->set("material.lineThickness",	1.0f + 3.0f * (rand() / (float)RAND_MAX)),
+			assets->effect("line")
+		))
+		->addComponent(Transform::create(Matrix4x4::create()->appendRotationZ(2.0f * PI * rand() / (float)RAND_MAX)));
+	
+	stars.push_back(Star());
+	
+	stars.back().node		= starNode;
+	stars.back().angRate	= 0.01f +  0.05f * (rand() / (float)RAND_MAX);
+	
+	root->addChild(stars.back().node);
+}
 
+LineGeometry::Ptr
+createStarLineGeometry(unsigned int numStarBranches,
+						float inRadius,
+						float outRadius,
+						AbstractContext::Ptr context)
+{
+	if (context == nullptr)
+		throw std::invalid_argument("context");
+	
+	const unsigned int	numBranches = std::max((unsigned int)3, numStarBranches);
+	const float			innerRadius	= std::min(inRadius, outRadius);
+	const float			outerRadius	= std::max(inRadius, outRadius);
+	const float			cStep		= cosf(PI / (float)numBranches);
+	const float			sStep		= sinf(PI / (float)numBranches);
+
+	LineGeometry::Ptr	lines		= LineGeometry::create(context);
+
+	float				cAng		= 1.0f;
+	float				sAng		= 0.0f;
+	for (unsigned int i = 0; i < numBranches; ++i)
+	{
+		const float	outX	= outerRadius * cAng;
+		const float	outY	= outerRadius * sAng;
+
+		if (lines->numLines() > 0)
+			lines->lineTo(outX, outY, 0.0f, 2);
+		else
+			lines->moveTo(outX, outY, 0.0f);
+
+		float	c	= cAng*cStep - sAng*sStep;
+		float	s	= sAng*cStep + cAng*sStep;
+		cAng		= c;
+		sAng		= s;
+		
+		const float	inX		= innerRadius * cAng;
+		const float	inY		= innerRadius * sAng;
+
+		lines->lineTo(inX, inY, 0.0f, 2);
+
+		c		= cAng*cStep - sAng*sStep;
+		s		= sAng*cStep + cAng*sStep;
+		cAng	= c;
+		sAng	= s;
+	}
+	lines->lineTo(outerRadius, 0.0f, 0.0f, 2);
+
+	return lines;
+}
