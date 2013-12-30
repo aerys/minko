beba96fd4363de4e7b61a0075a1a12fe6e441811
Alexandre Cyprien <alexandre.cyprien@aerys.in>
2013-12-30 17:15:51 +0100

    Fix serializer compilation/linkage
    Former-commit-id: e65236cf3824295c157cd3515bc08c19a2e8d12e
    Former-commit-id: cda4d2c661c3e06ae8b3a899d2b05e250fe6b505

--- examples/serializer/src/Main.cpp
+++ examples/serializer/src/Main.cpp
@@ -142,19 +142,20 @@ int main(int argc, char** argv)
 	sceneManager->assets()->defaultOptions()->generateMipmaps(true);
 	sceneManager->assets()
 		->registerParser<file::PNGParser>("png")
-		->registerParser<file::SceneParser>("scene");
+		->registerParser<file::SceneParser>("scene")
+		->queue("texture/box.png")
 		//->queue("subScene.scene");
-		//->queue("effect/Basic.effect");
+		->queue("effect/Basic.effect");
 
-	//sceneManager->assets()->geometry("cube", geometry::CubeGeometry::create(sceneManager->assets()->context()));
-	//sceneManager->assets()->geometry("sphere", geometry::SphereGeometry::create(sceneManager->assets()->context(), 20, 20));
+	sceneManager->assets()->geometry("cube", geometry::CubeGeometry::create(sceneManager->assets()->context()));
+	sceneManager->assets()->geometry("sphere", geometry::SphereGeometry::create(sceneManager->assets()->context(), 20, 20));
 	
 	auto _ = sceneManager->assets()->complete()->connect([=](file::AssetLibrary::Ptr assets)
 	{
 		if (loaded == 1)
 			return;
 		loaded = 1;
-		/*auto cubeMaterial = material::BasicMaterial::create()
+		auto cubeMaterial = material::BasicMaterial::create()
 			->diffuseMap(assets->texture("texture/box.png"))
 			->diffuseColor(math::Vector4::create(1, 0, 0, 1))
 			->blendMode(render::Blending::Mode::DEFAULT)
@@ -169,10 +170,6 @@ int main(int argc, char** argv)
 		assets->material("boxMaterial", cubeMaterial);
 		assets->material("sphereMaterial", sphereMaterial);
 
-		
-
-
-		
 		auto mesh = scene::Node::create("mesh")
 			->addComponent(Transform::create())
 			->addComponent(Surface::create(
@@ -197,6 +194,9 @@ int main(int argc, char** argv)
 				assets->effect("effect/Basic.effect")
 			));
 
+		auto root = scene::Node::create("root")
+			->addComponent(sceneManager);
+
 		root->addChild(mesh);
 		root->addChild(mesh2);
 		root->addChild(mesh3);
@@ -205,31 +205,17 @@ int main(int argc, char** argv)
 		mesh3->component<Transform>()->transform()->appendTranslation(0, -1, 0);
 		
 		unitTest(assets, canvas->context());
-
-		std::shared_ptr<file::SceneWriter> sceneWriter = file::SceneWriter::create();
-		sceneWriter->data(root);
-		sceneWriter->write("subScene.scene", assets, file::Options::create(canvas->context()));
-		*/
+		
 		//assets->load("subScene.scene");
 
-		auto root = scene::Node::create("root")
-			->addComponent(sceneManager);
-		
-		/*
 		auto camera = scene::Node::create("camera")
 			->addComponent(Renderer::create(0x7f7f7fff))
 			->addComponent(Transform::create(
 				Matrix4x4::create()->lookAt(Vector3::zero(), Vector3::create(0.f, 0.f, 3.f))
 			))
-			->addComponent(PerspectiveCamera::create(800.f / 600.f, (float)PI * 0.25f, .1f, 1000.f));*/
-		//root->addChild(camera);
-		/*
-		root->removeChild(camera);
-		*/
-		//root->removeChildren();
+			->addComponent(PerspectiveCamera::create(800.f / 600.f, (float)PI * 0.25f, .1f, 1000.f));
+		root->addChild(camera);
 
-		//for (int i = assets->node("subScene.scene")->children().size() - 1; i >= 0; --i)
-		//	root->addChild(assets->node("subScene.scene")->children()[i]);
 		//root->addChild(assets->node("subScene.scene"));
 		
 		auto resized = canvas->resized()->connect([&](Canvas::Ptr canvas, uint w, uint h)
@@ -237,11 +223,19 @@ int main(int argc, char** argv)
 			root->children()[0]->children()[0]->component<PerspectiveCamera>()->aspectRatio((float)w / (float)h);
 		});
 
+		int frameId = 0;
+
 		auto enterFrame = canvas->enterFrame()->connect([&](Canvas::Ptr canvas, uint time, uint deltaTime)
 		{
-			root->children()[0]->children()[1]->component<Transform>()->transform()->appendRotationY(.01f);
-			
 			sceneManager->nextFrame();
+			frameId++;
+
+			if (frameId == 3)
+			{
+				std::shared_ptr<file::SceneWriter> sceneWriter = file::SceneWriter::create();
+				sceneWriter->data(root);
+				sceneWriter->write("subScene.scene", assets, file::Options::create(canvas->context()));
+			}
 		});
 
 		canvas->run();
