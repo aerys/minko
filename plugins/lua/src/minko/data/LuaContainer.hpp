b9e186bc3d9f6a55681084e13021c4374731b44f
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2013-12-30 23:42:07 +0100

    add Lua bindings for data::Container::getVector2, getVector3 and getVector4 methods
    Former-commit-id: c59093cb59bbf97f609bcd89ff1a46cd46bea237
    Former-commit-id: 8929d10840750cf32199821a179d4c48c35e7e71
    Former-commit-id: 9980392133cb860e8c090234a5c944fedd563558

--- plugins/lua/src/minko/data/LuaContainer.hpp
+++ plugins/lua/src/minko/data/LuaContainer.hpp
@@ -22,6 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 #include "minko/Common.hpp"
 
 #include "minko/data/Container.hpp"
+#include "minko/data/Provider.hpp"
 
 #include "minko/LuaWrapper.hpp"
 
@@ -36,13 +37,19 @@ namespace minko
 			void
 			bind(LuaGlue& state)
 			{
-				state.Class<data::Container>("Container")
-		            //.method("create",       &data::Container::create)
-		            //.method("hasProperty",  &data::Container::hasProperty)
-		            .method("getFloat",     &data::Container::get<float>)
-		            .method("getInt",       &data::Container::get<int>)
-		            .method("getUint",      &data::Container::get<unsigned int>)
-		            .method("getMatrix4x4", &data::Container::get<math::Matrix4x4::Ptr>);
+				state.Class<Container>("Container")
+		            //.method("create",       &Container::create)
+		            //.method("hasProperty",  &Container::hasProperty)
+					.method("addProvider",		static_cast<void (Container::*)(Provider::Ptr)>(&Container::addProvider))
+					.method("removeProvider",	static_cast<void (Container::*)(Provider::Ptr)>(&Container::removeProvider))
+					.method("hasProvider",		static_cast<bool (Container::*)(Provider::Ptr)>(&Container::hasProvider))
+		            .method("getFloat",     	&Container::get<float>)
+		            .method("getVector2",     	&Container::get<std::shared_ptr<math::Vector2>>)
+		            .method("getVector3",     	&Container::get<std::shared_ptr<math::Vector3>>)
+		            .method("getVector4",     	&Container::get<std::shared_ptr<math::Vector4>>)
+		            .method("getInt",       	&Container::get<int>)
+		            .method("getUint",      	&Container::get<unsigned int>)
+		            .method("getMatrix4x4", 	&Container::get<std::shared_ptr<math::Matrix4x4>>);
 			}
 		};
 	}
