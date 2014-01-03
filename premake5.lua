74611005d6a270d866ffd94d22f9605ce1ad8e85
Warren Seine <warren.seine@aerys.in>
2014-01-03 20:01:32 +0100

    Fix clean action.
    Former-commit-id: 82f0ec62c370ba5f4963710e5a5b2fcec5c0c22e
    Former-commit-id: c0607ab29d6afe33ee124ed6d0b61e751f9a7c94

--- premake5.lua
+++ premake5.lua
@@ -174,11 +174,6 @@ newaction {
 	trigger			= "clean",
 	description		= "Remove generated files.",
 	execute			= function()
-	local prjs = solution().projects
-		for i, prj in ipairs(prjs) do
-			os.rmdir(prj.basedir .. "/bin")
-			os.rmdir(prj.basedir .. "/obj")
-		end
-		os.rmdir(solution().basedir .. "/doc")
+		os.execute(minko.sdk.path("tools/all/bin/clean.sh"))
 	end
 }
