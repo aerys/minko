74358ae46a213895ede1c338815f26ba560fa5ef
Pierre-Edouard Landes <pierreedouard.landes@aerys.in>
2014-01-07 14:52:31 +0100

    Updated the leap-motion premake file.
    Former-commit-id: 4c4e6d2732a69e0e31473ce98964f2a7061753de
    Former-commit-id: 1b8ba8c6256f4d548a43a21fa40ecd1b3603d150

--- examples/leap-motion/premake4.lua
+++ examples/leap-motion/premake4.lua
@@ -1,15 +1,14 @@
-minko.project.application "example-leapmotion"
+PROJECT_NAME = path.getname(os.getcwd())
+
+minko.project.application("minko-example-" .. PROJECT_NAME)
+
+	language "c++"
 	kind "ConsoleApp"
-	language "C++"
-	files {
-		"src/**.hpp",
-		"src/**.cpp"
-	}
-	includedirs {
-		"src"
-	}
-	
-	-- plugins 
+
+	files { "src/**.cpp", "src/**.hpp", "asset/**" }
+	includedirs { "src" }
+
+	-- plugins
 	minko.plugin.enable("mk")
 	minko.plugin.enable("png")
 	minko.plugin.enable("jpeg")
@@ -17,27 +16,3 @@ minko.project.application "example-leapmotion"
 	minko.plugin.enable("leap")
 	
 	minko.plugin.import("angle")
-	
-	links { "framework" }
-
-	-- configurations
-	configuration { "debug"}
-		defines { "DEBUG" }
-		flags { "Symbols" }
-		targetdir "bin/debug"
-
-	configuration { "release" }
-		defines { "NDEBUG" }
-		flags { "OptimizeSpeed" }
-		targetdir "bin/release"
-
-	-- emscripten
-	configuration { "emscripten" }
-		local bin = "bin/release/" .. project().name
-		postbuildcommands {
-			'cp ' .. bin .. ' ' .. bin .. '.bc',
-			'cp -r asset/* bin/release/ || :',
-			'rm bin/release/model/Sponza_lite.mks',
-			'cd bin/release && emcc ' .. project().name .. '.bc -o ' .. project().name .. '.html -O2 -s ASM_JS=1 -s TOTAL_MEMORY=268435456 --preload-file effect --preload-file texture --preload-file model --compression /home/vagrant/src/emscripten/third_party/lzma.js/lzma-native,/home/vagrant/src/emscripten/third_party/lzma.js/lzma-decoder.js,LZMA.decompress',
---			'emcc ' .. bin .. '.bc -o ' .. bin .. '.js -O2 -s ASM_JS=1 -s TOTAL_MEMORY=268435456 --preload-file effect --preload-file texture --preload-file model'
-		}
