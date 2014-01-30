a06f6ba73c6c105cfeeec5dd9af352fce0dfd339
Pierre-Edouard Landes <pierreedouard.landes@aerys.in>
2014-01-30 16:18:27 +0100

    Now tracks a couple of drawcall properties (zsort flag, priority, vertex positions, model to world, world to screen) to decide when to perform z-sorting of the drawcalls.
    Former-commit-id: 836ac09c9b4c0afb463043cf33f26cf007c14634
    Former-commit-id: 3bbd4192e0ae8f82807b4b26d9d3de73a8d406bd

--- framework/include/minko/render/DrawCall.hpp
+++ framework/include/minko/render/DrawCall.hpp
@@ -28,11 +28,14 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 #include "minko/render/ProgramInputs.hpp"
 #include "minko/render/States.hpp"
 #include "minko/render/AbstractTexture.hpp"
+#include "minko/render/Priority.hpp"
 
 namespace minko
 {
 	namespace render
 	{
+		class ZSortSignalManager;
+
 		class DrawCall :
             public std::enable_shared_from_this<DrawCall>
 		{
@@ -46,13 +49,9 @@ namespace minko
             typedef std::shared_ptr<data::Container>				ContainerPtr;
 			typedef data::Container::PropertyChangedSignal::Slot	ContainerPropertyChangedSlot;
 
-			typedef std::tuple<int, int>			Int2;
-			typedef std::tuple<int, int, int>		Int3;
-			typedef std::tuple<int, int, int, int>	Int4;
-			
-			typedef unsigned char					Bool2;
-			typedef unsigned char					Bool3;
-			typedef unsigned char					Bool4;						
+			typedef std::tuple<int, int>							Int2;
+			typedef std::tuple<int, int, int>						Int3;
+			typedef std::tuple<int, int, int, int>					Int4;					
 
 		private:
 			static const unsigned int									MAX_NUM_TEXTURES;
@@ -118,8 +117,9 @@ namespace minko
 			std::unordered_map<uint, data::UniformArrayPtr<int>>		_uniformInts4;
 
 			std::unordered_map<std::string, std::list<Any>>				_referenceChangedSlots; // Any = ContainerPropertyChangedSlot
-			std::list<Any>												_zsortRequestingSlots;
-			std::shared_ptr<Signal<Ptr>>								_zsortUpdateNeeded;
+
+			std::shared_ptr<Signal<Ptr>>								_zsortNeeded;
+			std::shared_ptr<ZSortSignalManager>							_zsortSignalManager;
 
 		public:
 			static inline
@@ -129,9 +129,16 @@ namespace minko
 				   const data::BindingMap&	stateBindings,
                    std::shared_ptr<States>  states)
 			{
-				return std::shared_ptr<DrawCall>(new DrawCall(
-                    attributeBindings, uniformBindings, stateBindings, states
-                ));;
+				Ptr ptr = std::shared_ptr<DrawCall>(new DrawCall(
+                    attributeBindings, 
+					uniformBindings, 
+					stateBindings, 
+					states
+                ));
+
+				ptr->initialize();
+
+				return ptr;
 			}
 
             inline
@@ -152,14 +159,14 @@ namespace minko
 			bool
 			zsorted() const
 			{
-				return _zsorted;
+				return _zsorted && priority::LAST < _priority && !( _priority > priority::TRANSPARENT);
 			}
-
+			
 			inline
 			std::shared_ptr<Signal<Ptr>>
-			zsortUpdateNeeded() const
+			zsortNeeded() const
 			{
-				return _zsortUpdateNeeded;
+				return _zsortNeeded;
 			}
 
             void
@@ -183,6 +190,9 @@ namespace minko
 				     const data::BindingMap&	uniformBindings,
 					 const data::BindingMap&	stateBindings,
                      std::shared_ptr<States>    states);
+
+			void
+			initialize();
 
 			void
 			reset();
@@ -280,12 +290,6 @@ namespace minko
 				// actually does nothing
 				return value;
 			}
-
-			void
-			watchZSortingProperties();
-
-			void
-			zsortingPropertyChanged();
 		};		
 	}
 }
---
+++ framework/src/minko/render/ZSortSignalManager.hpp
@@ -0,0 +1,85 @@
+/*
+Copyright (c) 2013 Aerys
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#pragma once
+
+#include "minko/Common.hpp"
+#include "minko/Signal.hpp"
+
+namespace minko
+{
+	namespace render
+	{
+		class ZSortSignalManager: 
+			public std::enable_shared_from_this<ZSortSignalManager>
+		{
+		public:
+			typedef std::shared_ptr<ZSortSignalManager>				Ptr;
+
+		private:
+			typedef std::shared_ptr<DrawCall>						DrawCallPtr;
+			typedef std::shared_ptr<data::Container>				ContainerPtr;
+			typedef std::shared_ptr<data::Value>					ValuePtr;
+
+		private:
+			typedef std::shared_ptr<Signal<DrawCallPtr>>			ZSortNeedSignalPtr;
+			typedef Signal<ContainerPtr, const std::string&>::Slot	PropertyChangedSlot;
+			typedef Signal<ValuePtr>::Slot							ValueChangedSlot;
+
+		private:
+			const DrawCallPtr										_drawcall;
+
+			PropertyChangedSlot										_targetPropAddedSlot;
+			PropertyChangedSlot										_targetPropRemovedSlot;
+			PropertyChangedSlot										_rendererPropAddedSlot;
+			PropertyChangedSlot										_rendererPropRemovedSlot;
+			std::unordered_map<std::string, PropertyChangedSlot>	_propChangedSlots;
+			std::unordered_map<std::string, ValueChangedSlot>		_matrixChangedSlots;
+
+			static const std::unordered_set<std::string>			TARGET_PNAMES;
+			static const std::unordered_set<std::string>			RENDERER_PNAMES;
+
+		public:
+			inline static
+			Ptr
+			create(DrawCallPtr drawCall)
+			{
+				return std::shared_ptr<ZSortSignalManager>(new ZSortSignalManager(drawCall));
+			}
+
+			void
+			initialize(ContainerPtr targetData, ContainerPtr rendererData, ContainerPtr rootData);
+
+			void
+			clear();
+
+		private:
+			ZSortSignalManager(DrawCallPtr drawcall);
+
+			void
+			propertyAddedHandler(ContainerPtr, const std::string&, const std::unordered_set<std::string>&);
+
+			void
+			propertyRemovedHandler(ContainerPtr, const std::string&, const std::unordered_set<std::string>&);
+
+			void
+			requestZSort();
+		};
+	}
+}
