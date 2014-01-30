17984ef0e440a7fc38b63c64767b3341aab5f99a
Pierre-Edouard Landes <pierreedouard.landes@aerys.in>
2014-01-30 11:29:37 +0100

    Restored the drawcall sort at drawcall creation *only* and temporarily deactivated the sky-box example until z-sort is triggered by signals.
    Former-commit-id: c307b3eafb380cfbea82ac8de61af71c0f86d795
    Former-commit-id: 126a6b92322545b2c9732cdf70e012156b6b81aa
    Former-commit-id: 026284c34727cbdadd463075f8505faaed783471

--- premake5.lua
+++ premake5.lua
@@ -62,7 +62,7 @@ solution "minko"
 		include('examples/visibility')
 		-- include('examples/leap-motion')
 		include('examples/jobs')
-		include('examples/sky-box')
+		-- include('examples/sky-box')
 
 	end
 
