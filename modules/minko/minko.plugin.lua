bc15e402858a5c3c1cae4003b57d7b6efac77699
Warren Seine <warren.seine@aerys.in>
2014-02-03 22:10:27 +0100

    Refactor plugin import (first step) (fixing #2913).
    Former-commit-id: 6249007b233c42717ff0189e452a7b9c5c81db3f
    Former-commit-id: b03d841f87c25ab5e858a29565d3eb4823ac3942

--- modules/minko/minko.plugin.lua
+++ modules/minko/minko.plugin.lua
@@ -1,51 +1,34 @@
 minko.plugin = {
-	_enabled = {},
-	_externalPlugins = {}
 }
 
--- register external plugin
-minko.plugin.register = function(name)
-	print ("register " .. name)
-	minko.plugin._externalPlugins[name] = os.getcwd()
-end
+-- register plugin from its path (must be called from plugin.lua)
+-- minko.plugin.register = function(name)
+-- end
 
 minko.plugin.import = function(name)
-	if not MINKO_SDK_DIST then
-
-		if os.isdir(minko.sdk.path("/plugins/" .. name)) then
-			include(minko.sdk.path("/plugins/" .. name))
-		else
-			include(minko.plugin._externalPlugins[name])
-		end
-	end
-
 	if minko.plugin[name] and minko.plugin[name].import then
 		minko.plugin[name]:import()
 	end
 end
 
+minko.plugin.include = function(path)
+	if not MINKO_SDK_DIST then
+		include(path)
+	end
+
+	dofile(path .. '/plugin.lua')
+end
+
 minko.plugin.enable = function(name)
 	local cfg = configuration().configset._current
-
-	local projectName = project().name
 	local terms = cfg._criteria.terms
 
-	minko.plugin._enabled[name] = true
-	minko.plugin.import(name)	
-	
-	project(projectName)
 	configuration { unpack(terms) }
 
-	print ("Minko enable " .. name .. " "  .. os.getcwd())
-
-	if os.isdir(minko.sdk.path("/plugins/" .. name)) then
-		dofile(minko.sdk.path("/plugins/" .. name .."/plugin.lua"))
-	elseif minko.plugin._externalPlugins[name] ~= nil then
-		dofile(minko.plugin._externalPlugins[name] .. "/plugin.lua")
-	else
-		print(color.fg.red ..'Plugin ' .. name .. " not found." .. color.reset)
-		os.exit(1)
+	if not minko.plugin[name] then
+		error(color.fg.red .. 'Plugin "' .. name .. '" not found (enabled from "' .. project().name .. '").' .. color.reset)
 	end
+
 	if minko.plugin[name] and minko.plugin[name].enable then
 		minko.plugin[name]:enable()
 	end
@@ -53,42 +36,31 @@ minko.plugin.enable = function(name)
 	configuration { unpack(terms) }
 end
 
-minko.plugin.enabled = function(name)
-	return minko.plugin._enabled[name] or _OPTIONS["with-" .. name] ~= nil
-end
-
-minko.plugin.path = function(name)
-			
-	if minko.plugin._externalPlugins[name] ~= nil then
-		name = path.getabsolute(minko.plugin._externalPlugins[name])
-	else
-		name = path.getabsolute(MINKO_HOME .. "/plugins/" .. name)
-	end
-
-	if os.get() == "windows" then
-		name = path.translate(name, "\\")
-	end
-	
-	return name
-end
-
 minko.plugin.links = function(names)
 	local cfg = configuration().configset._current
-
-	local projectName = project().name
 	local terms = cfg._criteria.terms
 
 	for _, name in ipairs(names) do
 		configuration { unpack(terms) }
 
-		local projectName = "minko-plugin-" .. name
+		local plugin = "minko-plugin-" .. name
 
-		if MINKO_SDK_DIST then
-			minko.sdk.links(projectName, "plugins/" .. name)
+		if not MINKO_SDK_DIST then
+			links { plugin }
 		else
-			links { projectName }
+			if minko.plugin[name] and minko.plugin[name].links then
+				minko.plugin[name]:links()
+			end
 		end
 	end
 
 	configuration { unpack(terms) }
 end
+
+setmetatable(minko.plugin, {
+	__newindex = function(t, k, v)
+		assert(type(v) == "table", 'The correct syntax for initializing a plugin is: minko.plugin.name = {}')
+		v._path = os.getcwd()
+		rawset(t, k, v)
+	end
+})
--- modules/minko/minko.sdk.lua
+++ modules/minko/minko.sdk.lua
@@ -1,33 +1,5 @@
 minko.sdk = {}
 
 minko.sdk.path = function(p)
-
-	if minko.plugin._externalPlugins[p] ~= nil then
-		p = path.getabsolute(minko.plugin._externalPlugins[p] .. "/" .. p)
-	else
-		p = path.getabsolute(MINKO_HOME .. "/" .. p)
-	end
-
-	if os.get() == "windows" then
-		p = path.translate(p, "\\")
-	end
-
-	return p
-end
-
-minko.sdk.links = function(name, path)
-	local cfg = configuration().configset._current
-	local terms = cfg._criteria.terms
-
-	for i, platform in ipairs(platforms()) do
-		for j, cfg in ipairs(configurations()) do
-			-- matching both the platform (windows, macosx...) and the config (debug, release)
-			-- but also the current scope configuration if there is one defined!
-
-			configuration { platform, cfg, unpack(terms) }
-				libdirs { minko.sdk.path(path .. "/bin/" .. platform .. "/" .. cfg) }
-				links { name }
-				-- linkoptions { minko.sdk.path(path .. "/bin/" .. platform .. "/" .. cfg) .. "/lib" .. name .. ".a" }
-		end
-	end
+	return path.translate(path.getabsolute(MINKO_HOME .. "/" .. p))
 end
