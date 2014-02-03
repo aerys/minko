c73f7e50a8ae5fa03dde4519e623d156d08a65f5
Warren Seine <warren.seine@aerys.in>
2014-02-03 17:36:05 +0100

    Refactor the SDL plugin to avoid duplication of include files.
    Former-commit-id: a86978a4ad0db591b17472a7738d7cfde57bb4d0

--- plugins/sdl/premake5.lua
+++ plugins/sdl/premake5.lua
@@ -20,27 +20,11 @@ minko.project.library("minko-plugin-" .. PROJECT_NAME)
 		"src/**.hpp"
 	}
 
-	includedirs { "include" }
+	includedirs {
+		"include",
+		"lib/sdl/include"
+	}
 	
 	minko.plugin.import("angle")
 	minko.plugin.enable("webgl")
 
-	configuration { "windows", "x32" }
-		includedirs { minko.sdk.path("plugins/sdl/lib/sdl/windows32/include") }
-
-	configuration { "windows", "x64" }
-		includedirs { minko.sdk.path("plugins/sdl/lib/sdl/windows64/include") }
-		
-	configuration { "linux", "x32" }
-		includedirs { minko.sdk.path("plugins/sdl/lib/sdl/linux32/include") }
-
-	configuration { "linux", "x64" }
-		includedirs { minko.sdk.path("plugins/sdl/lib/sdl/linux64/include") }
-
-	configuration { "macosx" }
-		includedirs { minko.sdk.path("plugins/sdl/lib/sdl/linux64/include") } -- FIXME
-
-	configuration { "html5" }
-		if EMSCRIPTEN_HOME then
-			includedirs { EMSCRIPTEN_HOME .. "/system/include" }
-		end
