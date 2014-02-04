58eb488022813459bf648b75e4bd5a84fb5c9670
Pierre-Edouard Landes <pierreedouard.landes@aerys.in>
2014-02-04 15:31:28 +0100

    Fixed the phong specular color issue (only its alpha was taken into account so far).
    Former-commit-id: dcadd44aef25140cf236a337d83b6e87f37f7c16

--- framework/effect/Phong.fragment.glsl
+++ framework/effect/Phong.fragment.glsl
@@ -67,27 +67,28 @@
 #endif // MINKO_NO_GLSL_STRUCT
 	
 // diffuse
-uniform vec4 diffuseColor;
-uniform sampler2D diffuseMap;
+uniform vec4 		diffuseColor;
+uniform sampler2D 	diffuseMap;
 
 // alpha
-uniform sampler2D alphaMap;
-uniform float alphaThreshold;
+uniform sampler2D 	alphaMap;
+uniform float 		alphaThreshold;
 
 // phong
-uniform vec4 specularColor;
-uniform sampler2D normalMap;
-uniform sampler2D specularMap;
-uniform float shininess;
-uniform vec3 cameraPosition;
+uniform vec4 		specularColor;
+uniform sampler2D 	normalMap;
+uniform sampler2D 	specularMap;
+uniform float 		shininess;
+uniform vec3 		cameraPosition;
 
 // env. mapping
-uniform float environmentAlpha;
+uniform float 		environmentAlpha;
 
-varying vec3 vertexPosition;
-varying vec2 vertexUV;
-varying vec3 vertexNormal;
-varying vec3 vertexTangent;
+
+varying vec3 		vertexPosition;
+varying vec2 		vertexUV;
+varying vec3 		vertexNormal;
+varying vec3 		vertexTangent;
 
 void main(void)
 {
@@ -117,29 +118,32 @@ void main(void)
 		
 	#endif
 	
-	vec3 phong = vec3(0.);
-	
+	vec3	ambientAccum	= vec3(0.0);
+	vec3	diffuseAccum	= vec3(0.0);
+	vec3	specularAccum	= vec3(0.0); 
+
 	#ifdef PRECOMPUTED_AMBIENT
 	//------------------------
-		phong += sumAmbients;
+		ambientAccum += sumAmbients;
 	#else
 	
 		#ifdef NUM_AMBIENT_LIGHTS
 			for (int i = 0; i < NUM_AMBIENT_LIGHTS; ++i)
 			{
 				#ifndef MINKO_NO_GLSL_STRUCT
-					phong += ambientLights[i].color * ambientLights[i].ambient;
+					ambientAccum 	+= ambientLights[i].color * ambientLights[i].ambient;
 				#else
-					phong += ambientLights_color[i] * ambientLights_ambient[i];
+					ambientAccum	+= ambientLights_color[i] * ambientLights_ambient[i];
 				#endif // MINKO_NO_GLSL_STRUCT
 			}
 		#endif // NUM_AMBIENT_LIGHTS
 
 	#endif // PRECOMPUTED_AMBIENT
 	
+
 	#if defined NUM_DIRECTIONAL_LIGHTS || defined NUM_POINT_LIGHTS || defined NUM_SPOT_LIGHTS || defined ENVIRONMENT_MAP
 
-	vec3 eyeVector	= normalize(cameraPosition - vertexPosition); // in world-space
+	vec3 eyeVector	= normalize(cameraPosition - vertexPosition); // always in world-space
 
 	#endif // NUM_DIRECTIONAL_LIGHTS || NUM_POINT_LIGHTS || NUM_SPOT_LIGHTS || ENVIRONMENT_MAP
 
@@ -156,7 +160,7 @@ void main(void)
 		float	lightCosOuterAng		= 0.0;
 		float 	contribution			= 0.0;
 		
-		vec3 	normalVector			= normalize(vertexNormal); // in world-space
+		vec3 	normalVector			= normalize(vertexNormal); // always in world-space
 		
 		#ifdef NORMAL_MAP
 			// warning: the normal vector must be normalized at this point!
@@ -181,21 +185,17 @@ void main(void)
 				lightDirection		= directionalLights_direction[i];
 			#endif // MINKO_NO_GLSL_STRUCT
 	
-		
 			lightDirection	= normalize(-lightDirection);
 			
-			contribution = phong_diffuseReflection(normalVector, lightDirection) * lightDiffuseCoeff;
+			diffuseAccum		+= phong_diffuseReflection(normalVector, lightDirection)
+				* lightColor
+				* lightDiffuseCoeff;
 
-			#ifdef SHININESS
-				contribution += phong_specularReflection(
-					normalVector,
-					lightDirection,
-					eyeVector,
-					shininess
-				) * lightSpecularCoeff * specular.a;
+			#if defined(SHININESS)
+				specularAccum	+= phong_specularReflection(normalVector, lightDirection, eyeVector, shininess) 
+					* lightColor
+					* lightSpecularCoeff;
 			#endif // SHININESS
-
-			phong += contribution * lightColor;
 		}
 		#endif // NUM_DIRECTIONAL_LIGHTS
 		
@@ -221,22 +221,19 @@ void main(void)
 			float distanceToLight 	= length(lightDirection);
 			lightDirection 			/= distanceToLight;
 			
-			contribution	= phong_diffuseReflection(normalVector, lightDirection) * lightDiffuseCoeff;
-
-			#ifdef SHININESS
-				contribution += phong_specularReflection(
-					normalVector,
-					lightDirection,
-					eyeVector,
-					shininess
-				) * lightSpecularCoeff * specular.a;
-			#endif // SHININESS
-			
 			float attenuation = lightAttenuationDist > 0.0
 				? max(0.0, 1.0 - distanceToLight / lightAttenuationDist) 
 				: 1.0;
-				
-			phong += attenuation * contribution * lightColor;
+
+			diffuseAccum		+= phong_diffuseReflection(normalVector, lightDirection)
+				* lightColor
+				* (lightDiffuseCoeff * attenuation);
+
+			#if defined(SHININESS)
+				specularAccum	+= phong_specularReflection(normalVector, lightDirection, eyeVector, shininess) 
+					* lightColor
+					* (lightSpecularCoeff * attenuation);
+			#endif // SHININESS	
 		}
 		#endif // NUM_POINT_LIGHTS
 		
@@ -274,28 +271,24 @@ void main(void)
 
 			if (lightCosOuterAng < cosSpot)
 			{
-				contribution	= phong_diffuseReflection(normalVector, lightDirection)
-					* lightDiffuseCoeff;
-
-				#ifdef SHININESS
-					contribution += phong_specularReflection(
-						normalVector,
-						lightDirection,
-						eyeVector,
-						shininess
-					) * lightSpecularCoeff * specular.a;
-				#endif // SHININESS
-				
-				float cutoff	= cosSpot < lightCosInnerAng && lightCosOuterAng < lightCosInnerAng 
-								? (cosSpot - lightCosOuterAng) / (lightCosInnerAng - lightCosOuterAng) 
-								: 1.0;
-				contribution	*= cutoff;
-				
 				float attenuation = lightAttenuationDist > 0.0
 					? max(0.0, 1.0 - distanceToLight / lightAttenuationDist)
 					: 1.0;
-				
-				phong += attenuation * contribution * lightColor;
+					
+				float cutoff	= cosSpot < lightCosInnerAng && lightCosOuterAng < lightCosInnerAng 
+					? (cosSpot - lightCosOuterAng) / (lightCosInnerAng - lightCosOuterAng) 
+					: 1.0;	
+
+
+				diffuseAccum		+= phong_diffuseReflection(normalVector, lightDirection)
+					* lightColor
+					* (lightDiffuseCoeff * attenuation * cutoff);
+
+				#ifdef SHININESS
+					specularAccum	+= phong_specularReflection(normalVector, lightDirection, eyeVector, shininess) 
+						* lightColor
+						* (lightSpecularCoeff * attenuation * cutoff);
+				#endif // SHININESS
 			}
 		}
 		#endif // NUM_SPOT_LIGHTS
@@ -317,6 +310,22 @@ void main(void)
 
 	#endif // defined(ENVIRONMENT_MAP_2D) || defined(ENVIRONMENT_CUBE_MAP)
 	
+
+	// Final blend of ambient, diffuse, and specular parts
+	//----------------------------------------------------
+	diffuseAccum		*= diffuse.rgb;
+	
+	#if defined(SHININESS)
+		specularAccum	*= specular.rgb;
+	#endif
+	
+	vec3 phong		= ambientAccum + diffuseAccum + specularAccum;
+	gl_FragColor	= vec4(phong.rgb, diffuse.a);
+	return;
+
+
+
+
 	diffuse = vec4(diffuse.rgb * phong, diffuse.a);
 	
 	gl_FragColor = diffuse;
