f73e154ee56e8963893267052de9f41baeaaab04
Benjamin Michel <benjamin.michel@aerys.in>
2014-02-05 17:39:30 +0100

    Added http example
    Former-commit-id: 235ea251033b1fb0abe2eb7601fd2aadff843497

---
+++ examples/http/premake5.lua
@@ -0,0 +1,26 @@
+PROJECT_NAME = path.getname(os.getcwd())
+
+minko.project.application("minko-example-" .. PROJECT_NAME)
+
+	language "c++"
+	kind "ConsoleApp"
+
+	files {
+		"src/**.cpp",
+		"src/**.hpp",
+		"asset/**"
+	}
+	
+	includedirs { "src" }
+
+	-- plugins
+	minko.plugin.enable("sdl")
+	minko.plugin.enable("http")
+	--minko.plugin.enable("webgl")
+	--minko.plugin.enable("bullet")
+	--minko.plugin.enable("jpeg")
+	--minko.plugin.enable("mk")
+	--minko.plugin.enable("particles")
+	minko.plugin.enable("png")
+	
+	minko.plugin.import("angle")
