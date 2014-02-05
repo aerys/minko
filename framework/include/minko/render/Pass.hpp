4c0de0c95524a412e18fb455630cc3e9b5cbbea3
Pierre-Edouard Landes <pierreedouard.landes@aerys.in>
2014-02-05 17:59:14 +0100

    Macro setters/unsetters in Effect class.
    Former-commit-id: 9c5a3c333bb4113ea102de5ffe3ab543bc17e72b
    Former-commit-id: 98618ac62c0519cd7e4673b4e99516e06106cf0d

--- framework/include/minko/render/Pass.hpp
+++ framework/include/minko/render/Pass.hpp
@@ -55,19 +55,22 @@ namespace minko
 			typedef std::function<std::string(const std::string&, StringToStringMap&)>	FormatFunction;
 
 		private:
-			const std::string		_name;
-			ProgramPtr				_programTemplate;
-			data::BindingMap		_attributeBindings;
-			data::BindingMap		_uniformBindings;
-			data::BindingMap		_stateBindings;
-			data::MacroBindingMap	_macroBindingsTemplate;
-            StatesPtr				_states;
-			std::string				_fallback;
-			SignatureProgramMap		_signatureToProgram;
+			const std::string						_name;
+			ProgramPtr								_programTemplate;
+			data::BindingMap						_attributeBindings;
+			data::BindingMap						_uniformBindings;
+			data::BindingMap						_stateBindings;
+			data::MacroBindingMap					_macroBindingsTemplate;
+            StatesPtr								_states;
+			std::string								_fallback;
+			SignatureProgramMap						_signatureToProgram;
 
-			OnProgramFunctionList	_uniformFunctions;
-			OnProgramFunctionList	_attributeFunctions;
-			OnProgramFunctionPtr	_indexFunction;
+			OnProgramFunctionList					_uniformFunctions;
+			OnProgramFunctionList					_attributeFunctions;
+			OnProgramFunctionPtr					_indexFunction;
+			std::set<std::string>					_undefinedMacros;
+			std::set<std::string>					_definedBoolMacros;
+			std::unordered_map<std::string, int>	_definedIntMacros;
 
 		public:
 			inline static
@@ -210,6 +213,7 @@ namespace minko
 					signatureAndProgram.second->setUniform(name, values...);
 			}
 
+			inline
 			void
 			setVertexAttribute(const std::string& name, unsigned int attributeSize, const std::vector<float>& data)
 			{
@@ -223,6 +227,7 @@ namespace minko
 					signatureAndProgram.second->setVertexAttribute(name, attributeSize, data);
 			}
 
+			inline
 			void
 			setIndexBuffer(const std::vector<unsigned short>& indices)
 			{
@@ -234,6 +239,40 @@ namespace minko
 					_programTemplate->setIndexBuffer(indices);
 				for (auto signatureAndProgram : _signatureToProgram)
 					signatureAndProgram.second->setIndexBuffer(indices);
+			}
+
+			inline
+			void
+			define(const std::string& macroName)
+			{
+				if (!macroName.empty())
+					_definedBoolMacros.insert(macroName);
+			}
+
+			inline
+			void
+			define(const std::string& macroName, int macroValue)
+			{
+				if (!macroName.empty())
+					_definedIntMacros[macroName] = macroValue;
+			}
+
+			inline
+			void
+			undefine(const std::string& macroName)
+			{
+				if (!macroName.empty())
+					_undefinedMacros.insert(macroName);
+			}
+
+			void
+			getExplicitDefinitions(std::unordered_map<std::string, data::MacroBindingDefault>& macroNameToValue) const;
+
+			inline
+			bool
+			isExplicitlyUndefined(const std::string& macroName) const
+			{
+				return _undefinedMacros.find(macroName) != _undefinedMacros.end();
 			}
 
 		private:
