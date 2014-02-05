c505afbdedeb2846a27de6dad51e93e5634eb0fd
Warren Seine <warren.seine@aerys.in>
2014-02-05 23:05:08 +0100

    Use "git clean" to clean the directory (also add a commented out classic simplier implementation).
    Former-commit-id: f09afd7e54eb7ede2a4078469ba87056e34c3dd4

--- modules/minko/minko.action.lua
+++ modules/minko/minko.action.lua
@@ -13,5 +13,20 @@ minko.action.copy = function(sourcePath)
 end
 
 minko.action.clean = function()
-	error("Not implemented yet")
+	-- if not os.isfile("sdk.lua") then
+	-- 	error("cannot clean from outside the Minko SDK")
+	-- end
+
+	-- for _, dir1 in ipairs { "framework", "plugins", "tests", "examples" } do
+	-- 	for _, dir2 in ipairs { "bin", "obj" } do
+	-- 		local dirs = os.matchdirs(dir1 .. "/**/" .. dir2)
+	-- 		for _, dir in ipairs(dirs) do
+	-- 			os.rmdir(dir)
+	-- 		end
+	-- 	end
+	-- end
+
+	-- os.rmdir("doc")
+
+	os.execute("git clean -X -f")
 end
