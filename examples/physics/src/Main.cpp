72d116dd1cbf4e05f59266f567b288a8536e214d
Pierre-Edouard Landes <pierreedouard.landes@aerys.in>
2014-02-06 20:50:40 +0100

    Added physics example.
    Former-commit-id: 22284c192565b6c02d466147c09a71942588f5c3
    Former-commit-id: 08eae65d529f3d728c8cb681e6113cbc38cfa813

---
+++ examples/physics/src/Main.cpp
@@ -0,0 +1,190 @@
+/*
+Copyright (c) 2013 Aerys
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include "minko/Minko.hpp"
+#include "minko/MinkoPNG.hpp"
+#include "minko/MinkoSDL.hpp"
+#include "minko/MinkoBullet.hpp"
+
+using namespace minko;
+using namespace minko::scene;
+using namespace minko::component;
+using namespace minko::math;
+
+const std::string	TEXTURE_FILENAME	= "texture/box.png";
+const float			GROUND_WIDTH		= 5.0f;
+const float			GROUND_HEIGHT		= 0.25f;
+const float			GROUND_DEPTH		= 5.0f;
+const float			GROUND_THICK		= 0.1f;
+
+const float			MIN_MASS			= 1.0f;
+const float			MAX_MASS			= 5.0f;
+const float			MIN_SCALE			= 0.2f;
+const float			MAX_SCALE			= 1.0f;
+
+const float			START_ALTITUDE		= 5.0f;
+
+const unsigned int	MAX_NUM_OBJECTS		= 32;
+
+Node::Ptr
+createPhysicsObject(unsigned int id, file::AssetLibrary::Ptr, bool isCube);
+
+int main(int argc, char** argv)
+{
+	auto canvas = Canvas::create("Minko Example - Cube", 800, 600);
+
+	auto sceneManager	= SceneManager::create(canvas->context());
+
+	// setup assets
+	sceneManager->assets()->defaultOptions()
+		->resizeSmoothly(true)
+		->generateMipmaps(true);
+	sceneManager->assets()
+		->registerParser<file::PNGParser>("png")
+		->queue(TEXTURE_FILENAME)
+		->queue("effect/Basic.effect")
+		->geometry("sphere",	geometry::SphereGeometry::create(sceneManager->assets()->context(), 16, 16))
+		->geometry("cube",		geometry::CubeGeometry::create(sceneManager->assets()->context()));
+	
+	auto _ = sceneManager->assets()->complete()->connect([=](file::AssetLibrary::Ptr assets)
+	{
+		auto root = scene::Node::create("root")
+			->addComponent(sceneManager)
+			->addComponent(bullet::PhysicsWorld::create());
+
+		auto camera = scene::Node::create("camera")
+			->addComponent(Renderer::create(0x7f7f7fff))
+			->addComponent(Transform::create(
+				Matrix4x4::create()->lookAt(Vector3::zero(), Vector3::create(3.0f, 1.0, 8.0f))
+			))
+			->addComponent(PerspectiveCamera::create(800.f / 600.f, (float)PI * 0.25f, .1f, 1000.f));
+		
+		auto groundNode = scene::Node::create("groundNode")->addComponent(Transform::create(
+				Matrix4x4::create()->appendRotationZ(-(float)PI * 0.1f)
+			));
+
+		auto groundNodeA = scene::Node::create("groundNodeA")
+			->addComponent(Transform::create(
+				Matrix4x4::create()->appendScale(GROUND_WIDTH, GROUND_THICK, GROUND_DEPTH)
+			))
+			->addComponent(Surface::create(
+				assets->geometry("cube"),
+				material::BasicMaterial::create()->diffuseMap(assets->texture(TEXTURE_FILENAME)),
+				assets->effect("effect/Basic.effect")
+			))
+			->addComponent(bullet::Collider::create(bullet::ColliderData::create(0.0f, bullet::BoxShape::create(GROUND_WIDTH * 0.5f, GROUND_THICK * 0.5f, GROUND_DEPTH * 0.5f))));
+
+		auto groundNodeB = scene::Node::create("groundNodeB")
+			->addComponent(Transform::create(
+				Matrix4x4::create()
+					->appendScale(GROUND_THICK, GROUND_HEIGHT, GROUND_DEPTH)
+					->appendTranslation(0.5f * GROUND_WIDTH, 0.5f * (GROUND_HEIGHT - GROUND_THICK), 0.0f)
+			))
+			->addComponent(Surface::create(
+				assets->geometry("cube"),
+				material::BasicMaterial::create()->diffuseMap(assets->texture(TEXTURE_FILENAME)),
+				assets->effect("effect/Basic.effect")
+			))
+			->addComponent(bullet::Collider::create(bullet::ColliderData::create(0.0f, bullet::BoxShape::create(GROUND_THICK * 0.5f, GROUND_HEIGHT * 0.5f, GROUND_DEPTH * 0.5f))));
+
+		root->addChild(camera);
+
+		groundNode
+			->addChild(groundNodeA)
+			->addChild(groundNodeB);
+
+		root->addChild(groundNode);
+
+		unsigned int numObjects = 0;
+		auto keyDown = canvas->keyboard()->keyDown()->connect([&](input::Keyboard::Ptr k)
+		{
+			if (k->keyIsDown(input::Keyboard::ScanCode::SPACE) && numObjects < MAX_NUM_OBJECTS)
+			{
+				root->addChild(createPhysicsObject(0, assets, true));
+				
+				//root->addChild(createPhysicsObject(numObjects, assets, rand() / (float)RAND_MAX > 0.5f));
+				++numObjects;
+			}
+			else
+				std::cout << "You threw away all your possible objects. Try again!" << std::endl;
+		});
+
+		auto resized = canvas->resized()->connect([&](AbstractCanvas::Ptr canvas, uint w, uint h)
+		{
+			camera->component<PerspectiveCamera>()->aspectRatio((float)w / (float)h);
+		});
+
+		auto enterFrame = canvas->enterFrame()->connect([&](Canvas::Ptr canvas, uint time, uint deltaTime)
+		{
+			sceneManager->nextFrame();
+		});
+
+		canvas->run();
+	});
+
+	sceneManager->assets()->load();
+
+	return 0;
+}
+
+
+Node::Ptr
+createPhysicsObject(unsigned int id, file::AssetLibrary::Ptr assets, bool isCube)
+{
+	const float mass		= MIN_MASS  + (rand() / (float)RAND_MAX) * (MAX_MASS - MIN_MASS);
+	const float size		= MIN_SCALE + (rand() / (float)RAND_MAX) * (MAX_SCALE - MIN_SCALE);
+	const float halfSize	= 0.5f * size;
+	auto		color		= Color::hslaToRgba(rand() / (float)RAND_MAX, 1.0f, 0.8f, 0.5f);
+
+	bullet::Collider::Ptr collider = nullptr;
+
+	if (isCube)
+	{
+		auto boxColliderData = bullet::ColliderData::create(
+			mass,
+			bullet::BoxShape::create(halfSize, halfSize, halfSize)
+		);
+
+		collider = bullet::Collider::create(boxColliderData);
+	}
+	else
+	{
+		auto sphColliderData = bullet::ColliderData::create(
+			mass,
+			bullet::SphereShape::create(halfSize) 
+		);
+		collider = bullet::Collider::create(sphColliderData);
+	}
+
+	return scene::Node::create("node_" + std::to_string(id))
+		->addComponent(Transform::create(
+			Matrix4x4::create()
+				->appendScale(size)
+				->appendTranslation(0.0f, START_ALTITUDE, 0.0f)
+		))
+		->addComponent(Surface::create(
+			assets->geometry(isCube ? "cube" : "sphere"),
+			material::BasicMaterial::create()
+				->diffuseColor(color)
+				->isTransparent(true, true)
+				->triangleCulling(render::TriangleCulling::BACK),
+			assets->effect("basic")
+		))
+		->addComponent(collider);
+}
