9426921cbeed4422624a536926b5f927b7fbc265
Pierre-Edouard Landes <pierreedouard.landes@aerys.in>
2014-02-07 13:43:02 +0100

    Started oculus example.
    Former-commit-id: 4e0193c9251a7e5629efe518a573b82df727e183
    Former-commit-id: 9a5a7df058325c318c509c4192a060943ce6689b
    Former-commit-id: f49f9c5e5b3787aea6503dbd1928a585bf3ca973

---
+++ examples/oculus/premake5.lua
@@ -0,0 +1,20 @@
+PROJECT_NAME = path.getname(os.getcwd())
+
+minko.project.application("minko-example-" .. PROJECT_NAME)
+
+	language "c++"
+	kind "ConsoleApp"
+
+	files {
+		"src/**.cpp",
+		"src/**.hpp",
+		"asset/**"
+	}
+	
+	includedirs { "src" }
+
+	-- plugins
+	minko.plugin.enable("oculus")
+	minko.plugin.enable("sdl")
+	
+	minko.plugin.import("angle")
