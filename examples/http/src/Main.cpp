312af9cc13712441dbe94cc8b70c25d4e50cf921
Warren Seine <warren.seine@aerys.in>
2014-02-11 16:15:56 +0100

    Fix thread synchronisation with workers.
    Former-commit-id: 839a0854c6d8f04b2aade46fcb0134010e274f5e

--- examples/http/src/Main.cpp
+++ examples/http/src/Main.cpp
@@ -33,6 +33,7 @@ int main(int argc, char** argv)
 {
 	auto canvas = Canvas::create("Minko Example - HTTP", 800, 600);
 
+	canvas->registerWorker<async::HTTPWorker>("http");
 	AbstractCanvas::defaultCanvas(canvas);
 
 	auto sceneManager = SceneManager::create(canvas->context());
