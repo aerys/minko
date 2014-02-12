d2a8e6a4965c88e13e5624e0314721a2b70a1bfd
Warren Seine <warren.seine@aerys.in>
2014-02-12 16:48:07 +0100

    Add Windows support to emscripten.lua.
    Former-commit-id: 815f8eb7f0f8d58a46339332394b5b6db14c0949
    Former-commit-id: 0b5c3688c247cecca56fbce614ab37e93b693a9e
    Former-commit-id: 166ff290704f02436426d52259825bb39af749e8

--- modules/emscripten/emscripten.lua
+++ modules/emscripten/emscripten.lua
@@ -12,21 +12,37 @@ local project = premake.project
 local config = premake.config
 local fileconfig = premake.fileconfig
 
-local insert = require 'insert'
-
-insert.insert(premake.tools.gcc, 'tools.emscripten', {
-	cc = MINKO_HOME .. '/tools/lin/scripts/emcc.sh',
-	cxx = MINKO_HOME .. '/tools/lin/scripts/em++.sh',
-	ar = MINKO_HOME .. '/tools/lin/scripts/emar.sh'
-})
-
-insert.insert(premake.tools.gcc, 'cppflags.system.emscripten', {
-	"-DEMSCRIPTEN",
-	"-Wno-warn-absolute-paths"
-})
-
 if os.getenv('EMSCRIPTEN') then
 	EMSCRIPTEN = os.getenv('EMSCRIPTEN');
 else
 	print(color.fg.yellow .. 'You must define the environment variable EMSCRIPTEN to be able to target HTML5.' .. color.reset)
 end
+
+print(color.fg.yellow .. 'Foo bar.' .. color.reset)
+
+local insert = require 'insert'
+
+if os.is('linux') then
+	insert.insert(premake.tools.gcc, 'tools.emscripten', {
+		cc = MINKO_HOME .. '/tools/lin/scripts/emcc.sh',
+		cxx = MINKO_HOME .. '/tools/lin/scripts/em++.sh',
+		ar = MINKO_HOME .. '/tools/lin/scripts/emar.sh'
+	})
+elseif os.is('macosx') then
+	insert.insert(premake.tools.gcc, 'tools.emscripten', {
+		cc = MINKO_HOME .. '/tools/mac/scripts/emcc.sh',
+		cxx = MINKO_HOME .. '/tools/mac/scripts/em++.sh',
+		ar = MINKO_HOME .. '/tools/mac/scripts/emar.sh'
+	})
+elseif os.is('windows') then
+	insert.insert(premake.tools.gcc, 'tools.emscripten', {
+		cc = '"' .. EMSCRIPTEN .. '\\emcc.bat"',
+		cxx = '"' .. EMSCRIPTEN .. '\\em++.bat"',
+		ar = '"' .. EMSCRIPTEN .. '\\emar.bat"'
+	})
+end
+
+insert.insert(premake.tools.gcc, 'cppflags.system.emscripten', {
+	"-DEMSCRIPTEN",
+	"-Wno-warn-absolute-paths"
+})
