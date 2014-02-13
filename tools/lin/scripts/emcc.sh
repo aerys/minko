64d3f8b3f9a73aacbedde11651c2d3af9bff7ae7
Warren Seine <warren.seine@aerys.in>
2014-02-13 12:14:57 +0100

    Highlight warnings and errors in emcc.
    Former-commit-id: dc926d72a83fbe4b87f0d00dab5e555c4fbc4ed4
    Former-commit-id: d759af21aceb0b27ad2fd26e0a889e1fbc6cb0cf

--- tools/lin/scripts/emcc.sh
+++ tools/lin/scripts/emcc.sh
@@ -17,6 +17,6 @@ done
 
 test "$verbose" != 0 && echo "${BIN} ${ARGS}"
 
-python "${BIN}" "${ARGS[@]}" #2>&1 >/dev/null | GREP_COLOR="${YELLOW}" grep -E -i --color 'warning|$'
+python "${BIN}" "${ARGS[@]}" 2>&1 >/dev/null | GREP_COLOR="${YELLOW}" grep -E -i --color '^warning|$' | GREP_COLOR="${RED}" grep -E -i --color '^error|$'
 
 exit ${PIPESTATUS[0]}
