3841795f97cda5582c0f2cbcb0bfe53ff64a3f39
Jonathan JARRI <jonathan.jarri@aerys.in>
2014-02-18 12:05:44 +0100

    Add skeleton for all tutorial about scenes and update premake files.
    Now, premake scripts work when we call them from shortcut.
    
    
    Former-commit-id: b9ddd280eaf807b203bc43770a598aaffe6d837e

--- tools/win/scripts/build_html5.bat
+++ tools/win/scripts/build_html5.bat
@@ -1,3 +1,8 @@
+:: If the script is called from another location
+:: => we go directly in the real script location
+set exdir=%~dp0
+cd /%exdir:~0,1% %exdir%
+
 chdir ..\..\..
 call "%EMSCRIPTEN%\..\..\emsdk_add_path.bat"
 tools\win\bin\premake5.exe --no-tests gmake
--- tools/win/scripts/premake_dist.bat
+++ tools/win/scripts/premake_dist.bat
@@ -1,5 +1,10 @@
 call premake_documentation.bat
 
+:: If the script is called from another location
+:: => we go directly in the real script location
+set exdir=%~dp0
+cd /%exdir:~0,1% %exdir%
+
 chdir ..\..\..
 tools\win\bin\premake5.exe dist
 cd tools\win\scripts
