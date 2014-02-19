a6c033f643085c53bd08a5e73411692c1b03de98
Julien Clouté <julien.cloute@aerys.in>
2014-02-19 14:56:17 +0100

    Update several tutorials. Sources and assets added.
    Former-commit-id: d4af8c0e96f4b6898ca2bce9e36dc93fce905ac8
    Former-commit-id: 7a95335debc2a1b68eb671c59bbab45fac644562

--- tutorial/binding-the-model-to-world-transform/src/main.cpp
+++ tutorial/binding-the-model-to-world-transform/src/main.cpp
@@ -21,62 +21,46 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 #include "minko/MinkoSDL.hpp"
 
 using namespace minko;
-using namespace minko::component;
 using namespace minko::math;
+using namespace minko::component;
 
-const uint WINDOW_WIDTH		= 800;
-const uint WINDOW_HEIGHT	= 600;
+const uint WINDOW_WIDTH = 800;
+const uint WINDOW_HEIGHT = 600;
 
-int main(int argc, char** argv)
+int
+main(int argc, char** argv)
 {
-	auto canvas = Canvas::create("Minko Application", WINDOW_WIDTH, WINDOW_HEIGHT);
-	auto sceneManager = SceneManager::create(canvas->context());
-	
-	sceneManager->assets()
-		->geometry("cube", geometry::CubeGeometry::create(sceneManager->assets()->context()))
-		->queue("effect/Phong.effect");
+	auto canvas = Canvas::create("Hello cube!", WINDOW_WIDTH, WINDOW_HEIGHT);
+	auto sceneManager = component::SceneManager::create(canvas->context());
 
-	auto _ = sceneManager->assets()->complete()->connect([=](file::AssetLibrary::Ptr assets)
+	sceneManager->assets()->queue("effect/Basic.effect");
+	auto complete = sceneManager->assets()->complete()->connect([&](file::AssetLibrary::Ptr assets)
 	{
 		auto root = scene::Node::create("root")
-			->addComponent(sceneManager);
+			->addComponent(sceneManager)
+			->addComponent(Renderer::create(0x7f7f7fff));
 
-		auto camera = scene::Node::create("camera")
-			->addComponent(Renderer::create(0x7f7f7fff))
-			->addComponent(Transform::create(
-				Matrix4x4::create()->lookAt(Vector3::zero(), Vector3::create(0.f, 0.f, 3.f))
+		auto myCustomEffect = assets->effect("effect/MyCustomEffect.effect");
+		auto myCustomMaterial = material::MyCustomMaterial::create();
+		auto cube = scene::Node::create("cube")
+		->addComponent(Transform::create(
+			Matrix4x4::create()->translation(0.f, 0.f, -5.f)
 			))
-			->addComponent(PerspectiveCamera::create(
-				(float)WINDOW_WIDTH / (float)WINDOW_HEIGHT, (float)PI * 0.25f, .1f, 1000.f)
-			);
-		root->addChild(camera);
-		
-		auto mesh = scene::Node::create("mesh")
-			->addComponent(Transform::create())
 			->addComponent(Surface::create(
-				assets->geometry("cube"),
-				material::Material::create()
-					->set("diffuseColor",	Vector4::create(1.f, 1.f, 1.f, 1.f)),
-				assets->effect("effect/Phong.effect")
+			geometry::CubeGeometry::create(assets->context()),
+			myCustomMaterial,
+			myCustomEffect
 			));
-		root->addChild(mesh);
+		root->addChild(cube);
 
-		auto light = scene::Node::create("light")
-			->addComponent(AmbientLight::create())
-			->addComponent(DirectionalLight::create())
-			->addComponent(Transform::create(
-				Matrix4x4::create()->lookAt(Vector3::zero(), Vector3::create(-2.f, -1.f, -1.f))
-			));
-		root->addChild(light);
+		myCustomMaterial->color(Vector4::create(1.f, 0.f, 0.f, 1.f));
 
-		auto resized = canvas->resized()->connect([&](AbstractCanvas::Ptr canvas, uint w, uint h)
-		{
-			camera->component<PerspectiveCamera>()->aspectRatio((float)w / (float)h);
-		});
+		myCustomEffect->setUniform("uViewMatrix", Matrix4x4::create());
+		myCustomEffect->setUniform("uProjectionMatrix", Matrix4x4::create()->perspective((float)WINDOW_WIDTH / (float)WINDOW_HEIGHT));
 
 		auto enterFrame = canvas->enterFrame()->connect([&](Canvas::Ptr canvas, uint time, uint deltaTime)
 		{
-			mesh->component<Transform>()->matrix()->appendRotationY(.01f);
+			cube->component<Transform>()->matrix()->prependRotationY(0.01f);
 
 			sceneManager->nextFrame();
 		});
--- tutorial/create-your-first-custom-effect/src/main.cpp
+++ tutorial/create-your-first-custom-effect/src/main.cpp
@@ -21,62 +21,47 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 #include "minko/MinkoSDL.hpp"
 
 using namespace minko;
-using namespace minko::component;
 using namespace minko::math;
+using namespace minko::component;
 
-const uint WINDOW_WIDTH		= 800;
-const uint WINDOW_HEIGHT	= 600;
+const uint WINDOW_WIDTH = 800;
+const uint WINDOW_HEIGHT = 600;
 
-int main(int argc, char** argv)
+int
+main(int argc, char** argv)
 {
-	auto canvas = Canvas::create("Minko Application", WINDOW_WIDTH, WINDOW_HEIGHT);
-	auto sceneManager = SceneManager::create(canvas->context());
-	
-	sceneManager->assets()
-		->geometry("cube", geometry::CubeGeometry::create(sceneManager->assets()->context()))
-		->queue("effect/Phong.effect");
+	auto canvas = Canvas::create("Hello cube!", WINDOW_WIDTH, WINDOW_HEIGHT);
+	auto sceneManager = component::SceneManager::create(canvas->context());
 
-	auto _ = sceneManager->assets()->complete()->connect([=](file::AssetLibrary::Ptr assets)
+	sceneManager->assets()->queue("effect/Basic.effect");
+	auto complete = sceneManager->assets()->complete()->connect([&](file::AssetLibrary::Ptr assets)
 	{
+		auto myCustomEffect = assets->effect("effect/MyCustomEffect.effect");
+
 		auto root = scene::Node::create("root")
-			->addComponent(sceneManager);
+			->addComponent(sceneManager)
+			->addComponent(Renderer::create(0x7f7f7fff));
 
-		auto camera = scene::Node::create("camera")
-			->addComponent(Renderer::create(0x7f7f7fff))
-			->addComponent(Transform::create(
-				Matrix4x4::create()->lookAt(Vector3::zero(), Vector3::create(0.f, 0.f, 3.f))
-			))
-			->addComponent(PerspectiveCamera::create(
-				(float)WINDOW_WIDTH / (float)WINDOW_HEIGHT, (float)PI * 0.25f, .1f, 1000.f)
-			);
-		root->addChild(camera);
-		
-		auto mesh = scene::Node::create("mesh")
-			->addComponent(Transform::create())
+		auto cube = scene::Node::create("cube")
 			->addComponent(Surface::create(
-				assets->geometry("cube"),
-				material::Material::create()
-					->set("diffuseColor",	Vector4::create(1.f, 1.f, 1.f, 1.f)),
-				assets->effect("effect/Phong.effect")
+			geometry::CubeGeometry::create(assets->context()),
+			material::BasicMaterial::create()->diffuseColor(Vector4::create(0.f, 0.f, 1.f, 1.f)),
+			myCustomEffect
 			));
-		root->addChild(mesh);
+		root->addChild(cube);
 
-		auto light = scene::Node::create("light")
-			->addComponent(AmbientLight::create())
-			->addComponent(DirectionalLight::create())
-			->addComponent(Transform::create(
-				Matrix4x4::create()->lookAt(Vector3::zero(), Vector3::create(-2.f, -1.f, -1.f))
-			));
-		root->addChild(light);
+		auto modelToWorldMatrix = Matrix4x4::create()->translation(0.f, 0.f, -5.f);
 
-		auto resized = canvas->resized()->connect([&](AbstractCanvas::Ptr canvas, uint w, uint h)
+		myCustomEffect->setUniform("uModelToWorldMatrix", modelToWorldMatrix);
+		myCustomEffect->setUniform("uViewMatrix", Matrix4x4::create());
+		myCustomEffect->setUniform("uProjectionMatrix", Matrix4x4::create()->perspective((float)WINDOW_WIDTH / (float)WINDOW_HEIGHT, (float)PI * 0.25f, .1f, 1000.f));
+
+		myCustomEffect->setUniform("uColor", Vector4::create(0.f, 0.f, 1.f, 1.f));
+
+		auto enterFrame = canvas->enterFrame()->connect([&](Canvas::Ptr canvas, uint t, uint dt)
 		{
-			camera->component<PerspectiveCamera>()->aspectRatio((float)w / (float)h);
-		});
-
-		auto enterFrame = canvas->enterFrame()->connect([&](Canvas::Ptr canvas, uint time, uint deltaTime)
-		{
-			mesh->component<Transform>()->matrix()->appendRotationY(.01f);
+			modelToWorldMatrix->prependRotationY(0.01f);
+			myCustomEffect->setUniform("uModelToWorldMatrix", modelToWorldMatrix);
 
 			sceneManager->nextFrame();
 		});
