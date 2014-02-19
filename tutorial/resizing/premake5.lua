065094b1f2186e0655f689fe66ffd1725aa78dbc
Jonathan JARRI <jonathan.jarri@aerys.in>
2014-02-19 11:28:55 +0100

    Add the new tutorial directory.
    Former-commit-id: d876fc7c21b23a162932f551c708a515ee64ee16

---
+++ tutorial/resizing/premake5.lua
@@ -0,0 +1,21 @@
+PROJECT_NAME = path.getname(os.getcwd())
+
+minko.project.application("minko-tutorial-" .. PROJECT_NAME)
+
+	language "c++"
+	kind "ConsoleApp"
+
+	files {
+		"src/**.cpp",
+		"src/**.hpp"
+	}
+	
+	includedirs { "src" }
+
+	-- plugins
+	minko.plugin.enable("sdl")
+	--minko.plugin.enable("bullet")
+	--minko.plugin.enable("jpeg")
+	--minko.plugin.enable("mk")
+	--minko.plugin.enable("particles")
+	--minko.plugin.enable("png")
