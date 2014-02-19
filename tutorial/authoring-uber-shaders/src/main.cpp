7a95335debc2a1b68eb671c59bbab45fac644562
Julien Clouté <julien.cloute@aerys.in>
2014-02-19 14:56:17 +0100

    Update several tutorials. Sources and assets added.
    Former-commit-id: d4af8c0e96f4b6898ca2bce9e36dc93fce905ac8

--- tutorial/authoring-uber-shaders/src/main.cpp
+++ tutorial/authoring-uber-shaders/src/main.cpp
@@ -18,66 +18,59 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 */
 
 #include "minko/Minko.hpp"
+#include "minko/MinkoPNG.hpp"
 #include "minko/MinkoSDL.hpp"
 
 using namespace minko;
-using namespace minko::component;
 using namespace minko::math;
+using namespace minko::component;
 
-const uint WINDOW_WIDTH		= 800;
-const uint WINDOW_HEIGHT	= 600;
+const uint WINDOW_WIDTH = 800;
+const uint WINDOW_HEIGHT = 600;
 
-int main(int argc, char** argv)
+int
+main(int argc, char** argv)
 {
-	auto canvas = Canvas::create("Minko Application", WINDOW_WIDTH, WINDOW_HEIGHT);
-	auto sceneManager = SceneManager::create(canvas->context());
-	
-	sceneManager->assets()
-		->geometry("cube", geometry::CubeGeometry::create(sceneManager->assets()->context()))
-		->queue("effect/Phong.effect");
+	auto canvas = Canvas::create("Hello cube!", WINDOW_WIDTH, WINDOW_HEIGHT);
+	auto sceneManager = component::SceneManager::create(canvas->context());
 
-	auto _ = sceneManager->assets()->complete()->connect([=](file::AssetLibrary::Ptr assets)
+	sceneManager->assets()
+		->registerParser<file::PNGParser>("png")
+		->queue("effect/MyCustomUberEffect.effect")
+		->queue("texture/box.png");
+
+	auto complete = sceneManager->assets()->complete()->connect([&](file::AssetLibrary::Ptr assets)
 	{
 		auto root = scene::Node::create("root")
 			->addComponent(sceneManager);
 
 		auto camera = scene::Node::create("camera")
 			->addComponent(Renderer::create(0x7f7f7fff))
-			->addComponent(Transform::create(
-				Matrix4x4::create()->lookAt(Vector3::zero(), Vector3::create(0.f, 0.f, 3.f))
-			))
 			->addComponent(PerspectiveCamera::create(
-				(float)WINDOW_WIDTH / (float)WINDOW_HEIGHT, (float)PI * 0.25f, .1f, 1000.f)
+			(float)WINDOW_WIDTH / (float)WINDOW_HEIGHT, (float)PI * 0.25f, .1f, 1000.f)
 			);
 		root->addChild(camera);
-		
-		auto mesh = scene::Node::create("mesh")
-			->addComponent(Transform::create())
+
+		auto texturedCube = scene::Node::create("texturedCube")
+		->addComponent(Transform::create(Matrix4x4::create()->translation(-2.f, 0.f, -5.f)))
 			->addComponent(Surface::create(
-				assets->geometry("cube"),
-				material::Material::create()
-					->set("diffuseColor",	Vector4::create(1.f, 1.f, 1.f, 1.f)),
-				assets->effect("effect/Phong.effect")
+			geometry::CubeGeometry::create(assets->context()),
+			material::Material::create()->set("diffuseMap", sceneManager->assets()->texture("texture/box.png")),
+			assets->effect("effect/MyCustomUberEffect.effect")
 			));
-		root->addChild(mesh);
+		root->addChild(texturedCube);
 
-		auto light = scene::Node::create("light")
-			->addComponent(AmbientLight::create())
-			->addComponent(DirectionalLight::create())
-			->addComponent(Transform::create(
-				Matrix4x4::create()->lookAt(Vector3::zero(), Vector3::create(-2.f, -1.f, -1.f))
+		auto coloredCube = scene::Node::create("coloredCube")
+		->addComponent(Transform::create(Matrix4x4::create()->translation(2.f, 0.f, -5.f)))
+			->addComponent(Surface::create(
+			geometry::CubeGeometry::create(assets->context()),
+			material::Material::create()->set("diffuseColor", Vector4::create(0.f, 0.f, 1.f, 1.f)),
+			assets->effect("effect/MyCustomUberEffect.effect")
 			));
-		root->addChild(light);
-
-		auto resized = canvas->resized()->connect([&](AbstractCanvas::Ptr canvas, uint w, uint h)
-		{
-			camera->component<PerspectiveCamera>()->aspectRatio((float)w / (float)h);
-		});
+		root->addChild(coloredCube);
 
 		auto enterFrame = canvas->enterFrame()->connect([&](Canvas::Ptr canvas, uint time, uint deltaTime)
 		{
-			mesh->component<Transform>()->matrix()->appendRotationY(.01f);
-
 			sceneManager->nextFrame();
 		});
 
