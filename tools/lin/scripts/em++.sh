732a02966db217944e5effc0adee50250f9fadab
Warren Seine <warren.seine@aerys.in>
2014-02-24 23:37:16 +0100

    Fix library ordering with em++ (again, this time for shared libraries).
    Former-commit-id: 8de3660abfd2f2a51a5728117145c91efd62a580

--- tools/lin/scripts/em++.sh
+++ tools/lin/scripts/em++.sh
@@ -44,10 +44,30 @@ for LIB in "${EXPECTED_ORDER[@]}"; do
 	done
 done
 
+declare -a NEW_SHARED_LIBS
+
+for ARG in "${SHARED_LIBS[@]}"; do
+	for LIB in "${EXPECTED_ORDER[@]}"; do
+		if [[ ${ARG} = -l*${LIB} ]]; then
+			continue 2
+		fi
+	done
+	NEW_SHARED_LIBS+=("${ARG}")
+done
+
+for LIB in "${EXPECTED_ORDER[@]}"; do
+	for ARG in "${SHARED_LIBS[@]}"; do
+		if [[ ${ARG} = -l*${LIB} ]]; then
+			NEW_SHARED_LIBS+=("${ARG}")
+			continue 2
+		fi
+	done
+done
+
 if [[ ${#NEW_STATIC_LIBS[0]} -eq 0 && ${#SHARED_LIBS[@]} -eq 0 ]]; then
 	test "$verbose" != 0 && echo "${BIN} ${ARGS[@]}"
-	python "${BIN}" "${ARGS[@]}"
+	python "${BIN}" "${ARGS[@]}" 2>&1 >/dev/null
 else
-	test "$verbose" != 0 && echo "${BIN} ${ARGS[@]} ${NEW_STATIC_LIBS[@]} ${SHARED_LIBS[@]}"
-	python "${BIN}" "${ARGS[@]}" "${NEW_STATIC_LIBS[@]}" "${SHARED_LIBS[@]}"
+	test "$verbose" != 0 && echo "${BIN} ${ARGS[@]} ${NEW_STATIC_LIBS[@]} ${NEW_SHARED_LIBS[@]}"
+	python "${BIN}" "${ARGS[@]}" "${NEW_STATIC_LIBS[@]}" "${NEW_SHARED_LIBS[@]}"
 fi
