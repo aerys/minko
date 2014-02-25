04d42b3df72ffec16f0ca3a27ceb07ba5712bf8a
Jonathan JARRI <jonathan.jarri@aerys.in>
2014-02-25 11:40:33 +0100

    Add FXAA tutorial structure and readd deleted 3D files for "Load 3D files" tutorial.
    Former-commit-id: b4caca460aea3895bad571556057086c6be3e053

--- premake5.lua
+++ premake5.lua
@@ -103,6 +103,7 @@ solution "minko"
 		include 'tutorial/27-working-with-point-lights'
 		include 'tutorial/28-working-with-spot-lights'
 		include 'tutorial/29-hello-falling-cube'
+		include 'tutorial/30-applying-antialiasing-effect'
 	end
 	
 	-- tests
