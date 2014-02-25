487adc7470f4e615da2d53c156b280063342014a
Benjamin Michel <benjamin.michel@aerys.in>
2014-02-25 16:18:54 +0100

    Added a check for texture size
    Former-commit-id: 866decc85b9e529728123f59fc4e567a7d48d3af

--- plugins/devil/src/minko/file/DevILParser.cpp
+++ plugins/devil/src/minko/file/DevILParser.cpp
@@ -105,6 +105,10 @@ DevILParser::computeDimensions()
 		maxPow = 1;
 
 	w = std::pow(2.f, maxPow);
+
+	if (w > render::AbstractTexture::MAX_SIZE)
+		w = render::AbstractTexture::MAX_SIZE;
+
 	h = w;
 
 	if (w != width || h != height)
