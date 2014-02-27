204897105c2b59b13caad26a17fc539da7edddd3
Pierre-Edouard Landes <pierreedouard.landes@aerys.in>
2014-02-27 11:01:51 +0100

    Updated particle code so that it fits Minko coding guidelines.
    Former-commit-id: 67f30917b0767f2ee71901b8b711f6a5d2aa3455

--- plugins/particles/src/minko/particle/shape/Cone.cpp
+++ plugins/particles/src/minko/particle/shape/Cone.cpp
@@ -1,3 +1,22 @@
+/*
+Copyright (c) 2013 Aerys
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 #include "minko/particle/shape/Cone.hpp"
 #include "minko/particle/ParticleData.hpp"
 
@@ -31,23 +50,23 @@ void
 Cone::initParticle(ParticleData& particle,
 				   bool direction) const
 {
-	float theta		= (rand01() * 2.f - 1.f) * (float)PI;
+	float theta		= (tools::rand01() * 2.f - 1.f) * (float)PI;
 	
-	float cosTheta	= cos(theta);
-	float sinTheta	= sin(theta);
+	float cosTheta	= cosf(theta);
+	float sinTheta	= sinf(theta);
 	
-	float r			= _innerRadius + sqrt(rand01()) * (_baseRadius - _innerRadius);
+	float r			= _innerRadius + sqrt(tools::rand01()) * (_baseRadius - _innerRadius);
 	
-	r = rand01() > .5 ? r : -r;
+	r = tools::rand01() > .5 ? r : -r;
 
 	particle.x = r * cosTheta;
 	particle.y = 0;
 	particle.z = r * sinTheta;
 
 	float angle		= _angle * r / _baseRadius;
-	float height	= rand01() * _length * cos(angle);
+	float height	= tools::rand01() * _length * cos(angle);
 
-	r += height * tan(angle);
+	r += height * tanf(angle);
 
 	if (direction)
 	{
