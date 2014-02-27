78ab2b36db6522c4440a48b2803a740b02dde6cb
Jonathan JARRI <jonathan.jarri@aerys.in>
2014-02-27 11:02:36 +0100

    Remove useless lines into Windows batch scripts.
    Former-commit-id: 6b93da08a557cee33addd8b666d91d5d2d6132e3

--- tool/win/script/build_html5.bat
+++ tool/win/script/build_html5.bat
@@ -1,8 +1,3 @@
-:: If the script is called from another location
-:: => we go directly in the real script location
-set exdir=%~dp0
-cd /%exdir:~0,1% %exdir%
-
 chdir ..\..\..
 call "%EMSCRIPTEN%\..\..\emsdk_add_path.bat"
 tool\win\bin\premake5.exe --no-test gmake
