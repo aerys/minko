d52e177bd7109d803ca07bf768b117be141dc83e
Warren Seine <warren.seine@aerys.in>
2014-02-27 11:25:07 +0100

    Remove double newaction.
    Former-commit-id: bd6eb28412a489cb34cc6ea096bb537fff7a1588

--- plugin/jpeg/premake5.lua
+++ plugin/jpeg/premake5.lua
@@ -1,8 +1,3 @@
-newoption {
-	trigger		= "with-jpeg",
-	description	= "Enable the Minko JPEG plugin."
-}
-
 PROJECT_NAME = path.getname(os.getcwd())
 
 minko.project.library("minko-plugin-" .. PROJECT_NAME)
