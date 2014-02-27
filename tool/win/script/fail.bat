6b93da08a557cee33addd8b666d91d5d2d6132e3
Jonathan JARRI <jonathan.jarri@aerys.in>
2014-02-27 11:02:36 +0100

    Remove useless lines into Windows batch scripts.

--- tool/win/script/fail.bat
+++ tool/win/script/fail.bat
@@ -1,10 +1,5 @@
 @echo off
 
-:: If the script is called from another location
-:: => we go directly in the real script location
-set exdir=%~dp0
-cd /%exdir:~0,1% %exdir%
-
 set target=%1
 set error=%errorlevel%
 
--- tool/win/script/premake_doc.bat
+++ tool/win/script/premake_doc.bat
@@ -1,10 +1,5 @@
 @echo off
 
-:: If the script is called from another location
-:: => we go directly in the real script location
-set exdir=%~dp0
-cd /%exdir:~0,1% %exdir%
-
 chdir ..\..\..
 tool\win\bin\premake5.exe doc
 pause
