796216a4cdd0f2f4154cb00f144707ed0b0e30e4
Benjamin Michel <benjamin.michel@aerys.in>
2014-03-03 13:30:01 +0100

    Fixed compilation of FileLoaderWorker in html5

--- framework/include/minko/async/FileLoaderWorker.hpp
+++ framework/include/minko/async/FileLoaderWorker.hpp
@@ -17,6 +17,7 @@ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
+#if !defined(EMSCRIPTEN)
 #include "minko/async/Worker.hpp"
 
 using namespace minko;
@@ -48,3 +49,4 @@ namespace minko
 		};
 	}
 }
+#endif
