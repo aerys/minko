600ae8691de4717ba7aa1fc185a8bd91cf296c9a
Warren Seine <warren.seine@aerys.in>
2014-03-06 17:33:44 +0100

    Only disable tests when generating the Xcode solution.

--- tool/lin/script/solution_xcode.sh
+++ tool/lin/script/solution_xcode.sh
@@ -3,5 +3,5 @@
 DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
 pushd ${DIR}/../../..
-tool/mac/script/premake5.sh --no-test --no-example --no-tutorial xcode
+tool/mac/script/premake5.sh --no-test xcode
 popd
