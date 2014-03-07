740958d6d3d7b4c1a2840eb82a4edf7ff09a840a
Warren Seine <warren.seine@aerys.in>
2014-03-07 21:38:50 +0100

    Add a Windows script to install the Emscripten SDK.

--- tool/win/script/build_html5.bat
+++ tool/win/script/build_html5.bat
@@ -3,19 +3,14 @@
 
 chdir ..\..\..
 
-if not defined EMSCRIPTEN (
-	echo Environment variable EMSCRIPTEN is not defined.
-	pause
-	exit /b 1
-)
-
 if not defined MINKO_HOME (
 	echo Environment variable MINKO_HOME is not defined.
 	pause
 	exit /b 1
 )
 
-call "%EMSCRIPTEN%\..\..\emsdk_add_path.bat"
+call "%MINKO_HOME%\tool\win\script\install_emscripten.bat"
+
 tool\win\bin\premake5.exe --no-test gmake
 mingw32-make SHELL=cmd.exe config=html5_release verbose=1
 pause
