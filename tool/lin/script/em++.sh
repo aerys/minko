1c1e4f1404f9dbcda5532ccb86f4b9ca8be68f5f
Warren Seine <warren.seine@aerys.in>
2014-03-13 17:38:49 +0100

    Fix em++ script when linking shared library.
    Former-commit-id: dacaccb48a335e4dc70cff9e04b99670ff9c3575

--- tool/lin/script/em++.sh
+++ tool/lin/script/em++.sh
@@ -23,6 +23,6 @@ if [[ ${#STATIC_LIBS[0]} -eq 0 && ${#SHARED_LIBS[@]} -eq 0 ]]; then
 	test "$verbose" != 0 && echo "${BIN} ${ARGS[@]}"
 	python "${BIN}" "${ARGS[@]}"
 else
-	test "$verbose" != 0 && echo "${BIN} ${ARGS[@]} ${STATIC_LIBS[@]} ${STATIC_LIBS[@]} ${SHARED_LIBS[@]}"
-	python "${BIN}" "${ARGS[@]}" "${STATIC_LIBS[@]}" "${STATIC_LIBS[@]}" "${SHARED_LIBS[@]}"
+	test "$verbose" != 0 && echo "${BIN} ${ARGS[@]} ${STATIC_LIBS[@]} ${STATIC_LIBS[@]} ${SHARED_LIBS[@]} ${SHARED_LIBS[@]}"
+	python "${BIN}" "${ARGS[@]}" "${STATIC_LIBS[@]}" "${STATIC_LIBS[@]}" "${SHARED_LIBS[@]}" "${SHARED_LIBS[@]}"
 fi
