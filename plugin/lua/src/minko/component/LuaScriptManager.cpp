5195011e7c43d3305f4ba84c485eaf66b03f7f1f
Benjamin Michel <benjamin.michel@aerys.in>
2014-03-17 17:07:19 +0100

    Fixed glue order in LuaScriptManager
    Former-commit-id: 39225c295a63bf01b6ba0ec2d1147506e5440a8e

--- plugin/lua/src/minko/component/LuaScriptManager.cpp
+++ plugin/lua/src/minko/component/LuaScriptManager.cpp
@@ -65,14 +65,9 @@ using namespace minko::component;
 void
 LuaScriptManager::initialize()
 {
-    AbstractScript::initialize();
-
+	AbstractScript::initialize();
+	_state.open();
 	initializeBindings();
-
-	_state
-		.func("getCanvas", &LuaContext::getCanvas)
-		.func("getSceneManager", &LuaContext::getSceneManager)
-		.func("getOption", &LuaContext::getOption);
 
 	_state.open().glue();
 }
@@ -80,10 +75,14 @@ LuaScriptManager::initialize()
 void
 LuaScriptManager::initialize(std::vector<std::function<void(LuaGlue&)>> bindingsFunctions)
 {
+	AbstractScript::initialize();
+	_state.open();
+	initializeBindings();
+
 	for (auto f : bindingsFunctions)
 		f(_state);
 
-	initialize();
+	_state.glue();
 }
 
 void
@@ -193,4 +192,9 @@ LuaScriptManager::initializeBindings()
         .property("assets",     &SceneManager::assets);
     MINKO_LUAGLUE_BIND_SIGNAL(_state, SceneManager::Ptr, float, float);
     sceneManager.property("nextFrame",  &SceneManager::frameBegin);
+
+	_state
+		.func("getCanvas", &LuaContext::getCanvas)
+		.func("getSceneManager", &LuaContext::getSceneManager)
+		.func("getOption", &LuaContext::getOption);
 }
