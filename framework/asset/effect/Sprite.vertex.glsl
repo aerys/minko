a92993bdb044a2c2f9cdd3ace5719dff828f13d9
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2014-04-06 15:09:23 +0200

    update the Sprite.effect to take the scale into account
    Former-commit-id: 6f08fe4c4feb48a4345308eeacc7044a4b55540a

--- framework/asset/effect/Sprite.vertex.glsl
+++ framework/asset/effect/Sprite.vertex.glsl
@@ -19,8 +19,9 @@ uniform mat4	uProjectionMatrix;
 void main()
 {
 	vec4 pos = uViewMatrix * uModelToWorldMatrix * vec4(0., 0., 0., 1.);
+	vec3 scale = mat3(uModelToWorldMatrix) * vec3(1.0);
 
-	pos += vec4(aPosition, 0.);
+	pos += vec4(aPosition * scale, 0.);
 	
 	#ifdef DIFFUSE_MAP
 		vUV = aUV;
