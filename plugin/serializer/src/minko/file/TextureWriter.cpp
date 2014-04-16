3190279c8a73cb826b4b37c47d5b09dfcc060704
Julien Clouté <julien.cloute@aerys.in>
2014-04-16 12:38:26 +0200

    Re-enable TextureWriter to use DevILWriter.
    Former-commit-id: 0ebc14151626f4ecd4ca662a3e30eca663f3bcd5

--- plugin/serializer/src/minko/file/TextureWriter.cpp
+++ plugin/serializer/src/minko/file/TextureWriter.cpp
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 #include "minko/render/AbstractTexture.hpp"
 #include "minko/render/Texture.hpp"
 #include "minko/file/Dependency.hpp"
-//#include "minko/file/DevILWriter.hpp"
+#include "minko/file/DevILWriter.hpp"
 #include "minko/Types.hpp"
 
 using namespace minko;
@@ -90,8 +90,6 @@ TextureWriter::embed(AssetLibraryPtr                    assetLibrary,
             break;
     }
 
-	return "";
-	/*
     auto textureData = std::vector<unsigned char> { };
 
     {
@@ -109,5 +107,5 @@ TextureWriter::embed(AssetLibraryPtr                    assetLibrary,
 
     auto textureContent = std::string(textureData.begin(), textureData.end());
 
-    return textureContent;*/
+    return textureContent;
 }
