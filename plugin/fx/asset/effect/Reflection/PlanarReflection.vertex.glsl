c2aa577a902a536de3c836d5ac50a3cbf734e44f
Benjamin Michel <benjamin.michel@aerys.in>
2014-05-13 18:29:33 +0200

    Fixed reflection and picking effects for rendering nodes with no position
    Former-commit-id: c003202aa8c993544775b30acec0474fa54dfae8

--- plugin/fx/asset/effect/Reflection/PlanarReflection.vertex.glsl
+++ plugin/fx/asset/effect/Reflection/PlanarReflection.vertex.glsl
@@ -34,20 +34,26 @@ void main(void)
 	#ifdef DIFFUSE_CUBEMAP
 		vertexUVW = position;
 	#endif 
-
-	vec4 pos = vec4(position, 1.0);
-
-	#ifdef NUM_BONES
-		pos = skinning_moveVertex(pos);
-	#endif // NUM_BONES
 	
-	#ifdef MODEL_TO_WORLD
-		pos = modelToWorldMatrix * pos;
-	#endif
+	#if defined(HAS_POSITION)
+		vec4 pos = vec4(position, 1.0);
 
-	gl_Position = Projection * ReflectedViewMatrix * pos;
+		#ifdef NUM_BONES
+			pos = skinning_moveVertex(pos);
+		#endif // NUM_BONES
 	
-	vPosition = pos.xyz / pos.w;
+		#ifdef MODEL_TO_WORLD
+			pos = modelToWorldMatrix * pos;
+		#endif
+
+		gl_Position = Projection * ReflectedViewMatrix * pos;
+	
+		vPosition = pos.xyz / pos.w;
+	#else //HAS_POSITION
+		gl_Position = vec4(0.0, 0.0, 1.1, 1.0);
+
+		vPosition = vec3(0.0, -1.0, 0.0);
+	#endif //HAS_POSITION
 }
 
 #endif // VERTEX_SHADER
