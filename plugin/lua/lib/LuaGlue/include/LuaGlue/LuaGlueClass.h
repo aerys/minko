ee74a3a191b427258bda14b8d48c25ae67af5ecc
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2014-05-20 09:04:15 +0200

    add LuaGlueClass::property() overrides to support chained setters than might return _Class*, _Class& or shared_ptr<Class>
    Former-commit-id: e510479f3c18137bbb621a404b8db7253d6106be

--- plugin/lua/lib/LuaGlue/include/LuaGlue/LuaGlueClass.h
+++ plugin/lua/lib/LuaGlue/include/LuaGlue/LuaGlueClass.h
@@ -260,11 +260,41 @@ class LuaGlueClass : public LuaGlueClassBase
 		LuaGlueClass<_Class> &property(const std::string &name, _Type(_Class::*getter)() const, void (_Class::*setter)(_Type))
 		{
 			//printf("property(%s)\n", name.c_str());
-			auto impl = new LuaGlueProperty<_Type, _Class>(this, name, getter, setter);
+			auto impl = new LuaGlueProperty<_Type, _Class, void>(this, name, getter, setter);
 			properties_.addSymbol(name.c_str(), impl);
 
 			return *this;
 		}
+
+        template<typename _Type>
+        LuaGlueClass<_Class> &property(const std::string &name, _Type(_Class::*getter)() const, _Class* (_Class::*setter)(_Type))
+        {
+            //printf("property(%s)\n", name.c_str());
+            auto impl = new LuaGlueProperty<_Type, _Class, _Class*>(this, name, getter, setter);
+            properties_.addSymbol(name.c_str(), impl);
+
+            return *this;
+        }
+
+        template<typename _Type>
+        LuaGlueClass<_Class> &property(const std::string &name, _Type(_Class::*getter)() const, _Class& (_Class::*setter)(_Type))
+        {
+            //printf("property(%s)\n", name.c_str());
+            auto impl = new LuaGlueProperty<_Type, _Class, _Class&>(this, name, getter, setter);
+            properties_.addSymbol(name.c_str(), impl);
+
+            return *this;
+        }
+
+        template<typename _Type>
+        LuaGlueClass<_Class> &property(const std::string &name, _Type(_Class::*getter)() const, std::shared_ptr<_Class> (_Class::*setter)(_Type))
+        {
+            //printf("property(%s)\n", name.c_str());
+            auto impl = new LuaGlueProperty<_Type, _Class, std::shared_ptr<_Class>>(this, name, getter, setter);
+            properties_.addSymbol(name.c_str(), impl);
+
+            return *this;
+        }
 		
 		template<typename _Type>
 		LuaGlueClass<_Class> &prop(const std::string &name, _Type (_Class::*getter)(), void (_Class::*setter)(_Type))
