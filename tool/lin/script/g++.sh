378b8df8208ac1d935a7ed67b37cfce8d519e347
Warren Seine <warren.seine@aerys.in>
2014-06-01 19:56:41 +0200

    Fix g++.sh script to support Android's gcc.
    Former-commit-id: ed7c184736d7d2e985c42613da51ebac2fd780ae

--- tool/lin/script/g++.sh
+++ tool/lin/script/g++.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-BIN="g++"
+BIN="$1"
 ARGS=""
+
+shift
 
 STATIC_LIBS=""
 SHARED_LIBS=""
@@ -16,10 +18,14 @@ for ARG in "$@"; do
 	fi
 done
 
-if [[ `uname -s` != "Darwin" && ( ! -z "${STATIC_LIBS}" || ! -z "${SHARED_LIBS}" ) ]]; then
-	 # Options --start-group/--end-group are not supported on OS X (because this is the default behavior)
-	START_GROUP="-Wl,--start-group"
-	END_GROUP="-Wl,--end-group"
+if [[ `uname -s` != "Darwin" || "${BIN}" != "g++" ]]; then
+	# Terrible workaround because --start-group/--end-group options are not
+	# supported on OS X (it's the default behavior).
+
+	if [[ ! -z "${STATIC_LIBS}" || ! -z "${SHARED_LIBS}" ]]; then
+		START_GROUP="-Wl,--start-group"
+		END_GROUP="-Wl,--end-group"
+	fi
 fi
 
 test "$verbose" != 0 && set -x
