52366a5dc7c9e61d641643b365d6212102f7a53b
Warren Seine <warren.seine@aerys.in>
2014-06-01 08:35:35 +0200

    Make tests runnable under an offscreen environment (pass --with-offscreen to premake when building).
    Former-commit-id: 57573e5147f2ffcef9ff5278c714ae7ed40a53f6

--- plugin/offscreen/plugin.lua
+++ plugin/offscreen/plugin.lua
@@ -21,7 +21,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 minko.plugin.offscreen = {}
 
 function minko.plugin.offscreen:enable()
-	defines { "MINKO_OFFSCREEN" }
+	defines { "MINKO_PLUGIN_OFFSCREEN" }
 
 	libdirs { "/opt/local/lib" }
 
