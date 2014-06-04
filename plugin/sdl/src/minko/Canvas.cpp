80240d7e45cbccf7e34c366ff80a8442a0b56eb0
Benjamin Michel <benjamin.michel@aerys.in>
2014-06-04 16:21:10 +0200

    Revert "Removed redeclaration of variable"
    This reverts commit 30ca5f293cd6f2d08e2d6b84808a138c2c871559 [formerly 68bb46c5940f8ee2e329000f550a9de10cdbd5bc].
    
    
    Former-commit-id: d2eaf7491d3b19f713765dc3f16c79998b1c8577

--- plugin/sdl/src/minko/Canvas.cpp
+++ plugin/sdl/src/minko/Canvas.cpp
@@ -463,8 +463,8 @@ Canvas::step()
 
 			if (windowW != _width || windowH != _height)
 			{
-				x = int(float(_width) * float(event.motion.x) / float(windowW));
-				y = int(float(_height) * float(event.motion.y) / float(windowH));
+				auto x = int(float(_width) * float(event.motion.x) / float(windowW));
+				auto y = int(float(_height) * float(event.motion.y) / float(windowH));
 			}
 
 			_mouse->x(x);
