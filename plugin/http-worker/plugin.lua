7e5b58a2f882c48c1bbcb17b21b56801c7f771fd
Jonathan Jarri <jonathan.jarri@aerys.in>
2014-06-25 16:46:30 +0200

    Add missing libcurl file for OSX and harmonize folder hierarchy.
    Former-commit-id: 2575901bbd0e069e9190a61a5199a0b6bdb56899

--- plugin/http-worker/plugin.lua
+++ plugin/http-worker/plugin.lua
@@ -61,15 +61,15 @@ minko.plugin["http-worker"].enable = function (self)
 		links { "curl" }
 
 	configuration { "osx64" }
-		libdirs { minko.plugin.path("http-worker") .. "/lib/curl/lib/osx64" }
+		libdirs { minko.plugin.path("http-worker") .. "/lib/curl/lib/osx64/release" }
 		links { "curl", "Security.framework", "LDAP.framework" }
 
 	configuration { "android" }
-		libdirs { minko.plugin.path("http-worker") .. "/lib/curl/lib/android/debug" }
+		libdirs { minko.plugin.path("http-worker") .. "/lib/curl/lib/android/release" }
 		links { "curl" }
 
 	configuration { "ios" }
-		libdirs { minko.plugin.path("http-worker") .. "/lib/curl/lib/ios/debug" }
+		libdirs { minko.plugin.path("http-worker") .. "/lib/curl/lib/ios/release" }
 		links { "curl", "Security.framework" }
 end
 
