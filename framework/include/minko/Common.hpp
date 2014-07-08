2ea005d4bf61e05ef460c3935a8630612db5964e
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2014-07-08 17:50:22 +0200

    remove minko::traits all together
    Former-commit-id: e6088585488487ddfd01ed85304febb757f7c585

--- framework/include/minko/Common.hpp
+++ framework/include/minko/Common.hpp
@@ -374,35 +374,6 @@ namespace minko
 	{
 		class Worker;
 	}
-
-    namespace traits
-    {
-        template <typename T>
-        struct remove_all_pointers
-        {
-            typedef T type;
-        };
-        template <typename T>
-        struct remove_all_pointers<T*>
-        {
-            typedef typename remove_all_pointers<T>::type type;
-        };
-        template <typename T>
-        struct remove_all_pointers<T* const>
-        {
-            typedef typename remove_all_pointers<T>::type type;
-        };
-        template <typename T>
-        struct remove_all_pointers<T* volatile>
-        {
-            typedef typename remove_all_pointers<T>::type type;
-        };
-        template <typename T>
-        struct remove_all_pointers<T* const volatile >
-        {
-            typedef typename remove_all_pointers<T>::type type;
-        };
-    }
 }
 
 namespace std
