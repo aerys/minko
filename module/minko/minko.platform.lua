7ec14ce07221b4f69623289392976009fae632e1
Warren Seine <warren.seine@aerys.in>
2014-07-11 20:46:15 +0200

    Update minko.platform.supports to check for any supported target in a list.
    Former-commit-id: e58b44f583d88ebd6578160d360a217e6554bd77

--- module/minko/minko.platform.lua
+++ module/minko/minko.platform.lua
@@ -8,31 +8,38 @@ minko.platform.platforms = function(t)
 	end
 end
 
-minko.platform.supports = function(target)
-	if target == "html5" then
-		if EMSCRIPTEN and _ACTION == "gmake" then
-			return true
-		end
-	elseif target == "linux32" or target == "linux64" or target == "linux" then
-		if os.is("linux") then
-			return true
-		end
-	elseif target == "windows32" or target == "windows64" or target == "windows" then
-		if os.is("windows") then
-			return true
-		end
-	elseif target == "osx64" or target == "osx" then
-		if os.is("macosx")  and (_ACTION == "gmake" or _ACTION == "xcode-osx") then
-			return true
-		end
-	elseif target == "ios" then
-		if os.is("macosx") and (_ACTION == "gmake" or _ACTION == "xcode-ios") then
-			return true
-		end
-	elseif target == "android" then
-		if ANDROID_HOME and _ACTION == "gmake" then
-			return true
+minko.platform.supports = function(targets)
+	if type(targets) == "string" then
+		targets = string.explode(targets, " ")
+	end
+
+	for _, target in pairs(targets) do
+		if target == "html5" then
+			if EMSCRIPTEN and _ACTION == "gmake" then
+				return true
+			end
+		elseif target == "linux32" or target == "linux64" or target == "linux" then
+			if os.is("linux") then
+				return true
+			end
+		elseif target == "windows32" or target == "windows64" or target == "windows" then
+			if os.is("windows") then
+				return true
+			end
+		elseif target == "osx64" or target == "osx" then
+			if os.is("macosx")  and (_ACTION == "gmake" or _ACTION == "xcode-osx") then
+				return true
+			end
+		elseif target == "ios" then
+			if os.is("macosx") and (_ACTION == "gmake" or _ACTION == "xcode-ios") then
+				return true
+			end
+		elseif target == "android" then
+			if ANDROID_HOME and _ACTION == "gmake" then
+				return true
+			end
 		end
 	end
+
 	return false
 end
