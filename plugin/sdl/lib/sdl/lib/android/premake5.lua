1f203a7e3e130b1e14e25668128742f77a384eae
Warren Seine <warren.seine@aerys.in>
2014-07-23 15:12:39 +0200

    Add a project for build SDL for Android (in case we want to target more than ARM).
    Former-commit-id: 8ef870c12f31ba4a155686bbbb641dabfe36c02f

---
+++ plugin/sdl/lib/sdl/lib/android/premake5.lua
@@ -0,0 +1,54 @@
+minko.project.library("SDL2")
+
+	-- project(name)
+
+	-- language "C++"
+	-- kind "StaticLib"
+	-- location "."
+
+	targetdir "."
+	objdir "/tmp/sdl2-android/obj"
+
+	configuration { "android" }
+		defines {
+			"ANDROID",
+			"GL_GLEXT_PROTOTYPES"
+		}
+		files {
+			"lib/sdl/src/*.c",
+			"lib/sdl/src/audio/*.c",
+			"lib/sdl/src/audio/dummy/*.c",
+			"lib/sdl/src/atomic/*.c",
+			"lib/sdl/src/cpuinfo/*.c",
+			"lib/sdl/src/dynapi/*.c",
+			"lib/sdl/src/events/*.c",
+			"lib/sdl/src/file/*.c",
+			"lib/sdl/src/haptic/*.c",
+			"lib/sdl/src/haptic/dummy/*.c",
+			"lib/sdl/src/joystick/*.c",
+			"lib/sdl/src/loadso/dlopen/*.c",
+			"lib/sdl/src/power/*.c",
+			"lib/sdl/src/filesystem/dummy/*.c",
+			"lib/sdl/src/render/*.c",
+			"lib/sdl/src/stdlib/*.c",
+			"lib/sdl/src/thread/*.c",
+			"lib/sdl/src/thread/pthread/*.c",
+			"lib/sdl/src/timer/*.c",
+			"lib/sdl/src/timer/unix/*.c",
+			"lib/sdl/src/video/*.c",
+			"lib/sdl/src/test/*.c"
+		}
+
+		files {
+			"lib/sdl/src/audio/android/*.c",
+			"lib/sdl/src/core/android/*.c",
+			"lib/sdl/src/joystick/android/*.c",
+			"lib/sdl/src/power/android/*.c",
+			"lib/sdl/src/video/android/*.c",
+			"lib/sdl/src/main/android/*.c",
+			"lib/sdl/src/render/opengles2/*.c",
+			"lib/sdl/src/render/opengles/*.c",
+			"lib/sdl/src/render/software/*.c",
+		}
+
+		includedirs { minko.plugin.path("android") .. "/include" }
