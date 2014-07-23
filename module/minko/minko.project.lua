67a900dfb95464eeddad8e62dc26768e86e91308
Warren Seine <warren.seine@aerys.in>
2014-07-23 15:10:00 +0200

    Remove SDL2.framework and use a static library instead.
    Former-commit-id: efa295705b90f83e750102945c42fdc8b8549083

--- module/minko/minko.project.lua
+++ module/minko/minko.project.lua
@@ -161,9 +161,9 @@ minko.project.application = function(name)
 			"OpenGL.framework",
 			"IOKit.framework"
 		}
-		linkoptions {
-			"-Wl,-rpath,."
-		}
+		-- linkoptions {
+		-- 	"-Wl,-rpath,."
+		-- }
 		prelinkcommands {
 			minko.action.copy(minko.sdk.path("/framework/asset")),
 		}
