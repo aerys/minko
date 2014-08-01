53a366ff9027b819b42b064b280a13b180b4a030
Warren Seine <warren.seine@aerys.in>
2014-08-01 21:34:10 +0200

    Update code formatting code.
    Former-commit-id: b75e87a906110c1469f236ec9b628f588dde7c15

--- tool/lin/script/format_code.sh
+++ tool/lin/script/format_code.sh
@@ -4,13 +4,18 @@ ASTYLE="astyle"
 SED="sed"
 
 ASTYLE_OPTIONS="--style=allman --indent=spaces=4 --convert-tabs --break-blocks --unpad-paren --pad-oper --pad-header --close-templates --indent-preproc-define --indent-namespaces --indent-col1-comments --align-pointer=type --align-reference=type --lineend=linux"
-MODE_OPTIONS="--suffix=none --recursive"
-PROJECTS="framework plugin/*"
-FILES="src/*.cpp src/*.hpp include/*.hpp"
+MODE_OPTIONS="--suffix=none"
+PROJECTS="framework plugin/* example/*"
 
 for PROJECT in $PROJECTS ; do
+	echo "============================="
+	echo "Entering: ${PROJECT}"
+	echo "============================="
     pushd ${PROJECT}/${DIR} > /dev/null
-    ${ASTYLE} ${MODE_OPTIONS} ${STYLE_OPTIONS} ${FILES}
-    ${SED} -i'' -e 's/[ \t]*$//' ${FILES}
+    FILES=$(find src include -iname "*.hpp" -or -iname "*.cpp")
+    [[ -n "${FILES}" ]] && {
+	    ${ASTYLE} ${MODE_OPTIONS} ${ASTYLE_OPTIONS} ${FILES}
+    	${SED} -i'' -e 's/[ \t]*$//;s/2013 Aerys/2014 Aerys/' ${FILES}
+    }
     popd > /dev/null
 done
