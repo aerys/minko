df7f07f46b0c7e0489d95c53247fb8050a040ba1
Warren Seine <warren.seine@aerys.in>
2014-08-06 20:08:55 +0200

    Update BFG script to reduce repository size.
    Former-commit-id: cbf761de9c0ffe0f3fef1d22ed8971fa31e075c2

--- tool/lin/script/bfg.sh
+++ tool/lin/script/bfg.sh
@@ -6,26 +6,40 @@ set -e
 BFG_URL="http://repo1.maven.org/maven2/com/madgag/bfg/1.11.7/bfg-1.11.7.jar"
 BFG_BIN=`basename ${BFG_URL}`
 BFG="/tmp/${BFG_BIN}"
-MINKO_TMP="/tmp/minko"
 
-test -e ${BFG} || {
-	wget -O ${BFG} ${BFG_URL}
-}
-
-test -n ${MINKO_HOME} || {
-	echo "MINKO_HOME is not defined"
+[[ -n "$1" ]] || {
+	echo "usage: bfg.sh path/to/local/repo.git" 2> /dev/null
 	exit 1
 }
 
-rm -rf ${MINKO_TMP}
-mkdir -p ${MINKO_TMP}
-cd ${MINKO_TMP}
+REPO=`readlink -f $1`
+TMP="/tmp/bfg/`basename ${REPO}`"
 
-git clone --mirror ${MINKO_HOME} .
+[[ -e "${BFG}" ]] || {
+	wget -O ${BFG} ${BFG_URL}
+}
+
+[[ -d "${REPO}/.git"  ]] || {
+	echo "${REPO} is not a valid git repository"
+	exit 1
+}
+
+rm -rf ${TMP}
+mkdir -p ${TMP}
+cd ${TMP}
+
+git clone ${REPO} .
 
 BRANCHES=`git branch -a -r | grep -v HEAD | cut -d/ -f2,3 | tr "\\n" "," | sed 's/,$//'`
+# BRANCHES=master,dev
 
-java -jar ${BFG} --strip-blobs-bigger-than 500K --protect-blobs-from ${BRANCHES} .
+for BRANCH in ${BRANCHES//,/ }; do
+	git checkout ${BRANCH}
+done
+
+git checkout master
+
+java -jar ${BFG} --strip-blobs-bigger-than 100K --protect-blobs-from ${BRANCHES} .
 
 git reflog expire --expire=now --all
 git gc --prune=now --aggressive
