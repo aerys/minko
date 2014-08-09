be125b13e1be3cf9204f6f6bc63aee9320d81863
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2014-08-09 10:25:21 +0200

    add tests for component::DirectionalLight
    Former-commit-id: fb063356aab01cb9710035bb78463f6dd48f28c6
    Former-commit-id: 0c20f640ccb29903df7cd5f98553b501d8f16072

---
+++ test/src/minko/component/DirectionalLightTest.cpp
@@ -0,0 +1,200 @@
+/*
+Copyright (c) 2013 Aerys
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include "DirectionalLightTest.hpp"
+
+using namespace minko;
+using namespace minko::component;
+
+TEST_F(DirectionalLightTest, Create)
+{
+    try
+    {
+        auto al = DirectionalLight::create();
+    }
+    catch (...)
+    {
+        ASSERT_TRUE(false);
+    }
+}
+
+TEST_F(DirectionalLightTest, AddLight)
+{
+    auto root = scene::Node::create("root");
+    auto lights = scene::Node::create("lights");
+
+    lights->addComponent(DirectionalLight::create());
+    root->addChild(lights);
+
+    ASSERT_TRUE(root->data()->hasProperty("directionalLights.length"));
+    ASSERT_EQ(root->data()->get<uint>("directionalLights.length"), 1);
+    ASSERT_TRUE(root->data()->hasProperty("directionalLights[0].color"));
+    ASSERT_EQ(root->data()->get<math::vec3>("directionalLights[0].color"), math::vec3(1.f));
+    ASSERT_TRUE(root->data()->hasProperty("directionalLights[0].diffuse"));
+    ASSERT_EQ(root->data()->get<float>("directionalLights[0].diffuse"), 1.f);
+}
+
+TEST_F(DirectionalLightTest, RemoveSingleLight)
+{
+    auto root = scene::Node::create("root");
+    auto lights = scene::Node::create("lights");
+    auto al = DirectionalLight::create();
+
+    lights->addComponent(al);
+    root->addChild(lights);
+    lights->removeComponent(al);
+
+    ASSERT_EQ(root->data()->get<uint>("directionalLights.length"), 0);
+    ASSERT_FALSE(root->data()->hasProperty("directionalLights[0].color"));
+    ASSERT_FALSE(root->data()->hasProperty("directionalLights[0].diffuse"));
+}
+
+TEST_F(DirectionalLightTest, AddMultipleLights)
+{
+    auto root = scene::Node::create("root");
+    auto lights = scene::Node::create("lights");
+
+    root->addChild(lights);
+
+    auto al1 = DirectionalLight::create(.1f);
+    al1->color(math::vec3(1.f, 0.f, 0.f));
+    lights->addComponent(al1);
+
+    ASSERT_EQ(root->data()->get<uint>("directionalLights.length"), 1);
+    ASSERT_TRUE(root->data()->hasProperty("directionalLights[0].color"));
+    ASSERT_TRUE(root->data()->hasProperty("directionalLights[0].diffuse"));
+    ASSERT_EQ(root->data()->get<math::vec3>("directionalLights[0].color"), math::vec3(1.f, 0.f, 0.f));
+    ASSERT_EQ(root->data()->get<float>("directionalLights[0].diffuse"), .1f);
+
+    auto al2 = DirectionalLight::create(.2f);
+    al2->color(math::vec3(0.f, 1.f, 0.f));
+    lights->addComponent(al2);
+
+    ASSERT_EQ(root->data()->get<uint>("directionalLights.length"), 2);
+    ASSERT_TRUE(root->data()->hasProperty("directionalLights[1].color"));
+    ASSERT_TRUE(root->data()->hasProperty("directionalLights[1].diffuse"));
+    ASSERT_EQ(root->data()->get<math::vec3>("directionalLights[1].color"), math::vec3(0.f, 1.f, 0.f));
+    ASSERT_EQ(root->data()->get<float>("directionalLights[1].diffuse"), .2f);
+
+    auto al3 = DirectionalLight::create(.3f);
+    al3->color(math::vec3(0.f, 0.f, 1.f));
+    lights->addComponent(al3);
+
+    ASSERT_EQ(root->data()->get<uint>("directionalLights.length"), 3);
+    ASSERT_TRUE(root->data()->hasProperty("directionalLights[2].color"));
+    ASSERT_TRUE(root->data()->hasProperty("directionalLights[2].diffuse"));
+    ASSERT_EQ(root->data()->get<math::vec3>("directionalLights[2].color"), math::vec3(0.f, 0.f, 1.f));
+    ASSERT_EQ(root->data()->get<float>("directionalLights[2].diffuse"), .3f);
+}
+
+TEST_F(DirectionalLightTest, RemoveFirstLight)
+{
+    auto root = scene::Node::create("root");
+    auto lights = scene::Node::create("lights");
+
+    root->addChild(lights);
+
+    auto al1 = DirectionalLight::create(.1f);
+    al1->color(math::vec3(1.f, 0.f, 0.f));
+    lights->addComponent(al1);
+
+    auto al2 = DirectionalLight::create(.2f);
+    al2->color(math::vec3(0.f, 1.f, 0.f));
+    lights->addComponent(al2);
+
+    auto al3 = DirectionalLight::create(.3f);
+    al3->color(math::vec3(0.f, 0.f, 1.f));
+    lights->addComponent(al3);
+
+    lights->removeComponent(al1);
+
+    ASSERT_EQ(root->data()->get<uint>("directionalLights.length"), 2);
+    ASSERT_TRUE(root->data()->hasProperty("directionalLights[0].color"));
+    ASSERT_TRUE(root->data()->hasProperty("directionalLights[0].diffuse"));
+    ASSERT_EQ(root->data()->get<math::vec3>("directionalLights[0].color"), math::vec3(0.f, 1.f, 0.f));
+    ASSERT_EQ(root->data()->get<float>("directionalLights[0].diffuse"), .2f);
+    ASSERT_TRUE(root->data()->hasProperty("directionalLights[1].color"));
+    ASSERT_TRUE(root->data()->hasProperty("directionalLights[1].diffuse"));
+    ASSERT_EQ(root->data()->get<math::vec3>("directionalLights[1].color"), math::vec3(0.f, 0.f, 1.f));
+    ASSERT_EQ(root->data()->get<float>("directionalLights[1].diffuse"), .3f);
+}
+
+TEST_F(DirectionalLightTest, RemoveNthLight)
+{
+    auto root = scene::Node::create("root");
+    auto lights = scene::Node::create("lights");
+
+    root->addChild(lights);
+
+    auto al1 = DirectionalLight::create(.1f);
+    al1->color(math::vec3(1.f, 0.f, 0.f));
+    lights->addComponent(al1);
+
+    auto al2 = DirectionalLight::create(.2f);
+    al2->color(math::vec3(0.f, 1.f, 0.f));
+    lights->addComponent(al2);
+
+    auto al3 = DirectionalLight::create(.3f);
+    al3->color(math::vec3(0.f, 0.f, 1.f));
+    lights->addComponent(al3);
+
+    lights->removeComponent(al2);
+
+    ASSERT_EQ(root->data()->get<uint>("directionalLights.length"), 2);
+    ASSERT_TRUE(root->data()->hasProperty("directionalLights[0].color"));
+    ASSERT_TRUE(root->data()->hasProperty("directionalLights[0].diffuse"));
+    ASSERT_EQ(root->data()->get<math::vec3>("directionalLights[0].color"), math::vec3(1.f, 0.f, 0.f));
+    ASSERT_EQ(root->data()->get<float>("directionalLights[0].diffuse"), .1f);
+    ASSERT_TRUE(root->data()->hasProperty("directionalLights[1].color"));
+    ASSERT_TRUE(root->data()->hasProperty("directionalLights[1].diffuse"));
+    ASSERT_EQ(root->data()->get<math::vec3>("directionalLights[1].color"), math::vec3(0.f, 0.f, 1.f));
+    ASSERT_EQ(root->data()->get<float>("directionalLights[1].diffuse"), .3f);
+}
+
+TEST_F(DirectionalLightTest, RemoveLastLight)
+{
+    auto root = scene::Node::create("root");
+    auto lights = scene::Node::create("lights");
+
+    root->addChild(lights);
+
+    auto al1 = DirectionalLight::create(.1f);
+    al1->color(math::vec3(1.f, 0.f, 0.f));
+    lights->addComponent(al1);
+
+    auto al2 = DirectionalLight::create(.2f);
+    al2->color(math::vec3(0.f, 1.f, 0.f));
+    lights->addComponent(al2);
+
+    auto al3 = DirectionalLight::create(.3f);
+    al3->color(math::vec3(0.f, 0.f, 1.f));
+    lights->addComponent(al3);
+
+    lights->removeComponent(al3);
+
+    ASSERT_EQ(root->data()->get<uint>("directionalLights.length"), 2);
+    ASSERT_TRUE(root->data()->hasProperty("directionalLights[0].color"));
+    ASSERT_TRUE(root->data()->hasProperty("directionalLights[0].diffuse"));
+    ASSERT_EQ(root->data()->get<math::vec3>("directionalLights[0].color"), math::vec3(1.f, 0.f, 0.f));
+    ASSERT_EQ(root->data()->get<float>("directionalLights[0].diffuse"), .1f);
+    ASSERT_TRUE(root->data()->hasProperty("directionalLights[1].color"));
+    ASSERT_TRUE(root->data()->hasProperty("directionalLights[1].diffuse"));
+    ASSERT_EQ(root->data()->get<math::vec3>("directionalLights[1].color"), math::vec3(0.f, 1.f, 0.f));
+    ASSERT_EQ(root->data()->get<float>("directionalLights[1].diffuse"), .2f);
+}
