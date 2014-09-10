07d2f903d2e4f7f28cc4e36fef5f2aebac84ad30
Warren Seine <warren.seine@aerys.in>
2014-09-10 08:29:06 +0200

    Allow passing custom arguments to solution generation scripts.

--- tool/lin/script/solution_gmake_gcc.sh
+++ tool/lin/script/solution_gmake_gcc.sh
@@ -3,5 +3,5 @@
 DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
 pushd ${DIR}/../../.. > /dev/null
-tool/lin/script/premake5.sh --no-test --cc=gcc gmake
+tool/lin/script/premake5.sh --no-test $@ --cc=gcc gmake
 popd > /dev/null
