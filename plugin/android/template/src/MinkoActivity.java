defc3beff4a3365e9311a9efa2dbfe1de6e0d080
Jonathan Jarri <jonathan.jarri@aerys.in>
2014-11-06 15:11:58 +0100

    Update Minko activity to get real filename from a downloaded file.

--- plugin/android/template/src/MinkoActivity.java
+++ plugin/android/template/src/MinkoActivity.java
@@ -5,7 +5,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.util.Log;
 import android.os.Bundle;
-
+import android.provider.OpenableColumns;
+import android.database.Cursor;
+import java.io.InputStream;
 /**
     Minko Activity, Java entry point
 */
@@ -16,18 +18,45 @@ public class MinkoActivity extends SDLActivity
 	{
 		super.onCreate(savedInstanceState);
 	}
-
+	
 	@Override
 	protected void onNewIntent(Intent intent)
 	{
 		super.onNewIntent(intent);
-
+		
 		if (intent != null && intent.getData() != null && intent.getData().getPath() != null)
 		{
-			String filename = intent.getData().getPath();
-
-			Log.v("MINKOJAVA", "Get filename: " + filename);
-
+			String filename = null;
+			Long filesize = null;
+			Cursor cursor = null;
+			
+			try 
+			{
+				cursor = this.getContentResolver().query(
+					intent.getData(), 
+					new String[] { OpenableColumns.DISPLAY_NAME, OpenableColumns.SIZE }, 
+					null, null, null
+				);
+				
+				if (cursor != null && cursor.moveToFirst()) 
+				{
+					filename = cursor.getString(0);
+					filesize = cursor.getLong(1);
+				}
+			} 
+			finally 
+			{
+				if (cursor != null)
+					cursor.close();
+			}
+			
+			Log.v("MINKOJAVA", "[onNewIntent]Filename: " + filename);
+		
+			if (filename == null)
+				filename = intent.getData().getPath();
+			
+			Log.v("MINKOJAVA", "[onNewIntent]Get filename: " + filename);
+			
 			SDLActivity.onNativeDropFile(filename);
 		}
 	}
