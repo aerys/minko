3aafb68e4c7490ff3dc1a5d6648159da84d94e83
Julien Clouté <julien.cloute@aerys.in>
2014-11-06 12:20:24 +0100

    Fix DXT1 texture format metadata.

--- framework/src/minko/render/TextureFormatInfo.cpp
+++ framework/src/minko/render/TextureFormatInfo.cpp
@@ -28,7 +28,7 @@ std::map<TextureFormat, TextureFormatInfo::Entry> TextureFormatInfo::_formats =
     { TextureFormat::RGB,               Entry("RGB",                false,    24,   16,  false,  false) },
     { TextureFormat::RGBA,              Entry("RGBA",               false,    32,   16,  true,   false) },
 
-    { TextureFormat::RGB_DXT1,          Entry("RGB_DXT1",           true,     4,    16,  false,  false) },
+    { TextureFormat::RGB_DXT1,          Entry("RGB_DXT1",           true,     4,    8,   false,  false) },
     { TextureFormat::RGBA_DXT3,         Entry("RGBA_DXT3",          true,     8,    16,  true,   false) },
     { TextureFormat::RGBA_DXT5,         Entry("RGBA_DXT5",          true,     8,    16,  true,   false) },
 
