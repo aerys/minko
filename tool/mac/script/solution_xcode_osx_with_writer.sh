520d33a0d6be535d7e251c433358368d086f4303
Julien Clouté <julien.cloute@aerys.in>
2014-11-10 16:41:20 +0100

    Remove unneeded script.

--- tool/mac/script/solution_xcode_osx_with_writer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
-
-pushd ${DIR}/../../.. > /dev/null
-tool/mac/script/premake5.sh --no-test --with-writer xcode-osx
-popd > /dev/null
