67816d54a26d3c4095e5a6dae876783132af58e3
Warren Seine <warren.seine@aerys.in>
2014-11-12 17:02:31 +0100

    Disable Oculus plugin on OS X.

--- plugin/oculus/premake5.lua
+++ plugin/oculus/premake5.lua
@@ -1,12 +1,12 @@
 PROJECT_NAME = path.getname(os.getcwd())
 
-if not minko.platform.supports { "windows32", "windows64", "linux32", "linux64", "osx64" } then
+if not minko.platform.supports { "windows32", "windows64", "linux32", "linux64" } then
 	return
 end
 
 minko.project.library("minko-plugin-" .. PROJECT_NAME)
 
-	removeplatforms { "html5", "ios", "android" }
+	removeplatforms { "html5", "ios", "android", "osx64" }
 
 	files {
 		"**.hpp",
