00c169bef48dc55e33e5cda804c88f50fe26ae86
Jonathan Jarri <jonathan.jarri@aerys.in>
2014-11-18 16:23:47 +0100

    We can now open a file with URI content from intent on Android (fix file opening with chrome and other browsers).

--- plugin/android/template/src/MinkoActivity.java
+++ plugin/android/template/src/MinkoActivity.java
@@ -2,12 +2,15 @@ package {{PACKAGE}};
 
 import org.libsdl.app.*;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
-import android.os.Bundle;
-import android.provider.OpenableColumns;
 import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import java.io.InputStream;
+
 /**
     Minko Activity, Java entry point
 */
@@ -17,6 +20,10 @@ public class MinkoActivity extends SDLActivity
     protected void onCreate(Bundle savedInstanceState) 
 	{
 		super.onCreate(savedInstanceState);
+		
+		Intent intent = getIntent();
+		
+		nativeDropFile(intent);
 	}
 	
 	@Override
@@ -24,40 +31,57 @@ public class MinkoActivity extends SDLActivity
 	{
 		super.onNewIntent(intent);
 		
+		nativeDropFile(intent);
+	}
+	
+	// Get real filepath from intent URI and fire SDL DropFile event
+	private void nativeDropFile(Intent intent)
+	{
 		if (intent != null && intent.getData() != null && intent.getData().getPath() != null)
-		{
-			String filename = null;
-			Long filesize = null;
-			Cursor cursor = null;
-			
-			try 
-			{
-				cursor = this.getContentResolver().query(
-					intent.getData(), 
-					new String[] { OpenableColumns.DISPLAY_NAME, OpenableColumns.SIZE }, 
-					null, null, null
-				);
-				
-				if (cursor != null && cursor.moveToFirst()) 
-				{
-					filename = cursor.getString(0);
-					filesize = cursor.getLong(1);
-				}
-			} 
-			finally 
-			{
-				if (cursor != null)
-					cursor.close();
-			}
-			
-			Log.v("MINKOJAVA", "[onNewIntent]Filename: " + filename);
+		{	
+			String filename = null; 
+			String intentDataString = intent.getDataString();
 		
-			if (filename == null)
+			if (intentDataString.startsWith("content://"))
+			{
+				Uri uri = Uri.parse(intentDataString);
+				filename = getRealPathFromURI(this.getApplicationContext(), uri);
+			}
+			else
 				filename = intent.getData().getPath();
 			
-			Log.v("MINKOJAVA", "[onNewIntent]Get filename: " + filename);
+			Log.v("minko-java", "[MinkoActivity]Get filename: " + filename);
 			
 			SDLActivity.onNativeDropFile(filename);
 		}
 	}
+	
+	private String getRealPathFromURI(Context context, Uri contentUri) 
+	{
+        Cursor cursor = null;
+		
+        try 
+		{
+			String filePath;
+			String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+			cursor = context.getContentResolver().query(contentUri, filePathColumn, null, null, null);
+			cursor.moveToFirst();
+			
+			int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+			
+			filePath = cursor.getString(columnIndex);
+			
+			cursor.close();
+			
+			return filePath;
+        }
+		finally 
+		{
+            if (cursor != null) 
+			{
+                cursor.close();
+            }
+        }
+    }
 }
