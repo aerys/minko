de2a9295be2fe6e5bf27aa94665be4d038275694
Julien Clouté <julien.cloute@aerys.in>
2015-01-12 19:08:52 +0100

    Fix SceneManager to match beta3 API.

--- framework/src/minko/component/SceneManager.cpp
+++ framework/src/minko/component/SceneManager.cpp
@@ -52,8 +52,8 @@ SceneManager::targetAdded(NodePtr target)
 	if (target->components<SceneManager>().size() > 1)
 		throw std::logic_error("The same root node cannot have more than one SceneManager.");
 
-	target->data()->addProvider(_data);
-    target->data()->addProvider(_canvas->data());
+	target->data().addProvider(_data);
+    target->data().addProvider(_canvas->data());
 
     _addedSlot = target->added().connect(std::bind(
         &SceneManager::addedHandler,
@@ -69,8 +69,8 @@ SceneManager::targetRemoved(NodePtr target)
 {
     _addedSlot = nullptr;
 
-	target->data()->removeProvider(_data);
-    target->data()->removeProvider(_canvas->data());
+	target->data().removeProvider(_data);
+    target->data().removeProvider(_canvas->data());
 }
 
 void
