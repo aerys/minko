6fdf76e183777cd14b05e371b0b9bd043d7fbe14
Julien Clouté <julien.cloute@aerys.in>
2015-01-12 16:57:57 +0100

    Update glsl texturelod to use placeholder color in case where desired mip level is not available.

--- framework/asset/effect/TextureLod.function.glsl
+++ framework/asset/effect/TextureLod.function.glsl
@@ -1,30 +1,40 @@
 #if __VERSION__ > 120 || defined(GL_OES_standard_derivatives)
 
-float texturelod_mipmapLevel(vec2 uv, vec2 texSize)
+float texturelod_mipmapLevel(sampler2D tex, vec2 uv, vec2 texSize)
 {
-  vec2 dx = dFdx(uv * texSize.x);
-  vec2 dy = dFdy(uv * texSize.y);
-  
-  float d = max(dot (dx, dx), dot(dy, dy));
-  
-  return 0.5 * log2(d);
+#if __VERSION__ >= 400
+    return textureQueryLod(tex, uv).x;
+#else
+    vec2 dx = dFdx(uv * texSize.x);
+    vec2 dy = dFdy(uv * texSize.y);
+
+    float d = max(dot(dx, dx), dot(dy, dy));
+
+    return max(0.5 * log2(d), 0.0);
+#endif
 }
 
 #endif
 
-vec4 texturelod_texture2D(sampler2D tex, vec2 uv, vec2 texSize, float baseLevel, float maxLevel, vec4 defaultColor)
+vec4 texturelod_texture2D(sampler2D tex, vec2 uv, vec2 texSize, float baseLod, float maxLod, vec4 defaultColor)
 {
 #if __VERSION__ <= 120
     #if defined(GL_OES_standard_derivatives) && defined(GL_EXT_shader_texture_lod)
-        float level = max(texturelod_mipmapLevel(uv, texSize), maxLevel);
+        float requiredLod = texturelod_mipmapLevel(tex, uv, texSize);
 
-        return texture2DLodEXT(tex, fract(uv), level);
+        if (requiredLod < maxLod)
+          return defaultColor;
+
+        return texture2DLodEXT(tex, fract(uv), requiredLod);
     #else
         return defaultColor;
     #endif
 #else
-    float level = max(texturelod_mipmapLevel(uv, texSize), maxLevel);
+    float requiredLod = texturelod_mipmapLevel(tex, uv, texSize);
 
-    return textureLod(tex, fract(uv), level);
+    if (requiredLod < maxLod)
+        return defaultColor;
+
+    return textureLod(tex, fract(uv), requiredLod);
 #endif
 }
