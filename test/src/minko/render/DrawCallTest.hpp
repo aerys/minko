89f999db4b40b7e0ab2abf38eaa0fa24aa52d0ab
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2015-01-15 12:50:41 +0100

    Fix DrawCallTest to match with the latest changes in the ProgramInputs::UniformInput interface.

--- test/src/minko/render/DrawCallTest.hpp
+++ test/src/minko/render/DrawCallTest.hpp
@@ -64,7 +64,7 @@ namespace minko
                     {
                         p->set<T>(propertyName, valueFunc());
                         bindings["u" + propertyName] = { propertyName, data::Binding::Source::ROOT };
-                        inputs.emplace_back("u" + propertyName, 1 + std::abs(rand()), inputType);
+                        inputs.emplace_back("u" + propertyName, 1 + std::abs(rand()), 1, inputType);
                     }
                 }
                 rootData.addProvider(p);
