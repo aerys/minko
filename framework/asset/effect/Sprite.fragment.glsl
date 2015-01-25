6fc08240da6ccafaafe351abc81b885f6be8ea6d
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2015-01-25 17:41:10 +0100

    Update Sprite.effect to match with the latest API.

--- framework/asset/effect/Sprite.fragment.glsl
+++ framework/asset/effect/Sprite.fragment.glsl
@@ -1,35 +1,33 @@
 #ifdef FRAGMENT_SHADER
 
 #ifdef GL_ES
-	precision mediump float;
+    #ifdef GL_FRAGMENT_PRECISION_HIGH
+        precision highp float;
+    #else
+        precision mediump float;
+    #endif
 #endif
 
 uniform vec4 uDiffuseColor;
+uniform sampler2D uDiffuseMap;
+uniform vec4 uDiffuseTint;
 
-#ifdef DIFFUSE_MAP
-	uniform sampler2D uDiffuseMap;
-	
-	varying vec2 vUV;
-#endif
-
-#ifdef DIFFUSE_TINT
-	uniform vec4 uDiffuseTint;
-#endif
+varying vec2 vUV;
 
 void main()
 {
 	vec4 diffuse = vec4(0.);
-	
+
 	#ifdef DIFFUSE_MAP
 		diffuse = texture2D(uDiffuseMap, vUV);
 	#else
 		diffuse = uDiffuseColor;
 	#endif
-	
+
 	#ifdef DIFFUSE_TINT
 		diffuse *= uDiffuseTint;
 	#endif
-	
+
 	gl_FragColor = diffuse;
 }
 
