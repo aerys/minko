2bbb3797840c51c268873d13297f8abce7f4b4ab
Jonathan Jarri <jonathan.jarri@aerys.in>
2015-01-26 15:21:04 +0100

    Update DrawCallZSorter to use pointer of worldToScreen and modelToWorld matrices instead of get them each frame + create signals to sort drawcall only when properties that affect z sorting change.

--- framework/src/minko/render/DrawCallZSorter.cpp
+++ framework/src/minko/render/DrawCallZSorter.cpp
@@ -1,4 +1,4 @@
-/*,
+/*
 Copyright (c) 2014 Aerys
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
@@ -36,25 +36,24 @@ DrawCallZSorter::initializeRawProperties()
 {
     PropertyInfos props;
 
-    props["material[${materialUuid}].priority"] = PropertyInfo(data::Binding::Source::TARGET, false);
-    props["material[${materialUuid}].zSort"] = PropertyInfo(data::Binding::Source::TARGET, false);
-    props["geometry[${geometryUuid}].position"] = PropertyInfo(data::Binding::Source::TARGET, false);
-    props["transform.modelToWorldMatrix"] = PropertyInfo(data::Binding::Source::TARGET, true);
-    props["camera.worldToScreenMatrix"] = PropertyInfo(data::Binding::Source::RENDERER, true);
+    props["material[${materialUuid}].priority"] = data::Binding::Source::TARGET;
+    props["material[${materialUuid}].zSorted"] = data::Binding::Source::TARGET;
+    props["geometry[${geometryUuid}].position"] = data::Binding::Source::TARGET;
+    props["modelToWorldMatrix"] = data::Binding::Source::TARGET;
+    props["worldToScreenMatrix"] = data::Binding::Source::RENDERER;
 
     return props;
 }
 
 DrawCallZSorter::DrawCallZSorter(DrawCall* drawcall):
     _drawcall(drawcall),
-    _properties(),
     _targetPropAddedSlot(nullptr),
     _targetPropRemovedSlot(nullptr),
     _rendererPropAddedSlot(nullptr),
     _rendererPropRemovedSlot(nullptr),
-    _vertexPositions(),
-    _modelToWorldMatrix(),
-    _worldToScreenMatrix()
+    _centerPosition(),
+    _modelToWorldMatrix(nullptr),
+    _worldToScreenMatrix(nullptr)
 {
     
 }
@@ -64,21 +63,97 @@ DrawCallZSorter::initialize(data::Store& targetData,
                             data::Store& rendererData,
                             data::Store& rootData)
 {
-    
+    if (targetData.hasProperty("centerPosition"))
+        _centerPosition = targetData.get<math::vec3>("centerPosition");
+
+    clear();
+
+    // Signals
+    _targetPropAddedSlot = targetData.propertyAdded().connect(
+        std::bind(
+            &DrawCallZSorter::propertyAddedHandler,
+            shared_from_this(),
+            std::placeholders::_1,
+            std::placeholders::_2,
+            std::placeholders::_3
+        )
+    );
+
+    _targetPropRemovedSlot = targetData.propertyRemoved().connect(
+        std::bind(
+            &DrawCallZSorter::propertyRemovedHandler,
+            shared_from_this(),
+            std::placeholders::_1,
+            std::placeholders::_2,
+            std::placeholders::_3
+        )
+    );
+
+    _rendererPropAddedSlot = rendererData.propertyAdded().connect(
+        std::bind(
+            &DrawCallZSorter::propertyAddedHandler,
+            shared_from_this(),
+            std::placeholders::_1,
+            std::placeholders::_2,
+            std::placeholders::_3
+        )
+    );
+
+    _rendererPropRemovedSlot = rendererData.propertyRemoved().connect(
+        std::bind(
+            &DrawCallZSorter::propertyRemovedHandler,
+            shared_from_this(),
+            std::placeholders::_1,
+            std::placeholders::_2,
+            std::placeholders::_3
+        )
+    );
+
+    _propertyChangedSlots.clear();
+
+    for (auto& prop : _rawProperties)
+    {
+        auto source = prop.second;
+        auto& store = (source == data::Binding::Source::RENDERER) ? rendererData : targetData;
+
+        if (store.hasProperty(prop.first))
+            propertyAddedHandler(store, nullptr, prop.first);
+    }
 }
 
 void
 DrawCallZSorter::clear()
 {
-    
+    _targetPropAddedSlot = nullptr;
+    _targetPropRemovedSlot = nullptr;
+    _rendererPropAddedSlot = nullptr;
+    _rendererPropRemovedSlot = nullptr;
 }
 
 void
 DrawCallZSorter::propertyAddedHandler(data::Store& store, 
-                                      std::shared_ptr<data::Provider>, 
+                                      std::shared_ptr<data::Provider> data, 
                                       const std::string& propertyName)
 {
-    
+    if (!store.hasProperty(propertyName))
+        return;
+
+    if (propertyName == "modelToWorldMatrix" && store.hasProperty("modelToWorldMatrix"))
+        _modelToWorldMatrix = store.getPointer<math::mat4>("modelToWorldMatrix");
+    else if (propertyName == "worldToScreenMatrix" && store.hasProperty("worldToScreenMatrix"))
+        _worldToScreenMatrix = store.getPointer<math::mat4>("worldToScreenMatrix");
+
+    if (_propertyChangedSlots.find(propertyName) == _propertyChangedSlots.end())
+    {
+        _propertyChangedSlots[propertyName] = store.propertyChanged(propertyName).connect(
+            std::bind(
+                &DrawCallZSorter::requestZSort,
+                shared_from_this()
+            )
+        );
+    }
+
+    requestZSort();
 }
 
 void
@@ -86,62 +161,38 @@ DrawCallZSorter::propertyRemovedHandler(data::Store& store,
                                         std::shared_ptr<data::Provider>,
                                         const std::string& propertyName)
 {
-   
+    if (!store.hasProperty(propertyName))
+        return;
+
+    if (propertyName == "modelToWorldMatrix")
+        _modelToWorldMatrix = nullptr;
+    else if (propertyName == "worldToScreenMatrix")
+        _worldToScreenMatrix = nullptr;
+
+    if (_propertyChangedSlots.find(propertyName) != _propertyChangedSlots.end())
+        _propertyChangedSlots.erase(propertyName);
+
+    requestZSort();
 }
 
 void
 DrawCallZSorter::requestZSort()
 {
-    
-}
-
-void
-DrawCallZSorter::recordIfPositionalMembers(data::Store&         store,
-                                           const std::string&   propertyName,
-                                           bool                 isPropertyAdded,
-                                           bool                 isPropertyRemoved)
-{
-    if (isPropertyAdded)
-    {
-        if (propertyName == _vertexPositions.first)
-        {
-            for (auto test : _drawcall->targetData().collections())
-            {
-                auto test2 = test.get();
-            }
-            //_vertexPositions.second = _drawcall->targetData().collections()."geometry"].get<std::shared_ptr<geometry::Geometry>>(propertyName);
-            //_vertexPositions.second = store.get<geometry::Geometry>(propertyName);
-        }
-        else if (propertyName == _modelToWorldMatrix.first)
-            _modelToWorldMatrix.second = store.get<mat4>(propertyName);
-        else if (propertyName == _worldToScreenMatrix.first)
-            _worldToScreenMatrix.second = store.get<mat4>(propertyName);
-    }
-    else if (isPropertyRemoved)
-    {
-        if (propertyName == _vertexPositions.first)
-            _vertexPositions.second = nullptr;
-        else if (propertyName == _modelToWorldMatrix.first)
-            _modelToWorldMatrix.second = math::mat4();
-        else if (propertyName == _worldToScreenMatrix.first)
-            _worldToScreenMatrix.second = math::mat4();
-    }
+    /*
+    if (_drawcall->zSorted())
+        _drawcall->zSortNeeded()->execute(_drawcall); // temporary ugly solution
+    */
 }
 
 math::vec3
 DrawCallZSorter::getEyeSpacePosition() const
 {
-    auto localPos = math::vec3();
     auto modelView = math::mat4();
     
-    if (_drawcall->targetData().hasProperty("centerPosition"))
-        localPos = _drawcall->targetData().get<math::vec3>("centerPosition");
-    
-    if (_drawcall->targetData().hasProperty("modelToWorldMatrix"))
-        modelView = _drawcall->targetData().get<math::mat4>("modelToWorldMatrix");
+    if (_modelToWorldMatrix != nullptr)
+        modelView = *_modelToWorldMatrix;
+    if (_worldToScreenMatrix != nullptr)
+        modelView = (*_worldToScreenMatrix) * modelView;
 
-    if (_drawcall->rendererData().hasProperty("worldToScreenMatrix"))
-        modelView = _drawcall->rendererData().get<math::mat4>("worldToScreenMatrix") * modelView;
-
-    return math::vec3(modelView * math::vec4(localPos, 1));
+    return math::vec3(modelView * math::vec4(_centerPosition, 1));
 }
