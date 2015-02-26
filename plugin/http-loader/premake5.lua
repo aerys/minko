8ab0f0a5d03a1bd3f4ac90e2925ccaea492f9593
Benjamin Michel <benjamin.michel@aerys.in>
2015-02-26 15:53:28 +0100

    Fix for HTTP plugin compilation

--- plugin/http-loader/premake5.lua
+++ plugin/http-loader/premake5.lua
@@ -15,9 +15,9 @@ minko.project.library("minko-plugin-" .. PROJECT_NAME)
 		"include"
 	}
 
+	minko.plugin.enable("http-worker")
+
 	if os.getenv('EMSCRIPTEN_WGET_HEADERS') then
 		configuration { "html5" }
 			defines { "EMSCRIPTEN_WGET_HEADERS" }
 	end
-
-	minko.plugin.enable("http-worker")
