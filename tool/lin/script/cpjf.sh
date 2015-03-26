7a0d045536296d053fa5ab9ae546f7d3bb103bc1
Jonathan Jarri <jonathan.jarri@aerys.in>
2015-03-26 17:00:59 +0100

    Add a script to copy all Java files from an absolute directory to a relative folder keeping files hierarchy.

---
+++ tool/lin/script/cpjf.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# The goal of this script is to copy all Java files from
+# an absolute source folder to a relative destination directory
+# keeping the hierarchy of each file
+
+set -e
+
+if [ "$#" -ne 2 ]; then
+    echo "usage: copy_java.sh absoluteSrcDir relativeDestDir" 2> /dev/null
+    exit 1
+fi
+
+curDir=$(cygpath -w $(pwd))
+#echo "Current directory: $curDir"
+
+# Move to the current plugin directory
+pushd "$1" > /dev/null
+
+java_files=$(find . -iname '*.java')
+
+for file in $java_files 
+do
+	dir="$2/"$(dirname "$file")
+	# create the dest dir if doesn't exist
+	if [ ! -d "$curDir/$dir" ]; then
+		#echo "Make a new directory: $dir"
+		mkdir -p "$curDir/$dir"
+	fi
+
+	# copy Java file
+	#echo "Copy file $1/$file to $curDir/$dir"
+	cp "$1/$file" "$curDir/$dir"
+done
+
+popd > /dev/null
