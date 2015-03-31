367237b9f556d2b0f27eb6f287a5232acd71c101
Julien Clouté <julien.cloute@aerys.in>
2015-03-31 13:37:24 +0200

    Update lod scheduler geometry error computation to support a base value of 0 meaning no visual error.

--- plugin/serializer/src/minko/component/POPGeometryLodScheduler.cpp
+++ plugin/serializer/src/minko/component/POPGeometryLodScheduler.cpp
@@ -310,7 +310,7 @@ POPGeometryLodScheduler::computeRequiredLod(const POPGeometryResourceInfo&  reso
 
     auto unitSize = std::abs(2.0f * std::tan(0.5f * _fov) * targetDistance / viewportHeight);
 
-    requiredPrecisionLevel = std::log2(glm::length(worldMaxBound - worldMinBound) / (unitSize * popErrorBound));
+    requiredPrecisionLevel = std::log2(glm::length(worldMaxBound - worldMinBound) / (unitSize * (popErrorBound + 1)));
 
     auto ceiledRequiredPrecisionLevel = static_cast<int>(std::ceil(requiredPrecisionLevel));
 
