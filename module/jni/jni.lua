bf1be189af4279a4b2e0a2132dd60290906bfdc4
Warren Seine <warren.seine@aerys.in>
2015-04-06 21:19:53 +0200

    Do not display an error message when ANDROID_HOME is not set.

--- module/jni/jni.lua
+++ module/jni/jni.lua
@@ -18,24 +18,13 @@ newoption {
    trigger     = "toolchain",
    description = "Android NDK toolchain"
 }
-
+do return end
 if _ACTION ~= "gmake" then
 	do return end
 end
 
 if not _OPTIONS["toolchain"] then
    _OPTIONS["toolchain"] = "arm-linux-androideabi-4.8"
-end
-
--- If we try to build Android on Windows without Cygwin
-if os.is("windows") and os.getenv('OSTYPE') == nil then
-	print(color.fg.yellow .. 'To build for Android on Windows, you have to use Cygwin. ' ..
-		'Please check that you exported OSTYPE environment variable.' .. color.reset)
-	do return end
-end
-
-if os.is("windows") then
-	MINKO_HOME = os.capture('cygpath -u "' .. MINKO_HOME .. '"')
 end
 
 local ANDROID_HOME = os.getenv('ANDROID_HOME') or os.getenv('ANDROID');
@@ -50,8 +39,19 @@ if not os.isfile(ANDROID_HOME .. "/tools/lib/sdk-common.jar") then
 		' (missing \'' .. ANDROID_HOME .. '/tools/lib/sdk-common.jar\')' .. color.reset)
 end
 
+-- if we try to build Android on Windows without Cygwin
+if os.is("windows") and os.getenv('OSTYPE') == nil then
+	print(color.fg.yellow .. 'To build for Android on Windows, you have to use Cygwin. ' ..
+		'Please check that you exported OSTYPE environment variable.' .. color.reset)
+	do return end
+end
+
+if os.is("windows") then
+	MINKO_HOME = os.capture('cygpath -u "' .. MINKO_HOME .. '"')
+end
+
 if not os.isdir(ANDROID_HOME .. "/toolchains") then
-	error(color.fg.red .. 'Cannot find NDK tools for Android. Please extract NDK in "' .. ANDROID_HOME .. '/ndk/android-ndk-<version>" and run `install_jni.sh` or `install_jni.bat`.' .. color.reset)
+	error(color.fg.red .. 'Cannot find NDK tools for Android. Please extract NDK in ${ANDROID_HOME}/ndk/android-ndk-<version>" and run `install_jni.sh` or `install_jni.bat`.' .. color.reset)
 end
 
 -- writing toolchain name in a fake symlink to avoid actual symlinks on Windows (requiring privileges)
