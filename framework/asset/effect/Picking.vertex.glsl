04e96bb1620c1d622ade4886afe55da81e6cfa60
Julien Clouté <julien.cloute@aerys.in>
2015-04-13 15:03:11 +0200

    Add support for POPGeometry in Picking.effect. Add guards arround Picking shader precision statements.

--- framework/asset/effect/Picking.vertex.glsl
+++ framework/asset/effect/Picking.vertex.glsl
@@ -1,12 +1,15 @@
 #ifdef VERTEX_SHADER
 
-#ifdef GL_FRAGMENT_PRECISION_HIGH
-    precision highp float;
-#else
-    precision mediump float;
+#ifdef GL_ES
+    #ifdef GL_FRAGMENT_PRECISION_HIGH
+        precision highp float;
+    #else
+        precision mediump float;
+    #endif
 #endif
 
 #pragma include "Skinning.function.glsl"
+#pragma include "Pop.function.glsl"
 
 attribute vec3 aPosition;
 attribute vec2 aUV;
@@ -19,9 +22,17 @@ attribute vec4 aBoneWeightsB;
 uniform mat4 uBoneMatrices[SKINNING_NUM_BONES];
 #endif
 
+attribute float aPopProtected;
+
 uniform mat4 uModelToWorldMatrix;
 uniform mat4 uWorldToViewMatrix;
 uniform mat4 uPickingProjection;
+
+uniform 	float 	uPopLod;
+uniform 	float 	uPopBlendingLod;
+uniform 	float 	uPopFullPrecisionLod;
+uniform 	vec3 	uPopMinBound;
+uniform 	vec3 	uPopMaxBound;
 
 varying vec2 vertexUV;
 
@@ -41,7 +52,21 @@ void main(void)
 		#ifdef SKINNING_NUM_BONES
 			pos = skinning_moveVertex(pos, uBoneMatrices, aBoneIdsA, aBoneIdsB, aBoneWeightsA, aBoneWeightsB);
 		#endif
-	
+
+		#ifdef POP_LOD_ENABLED
+			float popProtected = 0.0;
+
+			#ifdef VERTEX_POP_PROTECTED
+				popProtected = aPopProtected;
+			#endif // VERTEX_POP_PROTECTED
+
+			#ifdef POP_BLENDING_ENABLED
+				pos = pop_blend(pos, vec3(0.0), uPopLod, uPopBlendingLod, uPopFullPrecisionLod, uPopMinBound, uPopMaxBound, popProtected);
+			#else
+				pos = pop_quantize(pos, vec3(0.0), uPopLod, uPopFullPrecisionLod, uPopMinBound, uPopMaxBound, popProtected);
+			#endif // POP_BLENDING_ENABLED
+		#endif // POP_LOD_ENABLED
+
 		#ifdef MODEL_TO_WORLD
 			pos = uModelToWorldMatrix * pos;
 		#endif
