7566998f21b10324fae6597a721bd876d6d7bb56
Warren Seine <warren.seine@aerys.in>
2015-05-02 18:37:42 +0200

    Fix shader compilation on OS X.

--- framework/asset/effect/TextureLod.extension.glsl
+++ framework/asset/effect/TextureLod.extension.glsl
@@ -3,9 +3,11 @@
 #endif
 
 #if __VERSION__ < 130
-    #if GL_ES
+    #ifdef GL_ES
         #extension GL_EXT_shader_texture_lod : enable
-    #elif __VERSION__ > 100
-        #extension GL_ARB_shader_texture_lod : enable
+    #else
+        #if __VERSION__ > 100
+            #extension GL_ARB_shader_texture_lod : enable
+        #endif
     #endif
 #endif
