e8f452808a4acd6a486bf9d4af7a04b48587ff32
Jonathan Jarri <jonathan.jarri@aerys.in>
2015-05-06 18:57:58 +0200

    Update sensors example rebuilding the camera transform matrix from rotation matrix retrieve by sensors.

--- example/sensors/src/main.cpp
+++ example/sensors/src/main.cpp
@@ -28,114 +28,120 @@ using namespace minko::component;
 int
 main(int argc, char** argv)
 {
-  auto canvas = Canvas::create("Minko Example - Sensors");
-  auto sceneManager = SceneManager::create(canvas);
+    auto canvas = Canvas::create("Minko Example - Sensors");
+    auto sceneManager = SceneManager::create(canvas);
 
-  auto headTracker = HeadTracker::create();
-  headTracker->initialize();
-  headTracker->startTracking();
+    auto headTracker = HeadTracker::create();
+    headTracker->initialize();
+    headTracker->startTracking();
 
-  sceneManager->assets()->loader()->queue("effect/Basic.effect");
+    sceneManager->assets()->loader()->queue("effect/Basic.effect");
 
-  auto cubeGeometry = geometry::CubeGeometry::create(sceneManager->assets()->context());
-  auto quadGeometry = geometry::QuadGeometry::create(sceneManager->assets()->context());
-  sceneManager->assets()->geometry("cubeGeometry", cubeGeometry);
-  sceneManager->assets()->geometry("quadGeometry", quadGeometry);
-    
-  auto root = scene::Node::create("root")
-    ->addComponent(sceneManager);
+    auto cubeGeometry = geometry::CubeGeometry::create(sceneManager->assets()->context());
+    auto quadGeometry = geometry::QuadGeometry::create(sceneManager->assets()->context());
+    sceneManager->assets()->geometry("cubeGeometry", cubeGeometry);
+    sceneManager->assets()->geometry("quadGeometry", quadGeometry);
 
-  auto greenCube = scene::Node::create("greenCube")
-    ->addComponent(Transform::create(math::translate(math::vec3(0.f, 0.f, -2.5f))));
-  
-  auto blueCube = scene::Node::create("blueCube")
-    ->addComponent(Transform::create(math::translate(math::vec3(-2.5f, 0.f, 0.f))));
-  
-  auto pinkCube = scene::Node::create("pinkCube")
-    ->addComponent(Transform::create(math::translate(math::vec3(2.5f, 0.f, 0.f))));
+    auto root = scene::Node::create("root")
+        ->addComponent(sceneManager);
 
-  auto groundTransform = Transform::create();
-  groundTransform->matrix(math::scale(math::vec3(10.f, 10.f, 10.f)) * groundTransform->matrix());
-  groundTransform->matrix(math::rotate((float)(-M_PI_2), math::vec3(1.f, 0.f, 0.f)) * groundTransform->matrix());
-  groundTransform->matrix(math::translate(math::vec3(0.f, -1.f, 0.f)) * groundTransform->matrix());
-  
-  auto ground = scene::Node::create("ground")
-    ->addComponent(groundTransform);
+    auto greenCube = scene::Node::create("greenCube")
+        ->addComponent(Transform::create(math::translate(math::vec3(0.f, 0.f, -2.5f))));
 
-  auto camera = scene::Node::create("camera")
-    ->addComponent(Renderer::create(0x7f7f7fff))
-    ->addComponent(
-      Transform::create(
-        math::inverse(
-          math::lookAt(
-            math::vec3(0.f, 0.f, 3.f), math::vec3(), math::vec3(0.f, 1.f, 0.f)
-          )
+    auto blueCube = scene::Node::create("blueCube")
+        ->addComponent(Transform::create(math::translate(math::vec3(-2.5f, 0.f, 0.f))));
+
+    auto pinkCube = scene::Node::create("pinkCube")
+        ->addComponent(Transform::create(math::translate(math::vec3(2.5f, 0.f, 0.f))));
+
+    auto groundTransform = Transform::create();
+    groundTransform->matrix(math::scale(math::vec3(10.f, 10.f, 10.f)) * groundTransform->matrix());
+    groundTransform->matrix(math::rotate((float)(-M_PI_2), math::vec3(1.f, 0.f, 0.f)) * groundTransform->matrix());
+    groundTransform->matrix(math::translate(math::vec3(0.f, -1.f, 0.f)) * groundTransform->matrix());
+
+    auto ground = scene::Node::create("ground")
+        ->addComponent(groundTransform);
+
+    auto camera = scene::Node::create("camera")
+        ->addComponent(Renderer::create(0x7f7f7fff))
+        ->addComponent(
+            Transform::create(
+                math::inverse(
+                    math::lookAt(
+                        math::vec3(0.f, 0.f, 3.f), math::vec3(), math::vec3(0.f, 1.f, 0.f)
+                    )
+                )
+            )
         )
-      )
-    )
-    ->addComponent(PerspectiveCamera::create(canvas->aspectRatio()));
+        ->addComponent(PerspectiveCamera::create(canvas->aspectRatio()));
 
-  root->addChild(greenCube);
-  root->addChild(blueCube);
-  root->addChild(pinkCube);
-  root->addChild(ground);
-  root->addChild(camera);
+    root->addChild(greenCube);
+    root->addChild(blueCube);
+    root->addChild(pinkCube);
+    root->addChild(ground);
+    root->addChild(camera);
 
-  auto _ = sceneManager->assets()->loader()->complete()->connect([=](file::Loader::Ptr loader)
-  {
-    auto greenMaterial = material::BasicMaterial::create();
-    greenMaterial->diffuseColor(math::vec4(0.f, 1.f, 0.f, 1.f));
-    auto blueMaterial = material::BasicMaterial::create();
-    blueMaterial->diffuseColor(math::vec4(0.f, 0.f, 1.f, 1.f));
-    auto pinkMaterial = material::BasicMaterial::create();
-    pinkMaterial->diffuseColor(math::vec4(1.f, 0.f, 1.f, 1.f));
-    auto groundMaterial = material::BasicMaterial::create();
-    groundMaterial->diffuseColor(math::vec4(1.f, 0.f, 0.f, 1.f));
+    auto _ = sceneManager->assets()->loader()->complete()->connect([=](file::Loader::Ptr loader)
+    {
+        auto greenMaterial = material::BasicMaterial::create();
+        greenMaterial->diffuseColor(math::vec4(0.f, 1.f, 0.f, 1.f));
+        auto blueMaterial = material::BasicMaterial::create();
+        blueMaterial->diffuseColor(math::vec4(0.f, 0.f, 1.f, 1.f));
+        auto pinkMaterial = material::BasicMaterial::create();
+        pinkMaterial->diffuseColor(math::vec4(1.f, 0.f, 1.f, 1.f));
+        auto groundMaterial = material::BasicMaterial::create();
+        groundMaterial->diffuseColor(math::vec4(1.f, 0.f, 0.f, 1.f));
 
-    greenCube->addComponent(Surface::create(
-      sceneManager->assets()->geometry("cubeGeometry"),
-      greenMaterial,
-      sceneManager->assets()->effect("effect/Basic.effect")
-    ));
+        greenCube->addComponent(Surface::create(
+            sceneManager->assets()->geometry("cubeGeometry"),
+            greenMaterial,
+            sceneManager->assets()->effect("effect/Basic.effect")
+        ));
 
-    blueCube->addComponent(Surface::create(
-      sceneManager->assets()->geometry("cubeGeometry"),
-      blueMaterial,
-      sceneManager->assets()->effect("effect/Basic.effect")
-    ));
+        blueCube->addComponent(Surface::create(
+            sceneManager->assets()->geometry("cubeGeometry"),
+            blueMaterial,
+            sceneManager->assets()->effect("effect/Basic.effect")
+        ));
 
-    pinkCube->addComponent(Surface::create(
-      sceneManager->assets()->geometry("cubeGeometry"),
-      pinkMaterial,
-      sceneManager->assets()->effect("effect/Basic.effect")
-    ));
+        pinkCube->addComponent(Surface::create(
+            sceneManager->assets()->geometry("cubeGeometry"),
+            pinkMaterial,
+            sceneManager->assets()->effect("effect/Basic.effect")
+        ));
 
-    ground->addComponent(Surface::create(
-      sceneManager->assets()->geometry("quadGeometry"),
-      groundMaterial,
-      sceneManager->assets()->effect("effect/Basic.effect")
-    ));
-  });
+        ground->addComponent(Surface::create(
+            sceneManager->assets()->geometry("quadGeometry"),
+            groundMaterial,
+            sceneManager->assets()->effect("effect/Basic.effect")
+        ));
+    });
+
+    auto resized = canvas->resized()->connect([&](AbstractCanvas::Ptr canvas, uint w, uint h)
+    {
+        camera->component<PerspectiveCamera>()->aspectRatio(float(w) / float(h));
+    });
     
-  auto resized = canvas->resized()->connect([&](AbstractCanvas::Ptr canvas, uint w, uint h)
-  {
-    camera->component<PerspectiveCamera>()->aspectRatio(float(w) / float(h));
-  });
-    
-  auto enterFrame = canvas->enterFrame()->connect([&](Canvas::Ptr canvas, float time, float deltaTime)
-  {
-    greenCube->component<Transform>()->matrix(
-      greenCube->component<Transform>()->matrix() * math::rotate(0.01f, math::vec3(0, 1, 0))
-    );
-    
-    // Update camera view matrix according to sensors data
-    camera->component<PerspectiveCamera>()->viewMatrix(headTracker->getLastHeadView());
+    auto enterFrame = canvas->enterFrame()->connect([&](Canvas::Ptr canvas, float time, float deltaTime)
+    {
+        greenCube->component<Transform>()->matrix(
+            greenCube->component<Transform>()->matrix() * math::rotate(0.01f, math::vec3(0, 1, 0))
+        );
 
-    sceneManager->nextFrame(time, deltaTime);
-  });
-    
-  sceneManager->assets()->loader()->load();
-  canvas->run();
+        // Rebuild camera matrix with rotation matrix retrieve from sensors
+        auto cameraPosition = camera->component<Transform>()->matrix()[3].xyz();
+        auto rotationMatrix = math::transpose(headTracker->getLastHeadView());
+        auto direction = math::vec3(-rotationMatrix[2][0], -rotationMatrix[2][1], -rotationMatrix[2][2]);
+        auto newMatrix = math::translate(cameraPosition) * rotationMatrix;
 
-  return 0;
+        camera->component<Transform>()->matrix(newMatrix);
+
+        camera->component<Transform>()->matrix(math::translate(direction * 0.01f) * camera->component<Transform>()->matrix());
+        sceneManager->nextFrame(time, deltaTime);
+    });
+    
+    sceneManager->assets()->loader()->load();
+    canvas->run();
+
+    return 0;
 }
