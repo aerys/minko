25576b0cc0f9138afbe8aa9bf1ae8791977e996c
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2015-05-12 15:01:01 +0200

    In the 'pbr' example, add a working implementation of IBL in the PBR.effect + multiple minor fixes.

--- example/pbr/asset/effect/PBR.function.glsl
+++ example/pbr/asset/effect/PBR.function.glsl
@@ -2,6 +2,7 @@
 #define _PBR_FUNCTION_GLSL_
 
 #pragma include "BRDF.function.glsl"
+#pragma include "TextureLod.function.glsl"
 
 vec3 pbr_diffuse(vec3 diffuseColor, float roughness, float NoV, float NoL, float VoH)
 {
@@ -14,8 +15,41 @@ vec3 pbr_specular(vec3 specularColor, float roughness, float NoL, float NoV, flo
     return brdf_distribution(roughness, NoH)
          * brdf_fresnel(specularColor, VoH)
          * brdf_geometricVisibility(roughness, NoV, NoL, VoH)//;
+         * NoL;
         //  * (4.0 * NoL * NoV);
-        * NoL;
+}
+
+vec3 pbr_envDiffuse(sampler2D irradianceMap, vec3 normal)
+{
+    return texture2D(irradianceMap, normalToLatLongUV(normal)).rgb;
+}
+
+vec3 pbr_fresnelSchlickWithRoughness(vec3 specularColor, vec3 e, vec3 n, float gloss)
+{
+    return specularColor + (max(vec3(gloss), specularColor) - specularColor) * pow(1.0 - saturate(dot(e, n)), 5.0);
+}
+
+vec3 pbr_envSpecular(vec3       specularColor,
+                     sampler2D  radianceMap,
+                     sampler2D  integratedBRDF,
+                     int        numMipmaps,
+                     float      roughness,
+                     vec3       worldSpaceReflectionVector,
+                     float      NoV,
+                     vec3       normal,
+                     vec3       view)
+{
+    float mipMapIndex = roughness * float(numMipmaps);
+    vec3 prefilteredColor = texturelod_texture(
+        radianceMap,
+        normalToLatLongUV(worldSpaceReflectionVector),
+        mipMapIndex
+    ).rgb;
+    vec2 envBRDF = texture2D(integratedBRDF, vec2(roughness, NoV)).xy;
+
+    return pbr_fresnelSchlickWithRoughness(specularColor, normal, view, 1.0 - roughness)
+        * prefilteredColor;
+        // * (specularColor * envBRDF.x + envBRDF.y);
 }
 
 #endif // _PBR_FUNCTION_GLSL_
