e208a2f47fbcd4eb0bbd3b0f09974767dc5edb2d
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2015-05-12 11:42:07 +0200

    In Math.function.glsl, add PI, INV_PI and EPSILON macros and normalToPanoramaUV() and normalToSphericalUV() functions.

--- example/pbr/asset/effect/Math.function.glsl
+++ example/pbr/asset/effect/Math.function.glsl
@@ -1,7 +1,9 @@
 #ifndef _MATH_FUNCTION_GLSL_
 #define _MATH_FUNCTION_GLSL_
 
-#define PI 3.1415926535897932384626433832795
+#define PI      3.141592653
+#define INV_PI  0.318309886
+#define EPSILON 1e-3
 
 float square(float x)
 {
@@ -46,6 +48,41 @@ vec3 saturate(vec3 x)
 vec4 saturate(vec4 x)
 {
     return clamp(x, 0.0, 1.0);
+}
+
+vec2 normalToPanoramaUV(const vec3 dir)
+{
+    float n = length(dir.xz);
+    vec2 pos = vec2((n > 0.0000001) ? dir.x / n : 0.0, dir.y);
+    pos = acos(pos)*INV_PI;
+    pos.x = (dir.z > 0.0) ? pos.x * 0.5 : 0.9999999 - (pos.x * 0.5);
+
+    return pos;
+}
+
+vec2 normalToSphericalUV(const vec3 n)
+{
+    // acos is defined [ -1: 1 ]
+    // atan( x , y ) require to have |y| > 0
+
+    // when n.y is amlost 1.0 it means that the normal is aligned on axis y
+    // so instead of fixing numerical issue we can directly return the supposed
+    // uv value
+    if (n.y > (1.0 - EPSILON))
+        return vec2(0.5, 0.0);
+    else if (n.y < -(1.0 - EPSILON))
+        return vec2(0.5, 1.0 - EPSILON);
+
+    float yaw = acos(n.y) * INV_PI;
+    float pitch;
+    float y = n.z;
+
+    if (abs(y) < EPSILON)
+        y = EPSILON;
+
+    pitch = (atan(n.x, y) + PI) * 0.5  * INV_PI;
+
+    return vec2(pitch, yaw);
 }
 
 #endif // _MATH_FUNCTION_GLSL_
