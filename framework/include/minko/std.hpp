872deef6e5cc00904cf91eb95092d3f3eae9736c
Warren Seine <warren.seine@aerys.in>
2015-05-21 10:37:41 +0200

    Add missing std::stoul() in NDK builds.

--- framework/include/minko/std.hpp
+++ framework/include/minko/std.hpp
@@ -154,15 +154,22 @@ namespace std
     }
 
 #ifdef __ANDROID__
-	template <typename T>
-	inline
-	string
-	to_string(T v)
-	{
-		ostringstream oss;
-		oss << v;
-		return oss.str();
-	}
+    template <typename T>
+    inline
+    string
+    to_string(T v)
+    {
+        ostringstream oss;
+        oss << v;
+        return oss.str();
+    }
+
+    inline
+    unsigned long
+    stoul(const std::string& v)
+    {
+        return strtoul(v.c_str(), nullptr, 10);
+    }
 #endif
 
     inline
