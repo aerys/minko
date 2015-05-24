680a41712c3b65cd7b919d5023e1989dac97be53
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2015-05-24 11:15:57 +0200

    In the "water" example, setup the html5 packaging configuration.

--- example/water/premake5.lua
+++ example/water/premake5.lua
@@ -13,7 +13,15 @@ minko.project.application("minko-example-" .. PROJECT_NAME)
 	-- plugins
 	minko.plugin.enable("sdl")
 	minko.plugin.enable("fx")
-	minko.plugin.enable("serializer")
+	-- minko.plugin.enable("serializer")
 	minko.plugin.enable("jpeg")
 	minko.plugin.enable("png")
 	minko.plugin.enable("assimp")
+
+	configuration { "html5" }
+		minko.package.assets {
+			['**.effect'] = { 'embed' },
+			['**.glsl'] = { 'embed' },
+			['**.jpg'] = { 'embed' },
+			['**.png'] = { 'embed' }
+		}
