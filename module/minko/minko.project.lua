efa432f5fc6475738c6217246b8ce523acbcda61
Warren Seine <warren.seine@aerys.in>
2015-11-09 19:39:53 +0100

    Set default iOS target to 7.0.

--- module/minko/minko.project.lua
+++ module/minko/minko.project.lua
@@ -33,6 +33,11 @@ minko.project.library = function(name)
 	configuration { "windows32 or windows64" }
 		includedirs { minko.sdk.path("/framework/lib/glew/include") }
 
+	configuration { "ios" }
+		xcodebuildsettings {
+			IPHONEOS_DEPLOYMENT_TARGET = "7.0"
+		}
+
 	configuration { "vs*" }
 		defines {
 			"NOMINMAX",										-- do not define min/max as macro in windows.h
