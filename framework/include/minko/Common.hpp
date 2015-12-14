635e900c8b2150611f793600ffd4e37114e6a7e4
Jonathan Jarri <jonathan.jarri@aerys.in>
2015-12-14 17:59:36 +0100

    Add a class with a static method to check if we are on mobile in HTML5.

--- framework/include/minko/Common.hpp
+++ framework/include/minko/Common.hpp
@@ -20,6 +20,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 #pragma once
 
 #include "minko/Setup.hpp"
+#include "minko/system/Platform.hpp"
 
 #include <algorithm>
 #include <array>
