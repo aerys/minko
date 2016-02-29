962536a4eb1edf34b99bb31749e62997e8029821
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2016-02-29 21:38:50 +0100

    Add missing packaging configuration for the "shadow-mapping" example.

--- example/shadow-mapping/premake5.lua
+++ example/shadow-mapping/premake5.lua
@@ -8,3 +8,20 @@ minko.project.application("minko-example-" .. PROJECT_NAME)
 	-- plugin
 	minko.plugin.enable("sdl")
 	minko.plugin.enable("debug")
+
+	configuration { "html5" }
+		minko.package.assets {
+			['**.effect'] = { 'embed' },
+			['**.glsl'] = { 'embed' },
+			['**.png'] = { 'embed' },
+			['*.dae'] = { 'embed' },
+			['*.jpg'] = { 'embed' }
+		}
+	configuration { "not html5" }
+		minko.package.assets {
+			['**.effect'] = { 'copy' },
+			['**.glsl'] = { 'copy' },
+			['**.png'] = { 'copy' },
+			['*.dae'] = { 'copy' },
+			['*.jpg'] = { 'copy' }
+		}
