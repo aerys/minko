3196472b5a65137c375c0c8ed44b04ad2405ff6d
Julien Cloute <julien.cloute@aerys.in>
2016-08-31 12:21:58 +0200

    Update Loader to avoid checking if asset exists when a byte range is required.

--- framework/src/minko/file/Loader.cpp
+++ framework/src/minko/file/Loader.cpp
@@ -75,7 +75,10 @@ Loader::load()
             auto options = _filenameToOptions[filename];
             auto assets = options->assetLibrary();
 
-            if (assets->cubeTexture(filename) != nullptr
+            const auto byteRangeRequested = options->seekedLength() > 0;
+
+            if (!byteRangeRequested && (
+                assets->cubeTexture(filename) != nullptr
                 || assets->texture(filename) != nullptr
                 || assets->geometry(filename) != nullptr
                 || assets->effect(filename) != nullptr
@@ -83,7 +86,7 @@ Loader::load()
                 || assets->sound(filename) != nullptr
                 || assets->material(filename) != nullptr
                 || assets->script(filename) != nullptr
-                || assets->hasBlob(filename))
+                || assets->hasBlob(filename)))
             {
                 _filesQueue.erase(std::find(_filesQueue.begin(), _filesQueue.end(), filename));
                 _filenameToOptions.erase(filename);
