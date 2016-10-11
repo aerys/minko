c83fde912395632683f72fe6085ce2a4edad0489
Warren Seine <warren.seine@aerys.in>
2016-10-11 16:32:35 +0200

    Fix "undefined reference to bsd_signal" in Android API > 19.

--- plugin/http-worker/src/minko/net/HTTPRequest.cpp
+++ plugin/http-worker/src/minko/net/HTTPRequest.cpp
@@ -25,6 +25,40 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 using namespace minko;
 using namespace minko::net;
 
+// From http://stackoverflow.com/questions/36746904/android-linker-undefined-reference-to-bsd-signal
+#if (__ANDROID_API__ > 19)
+# include <android/api-level.h>
+# include <android/log.h>
+# include <signal.h>
+# include <dlfcn.h>
+
+extern "C"
+{
+    typedef __sighandler_t (*bsd_signal_func_t)(int, __sighandler_t);
+    bsd_signal_func_t bsd_signal_func = NULL;
+
+    __sighandler_t bsd_signal(int s, __sighandler_t f)
+    {
+        if (bsd_signal_func == NULL)
+        {
+            // For now (up to Android 7.0) this is always available.
+            bsd_signal_func = (bsd_signal_func_t) dlsym(RTLD_DEFAULT, "bsd_signal");
+
+            if (bsd_signal_func == NULL)
+            {
+                // You may try dlsym(RTLD_DEFAULT, "signal") or dlsym(RTLD_NEXT, "signal") here
+                // Make sure you add a comment here in StackOverflow
+                // if you find a device that doesn't have "bsd_signal" in its libc.so!!!
+
+                __android_log_assert("", "bsd_signal_wrapper", "bsd_signal symbol not found!");
+            }
+        }
+
+        return bsd_signal_func(s, f);
+      }
+}
+#endif
+
 HTTPRequest::HTTPRequest(const std::string& url,
                          const std::string& username,
                          const std::string& password,
