18bab0c763f3996f42ef3db140c4113406d8d858
Warren Seine <warren.seine@aerys.in>
2016-10-11 19:04:11 +0200

    Use NeDB in the NodeJS example to store a persisting counter.

--- example/nodejs/server/src/index.js
+++ example/nodejs/server/src/index.js
@@ -1,8 +1,11 @@
 require("source-map-support").install();
 
+const path = require('path');
 const express = require('express');
 const superagent = require('superagent');
 const echo = require('./echo');
+
+const Datastore = require('nedb');
 
 superagent
     .get('http://www.google.com/index.html')
@@ -16,8 +19,24 @@ superagent
 echo.echo("hello world");
 
 let app = express();
-let counter = 0;
 
-app.get('/hello', (req, res) => res.json({ 'success': true, 'counter': ++counter }));
+let counter = new Datastore({ filename: path.dirname(process.argv[1]) + '/counter.db', autoload: true });
+
+app.get('/hello', (req, res) => {
+    counter.update({ name: 'my-counter' }, { $inc: { value: 1 } }, { upsert: true }, (error) => {
+        if (error)
+            res.json({ 'success': false, 'error': error });
+        else
+            counter.find({ name: 'my-counter' }, (error, documents) =>
+            {
+                if (error)
+                    res.json({ 'success': false, 'error': error });
+                else
+                    res.json({ 'success': true, 'counter': documents[0].value })
+
+                counter.persistence.compactDatafile();
+            });
+    });
+});
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
