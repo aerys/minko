3d6c1af8f8dca9230e51ebf502f750d8abfe4eb1
Warren Seine <warren.seine@aerys.in>
2016-10-12 20:08:49 +0200

    Use a NeDB driver for Mongoose in the NodeJS example.

--- example/nodejs/server/src/index.js
+++ example/nodejs/server/src/index.js
@@ -1,42 +1,59 @@
-require("source-map-support").install();
+require('source-map-support').install();
+require('mongoose-nedb').install();
 
 const path = require('path');
 const express = require('express');
 const superagent = require('superagent');
+const mongoose = require('mongoose');
 const echo = require('./echo');
 
 const Datastore = require('nedb');
 
-superagent
-    .get('http://www.google.com/index.html')
-    .end((err, res) => {
-        if (err)
-            console.log(`Got error: ${err.status}`);
-        else
-            console.log(`Got response: ${res.status}`);
-    });
+mongoose.Promise = require('bluebird');
 
 echo.echo("hello world");
 
 let app = express();
 
-let counter = new Datastore({ filename: path.dirname(process.argv[1]) + '/counter.db', autoload: true });
+mongoose.connect('mongodb://localhost/counters');
+mongoose.connection.on('error', (e) => console.error('Mongoose connection error:', e));
+mongoose.connection.once('open', () => console.log('Mongoose connected'));
+
+var counterSchema = new mongoose.Schema({
+    name: String,
+    value: Number
+});
+
+let Counter = mongoose.model('Counter', counterSchema);
 
 app.get('/hello', (req, res) => {
-    counter.update({ name: 'my-counter' }, { $inc: { value: 1 } }, { upsert: true }, (error) => {
+    Counter.update({ name: 'my-counter' }, { $inc: { value: 1 } }, { upsert: true }, (error) => {
         if (error)
             res.json({ 'success': false, 'error': error });
         else
-            counter.find({ name: 'my-counter' }, (error, documents) =>
+            Counter.find({ name: 'my-counter' }, (error, documents) =>
             {
                 if (error)
                     res.json({ 'success': false, 'error': error });
                 else
-                    res.json({ 'success': true, 'counter': documents[0].value })
+                    res.json({ 'success': true, 'counter': documents[0].value });
 
-                counter.persistence.compactDatafile();
+                // counter.persistence.compactDatafile();
             });
     });
+
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+
+    // Call itself.
+    superagent
+        .get('http://localhost:3000/hello')
+        .end((err, res) => {
+            if (err)
+                console.log(`Got error: ${err.status}`);
+            else
+                console.log(`Got response: ${res.status}`);
+        });
+});
