6556c3b774ee426a515abab8a5fc0610219d9f32
Warren Seine <warren.seine@aerys.in>
2016-10-20 19:11:45 +0200

    Disable the NodeJS plugin/example everywhere except Android.

--- example/nodejs/premake5.lua
+++ example/nodejs/premake5.lua
@@ -1,6 +1,12 @@
 PROJECT_NAME = path.getname(os.getcwd())
 
+if not minko.platform.supports { "android" } then
+	return
+end
+
 minko.project.application("minko-example-" .. PROJECT_NAME)
+
+	removeplatforms { "html5", "windows32", "windows64", "linux32", "linux64", "osx64", "ios" }
 
 	files {
 		"src/**.cpp",
--- plugin/nodejs-worker/premake5.lua
+++ plugin/nodejs-worker/premake5.lua
@@ -1,6 +1,6 @@
 PROJECT_NAME = path.getname(os.getcwd())
 
-if not minko.platform.supports { "windows32", "windows64", "linux32", "linux64", "osx64", "ios", "android" } then
+if not minko.platform.supports { "android" } then
 	return
 end
 
