5ce80f389267837dcf9fe75ac8af137fc5876fc4
Benjamin Michel <benjamin.michel@aerys.in>
2016-10-25 15:29:52 +0200

    Changed how skybox plane is set at zfar

--- plugin/fx/asset/effect/Skybox/Skybox.vertex.glsl
+++ plugin/fx/asset/effect/Skybox/Skybox.vertex.glsl
@@ -16,15 +16,13 @@ uniform mat4 uViewMatrix;
 varying vec3 vDirection;
 
 void main() {
-    float w = 10000.0;
-
-    vec4 position = vec4(aPosition, 0.5) * vec4(w, -w, w, w);
+    vec4 position = vec4(aPosition, 1.0) * vec4(1.0, -1.0, 1.0, 0.5);
     mat4 inverseProjection = inverse(uProjectionMatrix);
     mat3 inverseModelview = transpose(mat3(uViewMatrix));
     vec3 unprojected = (inverseProjection * position).xyz;
     vDirection = inverseModelview * unprojected;
 
-    position.z = position.w - EPSILON;
+    position.z =  position.w * ((1000.0 - EPSILON) / 1000.0);
 
     gl_Position = position;
 }
