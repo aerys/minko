9d15718eb97c00f008a4f9b0eff060c3bb9995ff
Julien Cloute <julien.cloute@aerys.in>
2017-08-03 15:51:48 +0200

    Revert unwanted changes from script/build_html5_release.bat.

--- script/build_html5_release.bat
+++ script/build_html5_release.bat
@@ -1,15 +1,15 @@
-::@echo off
+@echo off
 @setlocal enabledelayedexpansion
 
 pushd ..
 
 if not defined MINKO_HOME (
-	echo Environment variable MINKO_HOME is not defined.
-	pause
-	exit /b 1
+    echo Environment variable MINKO_HOME is not defined.
+    pause
+    exit /b 1
 )
 
-mingw32-make SHELL=cmd.exe -j8 config=html5_release verbose=1
+mingw32-make SHELL=cmd.exe config=html5_release verbose=1
 
 popd
 pause
