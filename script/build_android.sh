aac11d6b4fd2fd80ede88cbc5c8d4e2a39d4899c
Jonathan Jarri <jonathan.jarri@aerys.in>
2017-10-11 15:19:10 +0200

    Udpate build_android.sh script to allow unsigned APK build if no keystore is found + remove the code to replace versionCode and versionName in the AndroidManifest.xml file.

--- script/build_android.sh
+++ script/build_android.sh
@@ -8,28 +8,26 @@ CWD=`pwd`
 TARGET=$1
 
 [[ -z ${TARGET} ]] && {
-	echo "usage: build_android.sh target" > /dev/stderr
-	exit 1
+    echo "usage: build_android.sh target" > /dev/stderr
+    exit 1
 }
 
 [[ -z ${ANDROID_KEYSTORE_PATH} ]] && {
-	echo "Missing environment variable ANDROID_KEYSTORE_PATH" > /dev/stderr
-	exit 1
+    echo "Warning: Missing environment variable ANDROID_KEYSTORE_PATH, generated APK will be unsigned."
 }
 
 [[ -z ${ANDROID_KEYSTORE_ALIAS} ]] && {
-	echo "Missing environment variable ANDROID_KEYSTORE_ALIAS" > /dev/stderr
-	exit 1
+    echo "Warning: Missing environment variable ANDROID_KEYSTORE_ALIAS, generated APK will be unsigned."
 }
 
 [[ -z ${ANDROID_KEYSTORE_PASSWORD} ]] && {
-	echo "Missing environment variable ANDROID_KEYSTORE_PASSWORD" > /dev/stderr
-	exit 1
+    echo "Warning: Missing environment variable ANDROID_KEYSTORE_PASSWORD, generated APK will be unsigned."
+    exit 1
 }
 
 [[ -z ${ANDROID_HOME} ]] && {
-	echo "Missing environment variable ANDROID_HOME" > /dev/stderr
-	exit 1
+    echo "Missing environment variable ANDROID_HOME" > /dev/stderr
+    exit 1
 }
 
 ANDROID="${ANDROID_HOME}"
@@ -37,30 +35,18 @@ ADB="${ANDROID_HOME}/platform-tools/adb"
 ZIPALIGN="${ANDROID_HOME}/tools/zipalign"
 
 [[ -x ${ADB} ]] || {
-	echo "${ADB} is not executable" > /dev/stderr
-	exit 1
+    echo "${ADB} is not executable" > /dev/stderr
+    exit 1
 }
 
 [[ -x ${ZIPALIGN} ]] || {
-	echo "${ZIPALIGN} is not executable" > /dev/stderr
-	exit 1
+    echo "${ZIPALIGN} is not executable" > /dev/stderr
+    exit 1
 }
 
-if [ -z "${VERSION_CODE}" ]; then
-    VERSION_CODE="1"
-fi
-
-if ! [ -z "${BUILD_NUMBER}" ]; then
-    VERSION_CODE="${BUILD_NUMBER}"
-fi
-
-if [ -z "${VERSION_NAME}" ]; then
-    VERSION_NAME="1.0"
-fi
-
 if [ ${OSTYPE} == "cygwin" ]; then
-	MINKO_HOME=`cygpath -u "${MINKO_HOME}"`
-	ANDROID=`cygpath -u "${ANDROID_HOME}"`
+    MINKO_HOME=`cygpath -u "${MINKO_HOME}"`
+    ANDROID=`cygpath -u "${ANDROID_HOME}"`
 fi
 
 #RSYNC_OPTIONS="--ignore-existing"
@@ -74,7 +60,7 @@ ARTIFACT_NAME=$(sed -r 's/ /-/g;s/(.*)/\L\1/' <<< "${APP_NAME}")
 pushd "${TARGET_DIR}" > /dev/null
 
 if [ -d "${CWD}/android/" ]; then
-	rsync -vr "${CWD}/android/" .
+    rsync -vr "${CWD}/android/" .
 fi
 
 mkdir -p src/${PACKAGE//.//}
@@ -82,10 +68,6 @@ mv src/*.java src/${PACKAGE//.//}
 
 sed -i "s/{{APP_NAME}}/${APP_NAME}/" res/values/strings.xml build.xml
 sed -i "s/{{PACKAGE}}/${PACKAGE}/" AndroidManifest.xml src/${PACKAGE//.//}/*.java
-
-# Update version info
-sed -i "s/{{VERSION_CODE}}/$VERSION_CODE/" AndroidManifest.xml
-sed -i "s/{{VERSION_NAME}}/$VERSION_NAME/" AndroidManifest.xml
 
 mkdir -p libs/armeabi-v7a/
 # mkdir -p libs/x86/
@@ -103,9 +85,9 @@ chmod u+rwx -R assets
 ant "${CONFIG}"
 
 if [[ "${CONFIG}" == "debug" ]]; then
-	UNSIGNED_APK_PATH="bin/${APP_NAME}-${CONFIG}.apk"
+    UNSIGNED_APK_PATH="bin/${APP_NAME}-${CONFIG}.apk"
 else
-	UNSIGNED_APK_PATH="bin/${APP_NAME}-${CONFIG}-unsigned.apk"
+    UNSIGNED_APK_PATH="bin/${APP_NAME}-${CONFIG}-unsigned.apk"
 fi
 
 ARTIFACT_PATH="bin/${ARTIFACT_NAME}-${CONFIG}.apk"
@@ -113,28 +95,28 @@ ARTIFACT_PATH="bin/${ARTIFACT_NAME}-${CONFIG}.apk"
 DEVICE_STATE=$("${ADB}" get-state | sed 's/\r$//')
 
 if [[ "${CONFIG}" == release && -n "${ANDROID_KEYSTORE_PATH}" && -n "${ANDROID_KEYSTORE_PASSWORD}" && -n "${ANDROID_KEYSTORE_ALIAS}" ]]; then
-	# Sign the app
-	jarsigner -tsa "http://timestamp.digicert.com" -keystore "${ANDROID_KEYSTORE_PATH}" -storepass "${ANDROID_KEYSTORE_PASSWORD}" -verbose -sigalg SHA1withRSA -digestalg SHA1 "${UNSIGNED_APK_PATH}" "${ANDROID_KEYSTORE_ALIAS}"
+    # Sign the app
+    jarsigner -tsa "http://timestamp.digicert.com" -keystore "${ANDROID_KEYSTORE_PATH}" -storepass "${ANDROID_KEYSTORE_PASSWORD}" -verbose -sigalg SHA1withRSA -digestalg SHA1 "${UNSIGNED_APK_PATH}" "${ANDROID_KEYSTORE_ALIAS}"
 
-	# Verify that the app is properly signed
-	jarsigner -verify -verbose -certs "${UNSIGNED_APK_PATH}"
+    # Verify that the app is properly signed
+    jarsigner -verify -verbose -certs "${UNSIGNED_APK_PATH}"
 
-	# zipalign ensures that all uncompressed data starts with a particular byte alignment relative to the start of the file,
-	# which reduces the amount of RAM consumed by an app.
-	"${ZIPALIGN}" -v -f 4 "${UNSIGNED_APK_PATH}" "${ARTIFACT_PATH}"
+    # zipalign ensures that all uncompressed data starts with a particular byte alignment relative to the start of the file,
+    # which reduces the amount of RAM consumed by an app.
+    "${ZIPALIGN}" -v -f 4 "${UNSIGNED_APK_PATH}" "${ARTIFACT_PATH}"
 
-	rm -f "${UNSIGNED_APK_PATH}"
+    rm -f "${UNSIGNED_APK_PATH}"
 
-	# Don't forget to uninstall the app to avoid INSTALL_PARSE_FAILED_INCONSISTENT_CERTIFICATES error
-	if [[ "${DEVICE_STATE}" == "device" ]]; then
-		"${ADB}" uninstall $PACKAGE
-	fi
+    # Don't forget to uninstall the app to avoid INSTALL_PARSE_FAILED_INCONSISTENT_CERTIFICATES error
+    if [[ "${DEVICE_STATE}" == "device" ]]; then
+        "${ADB}" uninstall $PACKAGE
+    fi
 else
-	mv "${UNSIGNED_APK_PATH}" "${ARTIFACT_PATH}"
+    mv "${UNSIGNED_APK_PATH}" "${ARTIFACT_PATH}"
 fi
 
 if [ "${DEVICE_STATE}" == "device" ]; then
-	"${ADB}" install -r "${ARTIFACT_PATH}"
+    "${ADB}" install -r "${ARTIFACT_PATH}"
 fi
 
 popd > /dev/null
