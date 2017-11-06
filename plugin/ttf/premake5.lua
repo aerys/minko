e034ddecf40f7a75c7ee169c6258081308827480
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2017-11-06 15:34:41 +0100

    Fix missing define of `MINKO_PLUGIN_TTF_FREETYPE` on linux.

--- plugin/ttf/premake5.lua
+++ plugin/ttf/premake5.lua
@@ -10,10 +10,10 @@ minko.project.library("minko-plugin-" .. PROJECT_NAME)
     }
 
     includedirs {
-    	"include",
+      "include",
       "lib/freetype/include/freetype2",
       "lib/freetype/include"
   }
 
-    configuration { "windows" }
+    configuration { "windows or linux" }
         defines { "MINKO_PLUGIN_TTF_FREETYPE" }
