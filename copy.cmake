109bc32faf1b4a16fc74050ded66fc1037e06570
EituKo <thomas.plisson@aerys.in>
2018-07-25 16:02:38 +0200

    Fixing minor error in package.cmake and adding copy.cmake, it contains a very complicated copy function :)

---
+++ copy.cmake
@@ -0,0 +1,18 @@
+function (copy source dest)
+
+    file (GLOB_RECURSE
+        SOURCE
+        "${source}"
+    )
+    if (dest)
+        set (DEST ${dest})
+    elseif()
+        set (DEST ${CMAKE_CURRENT_SOURCE_DIR})
+    endif()
+    foreach (OBJ IN LISTS SOURCE)
+        configure_file("${OBJ}"
+            "${DEST}"
+            COPYONLY
+        )
+    endforeach()
+endfunction ()
