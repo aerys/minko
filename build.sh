f482c875b67450b0cf9d28db11042ec80eff81b6
EituKo <thomas.plisson@aerys.in>
2018-07-25 18:49:36 +0200

    Brand new build.sh

--- build.sh
+++ build.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
 
-OPTIMIZER="OFF"
 TYPE="Debug"
 
-for i in "$@"
-do
-    if [[ "$i" == "optimize" ]]; then
-        OPTIMIZER="ON"
-    elif [[ "$i" == "release" ]]; then
-        TYPE="Release"
-    fi
-done;
+if [[ "$1" != "linux" && "$1" != "android" && "$1" != "asmjs" && "$1" != "wasm" ]]; then
+    echo "target error!"
+    exit 2;
+fi
+
+if [[ "$2" != "debug" && "$2" != "release" ]]; then
+    echo "buid type error!"
+    exit 2;
+fi
+
+if [[ "$3" != "" && "$3" != "-j4" && "$3" != "-j8" ]]; then
+    echo "invalid '-j' number!"
+    exit 2;
+fi
+
+if [[ "$4" != "reset" ]]; then
+    sudo rm -rf build
+fi
 
 mkdir -p build
 cd build
 
-# build for linux64
-rm CMakeCache.txt
-cmake .. -DCMAKE_BUILD_TYPE="${TYPE}" -DWITH_GLSL_OPTIMIZER="${OPTIMIZER}" && make -j4 VERBOSE=1
+if [[ "$1" == "linux" ]]; then
+    cmake .. -DCMAKE_BUILD_TYPE="$2" -DWITH_GLSL_OPTIMIZER=ON && make "$3" VERBOSE=1
+fi
 
-# build for WASM
-rm CMakeCache.txt
-cmake .. -DCMAKE_BUILD_TYPE="${TYPE}" -DWITH_GLSL_OPTIMIZER="${OPTIMIZER}" -DWASM=ON -DCMAKE_TOOLCHAIN_FILE=/home/thomas/Aerys/emsdk/emscripten/1.38.8/cmake/Modules/Platform/Emscripten.cmake && make -j4 VERBOSE=1
+if [[ "$1" == "asmjs" ]]; then
+    cmake .. -DCMAKE_BUILD_TYPE="$2" -DWITH_GLSL_OPTIMIZER=ON -DCMAKE_TOOLCHAIN_FILE=/home/thomas/Aerys/emsdk/emscripten/1.38.8/cmake/Modules/Platform/Emscripten.cmake && make "$3" VERBOSE=1
+fi
 
-# build for Android
-rm CMakeCache.txt
-cd ..
-docker run --rm -it -v $PWD:/srv/smartshape-engine android-latest bash -c "cd /srv/smartshape-engine && mkdir -p build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release -DWITH_GLSL_OPTIMIZER=ON -DCMAKE_TOOLCHAIN_FILE=/opt/android-ndk-linux/build/cmake/android.toolchain.cmake && make -j4 VERBOSE=1"
+if [[ "$1" == "wasm" ]]; then
+    cmake .. -DCMAKE_BUILD_TYPE="$2" -DWITH_GLSL_OPTIMIZER=ON -DWASM=ON -DCMAKE_TOOLCHAIN_FILE=/home/thomas/Aerys/emsdk/emscripten/1.38.8/cmake/Modules/Platform/Emscripten.cmake && make "$3" VERBOSE=1
+fi
+
+if [[ "$1" == "android" ]]; then
+    cd ..
+    docker run --rm -it -v $PWD:/srv/smartshape-engine android-latest bash -c "cd /srv/smartshape-engine && mkdir -p build && cd build && cmake .. -DCMAKE_BUILD_TYPE=$2 -DWITH_GLSL_OPTIMIZER=ON -DCMAKE_TOOLCHAIN_FILE=/opt/android-ndk-linux/build/cmake/android.toolchain.cmake && make $3 VERBOSE=1"
+fi
