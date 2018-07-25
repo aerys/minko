d1326fc42f0f039d0a71ca9006dd8cb9bdfd748d
EituKo <thomas.plisson@aerys.in>
2018-07-25 13:41:05 +0200

    Pushing update on the build script, now works properly without options.

--- build.sh
+++ build.sh
@@ -19,10 +19,14 @@ mkdir -p build
 cd build
 
 # build for linux64
+rm CMakeCache.txt
 cmake .. -DCMAKE_BUILD_TYPE=Release -DWITH_GLSL_OPTIMIZER="${OPTIMIZER}" && make -j4 VERBOSE="${VERBO}"
 
 # build for WASM
+rm CMakeCache.txt
 cmake .. -DCMAKE_BUILD_TYPE=Release -DWITH_GLSL_OPTIMIZER="${OPTIMIZER}" -DWASM=ON -DCMAKE_TOOLCHAIN_FILE=/home/thomas/Aerys/emsdk/emscripten/1.38.8/cmake/Modules/Platform/Emscripten.cmake && make -j4 VERBOSE="${VERBO}"
 
 # build for Android
-# docker run --rm -it -v $PWD:/srv/smartshape-engine android-latest bash -c "cd /srv/smartshape-engine/framework && mkdir -p build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release -DWITH_GLSL_OPTIMIZER=${OPTIMIZER} -DCMAKE_TOOLCHAIN_FILE=/opt/android-ndk-linux/build/cmake/android.toolchain.cmake && make -j4 VERBOSE=${VERBO}"
+rm CMakeCache.txt
+cd ..
+docker run --rm -it -v $PWD:/srv/smartshape-engine android-latest bash -c "cd /srv/smartshape-engine && mkdir -p build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release -DWITH_GLSL_OPTIMIZER=${OPTIMIZER} -DCMAKE_TOOLCHAIN_FILE=/opt/android-ndk-linux/build/cmake/android.toolchain.cmake && make -j4 VERBOSE=${VERBO}"
