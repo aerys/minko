5aa2d3d4f116eb2c81560f9908c08b00b1c58bba
EituKo <thomas.plisson@aerys.in>
2018-07-30 15:23:37 +0200

    More explicit code on android-build.cmake.

--- android-build.cmake
+++ android-build.cmake
@@ -9,15 +9,18 @@ function (build_android target target_name target_dir config)
     # regex commands
     execute_process(COMMAND
         sed -r 's/lib(.*).so/\1/;s/-/ /g;s/([A-Za-z])([A-Za-z]+)/\U\1\L\2/g;s/([0-9]+)//g;s/[^[:alpha:]]\s//g' <<< ${TARGET_NAME}
-        OUTPUT_VARIABLE APP_NAME
+        OUTPUT_VARIABLE
+        APP_NAME
     )
     execute_process(COMMAND
         sed -r 's/lib(.*).so/com.\1/;s/-/\./g;s/\.([0-9]+)//g;s/(.*)/\L\1/' <<< ${TARGET_NAME}
-        OUTPUT_VARIABLE PACKAGE
+        OUTPUT_VARIABLE
+        PACKAGE
     )
     execute_process(COMMAND
         sed -r 's/ /-/g;s/(.*)/\L\1/' <<< ${APP_NAME}
-        OUTPUT_VARIABLE ARTIFACT_NAME
+        OUTPUT_VARIABLE
+        ARTIFACT_NAME
     )
     
     # replace what needs to be replaced
@@ -25,7 +28,8 @@ function (build_android target target_name target_dir config)
         sed -i 's/{{APP_NAME}}/${APP_NAME}/' res/values/strings.xml build.xml
     )
     execute_process(COMMAND
-        sed -i 's/{{PACKAGE}}/${PACKAGE}/' AndroidManifest.xml src/${PACKAGE//.//}/*.java
+        sed -i 's/{{PACKAGE}}/${PACKAGE}/' AndroidManifest.xml src/${PACKAGE}/*.java
+        # might need to add the // extension parameter
     )
     execute_process(COMMAND
         sed -i 's/{{VERSION_CODE}}/${VERSION_CODE}/' AndroidManifest.xml
