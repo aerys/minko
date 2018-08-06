cec0ecd4b194d01bd2db95f7a3e63887cf24d242
EituKo <thomas.plisson@aerys.in>
2018-08-06 18:05:51 +0200

    Pushing new enable.cmake for zlib.

---
+++ plugin/zlib/enable.cmake
@@ -0,0 +1,11 @@
+function (enable_zlib target)
+    set (ZLIB_PATH "${MINKO_HOME}/plugin/zlib")
+    
+    plugin_link ("zlib" ${PROJECT_NAME})
+    
+    file (GLOB ${PROJECT_NAME}_INCLUDE "${ZLIB_PATH}/lib/zlib")
+
+    target_compile_options (${PROJECT_NAME} PUBLIC
+        -DMINKO_PLUGIN_ZLIB
+    )
+endfunction ()
