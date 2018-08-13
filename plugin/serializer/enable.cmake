bbbaa438b0917687c848692ff80d62d4872cea3c
AurelienFOUCAULT <aurelien.foucault@aerys.in>
2018-08-13 16:59:06 +0200

    Change project_name values in enable of serializer by target argument

--- plugin/serializer/enable.cmake
+++ plugin/serializer/enable.cmake
@@ -12,15 +12,15 @@ function (enable_serializer target)
         call_plugin (enable_${NEEDED_PLUGIN} ${NEEDED_PLUGIN} ${target})
     endforeach ()
 
-    plugin_link ("serializer" ${PROJECT_NAME})
+    plugin_link ("serializer" ${target})
     
     file (GLOB
-        ${PROJECT_NAME}_INCLUDE
+        ${PROJECT_NAME}_INCLUDE_SERIALIZER
         "${SERIALIZER_PATH}/include"
         "${SERIALIZER_PATH}/lib/msgpack-c/include"
     )
-
-    target_compile_options (${PROJECT_NAME} PUBLIC -DMINKO_PLUGIN_SERIALIZER)
+    target_include_directories(${target} PRIVATE ${${PROJECT_NAME}_INCLUDE_SERIALIZER})
+    target_compile_options (${target} PUBLIC -DMINKO_PLUGIN_SERIALIZER)
 
     if (WITH_TEXTURE_COMPRESSOR STREQUAL "on")
         if (WIN32 AND BITNESS EQUAL 32)
@@ -33,7 +33,7 @@ function (enable_serializer target)
                 "${SERIALIZER_PATH}/lib/PVRTexTool/Windows_x86_32/Dynamic"
                 "${SERIALIZER_PATH}/lib/QCompress/Lib/windows/Win32"
             )
-            target_link_libraries(${PROJECT_NAME} RET_PATH)
+            target_link_libraries(${target} RET_PATH)
             file (COPY ${SERIALIZER_PATH}/lib/PVRTexTool/Windows_x86_32/Dynamic/*.dll ${OUTPUT_PATH})
             file (COPY ${SERIALIZER_PATH}/lib/QCompress/Lib/windows/Win32/*.dll ${OUTPUT_PATH})
         endif ()
@@ -45,7 +45,7 @@ function (enable_serializer target)
                 HINTS
                 "${SERIALIZER_PATH}/lib/PVRTexTool/Windows_x86_64/Dynamic"
             )
-            target_link_libraries(${PROJECT_NAME} RET_PATH)
+            target_link_libraries(${target} RET_PATH)
             file (COPY ${SERIALIZER_PATH}/lib/PVRTexTool/Windows_x86_32/Dynamic/*.dll ${OUTPUT_PATH})
         endif ()
 
@@ -59,7 +59,7 @@ function (enable_serializer target)
                 "${SERIALIZER_PATH}/lib/PVRTexTool/Linux_x86_32/Dynamic"
                 "${SERIALIZER_PATH}/lib/QCompress/Lib/ubuntu/i386"
             )
-            target_link_libraries(${PROJECT_NAME} RET_PATH)
+            target_link_libraries(${target} RET_PATH)
             file (COPY ${SERIALIZER_PATH}/lib/PVRTexTool/Linux_x86_32/Dynamic/*.so ${OUTPUT_PATH})
             file (COPY ${SERIALIZER_PATH}/lib/QCompress/Lib/ubuntu/i386/*.so ${OUTPUT_PATH})
         endif ()
@@ -71,12 +71,12 @@ function (enable_serializer target)
                 HINTS
                 "${SERIALIZER_PATH}/lib/PVRTexTool/Linux_x86_32/Dynamic"
             )
-            target_link_libraries(${PROJECT_NAME} RET_PATH)
+            target_link_libraries(${target} RET_PATH)
             file (COPY ${SERIALIZER_PATH}/lib/PVRTexTool/Linux_x86_32/Dynamic/*.so ${OUTPUT_PATH})
         endif ()
 
         if (UNIX AND NOT APPLE AND NOT EMSCRIPTEN)
-            set_target_properties (${PROJECT_NAME} PROPERTIES LINK_FLAGS "-Wl,-rpath=.")
+            set_target_properties (${target} PROPERTIES LINK_FLAGS "-Wl,-rpath=.")
         endif ()
 
         if (APPLE AND NOT IOS AND BITNESS EQUAL 32)
@@ -88,7 +88,7 @@ function (enable_serializer target)
                 "${SERIALIZER_PATH}/lib/PVRTexTool/Linux_x86_32/Dynamic"
                 "${SERIALIZER_PATH}/lib/QCompress/Lib/osx/x86"
             )
-            target_link_libraries(${PROJECT_NAME} RET_PATH)
+            target_link_libraries(${target} RET_PATH)
             file (COPY ${SERIALIZER_PATH}/lib/PVRTexTool/OSX_x86/Dynamic/*.dylib ${OUTPUT_PATH})
             file (COPY ${SERIALIZER_PATH}/lib/QCompress/Lib/osx/x86/*.dylib ${OUTPUT_PATH})
         endif ()
@@ -100,7 +100,7 @@ function (enable_serializer target)
                 HINTS
                 "${SERIALIZER_PATH}/lib/PVRTexTool/Linux_x86_32/Dynamic"
             )
-            target_link_libraries(${PROJECT_NAME} RET_PATH)
+            target_link_libraries(${target} RET_PATH)
             file (COPY ${SERIALIZER_PATH}/lib/PVRTexTool/OSX_x86/Dynamic/*.dylib ${OUTPUT_PATH})
         endif ()
     endif ()
