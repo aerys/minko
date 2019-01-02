d400aa573069810ed9f50aacebfd59310fdcfa08
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2019-01-02 16:11:00 +0100

    Upgrade libnode.so to NodeJS 8.6.0 prebuilt for Android ARM 7 (#112).

--- plugin/nodejs-worker/enable.cmake
+++ plugin/nodejs-worker/enable.cmake
@@ -1,36 +1,29 @@
 function (minko_enable_plugin_nodejs_worker target)
-    set (NODEJS-WORKER_PATH "${MINKO_HOME}/plugin/nodejs-worker")
+    set (NODEJS_WORKER_PATH "${MINKO_HOME}/plugin/nodejs-worker")
     target_include_directories (
         ${target}
         PRIVATE
-        "${NODEJS-WORKER_PATH}/include"
+        "${NODEJS_WORKER_PATH}/include"
     )
     target_compile_definitions (
         ${target}
         PRIVATE
         "MINKO_PLUGIN_NODEJS_WORKER"
     )
-    if (WIN32)
-        execute_process(
-            COMMAND bash ./script/build_nodejs.sh --use-prebuilt
-            WORKING_DIRECTORY ${MINKO_HOME}/plugin/nodejs-worker
-        )
-    else ()
-        execute_process(
-            COMMAND bash ./script/build_nodejs.sh --use-prebuilt
-            WORKING_DIRECTORY ${MINKO_HOME}/plugin/nodejs-worker
-        )
-    endif ()
     if (NOT EMSCRIPTEN)
         minko_enable_plugin_ssl (${target})
     endif ()
     if (ANDROID)
         minko_plugin_link ("nodejs-worker" ${target})
-        set (NODE_LIB "${NODEJS-WORKER_PATH}/lib/nodejs/android/libnode.so")
+        set (NODE_LIB "${NODEJS_WORKER_PATH}/lib/nodejs/lib/android/libnode.so")
         file (
             COPY ${NODE_LIB}
             DESTINATION ${OUTPUT_PATH}
         )
-        target_link_libraries(${target} "${NODEJS-WORKER_PATH}/lib/nodejs/android/libnode.so")
+        target_link_libraries(${target} "${NODEJS_WORKER_PATH}/lib/nodejs/lib/android/libnode.so")
+
+        if (NOT "${ANDROID_STL}" STREQUAL "c++_shared")
+            message(FATAL_ERROR "ANDROID_STL has to be set to c++_shared when enabling the nodejs-worker plugin")
+        endif ()
     endif ()
 endfunction ()
