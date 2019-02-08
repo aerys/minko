5398497124dfbecad6af4ed4478280c6e3990251
sebcrozet <sebastien.crozet@aerys.in>
2019-02-08 18:58:33 +0100

    Add scripts for building nodejs in 32 and 64 bits.

---
+++ plugin/nodejs-worker/script/build_nodejs64.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+[[ -f plugin.lua ]] || {
+    echo "Script must be executed from the plugin root." > /dev/stderr
+    exit 1
+}
+
+# Example: ANDROID_NDK_HOME=/home/sebcrozet/Android/Sdk/ndk-bundle
+[[ -n ${ANDROID_NDK_HOME} ]] || {
+    echo "Missing ANDROID_NDK_HOME environment variable." > /dev/stderr
+    exit 1
+}
+
+#
+#
+# Download and extract node 10.
+#
+#
+rm -rf lib/node
+
+ANDROID_NDK=
+NODE_VERSION="10.x"
+wget https://github.com/nodejs/node/archive/v${NODE_VERSION}.zip -O node-${NODE_VERSION}.zip
+unzip node-${NODE_VERSION}.zip -d lib
+mv lib/node-${NODE_VERSION} lib/node
+rm -f node-${NODE_VERSION}.zip
+
+# getservbyport_r is not defined in Android NDK libc.
+sed -i 's/.*HAVE_GETSERVBYPORT_R.*//g' lib/node/deps/cares/config/android/ares_config.h
+
+# remove linker '-pie' flag since we generate a shared
+# library instead of an executable
+sed -i "s/, '-pie'//g" lib/node/common.gypi
+sed -i "s/-fPIE/-fPIC/g" lib/node/common.gypi
+# Add missing -lpthread when the host is linux.
+# Not doing this leads to linker errors.
+sed -i "s/-lrt/-lrt -lpthread/g" lib/node/common.gypi
+
+### First, generate a 64-bits standalone toolchain:
+TOOLCHAIN_PATH="${CWD}/toolchain" # Change this to the path where you want the toolchain to be created.
+${ANDROID_NDK_HOME}/build/tools/make_standalone_toolchain.py --arch arm64 --api 28 --install-dir ${TOOLCHAIN_PATH}
+
+export GYP_DEFINES="host_os=linux"
+target_compiler="${TOOLCHAIN_PATH}/toolchain/bin/clang"
+target_compilerpp="${TOOLCHAIN_PATH}/toolchain/bin/clang++"
+
+pushd lib/node
+export CC=${target_compiler}
+export CXX=${target_compilerpp}
+export CC_host="clang"
+export CXX_host="clang++"
+
+./configure \
+    --prefix=../install \
+    --dest-cpu=arm64 \
+    --cross-compiling \
+    --dest-os=android \
+    --without-intl \
+    --without-inspector \
+    --without-snapshot \
+    --openssl-no-asm \
+    --shared
+
+popd
+
+make -j8 -C lib/node
