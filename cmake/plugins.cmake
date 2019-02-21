ddc2774d7010bf78cda112c1ecdbb07cd5cd10da
Julien Clouté <julien.cloute@aerys.in>
2019-02-21 17:12:59 +0100

    Fix an issue where the executable binaries would not properly link with the plugins.

--- cmake/plugins.cmake
+++ cmake/plugins.cmake
@@ -23,10 +23,10 @@ function (minko_plugin_link plugin_name target)
     string (FIND ${target} "minko-framework" CHECK_FRAMEWORK)
     string (FIND ${target} "minko-test" CHECK_TEST)
     if (CHECK_EXAMPLE EQUAL -1 AND CHECK_PLUGIN EQUAL -1 AND CHECK_FRAMEWORK EQUAL -1 AND CHECK_TEST EQUAL -1)
-        if (NOT EMSCRIPTEN AND NOT ANDROID)
+        if (WIN32)
             find_library (
                 ${plugin_name}_LIB
-                NAMES minko-plugin-${plugin_name} 
+                NAMES minko-plugin-${plugin_name}
                 HINTS "${MINKO_HOME}/plugin/${plugin_name}/bin/"
             )
             target_link_libraries (${target} ${${plugin_name}_LIB})
@@ -35,7 +35,7 @@ function (minko_plugin_link plugin_name target)
             # order of the libs as they are listed on the command line. To avoid undefined symbol errors,
             # we simply link with both the plugin and the framework in a "group".
             target_link_libraries(
-                ${target} 
+                ${target}
                 -Wl,--start-group
                 ${MINKO_HOME}/plugin/${plugin_name}/bin/libminko-plugin-${plugin_name}.a
                 ${MINKO_HOME}/framework/bin/libminko-framework.a
