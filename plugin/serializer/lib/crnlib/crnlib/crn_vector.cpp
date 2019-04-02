977cbf94b5b75eb4d52abea44e1208d1282c7b35
Julien Clouté <julien.cloute@aerys.in>
2019-04-02 18:56:21 +0200

    Fix a compile error from crn_vector (a PR should be issued).

--- plugin/serializer/lib/crnlib/crnlib/crn_vector.cpp
+++ plugin/serializer/lib/crnlib/crnlib/crn_vector.cpp
@@ -22,8 +22,8 @@ namespace crnlib
          return true;
 
       size_t new_capacity = min_new_capacity;
-      if ((grow_hint) && (!math::is_power_of_2(new_capacity)))
-         new_capacity = math::next_pow2(new_capacity);
+      if ((grow_hint) && (!math::is_power_of_2((uint64)new_capacity)))
+         new_capacity = math::next_pow2((uint64)new_capacity);
 
       CRNLIB_ASSERT(new_capacity && (new_capacity > m_capacity));
 
