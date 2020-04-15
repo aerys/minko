b9c206f8c005d77f657a8c4aa15455331e4d02a2
Julien Clouté <julien.cloute@aerys.in>
2020-04-15 16:25:21 +0200

    Add some missing link command to DevIL's dependencies.

--- plugin/devil/enable.cmake
+++ plugin/devil/enable.cmake
@@ -4,6 +4,27 @@ function (minko_enable_plugin_devil target)
     target_include_directories(${target} PRIVATE "${DEVIL_PATH}/include")
     target_compile_options(${target} PRIVATE -DMINKO_PLUGIN_DEVIL)
 
+    get_target_property(TARGET_TYPE ${target} TYPE)
+    get_target_property(OUTPUT_PATH ${target} RUNTIME_OUTPUT_DIRECTORY)
+
+    if ((LINUX OR WIN32) AND BITNESS EQUAL 64)
+        if (WIN32)
+            set (DEVIL_SHARED_LIBRARIES_LOCATION "${MINKO_HOME}/plugin/devil/lib/devil/DevIL/lib/x64/*.dll")
+        endif ()
+        if (LINUX)
+            set (DEVIL_SHARED_LIBRARIES_LOCATION "${MINKO_HOME}/plugin/devil/lib/devil/DevIL/lib/x64/*.so")
+        endif ()
+        if (TARGET_TYPE STREQUAL "EXECUTABLE")
+            file (GLOB DEVIL_SHARED_LIBRARIES ${DEVIL_SHARED_LIBRARIES_LOCATION})
+            file (COPY ${DEVIL_SHARED_LIBRARIES} DESTINATION ${OUTPUT_PATH})
+        endif ()
+    endif ()
+
+    target_link_libraries(
+        ${target}
+        ${DEVIL_SHARED_LIBRARIES}
+    )
+
     # Enabling zlib by default my cause multiple definition linking errors.
     # So the app CMakeLists.txt must enable the zlib plugin on a case-by-case basis.
     # minko_enable_plugin_zlib (${target})
