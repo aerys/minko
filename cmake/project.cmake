f8b40d8d7140fd1e33908ba4ec6d83899230ca6b
Mahé Charpy <mahe.charpy@aerys.in>
2020-12-17 10:46:10 +0100

    Add comment about why the smartshape-player.bc file is removed.

--- cmake/project.cmake
+++ cmake/project.cmake
@@ -297,6 +297,8 @@ function (minko_add_executable target_name sources)
                     ${MINKO_HOME}/cmake/empkg.py
                     ${CMAKE_CURRENT_BINARY_DIR}/bin/${PROJECT_NAME}.data
 
+                # This file is an intermediate bytecode we don't need once the project is built.
+                # This is why, after the build is finished, we delete it.
                 COMMAND rm ${CMAKE_CURRENT_BINARY_DIR}/bin/${PROJECT_NAME}.bc
             )
         endif ()
