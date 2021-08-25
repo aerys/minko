d5d49797f251246b719efe8174ab2fdd721ee515
Thomas Ville <thomas.ville@aerys.in>
2021-08-25 15:15:46 +0200

    Store the self-signed certificate in a different file than the private key.
    Document the command used to generate the self-signed certificate.

--- test/websocket-server/tls_server.py
+++ test/websocket-server/tls_server.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
-# WSS (WS over TLS) server example, with a self-signed certificate
+# WSS (WS over TLS) server example, with a self-signed certificate generated with:
+# openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout key.pem -out cert.pem
+# And by hitting the Enter key at each question to use the default values.
 
 import asyncio
 import pathlib
@@ -17,8 +19,9 @@ async def echo(websocket, path):
         await websocket.send(message)
 
 ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-localhost_pem = pathlib.Path(__file__).with_name("ca.pem")
-ssl_context.load_cert_chain(localhost_pem)
+cert_pem = pathlib.Path(__file__).with_name("cert.pem")
+key_pem = pathlib.Path(__file__).with_name("key.pem")
+ssl_context.load_cert_chain(cert_pem, key_pem)
 
 start_server = websockets.serve(
     echo, "127.0.0.1", 8766, ssl=ssl_context
