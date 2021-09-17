8a79c17b4804e5a37ee98984b4d7fee0a38134d8
Sarkis Halladjian <sarkis.halladjian@aerys.in>
2021-09-17 14:19:16 +0200

    Delete VAO before resetting it.

--- framework/src/minko/render/DrawCall.cpp
+++ framework/src/minko/render/DrawCall.cpp
@@ -114,6 +114,18 @@ DrawCall::getStore(data::Binding::Source source)
 void
 DrawCall::reset()
 {
+    if (_program)
+    {
+        auto context = _program->context();
+
+        if (_vertexAttribArray > 0)
+        {
+            LOG_DEBUG("deleteVertexAttributeArray called.");
+            context->deleteVertexAttributeArray(_vertexAttribArray);
+        }
+    }
+    _vertexAttribArray = 0;
+
     _program = nullptr;
     _indexBuffer = nullptr;
     _firstIndex = nullptr;
@@ -123,7 +135,6 @@ DrawCall::reset()
     _uniformBool.clear();
     _samplers.clear();
     _attributes.clear();
-    _vertexAttribArray = 0;
 }
 
 void
