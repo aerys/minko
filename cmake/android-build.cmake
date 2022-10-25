890901b9bacce3d46c9a8c301f91cf87467a8b90
Thomas Ville <thomas.ville@aerys.in>
2022-10-25 08:33:12 +0000

    Revert "Statically set the Android package name to `com.smartshape.player11`."
    This reverts commit 51316466d02fdfe16f2c3e4a9e540b069a84e9be

--- cmake/android-build.cmake
+++ cmake/android-build.cmake
@@ -25,7 +25,6 @@ function (build_android target target_name)
     string (REGEX REPLACE "-" " " APP_NAME ${APP_CUT})
     string (REGEX REPLACE "lib(.*).so" "com.\\1" PACKAGE_CUT ${target_name})
     string (REGEX REPLACE "-" "." PACKAGE ${PACKAGE_CUT})
-    # set(PACKAGE "com.smartshape.player11")
 
     # might need to add the rsync command (cf build_android.sh:65)
 
